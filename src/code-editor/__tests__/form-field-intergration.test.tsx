1bad616647b5317efde2cabaa2a350aa6057ca93
Boris Serdiuk <serdiuk@amazon.com>
2022-07-06 16:44:19 +0200

    fix(code-editor): Do not propagate error state inside preferences modal (#13)

--- src/code-editor/__tests__/form-field-intergration.test.tsx
+++ src/code-editor/__tests__/form-field-intergration.test.tsx
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 import React from 'react';
 import { render } from '@testing-library/react';
+import createWrapper from '../../../lib/components/test-utils/dom';
 import CodeEditor from '../../../lib/components/code-editor';
 import FormField from '../../../lib/components/form-field';
 import { defaultProps, editorMock } from './util';
@@ -40,4 +41,15 @@ test('inherits aria-attributes from surrounding form field', () => {
   expect(editorMock.renderer.textarea).toHaveAttribute('id', '123');
   expect(editorMock.renderer.textarea).toHaveAttribute('aria-labelledby', '123-label');
   expect(editorMock.renderer.textarea).toHaveAttribute('aria-describedby', '123-description');
+});
+
+test('does not render invalid state inside preferences modal', () => {
+  render(
+    <FormField label="Some code" errorText="Some error">
+      <CodeEditor {...defaultProps} />
+    </FormField>
+  );
+  const wrapper = createWrapper();
+  wrapper.findCodeEditor()!.findSettingsButton()!.click();
+  expect(wrapper.findModal()!.findContent().find('[aria-invalid]')).toBeFalsy();
 });
