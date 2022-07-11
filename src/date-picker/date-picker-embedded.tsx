6681c0e462ded230974c54fe7e547747dd12d1bd
Shakeel Mohamed <zshakeel@amazon.com>
2022-07-11 16:32:52 +0200

    Add ref prop to embedded date picker to resolve test issues

--- src/date-picker/date-picker-embedded.tsx
+++ src/date-picker/date-picker-embedded.tsx
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React, { useRef } from 'react';
+import React, { useRef, Ref } from 'react';
 import { DatePickerProps } from './interfaces';
 import Calendar from './calendar';
 import { memoizedDate } from './calendar/utils/date';
@@ -9,20 +9,24 @@ import { applyDisplayName } from '../internal/utils/apply-display-name.js';
 import useBaseComponent from '../internal/hooks/use-base-component';
 import { useMergeRefs } from '../internal/hooks/use-merge-refs';
 import { UseDatePicker } from './use-date-picker';
+import useForwardFocus from '../internal/hooks/forward-focus';
 
 export { DatePickerProps };
 
 const DatePickerEmbedded = React.forwardRef(
-  ({
-    locale = '',
-    startOfWeek,
-    isDateEnabled,
-    nextMonthAriaLabel,
-    previousMonthAriaLabel,
-    todayAriaLabel,
-    value,
-    ...rest
-  }: DatePickerProps) => {
+  (
+    {
+      locale = '',
+      startOfWeek,
+      isDateEnabled,
+      nextMonthAriaLabel,
+      previousMonthAriaLabel,
+      todayAriaLabel,
+      value,
+      ...rest
+    }: DatePickerProps,
+    ref: Ref<DatePickerProps.Ref>
+  ) => {
     const {
       normalizedLocale,
       normalizedStartOfWeek,
@@ -44,6 +48,8 @@ const DatePickerEmbedded = React.forwardRef(
 
     const rootRef = useRef<HTMLDivElement>(null);
     const calendarRef = useRef<HTMLDivElement>(null);
+    useForwardFocus(ref, calendarRef);
+
     const mergedRef = useMergeRefs(rootRef, __internalRootRef);
 
     return (
