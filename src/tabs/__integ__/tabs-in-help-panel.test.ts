b7378202e232bc322c2fa6e5a6d5ebff28410437
Yueying Lu <98534165+YueyingLu@users.noreply.github.com>
2022-07-12 13:31:46 +0200

    fix: close button not work in side panel in VR (#23)
    * fix: vr close button not work in side panel
    
    * Trigger checks

--- src/tabs/__integ__/tabs-in-help-panel.test.ts
+++ src/tabs/__integ__/tabs-in-help-panel.test.ts
@@ -7,31 +7,36 @@ import createWrapper from '../../../lib/components/test-utils/selectors';
 const appLayoutWrapper = createWrapper().findAppLayout();
 const tabsWrapper = createWrapper().findTabs();
 
-const setupTest = (testFn: (page: BasePageObject) => Promise<void>) => {
+const setupTest = (visualRefresh = false, testFn: (page: BasePageObject) => Promise<void>) => {
   return useBrowser(async browser => {
     const page = new BasePageObject(browser);
-    // TODO: Remove VR flag once AWSUI-18705 is fixed
-    await browser.url('#/light/tabs/in-app-layout-help-panel/?motionDisabled=true&visualRefresh=false');
+    await browser.url(
+      `#/light/tabs/in-app-layout-help-panel/?motionDisabled=true${visualRefresh ? '&visualRefresh=true' : ''}`
+    );
     await page.waitForVisible(appLayoutWrapper.findContentRegion().toSelector());
     await testFn(page);
   });
 };
 
-test(
-  'closes the tools drawer upon clicking on the toggle in large viewports',
-  setupTest(async page => {
-    await expect(page.isDisplayed(tabsWrapper.toSelector())).resolves.toBe(true);
-    await page.click(appLayoutWrapper.findToolsClose().toSelector());
-    await expect(page.isDisplayed(tabsWrapper.toSelector())).resolves.toBe(false);
-  })
-);
+[false, true].forEach(visualRefresh =>
+  describe(`visualRefresh=${visualRefresh}`, () => {
+    test(
+      'closes the tools drawer upon clicking on the toggle in large viewports',
+      setupTest(visualRefresh, async page => {
+        await expect(page.isDisplayed(tabsWrapper.toSelector())).resolves.toBe(true);
+        await page.click(appLayoutWrapper.findToolsClose().toSelector());
+        await expect(page.isDisplayed(tabsWrapper.toSelector())).resolves.toBe(false);
+      })
+    );
 
-test(
-  'closes the tools drawer upon clicking on the toggle in small viewports',
-  setupTest(async page => {
-    await page.setWindowSize({ width: 500, height: 1000 });
-    await expect(page.isDisplayed(tabsWrapper.toSelector())).resolves.toBe(true);
-    await page.click(appLayoutWrapper.findToolsClose().toSelector());
-    await expect(page.isDisplayed(tabsWrapper.toSelector())).resolves.toBe(false);
+    test(
+      'closes the tools drawer upon clicking on the toggle in small viewports',
+      setupTest(visualRefresh, async page => {
+        await page.setWindowSize({ width: 500, height: 1000 });
+        await expect(page.isDisplayed(tabsWrapper.toSelector())).resolves.toBe(true);
+        await page.click(appLayoutWrapper.findToolsClose().toSelector());
+        await expect(page.isDisplayed(tabsWrapper.toSelector())).resolves.toBe(false);
+      })
+    );
   })
 );
