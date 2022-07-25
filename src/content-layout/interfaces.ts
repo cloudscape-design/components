e610708fed2c99279c7fa13f12e23ff774ae8147
Scott O'Brien <scottob@amazon.com>
2022-07-25 13:58:00 -0400

    First pass at API documentation.

--- src/content-layout/interfaces.ts
+++ src/content-layout/interfaces.ts
@@ -3,7 +3,21 @@
 import { BaseComponentProps } from '../internal/base-component';
 
 export interface ContentLayoutProps extends BaseComponentProps {
+  /**
+   * Use this slot to render the main content of the layout below the header.
+   * @displayname content
+   */
   children?: React.ReactNode;
+
+  /**
+   * Determines whether the layout has an overlap between the header and content.
+   * If true, the overlap will be removed.
+   * @visualrefresh
+   */
   disableOverlap?: boolean;
+
+  /**
+   * Use this slot to render the header content for the layout.
+   */
   header?: React.ReactNode;
 }
