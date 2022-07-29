a216cc8bd51ce0e64b861badbbb1db8aeede14d7
Scott O'Brien <scottob@amazon.com>
2022-07-29 13:12:07 -0400

    Incorporated PR feedback.

--- src/content-layout/index.tsx
+++ src/content-layout/index.tsx
@@ -4,27 +4,35 @@ import React, { useContext, useRef } from 'react';
 import clsx from 'clsx';
 import { AppLayoutContext } from '../app-layout/visual-refresh/context';
 import { ContentLayoutProps } from './interfaces';
+import { getBaseProps } from '../internal/base-component';
+import useBaseComponent from '../internal/hooks/use-base-component';
 import { useDynamicOverlap } from '../app-layout/visual-refresh/hooks/use-dynamic-overlap';
+import { useMergeRefs } from '../internal/hooks/use-merge-refs';
 import { useVisualRefresh } from '../internal/hooks/use-visual-mode';
 import styles from './styles.css.js';
 
 export { ContentLayoutProps };
 
-export default function ContentLayout({ children, disableOverlap, header }: ContentLayoutProps) {
+export default function ContentLayout({ children, disableOverlap, header, ...rest }: ContentLayoutProps) {
+  const baseProps = getBaseProps(rest);
   const { breadcrumbs } = useContext(AppLayoutContext);
+
   const rootElement = useRef<HTMLDivElement>(null);
+  const { __internalRootRef } = useBaseComponent('ContentLayout');
+  const overlapElement = useDynamicOverlap();
+  const mergedRef = useMergeRefs(rootElement, __internalRootRef, overlapElement);
+
   const isVisualRefresh = useVisualRefresh(rootElement);
   const isOverlapDisabled = !children || !header || disableOverlap;
 
-  const overlapElement = useDynamicOverlap();
-
   return (
     <div
-      className={clsx(styles.layout, {
+      {...baseProps}
+      className={clsx(baseProps.className, styles.layout, {
         [styles['is-overlap-disabled']]: isOverlapDisabled,
         [styles['is-visual-refresh']]: isVisualRefresh,
       })}
-      ref={rootElement}
+      ref={mergedRef}
     >
       <div
         className={clsx(
