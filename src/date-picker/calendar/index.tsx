youkoucoding/cloudscape-components

Include calendar into tab trap

src/date-picker/calendar/index.tsx
@@ -12,7 +12,6 @@ import CalendarHeader from './header';
 import Grid, { DateChangeHandlerNullable } from './grid';
 import moveFocusHandler from './utils/move-focus-handler';
 import { useUniqueId } from '../../internal/hooks/use-unique-id/index.js';
-import { useMergeRefs } from '../../internal/hooks/use-merge-refs/index.js';
 
 export interface DateChangeHandler {
   (detail: CalendarTypes.DateDetail): void;
@@ -45,136 +44,129 @@ interface CalendarProps extends BaseComponentProps {
   onFocusDate: DateChangeHandlerNullable;
 }
 
-const Calendar = React.forwardRef(
-  (
-    {
-      locale,
-      startOfWeek,
-      displayedDate,
-      focusedDate = null,
-      todayAriaLabel,
-      calendarHasFocus,
-      selectedDate,
-      isDateEnabled,
-      onChangeMonth,
-      onSelectDate,
-      onFocusDate,
-      previousMonthLabel,
-      nextMonthLabel,
-    }: CalendarProps,
-    ref
-  ) => {
-    const focusVisible = useFocusVisible();
-    const headerId = useUniqueId('calendar-dialog-title-');
-    const elementRef = useRef<HTMLDivElement>(null);
-    const calendarRef = useMergeRefs(elementRef, ref);
-    const gridWrapperRef = useRef<HTMLDivElement>(null);
-    const [gridHasFocus, setGridHasFocus] = useState(false);
+const Calendar = ({
+  locale,
+  startOfWeek,
+  displayedDate,
+  focusedDate = null,
+  todayAriaLabel,
+  calendarHasFocus,
+  selectedDate,
+  isDateEnabled,
+  onChangeMonth,
+  onSelectDate,
+  onFocusDate,
+  previousMonthLabel,
+  nextMonthLabel,
+}: CalendarProps) => {
+  const focusVisible = useFocusVisible();
+  const headerId = useUniqueId('calendar-dialog-title-');
+  const elementRef = useRef<HTMLDivElement>(null);
+  const gridWrapperRef = useRef<HTMLDivElement>(null);
+  const [gridHasFocus, setGridHasFocus] = useState(false);
 
-    const selectFocusedDate = (selected: Date | null, baseDate: Date): Date | null => {
-      if (selected && isDateEnabled(selected) && isSameMonth(selected, baseDate)) {
-        return selected;
-      }
-      const today = new Date();
-      if (isDateEnabled(today) && isSameMonth(today, baseDate)) {
-        return today;
-      }
-      if (isDateEnabled(baseDate)) {
-        return baseDate;
-      }
-      return null;
-    };
+  const selectFocusedDate = (selected: Date | null, baseDate: Date): Date | null => {
+    if (selected && isDateEnabled(selected) && isSameMonth(selected, baseDate)) {
+      return selected;
+    }
+    const today = new Date();
+    if (isDateEnabled(today) && isSameMonth(today, baseDate)) {
+      return today;
+    }
+    if (isDateEnabled(baseDate)) {
+      return baseDate;
+    }
+    return null;
+  };
 
-    const getBaseDate = (date: Date) => {
-      const startDate = startOfMonth(date);
-      if (isDateEnabled(startDate)) {
-        return startDate;
-      }
-      return moveFocusHandler(startDate, isDateEnabled, (date: Date) => addDays(date, 1));
-    };
+  const getBaseDate = (date: Date) => {
+    const startDate = startOfMonth(date);
+    if (isDateEnabled(startDate)) {
+      return startDate;
+    }
+    return moveFocusHandler(startDate, isDateEnabled, (date: Date) => addDays(date, 1));
+  };
 
-    const baseDate: Date = getBaseDate(displayedDate);
+  const baseDate: Date = getBaseDate(displayedDate);
 
-    const focusCurrentDate: FocusNextElement = () =>
-      (elementRef.current?.querySelector(`.${styles['calendar-day-focusable']}`) as HTMLDivElement)?.focus();
+  const focusCurrentDate: FocusNextElement = () =>
+    (elementRef.current?.querySelector(`.${styles['calendar-day-focusable']}`) as HTMLDivElement)?.focus();
 
-    const onHeaderChangeMonthHandler: HeaderChangeMonthHandler = isPrevious =>
-      onChangeMonth(addMonths(baseDate, isPrevious ? -1 : 1));
+  const onHeaderChangeMonthHandler: HeaderChangeMonthHandler = isPrevious =>
+    onChangeMonth(addMonths(baseDate, isPrevious ? -1 : 1));
 
-    useEffect(() => {
-      // focus current date if the focus is already inside the calendar grid
-      if (focusedDate instanceof Date && isSameMonth(focusedDate, baseDate) && gridHasFocus) {
-        focusCurrentDate();
-      }
-    }, [baseDate, focusedDate, gridHasFocus]);
+  useEffect(() => {
+    // focus current date if the focus is already inside the calendar grid
+    if (focusedDate instanceof Date && isSameMonth(focusedDate, baseDate) && gridHasFocus) {
+      focusCurrentDate();
+    }
+  }, [baseDate, focusedDate, gridHasFocus]);
 
-    useEffect(() => {
-      const calendarShouldHaveFocus = calendarHasFocus;
-      const calendarActuallyHasFocus = elementRef.current?.contains(document.activeElement);
+  useEffect(() => {
+    const calendarShouldHaveFocus = calendarHasFocus;
+    const calendarActuallyHasFocus = elementRef.current?.contains(document.activeElement);
 
-      if (calendarShouldHaveFocus && !calendarActuallyHasFocus) {
-        elementRef.current?.focus();
-      }
-
-      // When the baseDate or isDateEnabled changes, there might not be a focusable date in the grid anymore
-    }, [calendarHasFocus, baseDate, isDateEnabled]);
-
-    if (calendarHasFocus && !focusedDate) {
-      focusedDate = selectFocusedDate(selectedDate, baseDate);
+    if (calendarShouldHaveFocus && !calendarActuallyHasFocus) {
+      elementRef.current?.focus();
     }
 
-    const onGridBlur = (event: React.FocusEvent) => {
-      const newFocusTargetIsInGrid =
-        event.relatedTarget && gridWrapperRef.current?.contains(event.relatedTarget as Node);
-      if (!newFocusTargetIsInGrid) {
-        setGridHasFocus(false);
-      }
-    };
+    // When the baseDate or isDateEnabled changes, there might not be a focusable date in the grid anymore
+  }, [calendarHasFocus, baseDate, isDateEnabled]);
 
-    const onGridFocus = () => {
-      if (!gridHasFocus) {
-        setGridHasFocus(true);
-      }
-    };
+  if (calendarHasFocus && !focusedDate) {
+    focusedDate = selectFocusedDate(selectedDate, baseDate);
+  }
 
-    return (
-      <div
-        {...focusVisible}
-        className={styles.calendar}
-        tabIndex={0}
-        role="application"
-        aria-describedby={headerId}
-        ref={calendarRef}
-      >
-        <div className={styles['calendar-inner']}>
-          <CalendarHeader
-            headerId={headerId}
-            baseDate={baseDate}
+  const onGridBlur = (event: React.FocusEvent) => {
+    const newFocusTargetIsInGrid = event.relatedTarget && gridWrapperRef.current?.contains(event.relatedTarget as Node);
+    if (!newFocusTargetIsInGrid) {
+      setGridHasFocus(false);
+    }
+  };
+
+  const onGridFocus = () => {
+    if (!gridHasFocus) {
+      setGridHasFocus(true);
+    }
+  };
+
+  return (
+    <div
+      {...focusVisible}
+      className={styles.calendar}
+      tabIndex={0}
+      role="application"
+      aria-describedby={headerId}
+      ref={elementRef}
+    >
+      <div className={styles['calendar-inner']}>
+        <CalendarHeader
+          headerId={headerId}
+          baseDate={baseDate}
+          locale={locale}
+          onChangeMonth={onHeaderChangeMonthHandler}
+          previousMonthLabel={previousMonthLabel}
+          nextMonthLabel={nextMonthLabel}
+          calendarHasFocus={calendarHasFocus}
+        />
+        <div onBlur={onGridBlur} onFocus={onGridFocus} ref={gridWrapperRef}>
+          <Grid
             locale={locale}
-            onChangeMonth={onHeaderChangeMonthHandler}
-            previousMonthLabel={previousMonthLabel}
-            nextMonthLabel={nextMonthLabel}
-            calendarHasFocus={calendarHasFocus}
+            baseDate={baseDate}
+            isDateEnabled={isDateEnabled}
+            focusedDate={focusedDate}
+            onSelectDate={onSelectDate}
+            onFocusDate={onFocusDate}
+            onChangeMonth={onChangeMonth}
+            startOfWeek={startOfWeek}
+            todayAriaLabel={todayAriaLabel}
+            selectedDate={selectedDate}
+            handleFocusMove={moveFocusHandler}
           />
-          <div onBlur={onGridBlur} onFocus={onGridFocus} ref={gridWrapperRef}>
-            <Grid
-              locale={locale}
-              baseDate={baseDate}
-              isDateEnabled={isDateEnabled}
-              focusedDate={focusedDate}
-              onSelectDate={onSelectDate}
-              onFocusDate={onFocusDate}
-              onChangeMonth={onChangeMonth}
-              startOfWeek={startOfWeek}
-              todayAriaLabel={todayAriaLabel}
-              selectedDate={selectedDate}
-              handleFocusMove={moveFocusHandler}
-            />
-          </div>
         </div>
       </div>
-    );
-  }
-);
+    </div>
+  );
+};
 
 export default Calendar;

cloudscape-design/components

src/date-picker/calendar/index.tsx
@@ -12,7 +12,6 @@ import CalendarHeader from './header';
 import Grid, { DateChangeHandlerNullable } from './grid';
 import moveFocusHandler from './utils/move-focus-handler';
 import { useUniqueId } from '../../internal/hooks/use-unique-id/index.js';
-import { useMergeRefs } from '../../internal/hooks/use-merge-refs/index.js';
 import { formatDate, memoizedDate } from './utils/date.js';
 
 export interface DateChangeHandler {
@@ -43,152 +42,145 @@ interface CalendarProps extends BaseComponentProps {
   onSelectDate: DateChangeHandler;
 }
 
-const Calendar = React.forwardRef(
-  (
-    {
-      locale,
-      startOfWeek,
-      displayedDate,
-      todayAriaLabel,
-      selectedDate,
-      isDateEnabled,
-      onChangeMonth,
-      onSelectDate,
-      previousMonthLabel,
-      nextMonthLabel,
-    }: CalendarProps,
-    ref
-  ) => {
-    const focusVisible = useFocusVisible();
-    const headerId = useUniqueId('calendar-dialog-title-');
-    const elementRef = useRef<HTMLDivElement>(null);
-    const calendarRef = useMergeRefs(elementRef, ref);
-    const gridWrapperRef = useRef<HTMLDivElement>(null);
-    const [gridHasFocus, setGridHasFocus] = useState(false);
-    const [focusedDate, setFocusedDate] = useState<string | null>(null);
-    let focusedDateMemoized: null | Date = memoizedDate('focused', focusedDate);
+const Calendar = ({
+  locale,
+  startOfWeek,
+  displayedDate,
+  todayAriaLabel,
+  selectedDate,
+  isDateEnabled,
+  onChangeMonth,
+  onSelectDate,
+  previousMonthLabel,
+  nextMonthLabel,
+}: CalendarProps) => {
+  const focusVisible = useFocusVisible();
+  const headerId = useUniqueId('calendar-dialog-title-');
+  const elementRef = useRef<HTMLDivElement>(null);
+  const gridWrapperRef = useRef<HTMLDivElement>(null);
+  const [gridHasFocus, setGridHasFocus] = useState(false);
+  const [focusedDate, setFocusedDate] = useState<string | null>(null);
+  let focusedDateMemoized: null | Date = memoizedDate('focused', focusedDate);
 
-    const selectFocusedDate = (selected: Date | null, baseDate: Date): Date | null => {
-      if (selected && isDateEnabled(selected) && isSameMonth(selected, baseDate)) {
-        return selected;
-      }
-      const today = new Date();
-      if (isDateEnabled(today) && isSameMonth(today, baseDate)) {
-        return today;
-      }
-      if (isDateEnabled(baseDate)) {
-        return baseDate;
-      }
-      return null;
-    };
+  const selectFocusedDate = (selected: Date | null, baseDate: Date): Date | null => {
+    if (selected && isDateEnabled(selected) && isSameMonth(selected, baseDate)) {
+      return selected;
+    }
+    const today = new Date();
+    if (isDateEnabled(today) && isSameMonth(today, baseDate)) {
+      return today;
+    }
+    if (isDateEnabled(baseDate)) {
+      return baseDate;
+    }
+    return null;
+  };
 
-    const getBaseDate = (date: Date) => {
-      const startDate = startOfMonth(date);
-      if (isDateEnabled(startDate)) {
-        return startDate;
-      }
-      return moveFocusHandler(startDate, isDateEnabled, (date: Date) => addDays(date, 1));
-    };
+  const getBaseDate = (date: Date) => {
+    const startDate = startOfMonth(date);
+    if (isDateEnabled(startDate)) {
+      return startDate;
+    }
+    return moveFocusHandler(startDate, isDateEnabled, (date: Date) => addDays(date, 1));
+  };
 
-    const baseDate: Date = getBaseDate(displayedDate);
+  const baseDate: Date = getBaseDate(displayedDate);
 
-    const focusCurrentDate: FocusNextElement = () =>
-      (elementRef.current?.querySelector(`.${styles['calendar-day-focusable']}`) as HTMLDivElement)?.focus();
+  const focusCurrentDate: FocusNextElement = () =>
+    (elementRef.current?.querySelector(`.${styles['calendar-day-focusable']}`) as HTMLDivElement)?.focus();
 
-    const onHeaderChangeMonthHandler: HeaderChangeMonthHandler = isPrevious => {
-      onChangeMonth(addMonths(baseDate, isPrevious ? -1 : 1));
-      setFocusedDate(null);
-    };
+  const onHeaderChangeMonthHandler: HeaderChangeMonthHandler = isPrevious => {
+    onChangeMonth(addMonths(baseDate, isPrevious ? -1 : 1));
+    setFocusedDate(null);
+  };
 
-    const onGridChangeMonthHandler: MonthChangeHandler = newMonth => {
-      onChangeMonth(newMonth);
-      setFocusedDate(null);
-    };
+  const onGridChangeMonthHandler: MonthChangeHandler = newMonth => {
+    onChangeMonth(newMonth);
+    setFocusedDate(null);
+  };
 
-    const onGridFocusDateHandler: DateChangeHandlerNullable = ({ date }) => {
-      if (date) {
-        const value = formatDate(date);
-        setFocusedDate(value);
-      }
-    };
+  const onGridFocusDateHandler: DateChangeHandlerNullable = ({ date }) => {
+    if (date) {
+      const value = formatDate(date);
+      setFocusedDate(value);
+    }
+  };
 
-    const onGridSelectDateHandler: DateChangeHandler = detail => {
-      onSelectDate(detail);
-      setFocusedDate(null);
-    };
+  const onGridSelectDateHandler: DateChangeHandler = detail => {
+    onSelectDate(detail);
+    setFocusedDate(null);
+  };
 
-    useEffect(() => {
-      // focus current date if the focus is already inside the calendar grid
-      if (focusedDateMemoized instanceof Date && isSameMonth(focusedDateMemoized, baseDate) && gridHasFocus) {
-        focusCurrentDate();
-      }
-    }, [baseDate, focusedDateMemoized, gridHasFocus]);
+  useEffect(() => {
+    // focus current date if the focus is already inside the calendar grid
+    if (focusedDateMemoized instanceof Date && isSameMonth(focusedDateMemoized, baseDate) && gridHasFocus) {
+      focusCurrentDate();
+    }
+  }, [baseDate, focusedDateMemoized, gridHasFocus]);
 
-    useEffect(() => {
-      const calendarHasFocus = elementRef.current?.contains(document.activeElement);
+  useEffect(() => {
+    const calendarHasFocus = elementRef.current?.contains(document.activeElement);
 
-      if (!calendarHasFocus) {
-        elementRef.current?.focus();
-      }
-
-      // When the baseDate or isDateEnabled changes, there might not be a focusable date in the grid anymore
-    }, [baseDate, isDateEnabled]);
-
-    if (!focusedDate) {
-      focusedDateMemoized = selectFocusedDate(selectedDate, baseDate);
+    if (!calendarHasFocus) {
+      elementRef.current?.focus();
     }
 
-    const onGridBlur = (event: React.FocusEvent) => {
-      const newFocusTargetIsInGrid =
-        event.relatedTarget && gridWrapperRef.current?.contains(event.relatedTarget as Node);
-      if (!newFocusTargetIsInGrid) {
-        setGridHasFocus(false);
-      }
-    };
+    // When the baseDate or isDateEnabled changes, there might not be a focusable date in the grid anymore
+  }, [baseDate, isDateEnabled]);
 
-    const onGridFocus = () => {
-      if (!gridHasFocus) {
-        setGridHasFocus(true);
-      }
-    };
+  if (!focusedDate) {
+    focusedDateMemoized = selectFocusedDate(selectedDate, baseDate);
+  }
 
-    return (
-      <div
-        {...focusVisible}
-        className={styles.calendar}
-        tabIndex={0}
-        role="application"
-        aria-describedby={headerId}
-        ref={calendarRef}
-      >
-        <div className={styles['calendar-inner']}>
-          <CalendarHeader
-            headerId={headerId}
-            baseDate={baseDate}
+  const onGridBlur = (event: React.FocusEvent) => {
+    const newFocusTargetIsInGrid = event.relatedTarget && gridWrapperRef.current?.contains(event.relatedTarget as Node);
+    if (!newFocusTargetIsInGrid) {
+      setGridHasFocus(false);
+    }
+  };
+
+  const onGridFocus = () => {
+    if (!gridHasFocus) {
+      setGridHasFocus(true);
+    }
+  };
+
+  return (
+    <div
+      {...focusVisible}
+      className={styles.calendar}
+      tabIndex={0}
+      role="application"
+      aria-describedby={headerId}
+      ref={elementRef}
+    >
+      <div className={styles['calendar-inner']}>
+        <CalendarHeader
+          headerId={headerId}
+          baseDate={baseDate}
+          locale={locale}
+          onChangeMonth={onHeaderChangeMonthHandler}
+          previousMonthLabel={previousMonthLabel}
+          nextMonthLabel={nextMonthLabel}
+        />
+        <div onBlur={onGridBlur} onFocus={onGridFocus} ref={gridWrapperRef}>
+          <Grid
             locale={locale}
-            onChangeMonth={onHeaderChangeMonthHandler}
-            previousMonthLabel={previousMonthLabel}
-            nextMonthLabel={nextMonthLabel}
+            baseDate={baseDate}
+            isDateEnabled={isDateEnabled}
+            focusedDate={focusedDateMemoized}
+            onSelectDate={onGridSelectDateHandler}
+            onFocusDate={onGridFocusDateHandler}
+            onChangeMonth={onGridChangeMonthHandler}
+            startOfWeek={startOfWeek}
+            todayAriaLabel={todayAriaLabel}
+            selectedDate={selectedDate}
+            handleFocusMove={moveFocusHandler}
           />
-          <div onBlur={onGridBlur} onFocus={onGridFocus} ref={gridWrapperRef}>
-            <Grid
-              locale={locale}
-              baseDate={baseDate}
-              isDateEnabled={isDateEnabled}
-              focusedDate={focusedDateMemoized}
-              onSelectDate={onGridSelectDateHandler}
-              onFocusDate={onGridFocusDateHandler}
-              onChangeMonth={onGridChangeMonthHandler}
-              startOfWeek={startOfWeek}
-              todayAriaLabel={todayAriaLabel}
-              selectedDate={selectedDate}
-              handleFocusMove={moveFocusHandler}
-            />
-          </div>
         </div>
       </div>
-    );
-  }
-);
+    </div>
+  );
+};
 
 export default Calendar;