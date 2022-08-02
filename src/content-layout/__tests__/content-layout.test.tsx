d742d42b0763fae7904bfdc46ed4ec748d317639
Connor Lanigan <dev@connorlanigan.com>
2022-08-02 18:22:29 +0200

    chore(content-layout): Convert integration tests to unit tests

--- src/content-layout/__tests__/content-layout.test.tsx
+++ src/content-layout/__tests__/content-layout.test.tsx
@@ -4,45 +4,107 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import ContentLayout, { ContentLayoutProps } from '../../../lib/components/content-layout';
 import createWrapper from '../../../lib/components/test-utils/dom';
+import styles from '../../../lib/components/content-layout/styles.selectors.js';
 
 function renderContentLayout(props: ContentLayoutProps = {}) {
-  const renderResult = render(<ContentLayout {...props} />);
-  return createWrapper(renderResult.container).findContentLayout()!;
+  const { container, rerender } = render(<ContentLayout {...props} />);
+  const wrapper = createWrapper(container).findContentLayout()!;
+
+  return {
+    wrapper,
+    isOverlapEnabled() {
+      const backgroundElement = wrapper.findByClassName(styles.background)!.getElement();
+      const classes = backgroundElement.getAttribute('class')!;
+      return !classes.includes(styles['is-overlap-disabled']);
+    },
+    rerender: (props: ContentLayoutProps) => rerender(<ContentLayout {...props} />),
+  };
 }
 
 describe('ContentLayout component', () => {
-  test('renders the header slot', () => {
-    const wrapper = renderContentLayout({
-      header: <>Header text</>,
+  describe('slots', () => {
+    test('renders the header slot', () => {
+      const { wrapper } = renderContentLayout({
+        header: <>Header text</>,
+      });
+
+      expect(wrapper.findHeader()!.getElement()).toHaveTextContent('Header text');
+      expect(wrapper.findContent()).toBeNull();
     });
 
-    expect(wrapper.findHeader()!.getElement()).toHaveTextContent('Header text');
-    expect(wrapper.findContent()).toBeNull();
-  });
+    test('renders the content slot', () => {
+      const { wrapper } = renderContentLayout({
+        children: <>Content text</>,
+      });
 
-  test('renders the content slot', () => {
-    const wrapper = renderContentLayout({
-      children: <>Content text</>,
+      expect(wrapper.findHeader()).toBeNull();
+      expect(wrapper.findContent()!.getElement()).toHaveTextContent('Content text');
     });
 
-    expect(wrapper.findHeader()).toBeNull();
-    expect(wrapper.findContent()!.getElement()).toHaveTextContent('Content text');
-  });
+    test('renders the header and content slots', () => {
+      const { wrapper } = renderContentLayout({
+        children: <>Content text</>,
+        header: <>Header text</>,
+      });
 
-  test('renders the header and content slots', () => {
-    const wrapper = renderContentLayout({
-      children: <>Content text</>,
-      header: <>Header text</>,
+      expect(wrapper.findHeader()!.getElement()).toHaveTextContent('Header text');
+      expect(wrapper.findContent()!.getElement()).toHaveTextContent('Content text');
     });
 
-    expect(wrapper.findHeader()!.getElement()).toHaveTextContent('Header text');
-    expect(wrapper.findContent()!.getElement()).toHaveTextContent('Content text');
+    test('renders nothing', () => {
+      const { wrapper } = renderContentLayout({});
+
+      expect(wrapper.findHeader()).toBeNull();
+      expect(wrapper.findContent()).toBeNull();
+    });
   });
 
-  test('renders nothing', () => {
-    const wrapper = renderContentLayout({});
+  describe('overlap', () => {
+    test('renders the overlap by default', () => {
+      const { isOverlapEnabled } = renderContentLayout({ children: <>Content text</>, header: <>Header text</> });
+      expect(isOverlapEnabled()).toBe(true);
+    });
 
-    expect(wrapper.findHeader()).toBeNull();
-    expect(wrapper.findContent()).toBeNull();
+    test('does not render the overlap if disableOverlap is set', () => {
+      const { isOverlapEnabled } = renderContentLayout({
+        children: <>Content text</>,
+        header: <>Header text</>,
+        disableOverlap: true,
+      });
+      expect(isOverlapEnabled()).toBe(false);
+    });
+
+    test('does not render the overlap if the content is empty', () => {
+      const { isOverlapEnabled } = renderContentLayout({
+        header: <>Header text</>,
+      });
+      expect(isOverlapEnabled()).toBe(false);
+    });
+
+    test('does not render the overlap if the header is empty', () => {
+      const { isOverlapEnabled } = renderContentLayout({
+        children: <>Content text</>,
+      });
+      expect(isOverlapEnabled()).toBe(false);
+    });
+
+    test('does not render the overlap if the header is toggled', () => {
+      const { isOverlapEnabled, rerender } = renderContentLayout({
+        children: <>Content text</>,
+        header: <>Header text</>,
+      });
+      expect(isOverlapEnabled()).toBe(true);
+
+      rerender({
+        children: <>Content text</>,
+      });
+      expect(isOverlapEnabled()).toBe(false);
+
+      rerender({
+        children: <>Content text</>,
+        header: <>Header text</>,
+      });
+      expect(isOverlapEnabled()).toBe(true);
+    });
   });
 });
