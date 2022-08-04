63b6afa2223e94d0c65c4efaf8515ba633f32d34
Francesco Longo <flongo@amazon.com>
2022-08-04 11:10:24 +0200

    chore: Rename border-radius tokens to follow tokens naming convention

--- style-dictionary/visual-refresh/metadata/borders.ts
+++ style-dictionary/visual-refresh/metadata/borders.ts
@@ -3,19 +3,19 @@
 import { StyleDictionary } from '../../utils/interfaces';
 
 const metadata: StyleDictionary.MetadataIndex = {
-  borderActiveRadius: { description: 'The border radius of the active tab indicator.' },
+  borderRadiusActive: { description: 'The border radius of the active tab indicator.' },
   borderActiveWidth: { description: 'The height of the active tab indicator.' },
   borderDividerListWidth: { description: 'Used for divider between repeated items such as dropdowns and tables.' },
   borderDividerSectionWidth: {
     description:
       'The default system divider width - used for dividers between sections of content such as key/value pairs and tabs, for both full width and inset dividers.',
   },
-  borderDropdownRadius: { description: 'Used for dropdown containers.' },
-  borderFieldRadius: {
+  borderRadiusDropdown: { description: 'Used for dropdown containers.' },
+  borderRadiusField: {
     description: 'The border radius of most small elements in the system, e.g. inputs, tiles, alerts, tokens.',
   },
   borderInvalidWidth: { description: 'Used for invalid input left border width.' },
-  borderItemRadius: { description: 'Used for list items that can be selected, e.g. dropdown options, table rows.' },
+  borderRadiusItem: { description: 'Used for list items that can be selected, e.g. dropdown options, table rows.' },
   borderPanelHeaderWidth: { description: 'The split panel header bottom divider width.' },
   borderPanelTopWidth: { description: 'The split panel top border width.' },
 };
