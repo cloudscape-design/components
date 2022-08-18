f85e6ad670db6c5d2ab71a4ae929cded778247c3
Scott O'Brien <scottob@amazon.com>
2022-08-18 13:44:31 -0400

    Incorporated PR feedback.

--- src/content-layout/index.tsx
+++ src/content-layout/index.tsx
@@ -1,28 +1,38 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import clsx from 'clsx';
 import { AppLayoutContext } from '../app-layout/visual-refresh/context';
 import { ContentLayoutProps } from './interfaces';
+import { getBaseProps } from '../internal/base-component';
+import useBaseComponent from '../internal/hooks/use-base-component';
 import { useDynamicOverlap } from '../app-layout/visual-refresh/hooks/use-dynamic-overlap';
+import { useMergeRefs } from '../internal/hooks/use-merge-refs';
 import { useVisualRefresh } from '../internal/hooks/use-visual-mode';
 import styles from './styles.css.js';
 
 export { ContentLayoutProps };
 
-export default function ContentLayout({ children, disableOverlap, header }: ContentLayoutProps) {
+export default function ContentLayout({ children, disableOverlap, header, ...rest }: ContentLayoutProps) {
+  const baseProps = getBaseProps(rest);
   const { breadcrumbs } = useContext(AppLayoutContext);
   const isVisualRefresh = useVisualRefresh();
-  const isOverlapDisabled = !children || !header || disableOverlap;
 
+  const rootElement = useRef<HTMLDivElement>(null);
+  const { __internalRootRef } = useBaseComponent('ContentLayout');
   const overlapElement = useDynamicOverlap();
+  const mergedRef = useMergeRefs(rootElement, __internalRootRef, overlapElement);
+
+  const isOverlapDisabled = !children || !header || disableOverlap;
 
   return (
     <div
-      className={clsx(styles.layout, {
+      {...baseProps}
+      className={clsx(baseProps.className, styles.layout, {
         [styles['is-overlap-disabled']]: isOverlapDisabled,
         [styles['is-visual-refresh']]: isVisualRefresh,
       })}
+      ref={mergedRef}
     >
       <div
         className={clsx(
