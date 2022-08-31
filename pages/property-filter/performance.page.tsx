fc9e5da3f5d23da3a276c740e0d8dcc1acf97551
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-08-31 13:52:47 +0200

    Fix test page layout

--- pages/property-filter/performance.page.tsx
+++ pages/property-filter/performance.page.tsx
@@ -1,12 +1,9 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import React, { useMemo } from 'react';
-import AppLayout from '~components/app-layout';
 import Table from '~components/table';
-import PropertyFilter from '~components/property-filter';
+import PropertyFilter, { PropertyFilterProps } from '~components/property-filter';
 import Header from '~components/header';
-import ScreenshotArea from '../utils/screenshot-area';
-import labels from '../app-layout/utils/labels';
 import { allItems, columnDefinitions, TableItem, i18nStrings, filteringProperties } from './table.data';
 import { useCollection } from '@cloudscape-design/collection-hooks';
 
@@ -23,7 +20,7 @@ export default function () {
 
   // Generate many autosuggest items.
   const filteringOptions = useMemo(() => {
-    const filteringOptions: any[] = [];
+    const filteringOptions: PropertyFilterProps.FilteringOption[] = [];
 
     for (let i = 0; i < 500; i++) {
       for (const option of propertyFilterProps.filteringOptions) {
@@ -35,31 +32,25 @@ export default function () {
   }, [propertyFilterProps.filteringOptions]);
 
   return (
-    <ScreenshotArea gutters={false}>
-      <AppLayout
-        ariaLabels={labels}
-        navigationHide={true}
-        content={
-          <Table<TableItem>
-            className="main-content"
-            stickyHeader={true}
-            header={<Header headingTagOverride={'h1'}>Performance test (many filtering options)</Header>}
-            items={items}
-            {...collectionProps}
-            filter={
-              <PropertyFilter
-                {...propertyFilterProps}
-                filteringOptions={filteringOptions}
-                virtualScroll={true}
-                countText={`${items.length} matches`}
-                i18nStrings={i18nStrings}
-                expandToViewport={true}
-              />
-            }
-            columnDefinitions={columnDefinitions.slice(0, 2)}
+    <div style={{ padding: 10 }}>
+      <Table<TableItem>
+        className="main-content"
+        stickyHeader={true}
+        header={<Header headingTagOverride={'h1'}>Performance test (many filtering options)</Header>}
+        items={items}
+        {...collectionProps}
+        filter={
+          <PropertyFilter
+            {...propertyFilterProps}
+            filteringOptions={filteringOptions}
+            virtualScroll={true}
+            countText={`${items.length} matches`}
+            i18nStrings={i18nStrings}
+            expandToViewport={true}
           />
         }
+        columnDefinitions={columnDefinitions.slice(0, 2)}
       />
-    </ScreenshotArea>
+    </div>
   );
 }
