152e19a9ca5aff4e23526ff9599f2f7079ecdab9
Connor Lanigan <dev@connorlanigan.com>
2022-09-01 16:55:25 +0200

    feat: Use React's `useId` hook if available

--- src/internal/hooks/use-unique-id/index.ts
+++ src/internal/hooks/use-unique-id/index.ts
@@ -1,16 +1,27 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { useRef } from 'react';
+import React, { useRef } from 'react';
 
 let counter = 0;
-export function generateUniqueId(prefix?: string) {
-  return `${prefix ? prefix : ''}${counter++}-${Date.now()}-${Math.round(Math.random() * 10000)}`;
+export function generateUniqueId(prefix = '') {
+  return `${prefix}${counter++}-${Date.now()}-${Math.round(Math.random() * 10000)}`;
 }
 
-export function useUniqueId(prefix?: string) {
+export function useUniqueId(prefix = '') {
   const idRef = useRef<string | null>(null);
+
+  // useId is only available in React 18+
+  if ((React as ReactWithUseId).useId !== undefined) {
+    return prefix + (React as ReactWithUseId).useId();
+  }
+
   if (!idRef.current) {
     idRef.current = generateUniqueId(prefix);
   }
   return idRef.current;
 }
+
+type ReactWithUseId = typeof React & {
+  // See https://github.com/DefinitelyTyped/DefinitelyTyped/blob/cc9a28eb5e1953f8f74ade87d7f16a8db8ab7552/types/react/index.d.ts#L1157
+  useId(): string;
+};
