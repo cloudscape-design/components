06de0f71406fb6c8444dbbc6d29e529c7d8c0a2c
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-09-02 14:20:44 +0200

    Add load-more controller util to autosuggest (#223)

---
+++ src/autosuggest/__tests__/load-more-controller.test.ts
@@ -0,0 +1,89 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useAutosuggestLoadMore } from '../load-more-controller';
+import { renderHook, act } from '../../__tests__/render-hook';
+import { OptionsLoadItemsDetail } from '../../internal/components/dropdown/interfaces';
+
+describe('Autosuggest load-more-controller', () => {
+  test('calls onLoadItems on input focus', () => {
+    const onLoadItems = jest.fn();
+    const { result } = renderHook(useAutosuggestLoadMore, {
+      initialProps: { options: [], statusType: 'error', onLoadItems },
+    });
+    act(() => result.current.fireLoadMoreOnInputFocus());
+    expect(onLoadItems).toBeCalledWith({ firstPage: true, samePage: false, filteringText: '' });
+  });
+
+  test('calls onLoadItems on input change', () => {
+    const onLoadItems = jest.fn();
+    const { result } = renderHook(useAutosuggestLoadMore, {
+      initialProps: { options: [], statusType: 'error', onLoadItems },
+    });
+    act(() => result.current.fireLoadMoreOnInputChange('a'));
+    act(() => result.current.fireLoadMoreOnInputChange('ab'));
+    act(() => result.current.fireLoadMoreOnInputChange('ab'));
+    expect(onLoadItems).toBeCalledTimes(2);
+    expect(onLoadItems).toBeCalledWith({ firstPage: true, samePage: false, filteringText: 'a' });
+    expect(onLoadItems).toBeCalledWith({ firstPage: true, samePage: false, filteringText: 'ab' });
+  });
+
+  test('calls onLoadItems on recovery click', () => {
+    const onLoadItems = jest.fn();
+    const { result } = renderHook(useAutosuggestLoadMore, {
+      initialProps: { options: [], statusType: 'error', onLoadItems },
+    });
+    act(() => result.current.fireLoadMoreOnRecoveryClick());
+    expect(onLoadItems).toBeCalledWith({ firstPage: false, samePage: true, filteringText: '' });
+  });
+
+  test('only calls onLoadItems on scroll when options are not empty and status is pending', () => {
+    const onLoadItems = jest.fn();
+    const { result, rerender } = renderHook(useAutosuggestLoadMore, {
+      initialProps: { options: [{ value: 'Option 0' }], statusType: 'pending', onLoadItems },
+    });
+    act(() => result.current.fireLoadMoreOnScroll());
+    expect(onLoadItems).toBeCalledWith({ firstPage: false, samePage: false, filteringText: '' });
+
+    rerender({ options: [], statusType: 'pending', onLoadItems });
+    act(() => result.current.fireLoadMoreOnScroll());
+
+    rerender({ options: [{ value: 'Option 0' }], statusType: 'finished', onLoadItems });
+    act(() => result.current.fireLoadMoreOnScroll());
+
+    rerender({ options: [{ value: 'Option 0' }], statusType: 'loading', onLoadItems });
+    act(() => result.current.fireLoadMoreOnScroll());
+
+    rerender({ options: [{ value: 'Option 0' }], statusType: 'error', onLoadItems });
+    act(() => result.current.fireLoadMoreOnScroll());
+
+    expect(onLoadItems).toBeCalledTimes(1);
+  });
+
+  test('overrides filtering text when called on input focus', () => {
+    let filteringText = '';
+    const onLoadItems = (detail: OptionsLoadItemsDetail) => {
+      filteringText = detail.filteringText;
+    };
+    const { result } = renderHook(useAutosuggestLoadMore, {
+      initialProps: { options: [], statusType: 'error', onLoadItems },
+    });
+    act(() => result.current.fireLoadMoreOnInputChange('input'));
+    act(() => result.current.fireLoadMoreOnInputFocus());
+    expect(filteringText).toBe('');
+  });
+
+  test('does not override filtering text when called on recovery or scroll', () => {
+    let filteringText = '';
+    const onLoadItems = (detail: OptionsLoadItemsDetail) => {
+      filteringText = detail.filteringText;
+    };
+    const { result } = renderHook(useAutosuggestLoadMore, {
+      initialProps: { options: [{ value: 'Option 0' }], statusType: 'pending', onLoadItems },
+    });
+    act(() => result.current.fireLoadMoreOnInputChange('input'));
+    act(() => result.current.fireLoadMoreOnRecoveryClick());
+    act(() => result.current.fireLoadMoreOnScroll());
+    expect(filteringText).toBe('input');
+  });
+});
