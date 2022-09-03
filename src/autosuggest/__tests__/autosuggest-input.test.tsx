3033a8972ddbe99e5c3a1ac93aea805cf674ab53
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-09-03 15:27:39 +0200

    Add unit tests for autosuggest-input imperative api

--- src/autosuggest/__tests__/autosuggest-input.test.tsx
+++ src/autosuggest/__tests__/autosuggest-input.test.tsx
@@ -4,9 +4,10 @@ import * as React from 'react';
 import { render as renderJsx } from '@testing-library/react';
 import dropdownStyles from '../../../lib/components/internal/components/dropdown/styles.selectors.js';
 import createWrapper, { ElementWrapper } from '../../../lib/components/test-utils/dom';
-import AutosuggestInput from '../../../lib/components/autosuggest/autosuggest-input';
+import AutosuggestInput, { AutosuggestInputRef } from '../../../lib/components/autosuggest/autosuggest-input';
 import { KeyCode } from '@cloudscape-design/test-utils-core/utils';
 import '../../__a11y__/to-validate-a11y';
+import { act } from 'react-dom/test-utils';
 
 function render(jsx: React.ReactElement) {
   const { container, rerender } = renderJsx(jsx);
@@ -21,6 +22,51 @@ function findInput(wrapper: ElementWrapper<Element>) {
 function findOpenDropdown(wrapper: ElementWrapper<Element>) {
   return wrapper.find(`.${dropdownStyles.dropdown}[data-open=true]`);
 }
+
+describe('imperative interface', () => {
+  test('focuses input and opens dropdown', () => {
+    const ref = React.createRef<AutosuggestInputRef>();
+    const { wrapper } = render(<AutosuggestInput ref={ref} value="" onChange={() => undefined} />);
+    expect(findInput(wrapper).getElement()).not.toHaveFocus();
+    expect(findOpenDropdown(wrapper)).toBe(null);
+    ref.current!.focus();
+    expect(findInput(wrapper).getElement()).toHaveFocus();
+    expect(findOpenDropdown(wrapper)).not.toBe(null);
+  });
+
+  test('focuses input and keeps dropdown closed', () => {
+    const ref = React.createRef<AutosuggestInputRef>();
+    const { wrapper } = render(<AutosuggestInput ref={ref} value="" onChange={() => undefined} />);
+    expect(findInput(wrapper).getElement()).not.toHaveFocus();
+    expect(findOpenDropdown(wrapper)).toBe(null);
+    ref.current!.focusNoOpen();
+    expect(findInput(wrapper).getElement()).toHaveFocus();
+    expect(findOpenDropdown(wrapper)).toBe(null);
+  });
+
+  test('selects input text', () => {
+    const ref = React.createRef<AutosuggestInputRef>();
+    const onChange = jest.fn();
+    const { wrapper } = render(<AutosuggestInput ref={ref} value="123" onChange={onChange} />);
+    const inputEl = findInput(wrapper).getElement() as any;
+    expect(inputEl.selectionStart).toBe(3);
+    expect(inputEl.selectionEnd).toBe(3);
+    ref.current!.select();
+    expect(inputEl.selectionStart).toBe(0);
+    expect(inputEl.selectionEnd).toBe(3);
+  });
+
+  test('opens and closes dropdown', () => {
+    const ref = React.createRef<AutosuggestInputRef>();
+    const onChange = jest.fn();
+    const { wrapper } = render(<AutosuggestInput ref={ref} value="123" onChange={onChange} />);
+    expect(findOpenDropdown(wrapper)).toBe(null);
+    act(() => ref.current!.open());
+    expect(findOpenDropdown(wrapper)).not.toBe(null);
+    act(() => ref.current!.close());
+    expect(findOpenDropdown(wrapper)).toBe(null);
+  });
+});
 
 describe('keyboard interactions', () => {
   test('closes dropdown on enter and opens it on arrow keys', () => {
