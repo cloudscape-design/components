7332076845e2f322e7b2c4765219c8852453875e
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-09-21 10:41:55 +0200

    Make date-input and calendar public

--- src/calendar/__tests__/calendar.test.tsx
+++ src/calendar/__tests__/calendar.test.tsx
@@ -6,8 +6,6 @@ import { render } from '@testing-library/react';
 import Calendar, { CalendarProps } from '../../../lib/components/calendar';
 import createWrapper from '../../../lib/components/test-utils/dom';
 import '../../__a11y__/to-validate-a11y';
-import styles from '../../../lib/components/calendar/styles.selectors.js';
-import CalendarWrapper from '../../../lib/components/test-utils/dom/calendar';
 
 // The calendar is mostly tested here: src/date-picker/__tests__/date-picker-calendar.test.tsx
 
@@ -21,7 +19,7 @@ describe('Calendar', () => {
 
   function renderCalendar(props: CalendarProps = defaultProps) {
     const { container, getByTestId } = render(<Calendar {...props} />);
-    const wrapper = createWrapper(container).findComponent(`.${styles.root}`, CalendarWrapper);
+    const wrapper = createWrapper(container).findCalendar();
     return { container, wrapper, getByTestId };
   }
 
