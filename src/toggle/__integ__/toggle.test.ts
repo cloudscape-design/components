73d2abf82a96ee6e17310763b6c7026bda8f8fb0
Yueying Lu <98534165+YueyingLu@users.noreply.github.com>
2022-09-22 12:50:44 +0200

    Align Checkbox Toggle implementation with description in Formfield co… (#288)
    * Align Checkbox Toggle implementation with description in Formfield context
    
    * update tests
    
    * Add tests checking formfield label control
    
    * Change api description

---
+++ src/toggle/__integ__/toggle.test.ts
@@ -0,0 +1,27 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { BasePageObject } from '@cloudscape-design/browser-test-tools/page-objects';
+import useBrowser from '@cloudscape-design/browser-test-tools/use-browser';
+import createWrapper from '../../../lib/components/test-utils/selectors';
+
+const wrapper = createWrapper();
+
+class TogglePage extends BasePageObject {}
+
+const setupTest = (testFn: (page: TogglePage) => Promise<void>) => {
+  return useBrowser(async browser => {
+    const page = new TogglePage(browser);
+    await browser.url('#/light/toggle/simple');
+    await testFn(page);
+  });
+};
+
+test(
+  'Clicking on form field label should not change toggle state',
+  setupTest(async page => {
+    await page.click(wrapper.findFormField().findLabel().toSelector());
+    await expect(
+      page.isSelected(wrapper.find('#formfield-with-toggle').findToggle().find('input').toSelector())
+    ).resolves.toBe(false);
+  })
+);
