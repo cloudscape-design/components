ff5b93b8a2f38983a0eed47ca4053103770f168d
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-10-04 14:10:15 +0200

    Add prefer-live-region eslint rule

---
+++ build-tools/eslint/prefer-live-region.js
@@ -0,0 +1,29 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+module.exports = {
+  meta: {
+    type: 'problem',
+    messages: {
+      'prefer-live-region': 'JSX element uses aria-live property. Prefer using LiveRegion component instead.',
+    },
+    docs: {
+      description: 'prevents direct use of aria-live to avoid a known issue with NVDA.',
+    },
+  },
+  create(context) {
+    return {
+      JSXElement(node) {
+        if (
+          node.openingElement.attributes.some(
+            attribute => attribute.type === 'JSXAttribute' && attribute.name.name === 'aria-live'
+          )
+        ) {
+          context.report({
+            node: node,
+            messageId: 'prefer-live-region',
+          });
+        }
+      },
+    };
+  },
+};
