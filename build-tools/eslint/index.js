48bf1085de05579d2b27c2682c36ec01e46cc64e
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-10-05 12:58:54 +0200

    Add prefer-live-region eslint rule (#327)

--- build-tools/eslint/index.js
+++ build-tools/eslint/index.js
@@ -3,4 +3,5 @@
 module.exports.rules = {
   'ban-files': require('./ban-files'),
   'no-implicit-svg-focusable': require('./no-implicit-svg-focusable'),
+  'prefer-live-region': require('./prefer-live-region'),
 };
