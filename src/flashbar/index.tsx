c88a6c918506588b02dd132d0e4dd32972a12420
Scott O'Brien <DaemonCahill@users.noreply.github.com>
2022-10-12 10:39:12 -0400

    Flashbar stacked items feature beta test (#294)
    * In progress commit of layout and demo page.
    
    * Temporary addition of test page.
    
    * Miscellaneous cleanup and documentation.
    
    * Adjusted the notification render in the demo page.
    
    * Removed temporary testing page.
    
    * Adjusted import order.
    
    * Refactored documentation.
    
    * Various adjustments to render logic.
    
    * Additional design iteration.
    
    * Adjust comments.
    
    * Additional design iteration.
    
    * Added styles for classic.
    
    * Abstracted into a single render function.
    
    * Added a comment for render item abstraction.
    
    * Adjustments to handle focus visible in the flashbar context.
    
    * Remove unused header context.
    
    * Added function documentation.
    
    * Adjust selectors after removal of the visual context div.
    
    * Added aria label to the expand collapse button.
    
    * Update the property name.
    
    * Added support for stack button aria labels.

--- src/flashbar/index.tsx
+++ src/flashbar/index.tsx
@@ -1,85 +1,184 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import clsx from 'clsx';
-import React from 'react';
-import { useMergeRefs } from '../internal/hooks/use-merge-refs';
-import { getBaseProps } from '../internal/base-component';
-import styles from './styles.css.js';
-import { Flash } from './flash';
-import { useContainerBreakpoints } from '../internal/hooks/container-queries';
+import React, { useState } from 'react';
 import { applyDisplayName } from '../internal/utils/apply-display-name';
-import VisualContext from '../internal/components/visual-context';
-import useBaseComponent from '../internal/hooks/use-base-component';
-import { useReducedMotion, useVisualRefresh } from '../internal/hooks/use-visual-mode';
+import customCssProps from '../internal/generated/custom-css-properties';
+import { Flash } from './flash';
 import { FlashbarProps } from './interfaces';
+import { getBaseProps } from '../internal/base-component';
+import InternalIcon from '../icon/internal';
+import { TIMEOUT_FOR_ENTERING_ANIMATION } from './constant';
 import { TransitionGroup } from 'react-transition-group';
 import { Transition } from '../internal/components/transition';
-import { TIMEOUT_FOR_ENTERING_ANIMATION } from './constant';
+import useBaseComponent from '../internal/hooks/use-base-component';
+import { useContainerBreakpoints } from '../internal/hooks/container-queries';
+import useFocusVisible from '../internal/hooks/focus-visible';
+import { useMergeRefs } from '../internal/hooks/use-merge-refs';
+import { useReducedMotion, useVisualRefresh } from '../internal/hooks/use-visual-mode';
+import { getVisualContextClassname } from '../internal/components/visual-context';
+import styles from './styles.css.js';
 
 export { FlashbarProps };
 
 export default function Flashbar({ items, ...restProps }: FlashbarProps) {
   const { __internalRootRef } = useBaseComponent('Flashbar');
   const [breakpoint, ref] = useContainerBreakpoints(['xs']);
-  const isRefresh = useVisualRefresh();
   const baseProps = getBaseProps(restProps);
-
   const mergedRef = useMergeRefs(ref, __internalRootRef);
+  const isFocusVisible = useFocusVisible();
+  const isVisualRefresh = useVisualRefresh();
 
-  // All the flash items should have ids so we can identify which DOM element is being removed from the DOM to animate it.
-  const motionDisabled = useReducedMotion(ref as any) || !isRefresh || (items && !items.every(item => 'id' in item));
-  // Motion will be disabled if any of the provided flash messages does not contain an `id`
+  /**
+   * All the flash items should have ids so we can identify which DOM element is being
+   * removed from the DOM to animate it. Motion will be disabled if any of the provided
+   * flash messages does not contain an `id`.
+   */
+  const motionDisabled =
+    useReducedMotion(ref as any) || !isVisualRefresh || (items && !items.every(item => 'id' in item));
 
-  const renderFlashItem = (item: FlashbarProps.MessageDefinition, index: number) => (
-    <Flash
-      key={item.id ?? index}
-      // eslint-disable-next-line react/forbid-component-props
-      className={clsx(isRefresh ? styles['flash-refresh'] : '')}
-      {...item}
-    />
-  );
-  const renderFlashItemsWithTransitions = (items: readonly FlashbarProps.MessageDefinition[]) => {
+  /**
+   * The `stackItems` property is a hidden boolean that allows for teams
+   * to beta test the flashbar stacking feature.
+   */
+  const stackItems = (restProps as any).stackItems;
+  const ariaLabels = (restProps as any).ariaLabels;
+  const isFlashbarStacked = stackItems && items?.length > 3;
+  const [isFlashbarStackExpanded, setIsFlashbarStackExpanded] = useState(false);
+
+  /**
+   * Compute the appropriate aria label for the stacked notifications toggle button
+   * based on the expanded/collapsed state of the stack and the presence of
+   * corresponding aria label properties.
+   */
+  function getStackButtonAriaLabel() {
+    let stackButtonAriaLabel;
+
+    if (isFlashbarStackExpanded) {
+      stackButtonAriaLabel = ariaLabels?.stackCollapseLabel ?? 'Collapse stacked notifications';
+    } else {
+      stackButtonAriaLabel = ariaLabels?.stackExpandLabel ?? 'Expand stacked notifications';
+    }
+
+    return stackButtonAriaLabel;
+  }
+
+  /**
+   * If the `isFlashbarStacked` is true (which is only possible if `stackItems` is true)
+   * then the first item should be rendered followed by two dummy items that visually indicate
+   * two, three, or more items exist in the stack.
+   */
+  function renderStackedItems() {
+    if (!isFlashbarStacked) {
+      return;
+    }
+
+    const stackDepth = Math.min(3, items.length);
+    const stackedItems = items.slice(0, stackDepth);
+
     return (
-      <>
-        <TransitionGroup component={null}>
-          {items &&
-            items.map((item, index) => (
-              <Transition
-                transitionChangeDelay={{ entering: TIMEOUT_FOR_ENTERING_ANIMATION }}
-                key={item.id ?? index}
-                in={true}
-              >
-                {(state: string, transitionRootElement: React.Ref<HTMLDivElement> | undefined) => (
-                  <Flash
-                    ref={transitionRootElement}
-                    key={item.id ?? index}
-                    transitionState={state}
-                    // eslint-disable-next-line react/forbid-component-props
-                    className={clsx(isRefresh ? styles['flash-refresh'] : '')}
-                    {...item}
-                  />
-                )}
-              </Transition>
+      <div className={styles.stack} style={{ [customCssProps.flashbarStackDepth]: stackDepth }}>
+        {!isFlashbarStackExpanded && (
+          <div className={clsx(styles.collapsed, isVisualRefresh && styles['visual-refresh'])}>
+            {stackedItems.map((item, index) => (
+              <div className={styles.item} style={{ [customCssProps.flashbarStackIndex]: index }} key={index}>
+                {index === 0 && renderItem(item, item.id ?? index)}
+                {index > 0 && <div className={clsx(styles.flash, styles[`flash-type-${item.type ?? 'info'}`])} />}
+              </div>
             ))}
-        </TransitionGroup>
-      </>
+          </div>
+        )}
+
+        {isFlashbarStackExpanded && (
+          <div className={styles.expanded}>{items.map((item, index) => renderItem(item, item.id ?? index))}</div>
+        )}
+
+        <button
+          aria-label={getStackButtonAriaLabel()}
+          className={clsx(styles.toggle, isVisualRefresh && styles['visual-refresh'])}
+          onClick={() => setIsFlashbarStackExpanded(!isFlashbarStackExpanded)}
+          {...isFocusVisible}
+        >
+          <InternalIcon
+            className={clsx(styles.icon, isFlashbarStackExpanded && styles.expanded)}
+            size="small"
+            name="angle-down"
+          />
+        </button>
+      </div>
     );
-  };
+  }
+
+  /**
+   * If the flashbar is flat and motion is `enabled` then the adding and removing of items
+   * from the flashbar will render with visual transitions.
+   */
+  function renderFlatItemsWithTransitions() {
+    if (isFlashbarStacked || motionDisabled || !items) {
+      return;
+    }
+
+    return (
+      <TransitionGroup component={null}>
+        {items &&
+          items.map((item, index) => (
+            <Transition
+              transitionChangeDelay={{ entering: TIMEOUT_FOR_ENTERING_ANIMATION }}
+              key={item.id ?? index}
+              in={true}
+            >
+              {(state: string, transitionRootElement: React.Ref<HTMLDivElement> | undefined) =>
+                renderItem(item, item.id ?? index, transitionRootElement, state)
+              }
+            </Transition>
+          ))}
+      </TransitionGroup>
+    );
+  }
+
+  /**
+   * If the flashbar is flat and motion is `disabled` then the adding and removing of items
+   * from the flashbar will render without visual transitions.
+   */
+  function renderFlatItemsWithoutTransitions() {
+    if (isFlashbarStacked || !motionDisabled || !items) {
+      return;
+    }
+
+    return <>{items.map((item, index) => renderItem(item, item.id ?? index))}</>;
+  }
+
+  /**
+   * This is a shared render function for a single flashbar item to be used
+   * by the stacking, motion, and non-motion item group render functions.
+   */
+  function renderItem(
+    item: FlashbarProps.MessageDefinition,
+    key: string | number,
+    transitionRootElement?: React.Ref<HTMLDivElement> | undefined,
+    transitionState?: string | undefined
+  ) {
+    return (
+      <Flash
+        // eslint-disable-next-line react/forbid-component-props
+        className={clsx(getVisualContextClassname('flashbar'), isVisualRefresh ? styles['flash-refresh'] : '')}
+        key={key}
+        ref={transitionRootElement}
+        transitionState={transitionState}
+        {...item}
+      />
+    );
+  }
+
   return (
     <div
       {...baseProps}
       className={clsx(baseProps.className, styles.flashbar, styles[`breakpoint-${breakpoint}`])}
       ref={mergedRef}
     >
-      <VisualContext contextName="flashbar">
-        {items && (
-          <>
-            {motionDisabled
-              ? items.map((item, index) => renderFlashItem(item, index))
-              : renderFlashItemsWithTransitions(items)}
-          </>
-        )}
-      </VisualContext>
+      {renderStackedItems()}
+      {renderFlatItemsWithTransitions()}
+      {renderFlatItemsWithoutTransitions()}
     </div>
   );
 }
