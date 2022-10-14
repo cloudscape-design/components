4e227757a4eeb678256be616da673e2038ddb341
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-10-14 13:31:07 +0200

    Make live-region use screenreader-only (#363)

--- src/date-range-picker/calendar/index.tsx
+++ src/date-range-picker/calendar/index.tsx
@@ -370,9 +370,7 @@ function Calendar(
           )}
         </InternalSpaceBetween>
       </InternalSpaceBetween>
-      <LiveRegion>
-        <span className={styles['calendar-aria-live']}>{announcement}</span>
-      </LiveRegion>
+      <LiveRegion className={styles['calendar-aria-live']}>{announcement}</LiveRegion>
     </>
   );
 }
