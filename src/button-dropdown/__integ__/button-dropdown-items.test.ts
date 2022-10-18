8843cd29530a58bec2d5818150dff6796ce9b89e
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-10-18 19:56:06 +0200

    Fix focus return logic when dropdown is closed (#387)

--- src/button-dropdown/__integ__/button-dropdown-items.test.ts
+++ src/button-dropdown/__integ__/button-dropdown-items.test.ts
@@ -17,3 +17,18 @@ test(
     expect(await browser.getWindowHandles()).toHaveLength(2);
   })
 );
+
+test(
+  'focuses on correct button-dropdown when the two are opened one after another',
+  useBrowser(async browser => {
+    const page1 = new ButtonDropdownPage('ButtonDropdown3', browser);
+    const page2 = new ButtonDropdownPage('ButtonDropdown1', browser);
+    await browser.url('#/light/button-dropdown/simple');
+
+    await page1.waitForVisible(page1.getTrigger());
+    await page1.openDropdown();
+    await page2.openDropdown();
+
+    await expect(page2.getFocusedElementText()).resolves.toBe('Two');
+  })
+);
