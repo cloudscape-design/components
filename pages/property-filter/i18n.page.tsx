93608fd6038c66acdf751aa33ac3511509d326c7
Connor Lanigan <dev@connorlanigan.com>
2022-10-20 16:23:29 +0200

    Update context

--- pages/property-filter/i18n.page.tsx
+++ pages/property-filter/i18n.page.tsx
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '~components/box';
 import Table from '~components/table';
 import PropertyFilter from '~components/property-filter/i18n';
@@ -9,8 +9,20 @@ import Button from '~components/button';
 import { allItems, TableItem } from './table.data';
 import { columnDefinitions, filteringProperties } from './common-props';
 import { useCollection } from '@cloudscape-design/collection-hooks';
+import { GlobalI18NContextProvider } from '~components/i18n/context';
+
+import propertyFilterMessagesGerman from '~components/i18n/messages/de-DE/property-filter';
+import propertyFilterMessagesDefault from '~components/i18n/messages/default/property-filter';
+import { FormField, Select, SpaceBetween } from '~components';
 
 export default function () {
+  const [language, setLanguage] = useState<'default' | 'german'>('default');
+
+  const languageSelectOptions = [
+    { value: 'default', label: 'Default' },
+    { value: 'german', label: 'German' },
+  ];
+
   const { items, collectionProps, actions, propertyFilterProps } = useCollection(allItems, {
     propertyFiltering: {
       empty: 'empty',
@@ -34,25 +46,48 @@ export default function () {
     },
     sorting: {},
   });
+
+  const messagesGerman = {
+    'property-filter': propertyFilterMessagesGerman,
+  };
+
+  const messagesDefault = {
+    'property-filter': propertyFilterMessagesDefault,
+  };
+
+  const messages = language === 'german' ? messagesGerman : messagesDefault;
+
   return (
-    <Box margin="m">
-      <Table<TableItem>
-        className="main-content"
-        stickyHeader={true}
-        header={<Header headingTagOverride={'h1'}>Instances</Header>}
-        items={items}
-        {...collectionProps}
-        filter={
-          <PropertyFilter
-            {...propertyFilterProps}
-            virtualScroll={true}
-            countText={`${items.length} matches`}
-            i18nStrings={{} as any}
-            expandToViewport={true}
+    <GlobalI18NContextProvider messages={messages}>
+      <Box margin="m">
+        <SpaceBetween size="l">
+          <FormField label="Interface language">
+            <Select
+              options={languageSelectOptions}
+              selectedOption={languageSelectOptions.find(o => o.value === language) ?? languageSelectOptions[0]}
+              onChange={event => setLanguage(event.detail.selectedOption.value as 'default' | 'german')}
+            />
+          </FormField>
+
+          <Table<TableItem>
+            className="main-content"
+            stickyHeader={true}
+            header={<Header headingTagOverride={'h1'}>Instances</Header>}
+            items={items}
+            {...collectionProps}
+            filter={
+              <PropertyFilter
+                {...propertyFilterProps}
+                virtualScroll={true}
+                countText={`${items.length} matches`}
+                i18nStrings={{} as any}
+                expandToViewport={true}
+              />
+            }
+            columnDefinitions={columnDefinitions.slice(0, 2)}
           />
-        }
-        columnDefinitions={columnDefinitions.slice(0, 2)}
-      />
-    </Box>
+        </SpaceBetween>
+      </Box>
+    </GlobalI18NContextProvider>
   );
 }
--- src/i18n/context.tsx
+++ src/i18n/context.tsx
@@ -2,15 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { ComponentsI18N } from './interfaces';
-import React, { createContext, useContext } from 'react';
-
-import messagesPropertyFilterDefault from './messages/default/property-filter';
-import messagesPropertyFilterGerman from './messages/de-DE/property-filter';
-
-export const messagesPropertyFilter: {
-  default: ComponentsI18N['property-filter'];
-  'de-DE': ComponentsI18N['property-filter'];
-} = { default: messagesPropertyFilterDefault, ['de-DE']: messagesPropertyFilterGerman };
+import React, { createContext, useContext, useMemo } from 'react';
 
 type DeepPartial<T> = {
   [P in keyof T]?: DeepPartial<T[P]>;
@@ -18,47 +10,41 @@ type DeepPartial<T> = {
 
 interface GlobalI18NContextProviderProps {
   children: React.ReactNode;
-  locale: 'default' | 'de-DE';
-  messages?: DeepPartial<ComponentsI18N>;
+  messages: DeepPartial<ComponentsI18N>;
 }
 
 interface ComponentI18NContext {
-  locale: 'default' | 'de-DE';
+  messages: GlobalI18NContextProviderProps['messages'];
 }
-
-// interface ComponentI18NContextProviderProps<ComponentName extends keyof ComponentsI18N> {
-//   children: React.ReactNode;
-//   componentName: ComponentName;
-//   messages: DeepPartial<ComponentsI18N[ComponentName]>;
-// }
-
-const supportedLocales = ['default', 'de-DE'];
-const browserLocale = new Intl.DateTimeFormat().resolvedOptions().locale;
-const defaultLocale = supportedLocales.indexOf(browserLocale) !== -1 ? browserLocale : 'default';
 
 const GlobalI18NContext = createContext<ComponentI18NContext>({
-  locale: defaultLocale as any,
+  messages: {},
 });
 
-export function GlobalI18NContextProvider({ children, locale }: GlobalI18NContextProviderProps) {
-  return <GlobalI18NContext.Provider value={{ locale }}>{children}</GlobalI18NContext.Provider>;
+/**
+ * To be used like this:
+ *
+ * import messagesGerman from '@cloudscape-design/translations/de-DE';
+ *
+ * function MyApp() {
+ *   return (
+ *     <GlobalI18NContextProvider messages={messagesGerman}>
+ *        <SpaceBetween>... application content ...</SpaceBetween>
+ *     </GlobalI18NContextProvider>
+ *   )
+ * }
+ *
+ */
+export function GlobalI18NContextProvider({ children, messages }: GlobalI18NContextProviderProps) {
+  const value = useMemo(() => ({ messages }), [messages]);
+
+  return <GlobalI18NContext.Provider value={value}>{children}</GlobalI18NContext.Provider>;
 }
 
-// export function ComponentI18NContextProvider<ComponentName extends keyof ComponentsI18N>({
-//   componentName,
-//   children,
-// }: ComponentI18NContextProviderProps<ComponentName>) {}
-
-export function useI18NContext<ComponentName extends keyof ComponentsI18N>({
-  componentName,
-}: {
-  componentName: ComponentName;
-}): ComponentsI18N[ComponentName] {
+export function useI18NContext<ComponentName extends keyof ComponentsI18N>(
+  componentName: ComponentName
+): DeepPartial<ComponentsI18N[ComponentName]> {
   const i18nContext = useContext(GlobalI18NContext);
 
-  if (componentName !== 'property-filter') {
-    throw new Error(`I18n is not implemented for component "${componentName}"`);
-  }
-
-  return messagesPropertyFilter[i18nContext.locale] as any;
+  return i18nContext.messages[componentName];
 }
