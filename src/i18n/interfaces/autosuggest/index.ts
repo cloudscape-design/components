8a3ccaf517065163dbf2401ac3b6e2fd2924ae1b
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-10-20 12:43:58 +0200

    Remove generated i18n artifacts

--- src/i18n/interfaces/autosuggest/index.ts
+++
@@ -1,7 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-export interface AutosuggestI18n {
-  enteredTextLabel: ({ value }: { value: string }) => string;
-  selectedAriaLabel: string;
-}
--- src/i18n/interfaces/cards/index.ts
+++
@@ -1,8 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-export interface CardsI18n {
-  ariaLabels: {
-    itemSelectionLabel: ({ itemName }: { itemName: string }) => string;
-  };
-}
