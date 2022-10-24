101162fe3d2dc0cf5f9a55e9ed719343c5bcaae2
Johannes Weber <jowejowe@amazon.com>
2022-10-24 16:41:06 +0200

    chore: Update documentation

--- src/internal/vendor/d3-scale.ts
+++ src/internal/vendor/d3-scale.ts
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-// This is because d3-scale and its transitive dependencies are using ESM format which will cause
-// issues for clients using jest and do not have the associated jest-preset package installed.
+// `d3-scale` and its transitive dependencies are using ESM format which causes
+// issues during unit tests at clients using jest and do not have the associated
+// jest-preset package installed.
 export { scaleLinear, scaleLog, scaleTime, scaleBand } from 'd3-scale';
