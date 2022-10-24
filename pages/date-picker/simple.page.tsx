6f01317a4bc3a4d05eb9ad229a67b2a5ba9515a1
Connor Lanigan <dev@connorlanigan.com>
2022-10-24 12:25:08 +0200

    feat(calendar, date-picker): Improve ARIA labels and descriptions (#358)

--- pages/date-picker/simple.page.tsx
+++ pages/date-picker/simple.page.tsx
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import React, { useState } from 'react';
-import { Box, DatePicker, Link } from '~components';
+import { Box, DatePicker, FormField, Link } from '~components';
 
 export default function DatePickerScenario() {
   const [value, setValue] = useState('');
@@ -11,20 +11,21 @@ export default function DatePickerScenario() {
       <h1>Date picker simple version</h1>
       <Link id="focus-dismiss-helper">Focusable element before the date picker</Link>
       <br />
-      <DatePicker
-        value={value}
-        name={'date-picker-name'}
-        ariaLabel={'date-picker-label'}
-        locale="en-GB"
-        previousMonthAriaLabel={'Previous month'}
-        nextMonthAriaLabel={'Next month'}
-        todayAriaLabel={'TEST TODAY'}
-        openCalendarAriaLabel={selectedDate =>
-          'Choose Date' + (selectedDate ? `, selected date is ${selectedDate}` : '')
-        }
-        placeholder={'YYYY/MM/DD'}
-        onChange={event => setValue(event.detail.value)}
-      />
+      <FormField label="Certificate expiry date" constraintText="Use YYYY/MM/DD format.">
+        <DatePicker
+          value={value}
+          name={'date-picker-name'}
+          locale="en-GB"
+          previousMonthAriaLabel={'Previous month'}
+          nextMonthAriaLabel={'Next month'}
+          todayAriaLabel={'TEST TODAY'}
+          openCalendarAriaLabel={selectedDate =>
+            'Choose certificate expiry date' + (selectedDate ? `, selected date is ${selectedDate}` : '')
+          }
+          placeholder={'YYYY/MM/DD'}
+          onChange={event => setValue(event.detail.value)}
+        />
+      </FormField>
       <br />
       <br />
       <Link id="focusable-element-after-date-picker">Focusable element after the date picker</Link>
