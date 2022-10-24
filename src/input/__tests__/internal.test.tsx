c8e4fdfc498bf26d3ca9cc280d9e083d1f6cca5f
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-10-24 13:09:51 +0200

    Revert "Move formFieldContext to internal Input component (#396)"
    This reverts commit a42106952cf883d5c44529476316a2205959f1f4.

--- src/input/__tests__/internal.test.tsx
+++ src/input/__tests__/internal.test.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import createWrapper, { InputWrapper } from '../../../lib/components/test-utils/dom';
-import InternalFormField from '../../../lib/components/form-field/internal';
 import InternalInput from '../../../lib/components/input/internal';
 import styles from '../../../lib/components/input/styles.css.js';
 import { DEBOUNCE_DEFAULT_DELAY } from '../../../lib/components/internal/debounce';
@@ -33,32 +32,4 @@ test('should call onDelayedInput and onChange handlers respectively', async () =
   await new Promise(resolve => setTimeout(resolve, DEBOUNCE_DEFAULT_DELAY + 50));
   expect(onDelayedInput).toHaveBeenCalledTimes(1);
   expect(onDelayedInput).toHaveBeenCalledWith({ value: 'second' });
-});
-
-test('inherits form-field properties', () => {
-  render(
-    <InternalFormField controlId="control-id" label="Label" description="description" errorText="error">
-      <InternalInput value="" />
-    </InternalFormField>
-  );
-  const element = createWrapper().find('input')!.getElement();
-
-  expect(element).toHaveAttribute('id', 'control-id');
-  expect(element).toHaveAttribute('aria-labelledby', 'control-id-label');
-  expect(element).toHaveAttribute('aria-describedby', 'control-id-error control-id-description');
-  expect(element).toHaveAttribute('aria-invalid', 'true');
-});
-
-test('overrides form-field properties', () => {
-  render(
-    <InternalFormField controlId="control-id" label="Label" description="description" errorText="error">
-      <InternalInput value="" controlId="id" ariaLabelledby="label" ariaDescribedby="description" invalid={false} />
-    </InternalFormField>
-  );
-  const element = createWrapper().find('input')!.getElement();
-
-  expect(element).toHaveAttribute('id', 'id');
-  expect(element).toHaveAttribute('aria-labelledby', 'label');
-  expect(element).toHaveAttribute('aria-describedby', 'description');
-  expect(element).not.toHaveAttribute('aria-invalid');
 });
