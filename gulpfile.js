400c8d6482ec30f9f91c506a5a87cad2e8369ea1
Johannes Weber <jowejowe@amazon.com>
2022-10-24 20:06:58 +0200

    chore: Add watcher task for internal/vendor files

--- gulpfile.js
+++ gulpfile.js
@@ -42,9 +42,17 @@ exports['test:a11y'] = a11y;
 
 exports.watch = () => {
   watch(
-    ['src/**/*.{ts,tsx}', '!src/test-utils/**/*.ts', '!**/__tests__/**', '!**/__integ__/**', '!**/__a11y__/**'],
+    [
+      'src/**/*.{ts,tsx}',
+      '!src/test-utils/**/*.ts',
+      '!**/__tests__/**',
+      '!**/__integ__/**',
+      '!**/__a11y__/**',
+      '!src/internal/vendor/**/*.ts',
+    ],
     typescript
   );
   watch(['src/test-utils/dom/**/*.ts', '!src/test-utils/dom/index.ts'], testUtils);
   watch(['style-dictionary/**/*.ts', 'src/**/*.scss'], styles);
+  watch(['src/internal/vendor/**/*.ts', '!src/internal/vendor/**/__tests__/**'], series(typescript, bundleVendorFiles));
 };
