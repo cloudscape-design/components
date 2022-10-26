e7c1d170b36ff4ef5e967ee4ae6416650ee8868e
Yueying Lu <lyueying@amazon.de>
2022-10-26 17:21:01 +0200

    Add test id for select and multiselect

--- pages/select/screenshot.page.tsx
+++ pages/select/screenshot.page.tsx
@@ -51,6 +51,7 @@ export default function () {
           onChange={event => setSelected(event.detail.selectedOption)}
           virtualScroll={virtualScroll}
           ariaLabel="select demo"
+          data-test-id="select-demo"
         />
         <Select
           placeholder="Select an option"
@@ -60,6 +61,7 @@ export default function () {
           onChange={event => setSelected(event.detail.selectedOption)}
           virtualScroll={virtualScroll}
           ariaLabel="select demo no filtering"
+          data-test-id="select-demo-no-filtering"
         />
       </ScreenshotArea>
     </>
