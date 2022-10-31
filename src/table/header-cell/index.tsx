c7c30c44a40094c2bfa423eecb6c400083c4424f
Katie George <katiegeo@amazon.com>
2022-10-31 14:47:48 -0700

    merge conflict

--- src/table/header-cell/index.tsx
+++ src/table/header-cell/index.tsx
@@ -80,6 +80,7 @@ export const TableHeaderCell = React.forwardRef(function TableHeaderCell(
       style={style}
       ref={ref}
       scope="col"
+      ref={ref}
     >
       <div
         className={clsx(styles['header-cell-content'], {
