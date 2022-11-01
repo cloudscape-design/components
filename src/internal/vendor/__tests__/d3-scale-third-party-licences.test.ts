9598a632b45808eb9296ccc121aea918deffbc86
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-11-01 13:19:44 +0100

    Fix test

--- src/internal/vendor/__tests__/d3-scale-third-party-licences.test.ts
+++ src/internal/vendor/__tests__/d3-scale-third-party-licences.test.ts
@@ -6,7 +6,10 @@ import fs from 'fs';
 describe('d3-scale-third-party-licenses.txt content', () => {
   test('stays unchanged without any d3-scale version changes', () => {
     // The file we assert against gets generated during building the project.
-    const content = fs.readFileSync(path.resolve(__dirname, '../../../../generated-third-party-licenses.txt'), 'utf8');
+    const content = fs.readFileSync(
+      path.resolve(__dirname, '../../../../vendor/generated-third-party-licenses.txt'),
+      'utf8'
+    );
     expect(content).toMatchSnapshot();
   });
 });
