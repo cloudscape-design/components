58af092496e87c77a19e2e94aee55409b8a01762
Yueying Lu <lyueying@amazon.de>
2022-11-13 19:15:55 +0100

    Fix test

--- pages/expandable-section/permutations.page.tsx
+++ pages/expandable-section/permutations.page.tsx
@@ -41,7 +41,7 @@ const permutations = createPermutations<ExpandableSectionProps>([
   {
     expanded: [true, false],
     variant: ['container'],
-    headerText: ['Container example header', 'Container example header'],
+    headerText: ['Container example header', 'Container example header - optional'],
     children: [
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     ],
