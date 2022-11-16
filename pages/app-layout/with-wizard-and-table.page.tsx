b98e86f54237b662fe5f831059dc07fabc47ec90
Scott O'Brien <scottob@amazon.com>
2022-11-16 09:58:18 -0500

    Used shared utils for table data.

--- pages/app-layout/with-wizard-and-table.page.tsx
+++ pages/app-layout/with-wizard-and-table.page.tsx
@@ -1,21 +1,12 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import React, { useState } from 'react';
-import {
-  AppLayout,
-  Box,
-  Button,
-  CollectionPreferences,
-  ColumnLayout,
-  Container,
-  Header,
-  Pagination,
-  SpaceBetween,
-  Table,
-  TextFilter,
-  Wizard,
-} from '~components';
+import { AppLayout, Box, Button, ColumnLayout, Container, Header, SpaceBetween, Table, Wizard } from '~components';
+import { generateItems, Instance } from '../table/generate-data';
+import { columnsConfig } from '../table/shared-configs';
 import ScreenshotArea from '../utils/screenshot-area';
+
+const items = generateItems(20);
 
 export default function () {
   const [activeStepIndex, setActiveStepIndex] = useState(0);
@@ -45,147 +36,18 @@ export default function () {
                 title: 'Add storage',
                 content: (
                   <Box>
-                    <Table
-                      ariaLabels={{
-                        selectionGroupLabel: 'Items selection',
-                        allItemsSelectionLabel: ({ selectedItems }) =>
-                          `${selectedItems.length} ${selectedItems.length === 1 ? 'item' : 'items'} selected`,
-                        itemSelectionLabel: ({ selectedItems }, item) => {
-                          const isItemSelected = selectedItems.filter(i => i.name === item.name).length;
-                          return `${item.name} is ${isItemSelected ? '' : 'not'} selected`;
-                        },
-                      }}
-                      columnDefinitions={[
-                        {
-                          id: 'variable',
-                          header: 'Variable name',
-                          cell: e => e.name,
-                          sortingField: 'name',
-                        },
-                        {
-                          id: 'value',
-                          header: 'Text value',
-                          cell: e => e.alt,
-                          sortingField: 'alt',
-                        },
-                        { id: 'type', header: 'Type', cell: e => e.type },
-                        {
-                          id: 'description',
-                          header: 'Description',
-                          cell: e => e.description,
-                        },
-                      ]}
-                      items={[
-                        {
-                          name: 'Item 1',
-                          alt: 'First',
-                          description: 'This is the first item',
-                          type: '1A',
-                          size: 'Small',
-                          testing: 'hello',
-                        },
-                        {
-                          name: 'Item 2',
-                          alt: 'Second',
-                          description: 'This is the second item',
-                          type: '1B',
-                          size: 'Large',
-                        },
-                        {
-                          name: 'Item 3',
-                          alt: 'Third',
-                          description: '-',
-                          type: '1A',
-                          size: 'Large',
-                        },
-                        {
-                          name: 'Item 4',
-                          alt: 'Fourth',
-                          description: 'This is the fourth item',
-                          type: '2A',
-                          size: 'Small',
-                        },
-                        {
-                          name: 'Item 5',
-                          alt: '-',
-                          description:
-                            'This is the fifth item with a longer description This is the fifth item with a longer description This is the fifth item with a longer description',
-                          type: '2A',
-                          size: 'Large',
-                        },
-                        {
-                          name: 'Item 6',
-                          alt: 'Sixth',
-                          description: 'This is the sixth item',
-                          type: '1A',
-                          size: 'Small',
-                        },
-                      ]}
-                      loadingText="Loading resources"
-                      selectionType="multi"
-                      trackBy="name"
-                      visibleColumns={['variable', 'value', 'type', 'description']}
-                      empty={
-                        <Box textAlign="center" color="inherit">
-                          <b>No resources</b>
-                          <Box padding={{ bottom: 's' }} variant="p" color="inherit">
-                            No resources to display.
-                          </Box>
-                          <Button>Create resource</Button>
-                        </Box>
+                    <Table<Instance>
+                      header={
+                        <Header
+                          variant="awsui-h1-sticky"
+                          description="Demo page with footer"
+                          actions={<Button variant="primary">Create</Button>}
+                        >
+                          Sticky Scrollbar Example
+                        </Header>
                       }
-                      filter={<TextFilter filteringPlaceholder="Find resources" filteringText="" />}
-                      header={<Header>Table with common features</Header>}
-                      pagination={
-                        <Pagination
-                          currentPageIndex={1}
-                          pagesCount={2}
-                          ariaLabels={{
-                            nextPageLabel: 'Next page',
-                            previousPageLabel: 'Previous page',
-                            pageLabel: pageNumber => `Page ${pageNumber} of all pages`,
-                          }}
-                        />
-                      }
-                      preferences={
-                        <CollectionPreferences
-                          title="Preferences"
-                          confirmLabel="Confirm"
-                          cancelLabel="Cancel"
-                          preferences={{
-                            pageSize: 10,
-                            visibleContent: ['variable', 'value', 'type', 'description'],
-                          }}
-                          pageSizePreference={{
-                            title: 'Select page size',
-                            options: [
-                              { value: 10, label: '10 resources' },
-                              { value: 20, label: '20 resources' },
-                            ],
-                          }}
-                          visibleContentPreference={{
-                            title: 'Select visible content',
-                            options: [
-                              {
-                                label: 'Main distribution properties',
-                                options: [
-                                  {
-                                    id: 'variable',
-                                    label: 'Variable name',
-                                    editable: false,
-                                  },
-                                  { id: 'value', label: 'Text value' },
-                                  { id: 'type', label: 'Type' },
-                                  {
-                                    id: 'description',
-                                    label: 'Description',
-                                  },
-                                ],
-                              },
-                            ],
-                          }}
-                        />
-                      }
+                      columnDefinitions={columnsConfig}
+                      items={items}
                     />
                   </Box>
                 ),
