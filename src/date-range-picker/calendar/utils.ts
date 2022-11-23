8841b5176ca9f1aa47578d7a6895317f33d8f433
Boris Serdiuk <serdiuk@amazon.com>
2022-11-23 12:54:07 +0100

    refactor: Make range calendar selection controllable

---
+++ src/date-range-picker/calendar/utils.ts
@@ -0,0 +1,37 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { addMonths, isSameMonth, startOfMonth } from 'date-fns';
+import { DateRangePickerProps, PendingAbsoluteValue } from '../interfaces';
+import { parseDate } from '../../internal/utils/date-time';
+
+export function findDateToFocus(
+  selected: Date | null,
+  baseDate: Date,
+  isDateEnabled: DateRangePickerProps.IsDateEnabledFunction
+) {
+  if (selected && isDateEnabled(selected) && isSameMonth(selected, baseDate)) {
+    return selected;
+  }
+  const today = new Date();
+  if (isDateEnabled(today) && isSameMonth(today, baseDate)) {
+    return today;
+  }
+  if (isDateEnabled(baseDate)) {
+    return baseDate;
+  }
+  return null;
+}
+
+export function findMonthToDisplay(value: PendingAbsoluteValue, isSingleGrid: boolean) {
+  if (value.start.date) {
+    const startDate = parseDate(value.start.date);
+    if (isSingleGrid) {
+      return startOfMonth(startDate);
+    }
+    return startOfMonth(addMonths(startDate, 1));
+  }
+  if (value.end.date) {
+    return startOfMonth(parseDate(value.end.date));
+  }
+  return startOfMonth(Date.now());
+}
