68a0cc5d3abacaa7a3126a0d255c87211b104b3b
Fahad Hossain <fhfahad@amazon.com>
2022-11-30 01:18:34 +0100

    fix: check node order correctly in useClickAway

--- src/table/body-cell/click-away.tsx
+++ src/table/body-cell/click-away.tsx
@@ -13,8 +13,11 @@ export function useClickAway(onClick: () => void) {
         onClickStable();
       }
     }
-    document.addEventListener('click', handleClick);
-    return () => document.removeEventListener('click', handleClick);
+    // contains returns wrong result if the next render would remove the element
+    // but capture phase happens before the render, so returns correct result
+    // Ref: https://github.com/facebook/react/issues/20325
+    document.addEventListener('click', handleClick, { capture: true });
+    return () => document.removeEventListener('click', handleClick, { capture: true });
   }, [onClickStable]);
   return awayRef;
 }
