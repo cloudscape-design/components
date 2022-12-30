02dcb4c713f3d5dd830bf662c208360b51579bcf
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-12-30 09:45:19 +0100

    fix: Container flex styles

--- pages/container/fit-height.page.tsx
+++ pages/container/fit-height.page.tsx
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React from 'react';
+import React, { useState } from 'react';
+import { Checkbox } from '~components';
 import ColumnLayout from '~components/column-layout';
 import Container from '~components/container';
 import Grid from '~components/grid';
@@ -9,26 +10,26 @@ import Link from '~components/link';
 import ScreenshotArea from '../utils/screenshot-area';
 import styles from './fit-height.scss';
 
-function SmallContainer() {
+function SmallContainer({ fitHeight }: { fitHeight: boolean }) {
   return (
-    <Container fitHeight={true} header={<Header>Short</Header>} footer="footer">
+    <Container fitHeight={fitHeight} header={<Header>Short</Header>} footer="footer">
       <p>One line of text</p>
     </Container>
   );
 }
 
-function MediumContainer() {
+function MediumContainer({ fitHeight }: { fitHeight: boolean }) {
   return (
-    <Container fitHeight={true} header={<Header>Mid size</Header>} footer="footer">
+    <Container fitHeight={fitHeight} header={<Header>Mid size</Header>} footer="footer">
       <p>Content placeholder</p>
       <div style={{ height: 100 }} className={styles.placeholder}></div>
     </Container>
   );
 }
 
-function LargeContainer() {
+function LargeContainer({ fitHeight }: { fitHeight: boolean }) {
   return (
-    <Container fitHeight={true} header={<Header>Large</Header>} footer="footer">
+    <Container fitHeight={fitHeight} header={<Header>Large</Header>} footer="footer">
       <p>
         This container overflows available space. <Link href="#">Learn more</Link>.
       </p>
@@ -38,31 +39,36 @@ function LargeContainer() {
 }
 
 export default function () {
+  const [fitHeight, setFitHeight] = useState(true);
+
   return (
     <article>
       <h1>Fit height property demo</h1>
+      <Checkbox checked={fitHeight} onChange={event => setFitHeight(event.detail.checked)}>
+        Fit height
+      </Checkbox>
       <ScreenshotArea>
         <h2>Inside display:grid</h2>
         <div className={styles.grid}>
-          <SmallContainer />
-          <MediumContainer />
-          <LargeContainer />
+          <SmallContainer fitHeight={fitHeight} />
+          <MediumContainer fitHeight={fitHeight} />
+          <LargeContainer fitHeight={fitHeight} />
         </div>
         <h2>Inside column layout</h2>
         <ColumnLayout columns={3}>
-          <SmallContainer />
-          <MediumContainer />
-          <LargeContainer />
+          <SmallContainer fitHeight={fitHeight} />
+          <MediumContainer fitHeight={fitHeight} />
+          <LargeContainer fitHeight={fitHeight} />
         </ColumnLayout>
         <h2>Inside grid</h2>
         <Grid gridDefinition={[{ colspan: 6 }, { colspan: 3 }, { colspan: 3 }]}>
-          <SmallContainer />
-          <MediumContainer />
-          <LargeContainer />
+          <SmallContainer fitHeight={fitHeight} />
+          <MediumContainer fitHeight={fitHeight} />
+          <LargeContainer fitHeight={fitHeight} />
         </Grid>
         <h2>Container inside height limit</h2>
         <div className={styles.heightLimit}>
-          <LargeContainer />
+          <LargeContainer fitHeight={fitHeight} />
         </div>
       </ScreenshotArea>
     </article>
