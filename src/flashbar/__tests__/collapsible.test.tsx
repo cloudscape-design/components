b614086ac95df2517bb2f3ee703cf21b4367df75
Joan Perals Tresserra <jotresse@amazon.com>
2023-01-17 10:03:44 +0100

    Refactor unit tests to make eventual API changes easier to handle

--- src/flashbar/__tests__/collapsible.test.tsx
+++ src/flashbar/__tests__/collapsible.test.tsx
@@ -6,70 +6,52 @@ import { render } from './common';
 import { FlashbarWrapper } from '../../../lib/components/test-utils/dom';
 import { FlashbarProps, StackedFlashbarProps } from '../interfaces';
 
-const sampleItems: Record<string, FlashbarProps.MessageDefinition> = {
-  error: { type: 'error', header: 'Error', content: 'There was an error' },
-  success: { type: 'success', header: 'Success', content: 'Everything went fine' },
-};
-
 describe('Collapsible Flashbar', () => {
   describe('Basic behavior', () => {
     it('shows only the header and content of the last item in the array when collapsed', () => {
-      const wrapper = render(<Flashbar {...{ stackItems: true }} items={[sampleItems.error, sampleItems.success]} />);
-      const items = wrapper.findItems();
+      const flashbar = renderFlashbar();
+      const items = flashbar.findItems();
       expect(items.length).toBe(1);
       expect(items[0].findHeader()!.getElement()).toHaveTextContent('Success');
       expect(items[0].findContent()!.getElement()).toHaveTextContent('Everything went fine');
     });
 
     it('shows toggle element with desired text', () => {
-      const wrapper = render(
-        <Flashbar
-          {...{
-            stackItems: true,
-            i18nStrings: {
-              toggleButtonText: 'Custom text',
-              toggleButtonAriaLabel: 'Collapsed ARIA label',
-            },
-          }}
-          items={[sampleItems.error, sampleItems.success]}
-        />
-      );
-      const button = findToggleElement(wrapper);
+      const flashbar = renderFlashbar({
+        i18nStrings: {
+          toggleButtonText: 'Custom text',
+          toggleButtonAriaLabel: 'Collapsed ARIA label',
+        },
+      });
+      const button = findToggleElement(flashbar);
       expect(button).toBeTruthy();
       expect(button).toHaveTextContent('Custom text');
     });
 
     it('does not show toggle element if there is only one item', () => {
-      const wrapper = render(<Flashbar {...{ stackItems: true }} items={[{ type: 'error' }]} />);
-      expect(findToggleElement(wrapper)).toBeFalsy();
+      const flashbar = renderFlashbar({ items: [{ type: 'error' }] });
+      expect(findToggleElement(flashbar)).toBeFalsy();
     });
 
     it('expands and collapses by clicking on toggle element', () => {
-      const wrapper = render(
-        <Flashbar
-          {...{
-            stackItems: true,
-          }}
-          items={[sampleItems.error, sampleItems.success]}
-        />
-      );
-      const items = wrapper.findItems();
+      const flashbar = renderFlashbar();
+      const items = flashbar.findItems();
       expect(items.length).toBe(1);
       expect(items[0].findHeader()!.getElement()).toHaveTextContent('Success');
       expect(items[0].findContent()!.getElement()).toHaveTextContent('Everything went fine');
 
-      findToggleElement(wrapper)!.click();
+      findToggleElement(flashbar)!.click();
 
-      const expandedItems = wrapper.findItems();
+      const expandedItems = flashbar.findItems();
       expect(expandedItems.length).toBe(2);
       expect(expandedItems[0].findHeader()!.getElement()).toHaveTextContent('Success');
       expect(expandedItems[0].findContent()!.getElement()).toHaveTextContent('Everything went fine');
       expect(expandedItems[1].findHeader()!.getElement()).toHaveTextContent('Error');
       expect(expandedItems[1].findContent()!.getElement()).toHaveTextContent('There was an error');
 
-      findToggleElement(wrapper)!.click();
+      findToggleElement(flashbar)!.click();
 
-      const collapsedItems = wrapper.findItems();
+      const collapsedItems = flashbar.findItems();
       expect(collapsedItems.length).toBe(1);
       expect(collapsedItems[0].findHeader()!.getElement()).toHaveTextContent('Success');
       expect(collapsedItems[0].findContent()!.getElement()).toHaveTextContent('Everything went fine');
@@ -78,37 +60,25 @@ describe('Collapsible Flashbar', () => {
 
   describe('Accessibility', () => {
     it('renders items in an unordered list', () => {
-      const wrapper = render(
-        <Flashbar
-          {...{
-            stackItems: true,
-            i18nStrings: {
-              toggleButtonText: 'Custom text',
-              toggleButtonAriaLabel: 'Toggle button ARIA label',
-            },
-          }}
-          items={[sampleItems.error, sampleItems.success]}
-        />
-      );
-      const list = wrapper.find('ul')!;
+      const flashbar = renderFlashbar({
+        i18nStrings: {
+          toggleButtonText: 'Custom text',
+          toggleButtonAriaLabel: 'Toggle button ARIA label',
+        },
+      });
+      const list = flashbar.find('ul')!;
       expect(list).toBeTruthy();
       expect(list.findAll('li')).toHaveLength(2);
     });
 
     it('applies ARIA label to the unordered list', () => {
       const expectedAriaLabel = 'Custom text';
-      const wrapper = render(
-        <Flashbar
-          {...{
-            stackItems: true,
-            i18nStrings: {
-              ariaLabel: expectedAriaLabel,
-            },
-          }}
-          items={[sampleItems.error, sampleItems.success]}
-        />
-      );
-      const list = wrapper.find('ul')!;
+      const flashbar = renderFlashbar({
+        i18nStrings: {
+          ariaLabel: expectedAriaLabel,
+        },
+      });
+      const list = flashbar.find('ul')!;
       expect(list).toBeTruthy();
       expect(list.getElement().getAttribute('aria-label')).toEqual(expectedAriaLabel);
     });
@@ -117,42 +87,30 @@ describe('Collapsible Flashbar', () => {
       const findAccessibleListItems = (wrapper: FlashbarWrapper) =>
         wrapper.findAll('li').filter(item => item.getElement().getAttribute('aria-hidden') !== 'true');
 
-      const wrapper = render(<Flashbar {...{ stackItems: true }} items={[sampleItems.error, sampleItems.success]} />);
-      const items = findAccessibleListItems(wrapper);
+      const flashbar = renderFlashbar();
+      const items = findAccessibleListItems(flashbar);
       expect(items.length).toBe(1);
     });
 
     it('applies desired ARIA label to toggle button', () => {
-      const wrapper = render(
-        <Flashbar
-          {...{
-            stackItems: true,
-            i18nStrings: {
-              toggleButtonText: 'Custom text',
-              toggleButtonAriaLabel: 'Toggle button ARIA label',
-            },
-          }}
-          items={[sampleItems.error, sampleItems.success]}
-        />
-      );
-      const button = findToggleButtonElement(wrapper);
+      const flashbar = renderFlashbar({
+        i18nStrings: {
+          toggleButtonText: 'Custom text',
+          toggleButtonAriaLabel: 'Toggle button ARIA label',
+        },
+      });
+      const button = findToggleButtonElement(flashbar);
       expect(button).toHaveAttribute('aria-label', 'Toggle button ARIA label');
     });
 
     it('applies aria-expanded attribute to toggle button', () => {
-      const wrapper = render(
-        <Flashbar
-          {...{
-            stackItems: true,
-            i18nStrings: {
-              toggleButtonText: 'Custom text',
-              toggleButtonAriaLabel: 'Toggle button ARIA label',
-            },
-          }}
-          items={[sampleItems.error, sampleItems.success]}
-        />
-      );
-      const button = findToggleButtonElement(wrapper)!;
+      const flashbar = renderFlashbar({
+        i18nStrings: {
+          toggleButtonText: 'Custom text',
+          toggleButtonAriaLabel: 'Toggle button ARIA label',
+        },
+      });
+      const button = findToggleButtonElement(flashbar)!;
       expect(button).toHaveAttribute('aria-expanded', 'false');
 
       button.click();
@@ -160,40 +118,23 @@ describe('Collapsible Flashbar', () => {
     });
 
     it('applies aria-controls attribute to toggle button referring to the unordered list', () => {
-      const wrapper = render(
-        <Flashbar
-          {...{
-            stackItems: true,
-            i18nStrings: {
-              toggleButtonText: 'Custom text',
-              toggleButtonAriaLabel: 'Toggle button ARIA label',
-            },
-          }}
-          items={[sampleItems.error, sampleItems.success]}
-        />
-      );
-      const listId = wrapper.find('ul')!.getElement().id;
+      const flashbar = renderFlashbar({
+        i18nStrings: {
+          toggleButtonText: 'Custom text',
+          toggleButtonAriaLabel: 'Toggle button ARIA label',
+        },
+      });
+      const listId = flashbar.find('ul')!.getElement().id;
       expect(listId).toBeTruthy();
-      const button = findToggleButtonElement(wrapper);
+      const button = findToggleButtonElement(flashbar);
       expect(button).toHaveAttribute('aria-controls', listId);
     });
 
     it('applies aria-describedby attribute to the list, referencing the item counter', () => {
-      const expectedAriaLabel = 'Custom text';
-      const wrapper = render(
-        <Flashbar
-          {...{
-            stackItems: true,
-            i18nStrings: {
-              ariaLabel: expectedAriaLabel,
-            },
-          }}
-          items={[sampleItems.error, sampleItems.success]}
-        />
-      );
-      const list = wrapper.find('ul')!;
+      const flashbar = renderFlashbar();
+      const list = flashbar.find('ul')!;
       expect(list).toBeTruthy();
-      const itemCountId = findItemCounter(wrapper)!.getElement().id;
+      const itemCountId = findItemCounter(flashbar)!.getElement().id;
       expect(itemCountId).toBeTruthy();
       expect(list.getElement().getAttribute('aria-describedby')).toEqual(itemCountId);
     });
@@ -223,6 +164,11 @@ function findItemCounter(flashbar: FlashbarWrapper) {
   return flashbar.find('[role="status"]');
 }
 
+const sampleItems: Record<string, FlashbarProps.MessageDefinition> = {
+  error: { type: 'error', header: 'Error', content: 'There was an error' },
+  success: { type: 'success', header: 'Success', content: 'Everything went fine' },
+};
+
 const defaultStrings = {
   ariaLabel: 'Notifications',
   toggleButtonText: 'Notifications',
@@ -234,17 +180,23 @@ const defaultStrings = {
   inProgressCountAriaLabel: 'In progress',
 };
 
+const defaultItems = [sampleItems.error, sampleItems.success];
+
 const defaultProps = {
   stackItems: true,
   i18nStrings: defaultStrings,
 };
 
 function renderFlashbar(
-  customProps: Omit<StackedFlashbarProps, 'stackItems'> = {
-    items: [sampleItems.error, sampleItems.success],
+  customProps: Partial<
+    Omit<StackedFlashbarProps, 'i18nStrings' | 'stackItems'> & {
+      i18nStrings?: Partial<StackedFlashbarProps.I18nStrings>;
+    }
+  > = {
+    items: defaultItems,
   }
 ) {
   const { items, ...restProps } = customProps;
   const props = { ...defaultProps, ...restProps, i18nStrings: { ...defaultStrings, ...restProps.i18nStrings } };
-  return render(<Flashbar {...props} items={items} />);
+  return render(<Flashbar {...props} items={items || defaultItems} />);
 }
