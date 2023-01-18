ac5bcc0feabdd981a39cc1ac25de959c2af77f74
Joan Perals Tresserra <jotresse@amazon.com>
2023-01-18 13:31:28 +0100

    Improve integration tests

--- src/flashbar/__integ__/collapsible.test.ts
+++ src/flashbar/__integ__/collapsible.test.ts
@@ -1,7 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import { setupTest } from './pages/interactive-page';
-import { testFocusInteractions } from './common';
 
 describe('Collapsible Flashbar', () => {
   describe('Keyboard navigation', () => {
@@ -24,21 +23,81 @@ describe('Collapsible Flashbar', () => {
   });
 
   describe('Focus interactions', () => {
-    describe('in collapsed state', () =>
-      testFocusInteractions({
-        prepareInitialState: async page => {
+    describe('in collapsed state', () => {
+      test(
+        'adding flash with ariaRole="status" does not move focus',
+        setupTest(async page => {
           await page.toggleCollapsibleFeature();
-        },
-        initialCount: 0,
-      }));
+          await page.addInfoFlash();
+          return expect(page.isFlashFocused(1)).resolves.toBe(false);
+        })
+      );
 
-    describe('in expanded state', () =>
-      testFocusInteractions({
-        prepareInitialState: async page => {
+      test(
+        'adding flash with ariaRole="alert" moves focus to the new flash',
+        setupTest(async page => {
+          await page.toggleCollapsibleFeature();
+          await page.addErrorFlash();
+          return expect(page.isFlashFocused(1)).resolves.toBe(true);
+        })
+      );
+
+      test(
+        'adding new non-alert flashes does not move focus to the new flash',
+        setupTest(async page => {
+          await page.toggleCollapsibleFeature();
+          await page.addErrorFlash();
+          await expect(page.isFlashFocused(1)).resolves.toBe(true);
+          await page.addInfoFlash();
+          await expect(page.isFlashFocused(1)).resolves.toBe(false);
+        })
+      );
+    });
+
+    describe('in expanded state', () => {
+      test(
+        'adding flash with ariaRole="status" does not move focus',
+        setupTest(async page => {
           await page.toggleCollapsibleFeature();
           await page.toggleCollapsedState();
-        },
-        initialCount: 0,
-      }));
+          await page.addInfoFlash();
+          return expect(page.isFlashFocused(1)).resolves.toBe(false);
+        })
+      );
+
+      test(
+        'adding flash with ariaRole="alert" moves focus to the new flash',
+        setupTest(async page => {
+          await page.toggleCollapsibleFeature();
+          await page.toggleCollapsedState();
+          await page.addErrorFlash();
+          return expect(page.isFlashFocused(1)).resolves.toBe(true);
+        })
+      );
+
+      test(
+        'adding new non-alert flashes does not move focus to the new flash',
+        setupTest(async page => {
+          await page.toggleCollapsibleFeature();
+          await page.toggleCollapsedState();
+          await page.addErrorFlash();
+          await expect(page.isFlashFocused(1)).resolves.toBe(true);
+          await page.addInfoFlash();
+          await expect(page.isFlashFocused(1)).resolves.toBe(false);
+        })
+      );
+
+      test(
+        'adding new non-alert flashes does not move focus to previous alert flashes',
+        setupTest(async page => {
+          await page.toggleCollapsibleFeature();
+          await page.toggleCollapsedState();
+          await page.addErrorFlash();
+          await expect(page.isFlashFocused(1)).resolves.toBe(true);
+          await page.addInfoFlash();
+          await expect(page.isFlashFocused(2)).resolves.toBe(false);
+        })
+      );
+    });
   });
 });
