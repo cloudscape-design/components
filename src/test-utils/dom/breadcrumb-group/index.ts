09ebf6523479a95b341feb442623c135e5782ff7
Gethin Webster <gethinw@amazon.com>
2023-02-07 14:19:30 +0100

    Update test util documentation

--- src/test-utils/dom/breadcrumb-group/index.ts
+++ src/test-utils/dom/breadcrumb-group/index.ts
@@ -9,18 +9,20 @@ import buttonDropdownStyles from '../../../button-dropdown/styles.selectors.js';
 export default class BreadcrumbGroupWrapper extends ComponentWrapper {
   static rootSelector: string = styles['breadcrumb-group'];
   /**
-   * Returns all links.
+   * Returns all breadcrumb items. Note that this includes the 'current' page item for backwards compatibility,
+   * even though it is not technically a link.
    *
-   * To find a specific link use the `findBreadcrumbLink(n)` function as chaining `findBreadcrumbLinks().get(n)` can return unexpected results.
+   * To find a specific item use the `findBreadcrumbLink(n)` function as chaining `findBreadcrumbLinks().get(n)` can return unexpected results.
    * @see findBreadcrumbLink
    */
   findBreadcrumbLinks(): Array<ElementWrapper> {
     return this.findAll(`.${itemStyles.breadcrumb} .${itemStyles.anchor}`);
   }
   /**
-   * Returns a link for a given index.
+   * Returns an item for a given index. Note that this may return the 'current' page item for backwards compatibility,
+   * even though it is not technically a link.
    *
-   * @param index 1-based link index
+   * @param index 1-based item index
    */
   findBreadcrumbLink(index: number): ElementWrapper | null {
     // We insert the breadcrumb-ellipsis as the second element so we have to filter it out.
