c7748a75ed2444c14dd92e2ee407c1fff6862f96
Joan Perals <jotresse@amazon.com>
2023-02-08 21:50:17 +0100

    fix: Keep focused button in viewport after collapsing Flashbar (#719)

--- src/flashbar/__tests__/utils.test.ts
+++ src/flashbar/__tests__/utils.test.ts
@@ -1,6 +1,10 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { getVisibleCollapsedItems, getItemType } from '../utils';
+import {
+  getVisibleCollapsedItems,
+  getItemType,
+  isElementTopBeyondViewport,
+} from '../../../lib/components/flashbar/utils';
 import { FlashbarProps, FlashType } from '../interfaces';
 
 describe('getVisibleCollapsedItems', () => {
@@ -85,5 +89,41 @@ describe('getVisibleCollapsedItems', () => {
       { type: 'success' },
     ];
     expect(getVisibleCollapsedItems(items, 4).map(({ type }) => ({ type }))).toEqual(items);
+  });
+});
+
+describe('isElementTopBeyondViewport', () => {
+  beforeAll(() => {
+    jest.spyOn(Element.prototype, 'getBoundingClientRect').mockImplementation(() => ({
+      width: 100,
+      height: 100,
+      top,
+      left: 0,
+      bottom: 0,
+      right: 0,
+      x: 0,
+      y: top,
+      toJSON: () => null,
+    }));
+  });
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+  let top = 0;
+  const element = document.createElement('div');
+
+  it('returns true when element top is negative', () => {
+    top = -1;
+    expect(isElementTopBeyondViewport(element)).toBe(true);
+  });
+
+  it('returns false when element top is positive', () => {
+    top = 1;
+    expect(isElementTopBeyondViewport(element)).toBe(false);
+  });
+
+  it('returns false when element top is zero', () => {
+    top = 0;
+    expect(isElementTopBeyondViewport(element)).toBe(false);
   });
 });
--- src/flashbar/utils.ts
+++ src/flashbar/utils.ts
@@ -123,3 +123,7 @@ export const counterTypes: {
   { type: 'info', labelName: 'infoIconAriaLabel', iconName: 'status-info' },
   { type: 'progress', labelName: 'inProgressIconAriaLabel', iconName: 'status-in-progress' },
 ];
+
+export function isElementTopBeyondViewport(element: HTMLElement) {
+  return element.getBoundingClientRect().top < 0;
+}
