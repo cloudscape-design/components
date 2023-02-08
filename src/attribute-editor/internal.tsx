6101418a91c40ac11cb7eefe28075ca66f2d9ebc
Fahad Hossain <fhfahad@amazon.com>
2023-02-08 11:30:12 +0100

    feat: Announce removal of items from AttributeEditor & TagEditor (#679)

--- src/attribute-editor/internal.tsx
+++ src/attribute-editor/internal.tsx
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React, { useImperativeHandle, useRef } from 'react';
+import React, { useImperativeHandle, useRef, useState } from 'react';
 import clsx from 'clsx';
 
 import { getBaseProps } from '../internal/base-component';
@@ -19,6 +19,8 @@ import { InternalBaseComponentProps } from '../internal/hooks/use-base-component
 import { useMergeRefs } from '../internal/hooks/use-merge-refs';
 import { SomeRequired } from '../internal/types';
 import { useUniqueId } from '../internal/hooks/use-unique-id';
+import { usePrevious } from '../internal/hooks/use-previous';
+import LiveRegion from '../internal/components/live-region';
 
 type InternalAttributeEditorProps<T> = SomeRequired<AttributeEditorProps<T>, 'items'> & InternalBaseComponentProps;
 
@@ -44,6 +46,7 @@ const InternalAttributeEditor = React.forwardRef(
     const [breakpoint, breakpointRef] = useContainerBreakpoints(['default', 'xxs', 'xs']);
     const removeButtonRefs = useRef<Array<ButtonProps.Ref | undefined>>([]);
     const wasNonEmpty = useRef<boolean>(false);
+    const [removalAnnouncement, setRemovalAnnouncement] = useState<string>('');
 
     const baseProps = getBaseProps(props);
     const isEmpty = items && items.length === 0;
@@ -60,6 +63,18 @@ const InternalAttributeEditor = React.forwardRef(
 
     const additionalInfoId = useUniqueId('attribute-editor-info');
     const infoAriaDescribedBy = additionalInfo ? additionalInfoId : undefined;
+
+    const prevItemsLength = usePrevious(items.length);
+
+    React.useEffect(() => {
+      if (prevItemsLength && prevItemsLength > items.length && i18nStrings?.itemRemovedAriaLive) {
+        setRemovalAnnouncement(i18nStrings.itemRemovedAriaLive);
+      } else {
+        setRemovalAnnouncement('');
+      }
+      // we only want to announce when the number of items decreases (i.e. when an item is removed)
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [items, i18nStrings?.itemRemovedAriaLive]);
 
     return (
       <div {...baseProps} ref={mergedRef} className={clsx(baseProps.className, styles.root)}>
@@ -80,6 +95,7 @@ const InternalAttributeEditor = React.forwardRef(
             />
           ))}
         </InternalBox>
+
         <InternalButton
           className={styles['add-button']}
           disabled={disableAddButton}
@@ -89,7 +105,10 @@ const InternalAttributeEditor = React.forwardRef(
         >
           {addButtonText}
         </InternalButton>
-        {additionalInfo && <AdditionalInfo id={infoAriaDescribedBy}>{additionalInfo}</AdditionalInfo>}
+        <LiveRegion data-testid="removal-announcement" delay={5} key={items.length}>
+          {removalAnnouncement}
+        </LiveRegion>
+        {!!additionalInfo && <AdditionalInfo id={infoAriaDescribedBy}>{additionalInfo}</AdditionalInfo>}
       </div>
     );
   }
