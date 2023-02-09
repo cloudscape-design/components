819fa08079aae3660e047b2d6a57653e1f228d16
Joan Perals <jotresse@amazon.com>
2023-02-09 15:40:43 +0100

    feat: Make Stacked notifications feature public (#731)
    Co-authored-by: Timo Gasda <gasda@amazon.com>

--- src/flashbar/index.tsx
+++ src/flashbar/index.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 import React, { useEffect } from 'react';
 import { applyDisplayName } from '../internal/utils/apply-display-name';
-import { FlashbarProps, CollapsibleFlashbarProps } from './interfaces';
+import { FlashbarProps } from './interfaces';
 import CollapsibleFlashbar from './collapsible-flashbar';
 import NonCollapsibleFlashbar from './non-collapsible-flashbar';
 
@@ -10,22 +10,18 @@ import { sendRenderMetric } from './internal/analytics';
 
 export { FlashbarProps };
 
-export default function Flashbar(props: FlashbarProps | CollapsibleFlashbarProps) {
+export default function Flashbar(props: FlashbarProps) {
   useEffect(() => {
     if (props.items.length > 0) {
       sendRenderMetric(props.items);
     }
   }, [props.items]);
 
-  if (isStackedFlashbar(props)) {
+  if (props.stackItems) {
     return <CollapsibleFlashbar {...props} />;
   } else {
     return <NonCollapsibleFlashbar {...props} />;
   }
-}
-
-function isStackedFlashbar(props: any): props is CollapsibleFlashbarProps {
-  return 'stackItems' in props && props.stackItems;
 }
 
 applyDisplayName(Flashbar, 'Flashbar');
