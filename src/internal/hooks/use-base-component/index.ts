9fe0c7e37c2403b1930c96c473421d185fd67912
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-02-13 17:10:23 +0100

    chore: Revert use base-component utils from the toolkit (#714) (#752)

--- src/internal/hooks/use-base-component/index.ts
+++ src/internal/hooks/use-base-component/index.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import { MutableRefObject } from 'react';
-import { useComponentMetadata } from '@cloudscape-design/component-toolkit/internal';
+import { useComponentMetadata } from './component-metadata';
 import { useTelemetry } from '../use-telemetry';
 import { PACKAGE_VERSION } from '../../environment';
 
