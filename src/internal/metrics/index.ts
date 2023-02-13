c70b69d84bcf303763da5d094a5a4fb7a1df2e9e
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-02-13 16:39:13 +0100

    Revert "chore: Use base-component utils from the toolkit (#714)"
    This reverts commit 78f5b59d86946d8af20b948b1cf5828a2ce308d1.

---
+++ src/internal/metrics/index.ts
@@ -0,0 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+export { Metrics, MetricsTestHelper } from './metrics';
+export { formatMajorVersionForMetricDetail, formatVersionForMetricName } from './metrics-formatters';
