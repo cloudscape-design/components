bc541ba270ed802546c804a454e56c64d7b2c167
Jessica Kuelz <15003460+jkuelz@users.noreply.github.com>
2023-02-24 08:09:08 -0800

    feat: Add full-page variant to Form (#762)

---
+++ pages/content-layout/with-header-toggles.page.tsx
@@ -0,0 +1,111 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React, { useState } from 'react';
+import AppLayout from '~components/app-layout';
+import Box from '~components/box';
+import ColumnLayout from '~components/column-layout';
+import Container from '~components/container';
+import ContentLayout from '~components/content-layout';
+import Form from '~components/form';
+import FormField from '~components/form-field';
+import Header from '~components/header';
+import Input from '~components/input';
+import SpaceBetween from '~components/space-between';
+import Toggle from '~components/toggle';
+
+import { Breadcrumbs } from '../app-layout/utils/content-blocks';
+import appLayoutLabels from '../app-layout/utils/labels';
+
+export default function () {
+  const [appLayoutHeader, toggleAppLayoutHeader] = useState(false);
+  const [appLayoutOverlap, toggleAppLayoutOverlap] = useState(true);
+
+  const [contentLayout, toggleContentLayout] = useState(false);
+  const [contentLayoutHeader, toggleContentLayoutHeader] = useState(true);
+  const [contentLayoutOverlap, toggleContentLayoutOverlap] = useState(true);
+
+  const [formHeader, toggleFormHeader] = useState(true);
+  const [formContent, toggleFormContent] = useState(true);
+
+  const form = (
+    <form>
+      <Form header={formHeader && <Header variant="h1">Form header</Header>}>
+        {formContent && (
+          <Container header={<Header variant="h2">Form section header</Header>}>
+            <FormField label="Form field">
+              <Input value="" readOnly={true} />
+            </FormField>
+          </Container>
+        )}
+      </Form>
+    </form>
+  );
+
+  return (
+    <AppLayout
+      ariaLabels={appLayoutLabels}
+      navigationOpen={false}
+      notifications={
+        <ColumnLayout columns={3}>
+          <SpaceBetween size="xxs">
+            <Toggle onChange={() => toggleAppLayoutHeader(!appLayoutHeader)} checked={appLayoutHeader}>
+              App layout header
+            </Toggle>
+            <Toggle onChange={() => toggleAppLayoutOverlap(!appLayoutOverlap)} checked={appLayoutOverlap}>
+              App layout overlap
+            </Toggle>
+          </SpaceBetween>
+          <SpaceBetween size="xxs">
+            <Toggle onChange={() => toggleContentLayout(!contentLayout)} checked={contentLayout}>
+              Content layout
+            </Toggle>
+            <Box padding={{ left: 'xxl' }}>
+              <Toggle
+                onChange={() => toggleContentLayoutHeader(!contentLayoutHeader)}
+                disabled={!contentLayout}
+                checked={contentLayoutHeader}
+                data-testid="toggle-content-layout-header"
+              >
+                Content layout header
+              </Toggle>
+              <Toggle
+                onChange={() => toggleContentLayoutOverlap(!contentLayoutOverlap)}
+                disabled={!contentLayout}
+                checked={contentLayoutOverlap}
+              >
+                Content layout overlap
+              </Toggle>
+            </Box>
+          </SpaceBetween>
+          <SpaceBetween size="xxs">
+            <Toggle
+              onChange={() => toggleFormHeader(!formHeader)}
+              checked={formHeader}
+              data-testid="toggle-form-header"
+            >
+              Form header
+            </Toggle>
+            <Toggle onChange={() => toggleFormContent(!formContent)} checked={formContent}>
+              Form content
+            </Toggle>
+          </SpaceBetween>
+        </ColumnLayout>
+      }
+      breadcrumbs={<Breadcrumbs />}
+      contentHeader={appLayoutHeader && <Header variant="h1">App layout header</Header>}
+      disableContentHeaderOverlap={!appLayoutOverlap}
+      content={
+        contentLayout ? (
+          <ContentLayout
+            header={contentLayoutHeader && <Header variant="h1">Content layout header</Header>}
+            disableOverlap={!contentLayoutOverlap}
+          >
+            {form}
+          </ContentLayout>
+        ) : (
+          form
+        )
+      }
+    />
+  );
+}
---
+++ src/content-layout/internal.tsx
@@ -0,0 +1,71 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React, { useRef } from 'react';
+import clsx from 'clsx';
+import { ContentLayoutProps } from './interfaces';
+import { getBaseProps } from '../internal/base-component';
+import { InternalBaseComponentProps } from '../internal/hooks/use-base-component';
+import { useAppLayoutContext } from '../internal/context/app-layout-context';
+import { useDynamicOverlap } from '../internal/hooks/use-dynamic-overlap';
+import { useMergeRefs } from '../internal/hooks/use-merge-refs';
+import { useVisualRefresh } from '../internal/hooks/use-visual-mode';
+import styles from './styles.css.js';
+
+type InternalContentLayoutProps = ContentLayoutProps & InternalBaseComponentProps;
+
+export default function InternalContentLayout({
+  children,
+  disableOverlap,
+  header,
+  __internalRootRef,
+  ...rest
+}: InternalContentLayoutProps) {
+  const baseProps = getBaseProps(rest);
+  const rootElement = useRef<HTMLDivElement>(null);
+  const mergedRef = useMergeRefs(rootElement, __internalRootRef);
+
+  const isVisualRefresh = useVisualRefresh();
+  const { hasBreadcrumbs } = useAppLayoutContext();
+  const overlapElement = useDynamicOverlap();
+
+  /**
+   * Disable the overlap if the component is missing either a header or child
+   * content. If the component is not using visual refresh then the overlap
+   * will not be displayed at all. This is handled in the CSS not the JavaScript.
+   */
+  const isOverlapDisabled = !children || !header || disableOverlap;
+
+  return (
+    <div
+      {...baseProps}
+      className={clsx(baseProps.className, styles.layout, {
+        [styles['is-overlap-disabled']]: isOverlapDisabled,
+        [styles['is-visual-refresh']]: isVisualRefresh,
+      })}
+      ref={mergedRef}
+    >
+      <div
+        className={clsx(
+          styles.background,
+          { [styles['is-overlap-disabled']]: isOverlapDisabled },
+          'awsui-context-content-header'
+        )}
+        ref={overlapElement}
+      />
+
+      {header && (
+        <div
+          className={clsx(
+            styles.header,
+            { [styles['has-breadcrumbs']]: isVisualRefresh && hasBreadcrumbs },
+            'awsui-context-content-header'
+          )}
+        >
+          {header}
+        </div>
+      )}
+
+      <div className={styles.content}>{children}</div>
+    </div>
+  );
+}
