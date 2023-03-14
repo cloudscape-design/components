1c2e4390322305da8e089dae42198d4b3f6a906e
Boris Serdiuk <serdiuk@amazon.com>
2023-03-14 22:01:19 +0100

    fix: Do not emit change events when use-entered option is selected

--- src/autosuggest/__integ__/events-autosuggest.test.ts
+++ src/autosuggest/__integ__/events-autosuggest.test.ts
@@ -25,7 +25,7 @@ describe.each<boolean>([false, true])('Autosuggest events (expandToViewport=%s)'
   );
 
   test(
-    'should fire change event when selecting suggestions with SPACE',
+    'should allow entering spaces after selecting an item',
     setupTest(async page => {
       await page.focusInput();
       await page.keys(['opt']);
@@ -33,6 +33,7 @@ describe.each<boolean>([false, true])('Autosuggest events (expandToViewport=%s)'
 
       await page.keys(['ArrowDown', 'Space']);
       await page.assertEventsFired(['onChange']);
+      await expect(page.getAutosuggestValue()).resolves.toEqual('opt ');
     })
   );
 
@@ -101,7 +102,7 @@ describe.each<boolean>([false, true])('Autosuggest events (expandToViewport=%s)'
       await page.keys(['opt']);
       await page.clearEventList();
 
-      await page.keys(['ArrowDown', 'Enter']);
+      await page.keys(['ArrowDown', 'ArrowDown', 'Enter']);
       await page.focusOutsideInput();
       await page.assertEventsFired(['onChange', 'onBlur']);
     })
