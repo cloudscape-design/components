20fdc3611015d43701afc1f0417811bc8b2076bd
Avinash Dwarapu <dwaraa@amazon.com>
2023-03-16 19:27:13 +0100

    Split locale files by language.

--- build-tools/tasks/generate-i18n-messages.js
+++ build-tools/tasks/generate-i18n-messages.js
@@ -8,43 +8,44 @@ const { parse } = require('@formatjs/icu-messageformat-parser');
 const { targetPath } = require('../utils/workspace');
 const { writeFile } = require('../utils/files');
 
+const namespace = '@cloudscape-design/components';
+
+const destinationDir = path.join(targetPath, 'components/internal/i18n/messages');
+const declarationFile = `import { I18nProviderProps } from "../provider";
+const messages: I18nProviderProps.Messages;
+export default messages;
+`;
+
 module.exports = function generateI18nMessages() {
   const messagesDir = path.resolve(__dirname, '../../src/internal/i18n/messages');
   const files = fs.readdirSync(messagesDir);
+
+  const allParsedMessages = {};
+
   for (const fileName of files) {
     const filePath = path.join(messagesDir, fileName);
     const messages = require(filePath);
+    const [subset, locale] = fileName.split('.');
 
     const parsedMessages = Object.fromEntries(
-      Object.entries(messages).map(([namespace, locales]) => [
-        namespace,
-        Object.fromEntries(
-          Object.entries(locales).map(([locale, components]) => [
-            locale,
-            Object.fromEntries(
-              Object.entries(components).map(([component, keys]) => [
-                component,
-                Object.fromEntries(
-                  Object.entries(keys).map(([key, icuMessage]) => [key, parse(icuMessage, { locale })])
-                ),
-              ])
-            ),
-          ])
-        ),
+      Object.entries(messages).map(([component, keys]) => [
+        component,
+        Object.fromEntries(Object.entries(keys).map(([key, icuMessage]) => [key, parse(icuMessage, { locale })])),
       ])
     );
+    allParsedMessages[locale] = { ...(allParsedMessages[locale] ?? {}), ...parsedMessages };
+    const resultFormat = { [namespace]: { [locale]: parsedMessages } };
 
-    writeFile(path.join(targetPath, 'components/internal/i18n/messages', fileName), JSON.stringify(parsedMessages));
-
-    // FIXME: HACK: Maybe this is worth keeping?
-    writeFile(
-      path.join(targetPath, 'components/internal/i18n/messages', fileName.replace(/\.json$/, '.d.ts')),
-      `import { I18nProviderProps } from "../provider";\nconst messages: I18nProviderProps.Messages;\nexport default messages;\n`
-    );
-    writeFile(
-      path.join(targetPath, 'components/internal/i18n/messages', fileName.replace(/\.json$/, '.js')),
-      `export default ${JSON.stringify(parsedMessages)}`
-    );
+    writeFile(path.join(destinationDir, `${subset}.${locale}.json`), JSON.stringify(resultFormat));
+    writeFile(path.join(destinationDir, `${subset}.${locale}.d.ts`), declarationFile);
+    writeFile(path.join(destinationDir, `${subset}.${locale}.js`), `export default ${JSON.stringify(resultFormat)}`);
   }
+
+  // Generate a ".all" file containing all locales.
+  const resultFormat = { [namespace]: allParsedMessages };
+  writeFile(path.join(destinationDir, `all.all.json`), JSON.stringify(resultFormat));
+  writeFile(path.join(destinationDir, `all.all.d.ts`), declarationFile);
+  writeFile(path.join(destinationDir, `all.all.js`), `export default ${JSON.stringify(resultFormat)}`);
+
   return Promise.resolve();
 };
