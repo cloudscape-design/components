78f3f096303f240a81c90d5c2cd8c8f571a8b8f6
Avinash Dwarapu <dwaraa@amazon.com>
2023-03-16 19:36:10 +0100

    Add i18n imports to package.json.

--- build-tools/tasks/package-json.js
+++ build-tools/tasks/package-json.js
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 const { parallel } = require('gulp');
 const path = require('path');
+const fs = require('fs');
 const { writeFile, listPublicItems, listBetaVersions } = require('../utils/files');
 const themes = require('../utils/themes');
 const { task } = require('../utils/gulp-utils');
@@ -35,6 +36,17 @@ function getComponentsExports() {
   for (const component of components) {
     result[`./${component}`] = `./${component}/index.js`;
   }
+
+  // For i18n beta: include internal i18n import and all messages.
+  result['./internal/i18n'] = './internal/i18n/index.js';
+  result[`./internal/i18n/messages/all.all`] = `./internal/i18n/messages/all.all.js`;
+  result[`./internal/i18n/messages/all.all.json`] = `./internal/i18n/messages/all.all.json`;
+  for (const translationFile of fs.readdirSync('src/internal/i18n/messages')) {
+    const [subset, locale] = translationFile.split('.');
+    result[`./internal/i18n/messages/${subset}.${locale}`] = `./internal/i18n/messages/${subset}.${locale}.js`;
+    result[`./internal/i18n/messages/${subset}.${locale}.json`] = `./internal/i18n/messages/${subset}.${locale}.json`;
+  }
+
   return result;
 }
 
