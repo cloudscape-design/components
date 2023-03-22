25bfb10c183664b71ba902776b476cf186679b09
Avinash Dwarapu <dwaraa@amazon.com>
2023-03-22 17:40:21 +0100

    refactor: Remove useContext hack from i18n context

--- src/internal/i18n/context.ts
+++ src/internal/i18n/context.ts
@@ -11,11 +11,9 @@ export interface FormatFunction {
   <T>(namespace: string, component: string, key: string, provided: T, handler?: CustomHandler<T>): T;
 }
 
-function defaultFormatFunction<T>(_namespace: string, _component: string, _key: string, provided: T) {
-  return provided;
-}
-
-export const InternalI18nContext = React.createContext<FormatFunction>(defaultFormatFunction);
+export const InternalI18nContext = React.createContext<FormatFunction>(
+  <T>(_namespace: string, _component: string, _key: string, provided: T) => provided
+);
 
 export interface ComponentFormatFunction {
   (key: string, provided: string): string;
@@ -24,10 +22,7 @@ export interface ComponentFormatFunction {
 }
 
 export function useInternalI18n(componentName: string): ComponentFormatFunction {
-  // HACK: useContext should return the default value if a provider
-  // isn't present, but some consumers mock out React.useContext globally
-  // in their tests, so we can't rely on this assumption.
-  const format = useContext(InternalI18nContext) || defaultFormatFunction;
+  const format = useContext(InternalI18nContext);
   return <T>(key: string, provided: T, customHandler?: CustomHandler<T>) => {
     return format<T>('@cloudscape-design/components', componentName, key, provided, customHandler);
   };
