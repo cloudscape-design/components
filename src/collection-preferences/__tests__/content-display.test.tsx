6aa1be95dca40ddf1b0f47566234bc484f353802
Joan Perals Tresserra <jotresse@amazon.com>
2023-03-23 12:36:27 +0100

    Add first basic unit tests for ContentDisplay component

---
+++ src/collection-preferences/__tests__/content-display.test.tsx
@@ -0,0 +1,36 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { renderCollectionPreferences } from './shared';
+import { CollectionPreferencesProps } from '../../../lib/components';
+
+describe('Content display', () => {
+  it('correctly displays title', () => {
+    const wrapper = renderContentDisplay({});
+    const titleElement = wrapper.findTitle().getElement();
+    expect(titleElement).toHaveTextContent('Content display title');
+    expect(titleElement.tagName).toBe('H3');
+  });
+
+  it('correctly displays label', () => {
+    const wrapper = renderContentDisplay({});
+    const labelElement = wrapper.findLabel().getElement();
+    expect(labelElement).toHaveTextContent('Content display label');
+  });
+});
+
+const contentDisplayPreference: CollectionPreferencesProps.ContentDisplayPreference = {
+  title: 'Content display title',
+  label: 'Content display label',
+  options: [
+    { id: 'id1', label: 'Item 1', editable: false },
+    { id: 'id2', label: 'Item 2' },
+    { id: 'id3', label: 'Item 3' },
+    { id: 'id4', label: 'Item 4' },
+  ],
+};
+
+function renderContentDisplay(props: Partial<CollectionPreferencesProps> = {}) {
+  const collectionPreferencesWrapper = renderCollectionPreferences({ contentDisplayPreference, ...props });
+  collectionPreferencesWrapper.findTriggerButton().click();
+  return collectionPreferencesWrapper.findModal()!.findContentDisplayPreference()!;
+}
