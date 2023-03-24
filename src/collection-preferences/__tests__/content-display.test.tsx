82cb3cef529365f2dbe24a1032aad60c84fe39b0
Joan Perals Tresserra <jotresse@amazon.com>
2023-03-24 18:09:37 +0100

    Improve unit test coverage for HTML semantics of options list

--- src/collection-preferences/__tests__/content-display.test.tsx
+++ src/collection-preferences/__tests__/content-display.test.tsx
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 import { renderCollectionPreferences } from './shared';
 import { CollectionPreferencesProps } from '../../../lib/components';
+import ContentDisplayPreferenceWrapper from '../../../lib/components/test-utils/dom/collection-preferences/content-display-preference';
+import dragHandleStyles from '../../../lib/components/internal/drag-handle/styles.css.js';
 
 describe('Content display', () => {
   it('correctly displays title', () => {
@@ -17,12 +19,17 @@ describe('Content display', () => {
     expect(labelElement).toHaveTextContent('Content display label');
   });
 
-  it('displays list of options as unordered list', () => {
+  it('displays list of options with correct semantics', () => {
     const wrapper = renderContentDisplay({});
     const items = wrapper.findOptions();
-    for (const item of items) {
-      expect(item.getElement().tagName).toBe('LI');
-      expect(item.getElement().parentElement!.tagName).toBe('UL');
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      const element = item.getElement();
+      expect(element.tagName).toBe('LI');
+      expect(element.parentElement!.tagName).toBe('UL');
+      expectLabel(wrapper, element, `Item ${i + 1}`);
+      const dragHandle = item.find(`.${dragHandleStyles.handle}`)!.getElement();
+      expectLabel(wrapper, dragHandle, 'Drag handle');
     }
   });
 });
@@ -36,10 +43,22 @@ const contentDisplayPreference: CollectionPreferencesProps.ContentDisplayPrefere
     { id: 'id3', label: 'Item 3' },
     { id: 'id4', label: 'Item 4' },
   ],
+  dragHandleAriaLabel: 'Drag handle',
 };
 
 function renderContentDisplay(props: Partial<CollectionPreferencesProps> = {}) {
   const collectionPreferencesWrapper = renderCollectionPreferences({ contentDisplayPreference, ...props });
   collectionPreferencesWrapper.findTriggerButton().click();
   return collectionPreferencesWrapper.findModal()!.findContentDisplayPreference()!;
+}
+
+function expectLabel(wrapper: ContentDisplayPreferenceWrapper, element: HTMLElement, label: string) {
+  const labelAttribute = element.getAttribute('aria-label');
+  if (labelAttribute === label) {
+    return;
+  }
+  const labelledBy = element.getAttribute('aria-labelledby');
+  expect(labelledBy).toBeTruthy();
+  const labelElement = wrapper.find(`#${labelledBy}`);
+  expect(labelElement!.getElement()).toHaveTextContent(label);
 }
