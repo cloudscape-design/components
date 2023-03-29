eb59f5446d49dbdbf6ff1ae639e0b2e841a5dee6
Joan Perals Tresserra <jotresse@amazon.com>
2023-03-29 20:39:13 +0200

    Use div for drag handle in order to work around Safari focus behavior

--- src/internal/drag-handle/index.tsx
+++ src/internal/drag-handle/index.tsx
@@ -8,11 +8,11 @@ import styles from './styles.css.js';
 import { SyntheticListenerMap } from '@dnd-kit/core/dist/hooks/utilities';
 
 export interface DragHandleProps {
-  attributes: ButtonHTMLAttributes<HTMLButtonElement>;
+  attributes: ButtonHTMLAttributes<HTMLDivElement>;
   listeners?: SyntheticListenerMap;
 }
 
-function DragHandle({ attributes, listeners }: DragHandleProps, ref: ForwardedRef<HTMLButtonElement>) {
+function DragHandle({ attributes, listeners }: DragHandleProps, ref: ForwardedRef<HTMLDivElement>) {
   return (
     <div role="application">
       <Handle ref={ref} className={styles.handle} {...attributes} {...listeners}>
--- src/internal/handle/index.tsx
+++ src/internal/handle/index.tsx
@@ -1,19 +1,32 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import clsx from 'clsx';
-import React, { ButtonHTMLAttributes, ForwardedRef, PointerEvent, forwardRef } from 'react';
+import React, { HTMLAttributes, ForwardedRef, PointerEvent, forwardRef } from 'react';
 import styles from './styles.css.js';
 
-function Handle(props: ButtonHTMLAttributes<HTMLButtonElement>, ref: ForwardedRef<HTMLButtonElement>) {
-  function handlePointerDown(event: PointerEvent<HTMLButtonElement>) {
+function Handle(props: HTMLAttributes<HTMLDivElement>, ref: ForwardedRef<HTMLDivElement>) {
+  function handlePointerDown(event: PointerEvent<HTMLDivElement>) {
     if (event.button !== 0) {
       return;
     }
     props.onPointerDown?.(event);
   }
 
+  // We need to use a div with button role instead of a button
+  // so that Safari will focus on it when clicking it.
+  // (See https://bugs.webkit.org/show_bug.cgi?id=22261)
+  // Otherwise, we can't reliably catch keyboard events coming from the handle
+  // when it is being dragged.
+
   return (
-    <button {...props} onPointerDown={handlePointerDown} className={clsx(styles.handle, props.className)} ref={ref} />
+    <div
+      role="button"
+      tabIndex={0}
+      {...props}
+      onPointerDown={handlePointerDown}
+      className={clsx(styles.handle, props.className)}
+      ref={ref}
+    />
   );
 }
 
