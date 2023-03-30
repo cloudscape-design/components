abd7a783d832adb785edd2a92d36dca24baed8ed
Joan Perals Tresserra <jotresse@amazon.com>
2023-03-30 15:45:35 +0200

    Remove unused functions

--- src/collection-preferences/content-display/reorder-utils.ts
+++ src/collection-preferences/content-display/reorder-utils.ts
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 import { CollectionPreferencesProps } from '../interfaces';
 
-type Order = ReadonlyArray<string>;
-
 export function getSortedOptions({
   options,
   order,
@@ -16,41 +14,4 @@ export function getSortedOptions({
     {}
   );
   return order.map(({ id }) => optionsSet[id]).filter(Boolean);
-}
-
-export function reorderOptions({
-  draggedOptionId,
-  verticalCenters,
-  dragAmount,
-}: {
-  draggedOptionId: string;
-  verticalCenters: Record<string, number>;
-  dragAmount: number;
-}) {
-  const centersArray = [];
-  for (const id of Object.keys(verticalCenters)) {
-    const center = id === draggedOptionId ? verticalCenters[id] + dragAmount : verticalCenters[id];
-    centersArray.push({ id, center });
-  }
-  const sortedCenters = [...centersArray].sort((center1, center2) => center1.center - center2.center);
-  return sortedCenters.map(({ id }) => id);
-}
-
-export function areOrdersEqual(order1: Order, order2: Order) {
-  if (order1.length !== order2.length) {
-    return false;
-  }
-  for (let i = 0; i < order1.length; i++) {
-    if (order1[i] !== order2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-export function getFlatOptionIds(options: ReadonlyArray<CollectionPreferencesProps.VisibleContentOptionsGroup>) {
-  return options.reduce<string[]>(
-    (ids, group) => [...ids, ...group.options.reduce<string[]>((groupIds, option) => [...groupIds, option.id], [])],
-    []
-  );
 }
