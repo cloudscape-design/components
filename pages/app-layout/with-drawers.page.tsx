c23c33ed5566cfbb15b2ea65418933c4e534821e
Scott O'Brien <scottob@amazon.com>
2023-04-04 10:15:57 -0400

    Added custom property for active drawer width.

--- pages/app-layout/with-drawers.page.tsx
+++ pages/app-layout/with-drawers.page.tsx
@@ -17,7 +17,7 @@ import ScreenshotArea from '../utils/screenshot-area';
 export default function WithDrawers() {
   const [activeDrawerId, setActiveDrawerId] = useState<string | null>(null);
   const [useDrawers, setUseDrawers] = useState(true);
-  const [useTools, setUseTools] = useState(false);
+  const [useTools, setUseTools] = useState(true);
 
   const drawers = !useDrawers
     ? null
