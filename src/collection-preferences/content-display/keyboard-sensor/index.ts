9c90c037641454a96029300604f4ffabb8658d54
Joan Perals Tresserra <jotresse@amazon.com>
2023-04-12 13:20:16 +0200

    Explicitly import React in KeyboardSensor

--- src/collection-preferences/content-display/keyboard-sensor/index.ts
+++ src/collection-preferences/content-display/keyboard-sensor/index.ts
@@ -1,5 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+import React from 'react';
 import {
   add as getAdjustedCoordinates,
   subtract as getCoordinatesDelta,
