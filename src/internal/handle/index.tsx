e807aa372778f645fae59727910e825676388213
Joan Perals Tresserra <jotresse@amazon.com>
2023-04-17 09:29:35 +0200

    Remove unnecessary event-handling logic in internal Handle component

--- src/internal/handle/index.tsx
+++ src/internal/handle/index.tsx
@@ -1,18 +1,10 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import clsx from 'clsx';
-import React, { HTMLAttributes, ForwardedRef, PointerEvent, forwardRef } from 'react';
+import React, { HTMLAttributes, ForwardedRef, forwardRef } from 'react';
 import styles from './styles.css.js';
 
 function Handle(props: HTMLAttributes<HTMLDivElement>, ref: ForwardedRef<HTMLDivElement>) {
-  function handlePointerDown(event: PointerEvent<HTMLDivElement>) {
-    // Listen only to left button
-    if (event.button !== 0) {
-      return;
-    }
-    props.onPointerDown?.(event);
-  }
-
   // We need to use a div with button role instead of a button
   // so that Safari will focus on it when clicking it.
   // (See https://bugs.webkit.org/show_bug.cgi?id=22261)
@@ -24,7 +16,7 @@ function Handle(props: HTMLAttributes<HTMLDivElement>, ref: ForwardedRef<HTMLDiv
       role="button"
       tabIndex={0}
       {...props}
-      onPointerDown={handlePointerDown}
+      onPointerDown={props.onPointerDown}
       className={clsx(styles.handle, props.className)}
       ref={ref}
     />
