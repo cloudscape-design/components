ec6e5132b805605e94846be0200ea3053bec9365
Francesco Longo <flongo@amazon.de>
2023-04-17 11:51:30 +0200

    feat: Expose runtime theming API for secondary themes

--- pages/theming/integration.page.tsx
+++ pages/theming/integration.page.tsx
@@ -34,26 +34,43 @@ const theme: Theme = {
 
 export default function () {
   const [themed, setThemed] = useState<boolean>(false);
+  const [secondaryTheme, setSecondaryTheme] = useState<boolean>(false);
 
   useEffect(() => {
     let reset: () => void = () => {};
     if (themed) {
-      const result = applyTheme({ theme });
+      const result = applyTheme({ theme, baseThemeId: secondaryTheme ? 'visual-refresh' : undefined });
       reset = result.reset;
     }
     return reset;
-  }, [themed]);
+  }, [themed, secondaryTheme]);
   return (
     <div style={{ padding: 15 }}>
       <h1>Theming Integration Page</h1>
       <p>Only for internal theme</p>
-      <button data-testid="change-theme" onClick={() => setThemed(!themed)}>
-        Change Theme
-      </button>
+      <label>
+        <input
+          type="checkbox"
+          data-testid="change-theme"
+          checked={themed}
+          onChange={evt => setThemed(evt.currentTarget.checked)}
+        />
+        <span style={{ marginLeft: 5 }}>Apply theme</span>
+      </label>
+      <label>
+        <input
+          style={{ marginLeft: 15 }}
+          type="checkbox"
+          data-testid="set-secondary"
+          checked={secondaryTheme}
+          onChange={evt => setSecondaryTheme(evt.currentTarget.checked)}
+        />
+        <span style={{ marginLeft: 5 }}>Secondary theme</span>
+      </label>
       <ScreenshotArea>
         <SpaceBetween direction="vertical" size="m">
           <Button variant="primary">Primary Button</Button>
-          <Link>Link</Link>
+          <Link href="#">Link</Link>
           <a data-testid="element-color-text-link-default" style={{ color: Tokens.colorTextLinkDefault }}>
             Anchor using colorTextLinkDefault
           </a>
