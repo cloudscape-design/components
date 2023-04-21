9ac7471ad351344773d6aa0eb27ccadbeaec3682
Timo Gasda <gasda@amazon.com>
2023-04-21 12:53:02 +0200

    Fix unit test

--- src/table/__tests__/body-cell.test.tsx
+++ src/table/__tests__/body-cell.test.tsx
@@ -99,21 +99,21 @@ describe('TableBodyCell', () => {
 
   it('should call onEditEnd', () => {
     render(<TestComponent isEditing={true} />);
-    fireEvent.click(screen.getByRole('button', { name: 'Submit editing test' }));
+    fireEvent.click(screen.getByRole('button', { name: 'submit edit' }));
     expect(onEditEnd).toHaveBeenCalled();
   });
 
   it('should call onEditEnd with value when submitting', () => {
     const { container } = render(<TestComponent isEditing={true} />);
     fireEvent.change(container.querySelector('input')!, { target: { value: 'test2' } });
-    fireEvent.click(screen.getByRole('button', { name: 'Submit editing test' }));
+    fireEvent.click(screen.getByRole('button', { name: 'submit edit' }));
     expect(onEditEnd).toHaveBeenCalled();
   });
 
   it('should call onEditEnd with value when cancelling', () => {
     const { container } = render(<TestComponent isEditing={true} />);
     fireEvent.change(container.querySelector('input')!, { target: { value: 'test2' } });
-    fireEvent.click(screen.getByRole('button', { name: 'Cancel editing test' }));
+    fireEvent.click(screen.getByRole('button', { name: 'cancel edit' }));
     expect(onEditEnd).toHaveBeenCalled();
   });
 
