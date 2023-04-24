b421763d1c5a578ba6780678e166aebf568afa42
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-04-24 13:43:25 +0200

    Removed unused line

--- src/table/header-cell/index.tsx
+++ src/table/header-cell/index.tsx
@@ -10,7 +10,6 @@ import styles from './styles.css.js';
 import { Resizer } from '../resizer';
 import { useUniqueId } from '../../internal/hooks/use-unique-id';
 import { InteractiveComponent } from '../thead';
-import { SortingState } from '@cloudscape-design/collection-hooks/dist/cjs/interfaces';
 
 interface TableHeaderCellProps<ItemType> {
   className?: string;
