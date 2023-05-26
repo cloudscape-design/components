107a837cfbb5a49cfc8433f7f43c4ff8c97d1dba
Timo Gasda <gasda@amazon.com>
2023-05-26 14:27:29 +0200

    chore: Prevent scroll when focusing SVG elements

--- src/internal/utils/focus-svg-element.ts
+++ src/internal/utils/focus-svg-element.ts
@@ -2,7 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 export default function focusSvgElement(element: SVGElement) {
   if (element.focus) {
-    element.focus();
+    // Using preventScroll here fixes a bug in Safari that causes the page to scroll to the top
+    // when hovering over a pie chart with no visible labels.
+    element.focus({ preventScroll: true });
   } else {
     // Deferring in a timeout because IE11 is not updating `tabIndex` and `focusable` attributes on time for some reason.
     setTimeout(() => {
