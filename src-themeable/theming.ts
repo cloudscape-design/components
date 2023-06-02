9c45003c931238e14bfcc2dc9904e504469ab206
Johannes Weber <jowejowe@amazon.com>
2023-06-02 11:23:27 +0200

    fix path for internal dir, add designTokensTemplateDir

--- src-themeable/theming.ts
+++ src-themeable/theming.ts
@@ -4,9 +4,10 @@ import { join } from 'path';
 import { buildThemedComponents as themingCoreBuild } from '@cloudscape-design/theming-build';
 import { preset, TypedOverride } from './internal/template/internal/generated/theming/index.cjs';
 
-const internalDir = join(__dirname, '../../lib/internal');
+const internalDir = join(__dirname, './internal');
 const scssDir = join(internalDir, './scss');
 const templateDir = join(internalDir, './template');
+const designTokensTemplateDir = join(internalDir, './template-tokens');
 
 export type Theme = TypedOverride;
 export interface BuildThemedComponentsParams {
@@ -23,6 +24,7 @@ export function buildThemedComponents({ theme, outputDir, baseThemeId }: BuildTh
     componentsOutputDir: join(outputDir, 'components'),
     designTokensOutputDir: join(outputDir, 'design-tokens'),
     templateDir,
+    designTokensTemplateDir,
     scssDir,
   });
 }
