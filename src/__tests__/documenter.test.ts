afa54e7ceb85a2974bbf1c5f32e37c7ecf2e7a8e
Avinash Dwarapu <dwaraa@amazon.com>
2023-06-02 14:47:54 +0200

    chore: Re-enable documenter tests

--- src/__tests__/documenter.test.ts
+++ src/__tests__/documenter.test.ts
@@ -2,9 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 import { getAllComponents, requireComponentDefinition } from './utils';
 
-// Skipping to let documenter format changes through.
-// See: https://github.com/cloudscape-design/documenter/pull/21
-describe.skip('Documenter', () => {
+describe('Documenter', () => {
   test.each<string>(getAllComponents())(`definition for %s matches the snapshot`, (componentName: string) => {
     const definition = requireComponentDefinition(componentName);
     expect(definition).toMatchSnapshot(componentName);
