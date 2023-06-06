857a687bbc18d08d6d19fb96f51f202ffd6dc66a
Johannes Weber <jwebe0pe@gmail.com>
2023-06-06 15:09:22 +0200

    chore: Prepare design tokens template (#1176)

--- build-tools/tasks/themeable-source.js
+++ build-tools/tasks/themeable-source.js
@@ -4,73 +4,68 @@ const { parallel } = require('gulp');
 const path = require('path');
 const { promises: fsp } = require('fs');
 const { join } = require('path');
-const { task, noop } = require('../utils/gulp-utils');
+const { task } = require('../utils/gulp-utils');
 const workspace = require('../utils/workspace');
 const themes = require('../utils/themes');
 const { generatePackageJson } = require('./package-json');
 const { copyThirdPartyLicenses } = require('./licenses');
 const execa = require('execa');
+const { compileTypescript } = require('./typescript');
 
-const templateDir = 'internal/template';
+const componentsTemplateDir = 'internal/template';
+const designTokensTemplateDir = 'internal/template-tokens';
 const stylesDir = 'internal/scss';
 
 const theme = themes.find(theme => theme.name === 'default');
-const themeables = [];
-
-if (theme) {
-  themeables.push({
-    name: 'components-themeable',
-    sourceDir: theme.outputPath,
-    targetDir: join(workspace.targetPath, 'components-themeable'),
-    packageJson: {
-      name: '@cloudscape-design/components-themed',
-    },
-  });
-}
-
-const createCopyStylesTask = themeable => {
-  return task(`themeable-source:copy-styles:${themeable.name}`, () => {
-    return execa('rsync', [
-      '--prune-empty-dirs',
-      '-a',
-      '--include',
-      '*/',
-      // Code Editor SVGs will be embedded into the styling
-      '--include',
-      'code-editor/assets/*.svg',
-      '--include',
-      '*.scss',
-      '--exclude',
-      '*',
-      `${workspace.sourcePath}/`,
-      `${path.join(themeable.targetDir, stylesDir)}/`,
-    ]);
-  });
+const themeable = {
+  name: 'components-themeable',
+  sourceDir: theme.outputPath,
+  targetDir: join(workspace.targetPath, 'components-themeable'),
+  packageJson: {
+    name: '@cloudscape-design/components-themed',
+  },
+  tokensPackageJson: {
+    name: '@cloudscape-design/design-tokens-themed',
+  },
 };
 
-const createCopyTemplateTask = themeable => {
-  return task(`themeable-source:copy-template:${themeable.name}`, async () => {
-    const dest = path.join(themeable.targetDir, templateDir);
-    await fsp.mkdir(dest, { recursive: true });
-    // The '.' tells cp to copy all files inside the source to the destination.
-    // Using it as part of path.join will simply resolve it. That's why, we use template strings.
-    return execa('cp', ['-R', `${path.join(themeable.sourceDir, '/')}.`, dest]);
-  });
-};
+const copyStylesTask = task(`themeable-source:copy-styles:${themeable.name}`, () => {
+  return execa('rsync', [
+    '--prune-empty-dirs',
+    '-a',
+    '--include',
+    '*/',
+    // Code Editor SVGs will be embedded into the styling
+    '--include',
+    'code-editor/assets/*.svg',
+    '--include',
+    '*.scss',
+    '--exclude',
+    '*',
+    `${workspace.sourcePath}/`,
+    `${path.join(themeable.targetDir, stylesDir)}/`,
+  ]);
+});
 
-const createGenerateExtrasTask = themeable => {
-  return parallel(
-    generatePackageJson(join(themeable.targetDir, templateDir), themeable.packageJson, {
-      injectDependencies: true,
-    }),
-    copyThirdPartyLicenses('themeable', join(themeable.targetDir, templateDir))
-  );
-};
+const copyTemplateTask = task(`themeable-source:copy-template:${themeable.name}`, async () => {
+  const dest = path.join(themeable.targetDir, componentsTemplateDir);
+  await fsp.mkdir(dest, { recursive: true });
+  // The '.' tells cp to copy all files inside the source to the destination.
+  // Using it as part of path.join will simply resolve it. That's why, we use template strings.
+  return execa('cp', ['-R', `${path.join(themeable.sourceDir, '/')}.`, dest]);
+});
 
-const parallelOrNOOP = tasks => (tasks.length ? parallel(...tasks) : noop);
-
-const copyStylesTask = parallelOrNOOP(themeables.map(themeable => createCopyStylesTask(themeable)));
-const copyTemplateTask = parallelOrNOOP(themeables.map(themeable => createCopyTemplateTask(themeable)));
-const generateExtraTask = parallelOrNOOP(themeables.map(themeable => createGenerateExtrasTask(themeable)));
-
-module.exports = parallel(copyStylesTask, copyTemplateTask, generateExtraTask);
+module.exports = parallel(
+  compileTypescript({
+    name: themeable.name,
+    tsConfigPath: 'tsconfig.src-themeable.json',
+    outputPath: themeable.targetDir,
+  }),
+  generatePackageJson(join(themeable.targetDir, componentsTemplateDir), themeable.packageJson, {
+    injectDependencies: true,
+  }),
+  generatePackageJson(join(themeable.targetDir, designTokensTemplateDir), themeable.tokensPackageJson),
+  copyThirdPartyLicenses('themeable', join(themeable.targetDir, componentsTemplateDir)),
+  copyStylesTask,
+  copyTemplateTask
+);
