55b2a3bd2918d7ff2ac94bd8b64a5cdcdbfed102
Timo <gasda@amazon.com>
2023-06-27 16:19:48 +0200

    remove

--- src/mixed-line-bar-chart/chart-container.tsx
+++ src/mixed-line-bar-chart/chart-container.tsx
@@ -266,7 +266,6 @@ export default function ChartContainer<T extends ChartDataTypes>({
     clearHighlightedSeries,
     isGroupNavigation,
     isHandlersDisabled,
-    isPinned: isPopoverPinned,
     highlightX,
   });
 
