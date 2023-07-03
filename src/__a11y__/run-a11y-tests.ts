805997612d67b38d1343dd6dce1c6873226e1f40
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-07-03 15:36:27 +0200

    Remove a11y tests filter

--- src/__a11y__/run-a11y-tests.ts
+++ src/__a11y__/run-a11y-tests.ts
@@ -24,23 +24,21 @@ function urlFormatter(inputUrl: string, theme: Theme, mode: Mode) {
 
 export default function runA11yTests(theme: Theme, mode: Mode, skip: string[] = []) {
   describe(`A11y checks for ${mode} ${theme}`, () => {
-    findAllPages()
-      .filter(url => url.includes('area-chart'))
-      .forEach(inputUrl => {
-        const testFunction =
-          [
-            ...skip,
-            'theming/tokens',
-            // this page intentionally has issues to test the helper
-            'undefined-texts',
-          ].indexOf(inputUrl) === -1
-            ? test
-            : test.skip;
-        const url = urlFormatter(inputUrl, theme, mode);
-        testFunction(
-          url,
-          setupTest(url, page => page.assertNoAxeViolations())
-        );
-      });
+    findAllPages().forEach(inputUrl => {
+      const testFunction =
+        [
+          ...skip,
+          'theming/tokens',
+          // this page intentionally has issues to test the helper
+          'undefined-texts',
+        ].indexOf(inputUrl) === -1
+          ? test
+          : test.skip;
+      const url = urlFormatter(inputUrl, theme, mode);
+      testFunction(
+        url,
+        setupTest(url, page => page.assertNoAxeViolations())
+      );
+    });
   });
 }
