3a65586c80ba9eb69cf9c92b2bdc46430cc6d104
Connor Lanigan <connlani@amazon.com>
2023-07-05 15:03:37 +0000

    Remove app layout from dev page

--- pages/form/with-validation.page.tsx
+++ pages/form/with-validation.page.tsx
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 import React, { useState } from 'react';
 import {
-  AppLayout,
   BreadcrumbGroup,
   Button,
   Checkbox,
@@ -47,66 +46,59 @@ export default function FormScenario() {
   };
 
   return (
-    <AppLayout
-      toolsHide={true}
-      navigationHide={true}
-      breadcrumbs={
-        <BreadcrumbGroup
-          items={[
-            { text: 'Breadcrumbs', href: '#' },
-            { text: 'Create a resource', href: '#' },
-          ]}
-        />
-      }
-      contentType="form"
-      content={
-        finished ? (
-          <Container>Finished</Container>
-        ) : (
-          <Form
-            header={<Header variant="h1">Form</Header>}
-            actions={
-              <SpaceBetween direction="horizontal" size="xs">
-                <Button variant="link" disabled={loading}>
-                  Cancel
-                </Button>
-                <Button variant="primary" onClick={onSubmit} loading={loading} loadingText="Loading">
-                  Submit
-                </Button>
-              </SpaceBetween>
-            }
-            errorText={showFormError ? 'A server-side error happened.' : undefined}
-            errorIconAriaLabel="Error"
-          >
-            <SpaceBetween direction="vertical" size="l">
-              <Container header={<Header variant="h2">Form section 1</Header>}>
-                <SpaceBetween direction="vertical" size="l">
-                  <FormField
-                    label="Form field"
-                    errorText={
-                      attemptedToSubmit && value.length > 4 ? 'The text must not be longer than 4 letters' : undefined
-                    }
-                    constraintText="Maximum length: 4 letters"
-                  >
-                    <Input value={value} onChange={e => setValue(e.detail.value)} disabled={loading} />
-                  </FormField>
-                </SpaceBetween>
-              </Container>
-              <Container header={<Header variant="h2">Form section 2</Header>}>
-                <FormField label="Error simulation">
-                  <Checkbox
-                    checked={simulateServerError}
-                    onChange={e => setSimulateServerError(e.detail.checked)}
-                    disabled={loading}
-                  >
-                    Show a server side error when this form is submitted
-                  </Checkbox>
-                </FormField>
-              </Container>
+    <>
+      <BreadcrumbGroup
+        items={[
+          { text: 'Breadcrumbs', href: '#' },
+          { text: 'Create a resource', href: '#' },
+        ]}
+      />
+      {finished ? (
+        <Container>Finished</Container>
+      ) : (
+        <Form
+          header={<Header variant="h1">Form</Header>}
+          actions={
+            <SpaceBetween direction="horizontal" size="xs">
+              <Button variant="link" disabled={loading}>
+                Cancel
+              </Button>
+              <Button variant="primary" onClick={onSubmit} loading={loading} loadingText="Loading">
+                Submit
+              </Button>
             </SpaceBetween>
-          </Form>
-        )
-      }
-    />
+          }
+          errorText={showFormError ? 'A server-side error happened.' : undefined}
+          errorIconAriaLabel="Error"
+        >
+          <SpaceBetween direction="vertical" size="l">
+            <Container header={<Header variant="h2">Form section 1</Header>}>
+              <SpaceBetween direction="vertical" size="l">
+                <FormField
+                  label="Form field"
+                  errorText={
+                    attemptedToSubmit && value.length > 4 ? 'The text must not be longer than 4 letters' : undefined
+                  }
+                  constraintText="Maximum length: 4 letters"
+                >
+                  <Input value={value} onChange={e => setValue(e.detail.value)} disabled={loading} />
+                </FormField>
+              </SpaceBetween>
+            </Container>
+            <Container header={<Header variant="h2">Form section 2</Header>}>
+              <FormField label="Error simulation">
+                <Checkbox
+                  checked={simulateServerError}
+                  onChange={e => setSimulateServerError(e.detail.checked)}
+                  disabled={loading}
+                >
+                  Show a server side error when this form is submitted
+                </Checkbox>
+              </FormField>
+            </Container>
+          </SpaceBetween>
+        </Form>
+      )}
+    </>
   );
 }
