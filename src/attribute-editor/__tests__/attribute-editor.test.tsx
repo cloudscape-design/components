3660271620dc3a14eb54bc8274c3c619e81f6288
Avinash Dwarapu <dwaraa@amazon.com>
2023-07-13 18:35:55 +0200

    feat: Move i18nStrings.removeButtonAriaLabel in attribute editor to top level (#1315)

--- src/attribute-editor/__tests__/attribute-editor.test.tsx
+++ src/attribute-editor/__tests__/attribute-editor.test.tsx
@@ -207,6 +207,23 @@ describe('Attribute Editor', () => {
       });
     });
 
+    test('uses `removeButtonAriaLabel` over `i18nStrings.removeButtonAriaLabel`', () => {
+      const removeButtonAriaLabel = (item: Item) => `Remove ${item.key}`;
+      const wrapper = renderAttributeEditor({
+        ...defaultProps,
+        i18nStrings: { ...defaultProps.i18nStrings, removeButtonAriaLabel: () => `Don't render` },
+        removeButtonAriaLabel,
+      });
+      defaultProps.items!.forEach((item, index) => {
+        expect(
+          wrapper
+            .findRow(index + 1)!
+            .findRemoveButton()!
+            .getElement()
+        ).toHaveAccessibleName(`Remove ${item.key}`);
+      });
+    });
+
     test('conditionally renders `remove` button depending on isItemRemovable return value', () => {
       const isItemRemovable = (item: Item) => item.key !== 'k1';
       const wrapper = renderAttributeEditor({ ...defaultProps, isItemRemovable });
