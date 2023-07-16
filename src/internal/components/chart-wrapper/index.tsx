4246dc69871d83b32409efa1ce431a61aa3b2d50
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-07-16 11:03:48 +0200

    feat: Add fit-height property to charts (#1273)

--- src/internal/components/chart-wrapper/index.tsx
+++ src/internal/components/chart-wrapper/index.tsx
@@ -10,6 +10,7 @@ import InternalBox from '../../../box/internal.js';
 import InternalSpaceBetween from '../../../space-between/internal.js';
 
 interface ChartWrapperProps extends BaseComponentProps {
+  fitHeight: boolean;
   defaultFilter: React.ReactNode;
   additionalFilters: React.ReactNode;
   reserveFilterSpace: boolean;
@@ -35,38 +36,49 @@ export const ChartWrapper = forwardRef(
       onBlur,
       contentClassName,
       contentMinHeight,
+      fitHeight,
       ...props
     }: ChartWrapperProps,
     ref: React.Ref<HTMLDivElement>
   ) => {
     const baseProps = getBaseProps(props);
     return (
-      <div ref={ref} {...baseProps} className={clsx(baseProps.className, styles.wrapper)} onBlur={onBlur}>
-        {(defaultFilter || additionalFilters) && (
-          <InternalBox className={styles['filter-container']} margin={{ bottom: 'l' }}>
-            <InternalSpaceBetween
-              size="l"
-              direction="horizontal"
-              className={clsx({ [styles['has-default-filter']]: !!defaultFilter })}
-            >
-              {defaultFilter}
-              {additionalFilters}
-            </InternalSpaceBetween>
-          </InternalBox>
-        )}
+      <div
+        ref={ref}
+        {...baseProps}
+        className={clsx(baseProps.className, styles.wrapper, fitHeight && styles['wrapper--fit-height'])}
+        onBlur={onBlur}
+      >
+        <div className={clsx(styles['inner-wrapper'], fitHeight && styles['inner-wrapper--fit-height'])}>
+          {(defaultFilter || additionalFilters) && (
+            <InternalBox className={styles['filter-container']} margin={{ bottom: 'l' }}>
+              <InternalSpaceBetween
+                size="l"
+                direction="horizontal"
+                className={clsx({ [styles['has-default-filter']]: !!defaultFilter })}
+              >
+                {defaultFilter}
+                {additionalFilters}
+              </InternalSpaceBetween>
+            </InternalBox>
+          )}
 
-        <div
-          className={clsx(styles.content, contentClassName, {
-            [styles['content--reserve-filter']]: reserveFilterSpace,
-            [styles['content--reserve-legend']]: reserveLegendSpace,
-          })}
-          style={{ minHeight: contentMinHeight }}
-        >
-          {chartStatus}
-          {chart}
+          <div
+            className={clsx(styles.content, contentClassName, {
+              [styles['content--reserve-filter']]: reserveFilterSpace,
+              [styles['content--reserve-legend']]: reserveLegendSpace,
+              [styles['content--fit-height']]: fitHeight,
+            })}
+            style={{
+              minHeight: !fitHeight ? contentMinHeight : undefined,
+            }}
+          >
+            {chartStatus}
+            {chart}
+          </div>
+
+          {legend && <InternalBox margin={{ top: 'm' }}>{legend}</InternalBox>}
         </div>
-
-        {legend && <InternalBox margin={{ top: 'm' }}>{legend}</InternalBox>}
       </div>
     );
   }
