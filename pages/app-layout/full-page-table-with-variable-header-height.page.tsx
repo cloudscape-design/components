0c987e7865594899f6bd7d475b6d669d6336bcb6
Joan Perals Tresserra <jotresse@amazon.com>
2023-07-19 17:53:47 +0200

    Add test page

---
+++ pages/app-layout/full-page-table-with-variable-header-height.page.tsx
@@ -0,0 +1,34 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React from 'react';
+import AppLayout from '~components/app-layout';
+import labels from './utils/labels';
+import Table from '~components/table';
+import { generateItems, Instance } from '../table/generate-data';
+import { columnsConfig } from '../table/shared-configs';
+import ExpandableSection from '~components/expandable-section';
+
+const items = generateItems(20);
+
+export default function () {
+  return (
+    <AppLayout
+      ariaLabels={labels}
+      contentType="table"
+      navigationHide={true}
+      content={
+        <Table<Instance>
+          header={
+            <ExpandableSection headerText="Click to expand header area">
+              <div style={{ height: '300px' }}>Content</div>
+            </ExpandableSection>
+          }
+          stickyHeader={true}
+          variant="full-page"
+          columnDefinitions={columnsConfig}
+          items={items}
+        />
+      }
+    />
+  );
+}
