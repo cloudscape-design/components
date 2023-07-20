1afeb061d1ecf72641d74796bc9a7479af493d9d
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-07-20 11:32:06 +0200

    refactor: Use visual-mode utils from the toolkit

--- src/internal/hooks/use-visual-mode/index.ts
+++ src/internal/hooks/use-visual-mode/index.ts
@@ -1,39 +1,9 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React, { useState } from 'react';
+
 import { ALWAYS_VISUAL_REFRESH } from '../../environment';
-import { isMotionDisabled } from '../../motion';
-import { findUpUntil } from '../../utils/dom';
-import { useMutationObserver } from '../use-mutation-observer';
 import { isDevelopment } from '../../is-development';
 import { warnOnce } from '@cloudscape-design/component-toolkit/internal';
-
-// Note that this hook doesn't take into consideration @media print (unlike the dark mode CSS),
-// due to challenges with cross-browser implementations of media/print state change listeners.
-// This means that components using this hook will render in dark mode even when printing.
-export function useCurrentMode(elementRef: React.RefObject<HTMLElement>) {
-  const [value, setValue] = useState<'light' | 'dark'>('light');
-  useMutationObserver(elementRef, node => {
-    const darkModeParent = findUpUntil(
-      node,
-      node => node.classList.contains('awsui-polaris-dark-mode') || node.classList.contains('awsui-dark-mode')
-    );
-    setValue(darkModeParent ? 'dark' : 'light');
-  });
-  return value;
-}
-
-export function useDensityMode(elementRef: React.RefObject<HTMLElement>) {
-  const [value, setValue] = useState<'comfortable' | 'compact'>('comfortable');
-  useMutationObserver(elementRef, node => {
-    const compactModeParent = findUpUntil(
-      node,
-      node => node.classList.contains('awsui-polaris-compact-mode') || node.classList.contains('awsui-compact-mode')
-    );
-    setValue(compactModeParent ? 'compact' : 'comfortable');
-  });
-  return value;
-}
 
 export const useVisualRefresh = ALWAYS_VISUAL_REFRESH ? () => true : useVisualRefreshDynamic;
 
@@ -64,12 +34,4 @@ export function useVisualRefreshDynamic() {
     }
   }
   return visualRefreshState;
-}
-
-export function useReducedMotion(elementRef: React.RefObject<HTMLElement>) {
-  const [value, setValue] = useState(false);
-  useMutationObserver(elementRef, node => {
-    setValue(isMotionDisabled(node));
-  });
-  return value;
 }
