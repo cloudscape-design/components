908294e2e10d7f122203db41853940b79d65ee3c
Joan Perals Tresserra <jotresse@amazon.com>
2023-07-24 08:59:34 +0200

    Improve tests

--- src/app-layout/__tests__/dynamic-overlap.test.tsx
+++ src/app-layout/__tests__/dynamic-overlap.test.tsx
@@ -18,29 +18,31 @@ jest.mock('../../../lib/components/internal/hooks/container-queries/utils', () =
   convertResizeObserverEntry: () => ({ contentBoxHeight: positiveHeight ? 800 : 0 }),
 }));
 
-describe('Dynamic overlap context', () => {
-  function renderApp(appLayoutProps?: AppLayoutProps) {
-    function InternalComponentWithOverlap() {
-      const ref = useDynamicOverlap();
-      const { isDynamicOverlapSet, isDynamicOverlapDisabled } = useAppLayoutInternals();
-      return (
-        <>
-          <div ref={ref} />
-          <div data-testid="is-dynamic-overlap-set">{isDynamicOverlapSet.toString()}</div>
-          <div data-testid="is-dynamic-overlap-disabled">{isDynamicOverlapDisabled.toString()}</div>
-        </>
-      );
-    }
-
-    function App(props: { children: React.ReactNode }) {
-      return <AppLayout {...appLayoutProps} content={props.children} />;
-    }
-
-    render(
-      <App>
-        <InternalComponentWithOverlap />
-      </App>
+describe('Dynamic overlap', () => {
+  function ComponentWithDynamicOverlap() {
+    const ref = useDynamicOverlap();
+    const { isDynamicOverlapSet, isDynamicOverlapDisabled } = useAppLayoutInternals();
+    return (
+      <>
+        <div ref={ref} />
+        <div data-testid="is-dynamic-overlap-set">{isDynamicOverlapSet.toString()}</div>
+        <div data-testid="is-dynamic-overlap-disabled">{isDynamicOverlapDisabled.toString()}</div>
+      </>
     );
+  }
+
+  function ComponentWithoutDynamicOverlap() {
+    const { isDynamicOverlapSet, isDynamicOverlapDisabled } = useAppLayoutInternals();
+    return (
+      <>
+        <div data-testid="is-dynamic-overlap-set">{isDynamicOverlapSet.toString()}</div>
+        <div data-testid="is-dynamic-overlap-disabled">{isDynamicOverlapDisabled.toString()}</div>
+      </>
+    );
+  }
+
+  function renderApp(appLayoutProps?: AppLayoutProps) {
+    render(<AppLayout {...appLayoutProps} />);
     return {
       isDynamicOverlapSet: () => screen.getByTestId('is-dynamic-overlap-set').textContent,
       isDynamicOverlapDisabled: () => screen.getByTestId('is-dynamic-overlap-disabled').textContent,
@@ -52,23 +54,41 @@ describe('Dynamic overlap context', () => {
   });
 
   test('sets dynamic overlap when content header is present', () => {
-    const { isDynamicOverlapSet, isDynamicOverlapDisabled } = renderApp({ contentHeader: 'Content header' });
+    const { isDynamicOverlapSet, isDynamicOverlapDisabled } = renderApp({
+      content: <ComponentWithDynamicOverlap />,
+      contentHeader: 'Content header',
+    });
     expect(isDynamicOverlapSet()).toBe('true');
     expect(isDynamicOverlapDisabled()).toBe('false');
   });
+
   test('sets dynamic overlap when height is higher than 0', () => {
-    const { isDynamicOverlapSet, isDynamicOverlapDisabled } = renderApp();
+    const { isDynamicOverlapSet, isDynamicOverlapDisabled } = renderApp({ content: <ComponentWithDynamicOverlap /> });
     expect(isDynamicOverlapSet()).toBe('true');
     expect(isDynamicOverlapDisabled()).toBe('false');
   });
+
   test('does not set dynamic overlap when no content header is present and height is 0', () => {
     positiveHeight = false;
-    const { isDynamicOverlapSet, isDynamicOverlapDisabled } = renderApp();
+    const { isDynamicOverlapSet, isDynamicOverlapDisabled } = renderApp({ content: <ComponentWithDynamicOverlap /> });
     expect(isDynamicOverlapSet()).toBe('false');
     expect(isDynamicOverlapDisabled()).toBe('true');
   });
+
+  test('does not set dynamic overlap when the useDynamicOverlap hook is not used', () => {
+    const { isDynamicOverlapSet, isDynamicOverlapDisabled } = renderApp({
+      content: <ComponentWithoutDynamicOverlap />,
+      disableContentHeaderOverlap: true,
+    });
+    expect(isDynamicOverlapSet()).toBe('false');
+    expect(isDynamicOverlapDisabled()).toBe('true');
+  });
+
   test('disables dynamic overlap when explicitly specified in the app layout props', () => {
-    const { isDynamicOverlapDisabled } = renderApp({ disableContentHeaderOverlap: true });
+    const { isDynamicOverlapDisabled } = renderApp({
+      content: <ComponentWithDynamicOverlap />,
+      disableContentHeaderOverlap: true,
+    });
     expect(isDynamicOverlapDisabled()).toBe('true');
   });
 });
