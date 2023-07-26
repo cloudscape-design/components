ab5088a6b0297673dd0064fce9f2b6fa89c9b004
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-07-26 14:22:58 +0200

    remove debug code

--- src/table/__integ__/resizable-columns-misc.test.ts
+++ src/table/__integ__/resizable-columns-misc.test.ts
@@ -79,7 +79,7 @@ test(
   })
 );
 
-test.each([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])(
+test(
   'should not have oscillating size when the last column has the minimal width',
   useBrowser(async browser => {
     const page = new ResizableColumnsPage(browser);
