65b9b05cb0376dfd94ae03f287d55bdcbb03491b
Timo <gasda@amazon.com>
2023-08-11 16:33:09 +0200

    page updates

--- pages/alert/simple.page.tsx
+++ pages/alert/simple.page.tsx
@@ -46,7 +46,7 @@ export default function AlertScenario() {
                 <Link href="#">This is a secondary link</Link>
               </Alert>
             </div>
-            <Alert header="Info" statusIconAriaLabel="Info" action={<Button>This is an action</Button>}>
+            <Alert header="Info" statusIconAriaLabel="Info">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
               ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
