24b344da157ff38d366f3e3dba5bd79df75ab4fd
Amr Mohamed <taheramr@amazon.de>
2023-09-04 04:52:07 +0200

    chore: Trying runInBand

--- build-tools/tasks/a11y.js
+++ build-tools/tasks/a11y.js
@@ -15,11 +15,13 @@ module.exports = task('test:a11y', async () => {
   await waitOn({ resources: ['http://localhost:8080'] });
 
   const files = glob.sync('src/**/__a11y__/**/*.test.ts');
-  console.log(shard, 'shard');
-  console.log(files, 'files');
-  await execa('jest', ['-c', 'jest.integ.config.js', ...files, `--shard=${shard}`, '--passWithNoTests'], {
-    stdio: 'inherit',
-  });
+  await execa(
+    'jest',
+    ['-c', 'jest.integ.config.js', ...files, `--shard=${shard}`, '--passWithNoTests', '--runInBand'],
+    {
+      stdio: 'inherit',
+    }
+  );
 
   devServer.cancel();
 });
