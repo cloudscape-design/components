a27a1915c5c3b8672c22f455989482ff25b7c6a7
Ruben Carvalho <rucarva@amazon.com>
2023-09-06 16:23:03 +0000

    chore: Revert unrelated changes

--- pages/alert/simple.page.tsx
+++ pages/alert/simple.page.tsx
@@ -51,8 +51,7 @@ export default function AlertScenario() {
               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
               ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
               fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-              mollitmollitmollitmollitmollitmollitmollitmollitmollitmollitmollitmollitmollitmollitmollitmollitmollitmollitmollit
-              anim id est laborum.
+              mollit anim id est laborum.
             </Alert>
           </SpaceBetween>
         </ScreenshotArea>
