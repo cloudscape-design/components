19a3c808f612332b6acc40a7a3bbefc1b330368f
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-09-25 14:00:46 +0200

    chore: Make property filter operator-tolerant (#1570)

--- src/property-filter/__tests__/property-filter.test.tsx
+++ src/property-filter/__tests__/property-filter.test.tsx
@@ -1334,28 +1334,28 @@ describe('property filter parts', () => {
 });
 
 describe('i18n', () => {
-  it('uses dropdown labels from i18n provider', () => {
+  const providerMessages = {
+    'property-filter': {
+      'i18nStrings.allPropertiesLabel': 'Custom All properties',
+      'i18nStrings.groupPropertiesText': 'Custom Properties',
+      'i18nStrings.groupValuesText': 'Custom Values',
+      'i18nStrings.operatorContainsText': 'Custom Contains',
+      'i18nStrings.operatorDoesNotContainText': 'Custom Does not contain',
+      'i18nStrings.operatorDoesNotEqualText': 'Custom Does not equal',
+      'i18nStrings.operatorEqualsText': 'Custom Equals',
+      'i18nStrings.operatorGreaterOrEqualText': 'Custom Greater than or equal',
+      'i18nStrings.operatorGreaterText': 'Custom Greater than',
+      'i18nStrings.operatorLessOrEqualText': 'Custom Less than or equal',
+      'i18nStrings.operatorLessText': 'Custom Less than',
+      'i18nStrings.operatorText': 'Custom Operator',
+      'i18nStrings.operatorsText': 'Custom Operators',
+      'i18nStrings.propertyText': 'Custom Property',
+    },
+  };
+
+  it('uses dropdown labels from i18n provider for a string property', () => {
     const { container } = render(
-      <TestI18nProvider
-        messages={{
-          'property-filter': {
-            'i18nStrings.allPropertiesLabel': 'Custom All properties',
-            'i18nStrings.groupPropertiesText': 'Custom Properties',
-            'i18nStrings.groupValuesText': 'Custom Values',
-            'i18nStrings.operatorContainsText': 'Custom Contains',
-            'i18nStrings.operatorDoesNotContainText': 'Custom Does not contain',
-            'i18nStrings.operatorDoesNotEqualText': 'Custom Does not equal',
-            'i18nStrings.operatorEqualsText': 'Custom Equals',
-            'i18nStrings.operatorGreaterOrEqualText': 'Custom Greater than or equal',
-            'i18nStrings.operatorGreaterText': 'Custom Greater than',
-            'i18nStrings.operatorLessOrEqualText': 'Custom Less than or equal',
-            'i18nStrings.operatorLessText': 'Custom Less than',
-            'i18nStrings.operatorText': 'Custom Operator',
-            'i18nStrings.operatorsText': 'Custom Operators',
-            'i18nStrings.propertyText': 'Custom Property',
-          },
-        }}
-      >
+      <TestI18nProvider messages={providerMessages}>
         <PropertyFilter
           {...defaultProps}
           i18nStrings={{ filteringAriaLabel: 'your choice', filteringPlaceholder: 'Search' }}
@@ -1373,6 +1373,35 @@ describe('i18n', () => {
     expect(
       dropdown.findOptions().map(optionWrapper => optionWrapper.findDescription()?.getElement().textContent)
     ).toEqual(['Custom Equals', 'Custom Does not equal', 'Custom Contains', 'Custom Does not contain']);
+  });
+
+  it('uses dropdown labels from i18n provider for a numeric property', () => {
+    const { container } = render(
+      <TestI18nProvider messages={providerMessages}>
+        <PropertyFilter
+          {...defaultProps}
+          i18nStrings={{ filteringAriaLabel: 'your choice', filteringPlaceholder: 'Search' }}
+        />
+      </TestI18nProvider>
+    );
+    const wrapper = createWrapper(container).findPropertyFilter()!;
+
+    wrapper.focus();
+    const dropdown = wrapper.findDropdown()!;
+    expect(dropdown.find('li')!.getElement()).toHaveTextContent('Custom Properties');
+
+    wrapper.selectSuggestion(6);
+    expect(dropdown.find('li:nth-child(2)')!.getElement()).toHaveTextContent('Custom Operators');
+    expect(
+      dropdown.findOptions().map(optionWrapper => optionWrapper.findDescription()?.getElement().textContent)
+    ).toEqual([
+      'Custom Equals',
+      'Custom Does not equal',
+      'Custom Greater than or equal',
+      'Custom Less than or equal',
+      'Custom Less than',
+      'Custom Greater than',
+    ]);
   });
 
   it('uses token and editor labels from i18n provider', () => {
@@ -1394,8 +1423,16 @@ describe('i18n', () => {
             'i18nStrings.tokenLimitShowFewer': 'Custom Show fewer',
             'i18nStrings.tokenLimitShowMore': 'Custom Show more',
             'i18nStrings.valueText': 'Custom Value',
-            'i18nStrings.removeTokenButtonAriaLabel':
-              '{token__operator, select, equals {Remove filter, {token__propertyKey} Custom equals {token__value}} not_equals {Remove filter, {token__propertyKey} Custom does not equal {token__value}} other {}}',
+            'i18nStrings.removeTokenButtonAriaLabel': `{token__operator, select, 
+              equals {Remove filter, {token__propertyKey} Custom equals {token__value}}
+              not_equals {Remove filter, {token__propertyKey} Custom does not equal {token__value}}
+              greater_than {Remove filter, {token__propertyKey} Custom greater than {token__value}}
+              greater_than_equal {Remove filter, {token__propertyKey} Custom greater than or equals {token__value}}
+              less_than {Remove filter, {token__propertyKey} Custom less than {token__value}}
+              less_than_equal {Remove filter, {token__propertyKey} Custom less than or equals {token__value}}
+              contains {Remove filter, {token__propertyKey} Custom contains {token__value}}
+              not_contains {Remove filter, {token__propertyKey} Custom does not contain {token__value}}
+              other {}}`,
           },
         }}
       >
@@ -1407,6 +1444,12 @@ describe('i18n', () => {
             tokens: [
               { propertyKey: 'string', operator: '=', value: 'value1' },
               { propertyKey: 'string', operator: '!=', value: 'value2' },
+              { propertyKey: 'string', operator: ':', value: 'value3' },
+              { propertyKey: 'string', operator: '!:', value: 'value4' },
+              { propertyKey: 'range', operator: '>', value: '1' },
+              { propertyKey: 'range', operator: '<', value: '2' },
+              { propertyKey: 'range', operator: '>=', value: '3' },
+              { propertyKey: 'range', operator: '<=', value: '4' },
             ],
           }}
           i18nStrings={{ filteringAriaLabel: 'your choice', filteringPlaceholder: 'Search' }}
@@ -1414,15 +1457,20 @@ describe('i18n', () => {
       </TestI18nProvider>
     );
     const wrapper = createWrapper(container).findPropertyFilter()!;
-    console.log(wrapper.getElement().innerHTML);
     expect(wrapper.findRemoveAllButton()!.getElement()).toHaveTextContent('Custom Clear filters');
     expect(wrapper.findTokenToggle()!.getElement()).toHaveTextContent('Custom Show more');
     wrapper.findTokenToggle()!.click();
     expect(wrapper.findTokenToggle()!.getElement()).toHaveTextContent('Custom Show fewer');
 
-    expect(wrapper.findTokens()[0].findRemoveButton().getElement()).toHaveAccessibleName(
-      'Remove filter, string Custom equals value1'
-    );
+    const getRemoveButton = (index: number) => wrapper.findTokens()[index].findRemoveButton().getElement();
+    expect(getRemoveButton(0)).toHaveAccessibleName('Remove filter, string Custom equals value1');
+    expect(getRemoveButton(1)).toHaveAccessibleName('Remove filter, string Custom does not equal value2');
+    expect(getRemoveButton(2)).toHaveAccessibleName('Remove filter, string Custom contains value3');
+    expect(getRemoveButton(3)).toHaveAccessibleName('Remove filter, string Custom does not contain value4');
+    expect(getRemoveButton(4)).toHaveAccessibleName('Remove filter, range Custom greater than 1');
+    expect(getRemoveButton(5)).toHaveAccessibleName('Remove filter, range Custom less than 2');
+    expect(getRemoveButton(6)).toHaveAccessibleName('Remove filter, range Custom greater than or equals 3');
+    expect(getRemoveButton(7)).toHaveAccessibleName('Remove filter, range Custom less than or equals 4');
 
     const tokenOperation = wrapper.findTokens()[1].findTokenOperation()!;
     tokenOperation.openDropdown();
