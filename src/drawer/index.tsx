950b00ca0524a24db53f6ace46f65765f10de911
Katie George <katiegeo@amazon.com>
2023-10-31 15:42:14 -0700

    feat: Moves drawer component out of internal

--- src/drawer/index.tsx
+++ src/drawer/index.tsx
@@ -2,14 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 import clsx from 'clsx';
 import React from 'react';
-import { getBaseProps } from '../../base-component';
-import InternalStatusIndicator from '../../../status-indicator/internal';
+import { getBaseProps } from '../internal/base-component';
+import InternalStatusIndicator from '../status-indicator/internal';
 import styles from './styles.css.js';
+import { applyDisplayName } from '../internal/utils/apply-display-name';
+import useBaseComponent from '../internal/hooks/use-base-component';
 import { DrawerProps } from './interfaces';
-import LiveRegion from '../../components/live-region';
-import { useInternalI18n } from '../../../i18n/context';
+import LiveRegion from '../internal/components/live-region';
+import { useInternalI18n } from '../i18n/context';
+
+export { DrawerProps };
 
 export default function Drawer({ header, children, loading, i18nStrings, ...restProps }: DrawerProps) {
+  const { __internalRootRef } = useBaseComponent('HelpPanel');
   const baseProps = getBaseProps(restProps);
   const i18n = useInternalI18n('drawer');
   const containerProps = {
@@ -17,15 +22,17 @@ export default function Drawer({ header, children, loading, i18nStrings, ...rest
     className: clsx(baseProps.className, styles.drawer),
   };
   return loading ? (
-    <div {...containerProps}>
+    <div {...containerProps} ref={__internalRootRef}>
       <InternalStatusIndicator type="loading">
         <LiveRegion visible={true}>{i18n('i18nStrings.loadingText', i18nStrings?.loadingText)}</LiveRegion>
       </InternalStatusIndicator>
     </div>
   ) : (
-    <div {...containerProps}>
+    <div {...containerProps} ref={__internalRootRef}>
       {header && <div className={clsx(styles.header)}>{header}</div>}
       <div className={clsx(styles['test-utils-drawer-content'])}>{children}</div>
     </div>
   );
 }
+
+applyDisplayName(Drawer, 'Drawer');
