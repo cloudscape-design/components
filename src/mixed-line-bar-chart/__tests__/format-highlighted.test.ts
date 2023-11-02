4ccef9135cb3119fefb25e2cc95b0cd932786ed1
Joan Perals Tresserra <jotresse@amazon.com>
2023-11-02 15:23:28 +0100

    Fix test to match new interface

--- src/mixed-line-bar-chart/__tests__/format-highlighted.test.ts
+++ src/mixed-line-bar-chart/__tests__/format-highlighted.test.ts
@@ -11,31 +11,34 @@ function createInternalSeries<T>(externalSeries: readonly MixedLineBarChartProps
 
 describe('formatHighlighted', () => {
   it('formats position values', () => {
-    const formatted = formatHighlighted(1, []);
+    const formatted = formatHighlighted({ position: 1, series: [] });
 
     expect(formatted.position).toBe('1');
   });
 
   it('formats threshold series details', () => {
-    const formatted = formatHighlighted(1, createInternalSeries([thresholdSeries]));
+    const formatted = formatHighlighted({ position: 1, series: createInternalSeries([thresholdSeries]) });
 
     expect(formatted.details).toEqual([{ key: 'Threshold 1', value: 8, markerType: 'dashed', color: '' }]);
   });
 
   it('formats line series details', () => {
-    const formatted = formatHighlighted(1, createInternalSeries([lineSeries1]));
+    const formatted = formatHighlighted({ position: 1, series: createInternalSeries([lineSeries1]) });
 
     expect(formatted.details).toEqual([{ key: 'Line Series 1', value: 8, markerType: 'line', color: '' }]);
   });
 
   it('formats bar series details', () => {
-    const formatted = formatHighlighted('Apples', createInternalSeries([barSeries]));
+    const formatted = formatHighlighted({ position: 'Apples', series: createInternalSeries([barSeries]) });
 
     expect(formatted.details).toEqual([{ key: 'Bar Series 1', value: 52, markerType: 'rectangle', color: '' }]);
   });
 
   it('formats mixed series details', () => {
-    const formatted = formatHighlighted('Potatoes', createInternalSeries([lineSeries3, barSeries, thresholdSeries]));
+    const formatted = formatHighlighted({
+      position: 'Potatoes',
+      series: createInternalSeries([lineSeries3, barSeries, thresholdSeries]),
+    });
 
     expect(formatted.details).toEqual([
       { key: 'Line Series 3', value: 7, markerType: 'line', color: '' },
@@ -45,7 +48,10 @@ describe('formatHighlighted', () => {
   });
 
   it('does not include missing y values', () => {
-    const formatted = formatHighlighted('Apples', createInternalSeries([barSeries, barSeries2, thresholdSeries]));
+    const formatted = formatHighlighted({
+      position: 'Apples',
+      series: createInternalSeries([barSeries, barSeries2, thresholdSeries]),
+    });
 
     expect(formatted.details).toEqual([
       { key: 'Bar Series 1', value: 52, markerType: 'rectangle', color: '' },
@@ -54,19 +60,19 @@ describe('formatHighlighted', () => {
   });
 
   it('uses xTickFormatter when available', () => {
-    const formatted = formatHighlighted(1, [], value => 60 * value + 's');
+    const formatted = formatHighlighted({ position: 1, series: [], xTickFormatter: value => 60 * value + 's' });
 
     expect(formatted.position).toBe('60s');
   });
 
   it('uses valueFormatter when available', () => {
-    const formatted = formatHighlighted(
-      'Potatoes',
-      createInternalSeries([
+    const formatted = formatHighlighted({
+      position: 'Potatoes',
+      series: createInternalSeries([
         { ...barSeries, valueFormatter: (y: number, x: string) => x.toUpperCase() + y },
         { ...thresholdSeries, valueFormatter: (y: number) => y.toFixed(2) },
-      ])
-    );
+      ]),
+    });
     expect(formatted.details).toEqual([
       { key: 'Bar Series 1', value: 'POTATOES77', markerType: 'rectangle', color: '' },
       { key: 'Threshold 1', value: '8.00', markerType: 'dashed', color: '' },
@@ -74,7 +80,10 @@ describe('formatHighlighted', () => {
   });
 
   it('includes x-thresholds if x matched', () => {
-    const formatted = formatHighlighted(3, createInternalSeries([lineSeries1, xThresholdSeries1]));
+    const formatted = formatHighlighted({
+      position: 3,
+      series: createInternalSeries([lineSeries1, xThresholdSeries1]),
+    });
     expect(formatted.details).toEqual([
       { key: 'Line Series 1', value: 10, markerType: 'line', color: '' },
       { key: 'X-Threshold 1', value: '', markerType: 'dashed', color: '' },
