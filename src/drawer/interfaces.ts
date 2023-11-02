f8b4082ba94ac2f67ee5e03d9bd391c77b0e6fdc
Katie George <katiegeorge94@gmail.com>
2023-11-02 10:24:33 -0700

    feat: Moves drawer component out of internal (#1708)
    Co-authored-by: Katie George <katiegeo@amazon.com>

--- src/drawer/interfaces.ts
+++ src/drawer/interfaces.ts
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { BaseComponentProps } from '../../base-component';
+import { BaseComponentProps } from '../internal/base-component';
 import React from 'react';
 
 export interface DrawerProps extends BaseComponentProps {
