2f8e3529b8dab57c6de006cb6c7b6b404370c855
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-11-16 15:35:17 +0100

    fix unit tests

--- src/table/__tests__/empty-state.test.tsx
+++ src/table/__tests__/empty-state.test.tsx
@@ -1,17 +1,18 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import Table, { TableProps } from '../../../lib/components/table';
 import createWrapper, { TableWrapper } from '../../../lib/components/test-utils/dom';
 import { supportsStickyPosition } from '../../../lib/components/internal/utils/dom';
-import { useContainerQuery } from '@cloudscape-design/component-toolkit';
+import { useResizeObserver } from '@cloudscape-design/component-toolkit/internal';
+import { ContainerQueryEntry } from '@cloudscape-design/component-toolkit';
 import styles from '../../../lib/components/table/styles.css.js';
 import { useStickyColumns, useStickyCellStyles } from '../../../lib/components/table/sticky-columns';
 
-jest.mock('@cloudscape-design/component-toolkit', () => ({
-  ...jest.requireActual('@cloudscape-design/component-toolkit'),
-  useContainerQuery: jest.fn(() => [600, () => {}]),
+jest.mock('@cloudscape-design/component-toolkit/internal', () => ({
+  ...jest.requireActual('@cloudscape-design/component-toolkit/internal'),
+  useResizeObserver: jest.fn().mockImplementation((_target, cb) => cb({ contentBoxWidth: 0 })),
 }));
 
 jest.mock('../../../lib/components/internal/utils/dom', () => ({
@@ -61,19 +62,29 @@ function findMergedCell(wrapper: TableWrapper) {
   return wrapper.findByClassName(styles['cell-merged']);
 }
 
+function mockResizeObserver(contentBoxWidth: number) {
+  // Use "act" to imitate firing observer's callback after component is rendered.
+  // In actual code the callback is fired twice - before and after rendering but the second part depends on ResizeObserver polyfill.
+  jest
+    .mocked(useResizeObserver)
+    .mockImplementation((_target, cb) => act(() => cb({ contentBoxWidth } as unknown as ContainerQueryEntry)));
+}
+
 test('should apply width to the empty state container', () => {
+  mockResizeObserver(600);
   const { wrapper } = renderTable(<Table columnDefinitions={defaultColumns} items={[]} />);
   expect(findStickyParent(wrapper).style.width).toEqual('600px');
 });
 
 test('should floor the value to prevent unwanted horizontal scrolling', () => {
-  (useContainerQuery as jest.Mock).mockReturnValue([123.4, () => {}]);
+  mockResizeObserver(123.4);
   const { wrapper } = renderTable(<Table columnDefinitions={defaultColumns} items={[]} />);
   expect(findStickyParent(wrapper).style.width).toEqual('123px');
 });
 
 test('should not apply width when browser does not support position sticky', () => {
-  (supportsStickyPosition as jest.Mock).mockReturnValue(false);
+  mockResizeObserver(600);
+  jest.mocked(supportsStickyPosition).mockReturnValue(false);
   const { wrapper } = renderTable(<Table columnDefinitions={defaultColumns} items={[]} />);
   expect(findStickyParent(wrapper).style.width).toEqual('');
 });
