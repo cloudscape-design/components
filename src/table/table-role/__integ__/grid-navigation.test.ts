8d8f443a75a3c8e20fa7c922ccd4aa517d13958e
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-11-23 17:12:38 +0100

    more integ tests

--- src/table/table-role/__integ__/grid-navigation.test.ts
+++ src/table/table-role/__integ__/grid-navigation.test.ts
@@ -3,10 +3,16 @@
 
 import { BasePageObject } from '@cloudscape-design/browser-test-tools/page-objects';
 import useBrowser from '@cloudscape-design/browser-test-tools/use-browser';
+import createWrapper from '../../../../lib/components/test-utils/selectors';
+
+interface ExtendedWindow extends Window {
+  refreshItems: () => void;
+}
+declare const window: ExtendedWindow;
 
 test(
   'cell action remains focused when row re-renders',
-  useBrowser({ width: 1800, height: 800 }, async browser => {
+  useBrowser({}, async browser => {
     const page = new BasePageObject(browser);
     await browser.url('#/light/table-fragments/grid-navigation-custom/?actionsMode=inline');
     await page.waitForVisible('table');
@@ -18,7 +24,21 @@ test(
 
 test(
   'cell focus stays in the same position when row gets removed',
-  useBrowser({ width: 1800, height: 800 }, async browser => {
+  useBrowser({}, async browser => {
+    const page = new BasePageObject(browser);
+    await browser.url('#/light/table-fragments/grid-navigation-custom');
+    await page.waitForVisible('table');
+
+    const firstButtonDropdown = createWrapper().findButtonDropdown();
+    await page.click(firstButtonDropdown.toSelector());
+    await page.click(firstButtonDropdown.findHighlightedItem().toSelector());
+    await expect(page.isFocused(firstButtonDropdown.findNativeButton().toSelector())).resolves.toBe(true);
+  })
+);
+
+test(
+  'cell focus stays in the same position when row gets removed with inline actions',
+  useBrowser({}, async browser => {
     const page = new BasePageObject(browser);
     await browser.url('#/light/table-fragments/grid-navigation-custom/?actionsMode=inline');
     await page.waitForVisible('table');
@@ -29,8 +49,55 @@ test(
 );
 
 test(
+  'cell focus stays in the same position when row gets removed with auto-refresh',
+  useBrowser({}, async browser => {
+    const page = new BasePageObject(browser);
+    await browser.url('#/light/table-fragments/grid-navigation-custom');
+    await page.waitForVisible('table');
+
+    const firstButtonDropdown = createWrapper().findButtonDropdown();
+    await page.click(firstButtonDropdown.toSelector());
+    await page.click(firstButtonDropdown.findHighlightedItem().toSelector());
+    await expect(page.isFocused(firstButtonDropdown.findNativeButton().toSelector())).resolves.toBe(true);
+
+    await browser.execute(() => window.refreshItems());
+    await expect(page.isFocused(firstButtonDropdown.findNativeButton().toSelector())).resolves.toBe(true);
+  })
+);
+
+test(
+  'keeps last focused cell position when row gets removed from outside table',
+  useBrowser({}, async browser => {
+    const page = new BasePageObject(browser);
+    await browser.url('#/light/table-fragments/grid-navigation-custom');
+    await page.waitForVisible('table');
+
+    const firstButtonDropdown = createWrapper().findButtonDropdown();
+    await page.click(firstButtonDropdown.toSelector());
+    await page.click('button[aria-label="Refresh"]');
+    await expect(page.isFocused('button[aria-label="Refresh"]')).resolves.toBe(true);
+
+    await page.keys(['Tab', 'Tab']);
+    await expect(page.isFocused(firstButtonDropdown.findNativeButton().toSelector())).resolves.toBe(true);
+  })
+);
+
+test(
+  'retains cell focus when existing inline edit',
+  useBrowser({}, async browser => {
+    const page = new BasePageObject(browser);
+    await browser.url('#/light/table-fragments/grid-navigation-custom');
+    await page.waitForVisible('table');
+
+    await page.click('[aria-label="Edit DNS name"]');
+    await page.click('button[aria-label="Save"]');
+    await expect(page.isFocused('[aria-label="Edit DNS name"]')).resolves.toBe(true);
+  })
+);
+
+test(
   'table has a single tab stop',
-  useBrowser({ width: 1800, height: 800 }, async browser => {
+  useBrowser({}, async browser => {
     const page = new BasePageObject(browser);
     await browser.url('#/light/table-fragments/grid-navigation-custom');
     await page.waitForVisible('table');
