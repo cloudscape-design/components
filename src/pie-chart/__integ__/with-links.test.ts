12d065db3119d22055cc4154ea1bfec81d49eb67
Joan Perals Tresserra <jotresse@amazon.com>
2023-11-24 11:20:24 +0100

    Use live announcements observer

--- src/pie-chart/__integ__/with-links.test.ts
+++ src/pie-chart/__integ__/with-links.test.ts
@@ -3,20 +3,10 @@
 
 import useBrowser from '@cloudscape-design/browser-test-tools/use-browser';
 import { BasePageObject } from '@cloudscape-design/browser-test-tools/page-objects';
-import createWrapper from '../../../lib/components/test-utils/selectors';
 
-export default class PieChartWithLinksPageObject extends BasePageObject {
-  async expectAnnouncement(announcement: string) {
-    const liveRegion = await this.browser.$(createWrapper().findPieChart().findAll('span[aria-live]').toSelector());
-    // Using getHTML because getText returns an empty string if the live region is outside the viewport.
-    // See https://webdriver.io/docs/api/element/getText/
-    return expect(liveRegion.getHTML(false)).resolves.toBe(announcement);
-  }
-}
-
-function setupTest(useLinks: string, testFn: (page: PieChartWithLinksPageObject) => Promise<void>) {
+function setupTest(useLinks: string, testFn: (page: BasePageObject) => Promise<void>) {
   return useBrowser(async browser => {
-    const page = new PieChartWithLinksPageObject(browser);
+    const page = new BasePageObject(browser);
     await browser.url(`#/light/pie-chart/with-links?useLinks=${useLinks}`);
     await testFn(page);
   });
@@ -41,9 +31,11 @@ describe('Pie chart with links in key-value pairs', () => {
     it(
       'announces detail popover content in plain text when focusing a segment',
       setupTest(useLinks, async page => {
+        await page.initLiveAnnouncementsObserver();
         await page.click('#focus-target');
-        await page.keys(['Tab', 'Enter']);
-        await page.expectAnnouncement('Popularity 50% Calories per 100g 77 kcal');
+        await page.keys(['Tab']);
+        await page.keys(['Enter']);
+        await expect(page.getLiveAnnouncements()).resolves.toContain('Popularity 50% Calories per 100g 77 kcal');
       })
     );
   });
