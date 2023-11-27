7c8c1496906031dec36a1076d338a94be7ba5ff3
Joan Perals Tresserra <jotresse@amazon.com>
2023-11-27 16:59:16 +0100

    Fine tune test

--- src/mixed-line-bar-chart/__integ__/announcements.test.ts
+++ src/mixed-line-bar-chart/__integ__/announcements.test.ts
@@ -5,8 +5,14 @@ import { setupTest } from './common';
 
 describe('Popover content is announced as plain text on hover', () => {
   describe.each(['keys', 'values'])('when using links in %s', useLinks => {
-    const alwaysVisibleTexts = ['Jun 2023,', 'Amazon Simple Storage Service $69.80,', 'Group 1 $83.31,'];
-    const collapsedText = 'AWS Config';
+    const alwaysVisibleTexts = [
+      'Jun 2023',
+      'Amazon Simple Storage Service $69.80',
+      'Amazon Relational Database Service $35.96',
+      'Group 1 $83.31',
+      'Group 2 $114.17',
+    ];
+    const group1SubItemsTexts = ['AWS Config $40.06', 'AWS Key Management Service $43.25'];
 
     test(
       'without expandable sub-items',
@@ -20,8 +26,7 @@ describe('Popover content is announced as plain text on hover', () => {
           for (const text of alwaysVisibleTexts) {
             expect(label).toContain(text);
           }
-          // Collapsed sub-items should not be announced
-          expect(label).toContain(collapsedText);
+          expect(label).toContain(group1SubItemsTexts.join(', '));
           expect(label).not.toContain('[object object]');
         });
       })
@@ -36,11 +41,11 @@ describe('Popover content is announced as plain text on hover', () => {
         await page.hoverElement(bar);
         await page.waitForAssertion(async () => {
           const label = await getLabel();
-          for (const text of alwaysVisibleTexts) {
-            expect(label).toContain(text);
-          }
+          expect(label).toBe(alwaysVisibleTexts.join(', '));
           // Collapsed sub-items should not be announced
-          expect(label).not.toContain(collapsedText);
+          for (const text of group1SubItemsTexts) {
+            expect(label).not.toContain(text);
+          }
           expect(label).not.toContain('[object object]');
         });
 
@@ -49,11 +54,11 @@ describe('Popover content is announced as plain text on hover', () => {
           wrapper.findDetailPopover().findContent().findExpandableSection().findExpandButton().toSelector()
         );
         // Pin and dismiss the poover,
-        // then hover over a different item and come back to hover the initial one
-        await page.buttonDownOnElement(bar);
+        // then hover over a different item and come back to hover the initial one.
         // We need to use `page.buttonDownOnElement` instead of `page.click` because the ancestor SVG element
         // intercepts the click and this is seen by Webdriver as an error and thrown as such, although it works for us
         // (the component manages the event accordingly as coming from the corresponding bar group).
+        await page.buttonDownOnElement(bar);
         await page.click(wrapper.findDetailPopover().findDismissButton().toSelector());
         await page.hoverElement(wrapper.findBarGroups().get(4).toSelector());
         await page.hoverElement(bar);
@@ -63,7 +68,7 @@ describe('Popover content is announced as plain text on hover', () => {
             expect(label).toContain(text);
           }
           // Previously collapsed sub-item is now expanded
-          expect(label).toContain(collapsedText);
+          expect(label).toContain(group1SubItemsTexts.join(', '));
           expect(label).not.toContain('[object object]');
         });
       })
