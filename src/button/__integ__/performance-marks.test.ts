4803ce1233aab440ee5e6aafc6e7ca4986af1ce7
Connor Lanigan <dev@connorlanigan.com>
2023-12-06 16:21:41 +0100

    chore: Replace 'cloudscape' string with 'awsui' (#1787)

--- src/button/__integ__/performance-marks.test.ts
+++ src/button/__integ__/performance-marks.test.ts
@@ -9,7 +9,7 @@ function setupTest(testFn: (page: BasePageObject, getMarks: () => Promise<Perfor
     await browser.url('#/light/button/performance-marks');
     const getMarks = async () => {
       const marks = await browser.execute(() => performance.getEntriesByType('mark') as PerformanceMark[]);
-      return marks.filter(m => m.detail?.source === 'cloudscape');
+      return marks.filter(m => m.detail?.source === 'awsui');
     };
     await testFn(page, getMarks);
   });
@@ -24,7 +24,7 @@ describe('Button', () => {
       expect(marks).toHaveLength(1);
       expect(marks[0].name).toBe('primaryButtonRendered');
       expect(marks[0].detail).toMatchObject({
-        source: 'cloudscape',
+        source: 'awsui',
         instanceId: expect.any(String),
         loading: true,
         disabled: false,
@@ -43,7 +43,7 @@ describe('Button', () => {
       expect(marks).toHaveLength(3);
       expect(marks[1].name).toBe('primaryButtonUpdated');
       expect(marks[1].detail).toMatchObject({
-        source: 'cloudscape',
+        source: 'awsui',
         instanceId: marks[0].detail.instanceId,
         loading: true,
         disabled: true,
@@ -51,7 +51,7 @@ describe('Button', () => {
       });
       expect(marks[2].name).toBe('primaryButtonUpdated');
       expect(marks[2].detail).toMatchObject({
-        source: 'cloudscape',
+        source: 'awsui',
         instanceId: marks[1].detail.instanceId,
         loading: false,
         disabled: true,
