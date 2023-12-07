78b0267ef546fd04398e277e1d3827e048218613
Connor Lanigan <connlani@amazon.com>
2023-12-07 13:34:36 +0100

    Allow data attributes

--- src/button/internal.tsx
+++ src/button/internal.tsx
@@ -25,7 +25,9 @@ import { useUniqueId } from '../internal/hooks/use-unique-id';
 export type InternalButtonProps = Omit<ButtonProps, 'variant'> & {
   variant?: ButtonProps['variant'] | 'flashbar-icon' | 'breadcrumb-group' | 'menu-trigger' | 'modal-dismiss';
   badge?: boolean;
-  __nativeAttributes?: React.HTMLAttributes<HTMLAnchorElement> & React.HTMLAttributes<HTMLButtonElement>;
+  __nativeAttributes?:
+    | (React.HTMLAttributes<HTMLAnchorElement> & React.HTMLAttributes<HTMLButtonElement>)
+    | Record<`data-${string}`, string>;
   __iconClass?: string;
   __activated?: boolean;
 } & InternalBaseComponentProps<HTMLAnchorElement | HTMLButtonElement>;
