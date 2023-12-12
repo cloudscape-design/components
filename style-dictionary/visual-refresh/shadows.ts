32177dddb3a5767b3dc5932711080f0f0576e61a
Atsushi Ienaka <atsuboy@gmail.com>
2023-12-12 12:56:43 +0100

    fix: Remove the new unnecessary mixin for container border
    And update the existing mixin to be used both for classic and VR

--- style-dictionary/visual-refresh/shadows.ts
+++ style-dictionary/visual-refresh/shadows.ts
@@ -6,12 +6,13 @@ import { expandColorDictionary } from '../utils';
 
 const tokens: StyleDictionary.ShadowsDictionary = {
   shadowContainer: {
-    light: '0px 0px 0px 1px #b6bec9', // 1px grey-350 faux border
-    dark: '0px 0px 0px 1px #414d5c',
+    light: '0px 0px 1px 1px #e9ebed, 0px 1px 8px 2px rgba(0, 7, 22, 0.12)', // 1px grey-200 faux border
+    dark: '0px 1px 8px 2px rgba(0, 7, 22, 0.6)',
   },
   shadowContainerStacked: {
-    light: '0px 0px 0px 1px #b6bec9',
-    dark: '0px 0px 0px 1px #414d5c',
+    light:
+      '-1px 1px 1px 0px #e9ebed, 1px 1px 1px 0px #e9ebed, 0px 9px 8px -7px rgb(0 7 22 / 12%), 8px 0px 8px -7px rgb(0 7 22 / 12%), -8px 0px 8px -7px rgb(0 7 22 / 12%)',
+    dark: '0px 9px 8px -7px rgb(0 7 22 / 60%), 8px 0px 8px -7px rgb(0 7 22 / 60%), -8px 0px 8px -7px rgb(0 7 22 / 60%)',
   },
   shadowContainerActive: {
     light: '0px 1px 1px 1px #e9ebed, 0px 6px 36px #0007161a',
