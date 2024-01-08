283c476fa15cf1fbbc0cd22a7de4a1ad62ab20bb
Andrei Zhaleznichenka <zhalea@amazon.de>
2024-01-08 12:19:17 +0100

    remove unneeded state in grid nav context

--- src/table/table-role/grid-navigation-context.tsx
+++ src/table/table-role/grid-navigation-context.tsx
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext } from 'react';
 import { FocusableDefinition } from './interfaces';
 
 /**
@@ -22,17 +22,13 @@ export const GridNavigationContext = createContext<{
  */
 export function useGridNavigationFocusable(focusable: null | FocusableDefinition, options?: { tabIndex?: number }) {
   const { focusTarget, keyboardNavigation } = useContext(GridNavigationContext);
-  const [focusTargetActive, setFocusTargetActive] = useState(false);
 
-  useEffect(() => {
-    if (!focusable) {
-      setFocusTargetActive(false);
-    } else if (typeof focusable === 'function') {
-      setFocusTargetActive(focusable() === focusTarget);
-    } else {
-      setFocusTargetActive(focusable.current === focusTarget);
-    }
-  }, [focusable, focusTarget]);
+  let focusTargetActive = false;
+  if (typeof focusable === 'function') {
+    focusTargetActive = focusable() === focusTarget;
+  } else if (focusable) {
+    focusTargetActive = focusable.current === focusTarget;
+  }
 
   let tabIndex: undefined | number = options?.tabIndex;
   if (keyboardNavigation) {
