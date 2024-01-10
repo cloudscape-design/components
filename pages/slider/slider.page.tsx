c6e0dc9ac707bf80da5529ec9cb67e1012687a1a
Katie George <katiegeo@amazon.com>
2024-01-09 16:13:38 -0800

    feat: WIP tooltip

--- pages/slider/slider.page.tsx
+++ pages/slider/slider.page.tsx
@@ -8,101 +8,113 @@ import FormField from '~components/form-field';
 import { Grid, Select, Slider, SliderProps } from '~components';
 
 function Sliders() {
-  const [value, setValue] = React.useState<SliderProps.ChangeDetail['value']>(50);
+  const [value, setValue] = React.useState<SliderProps.ChangeDetail['value']>(2);
   const [sliderValue, setSliderValue] = useState(20);
   const [rangeMin, setRangeMin] = useState(20);
   const [rangeMax, setRangeMax] = useState(80);
+  const [error, setError] = useState(false);
   const [minValue, setMinValue] = React.useState<any>({ value: '5' });
-  const rangeOptions = [{ value: '0' }, { value: '5' }, { value: '10' }, { value: '15' }, { value: '20' }];
+  const rangeOptions = [{ value: '5' }, { value: '10' }, { value: '15' }, { value: '20' }, { value: '25' }];
 
   return (
     <SpaceBetween size="xxl">
-      <Slider value={sliderValue} min={0} max={100} step={10} onChange={({ detail }) => setSliderValue(detail.value)} />
+      <Slider value={sliderValue} min={0} max={100000} onChange={({ detail }) => setSliderValue(detail.value)} />
       <Slider value={50} min={0} max={100} disabled={true} />
-
       <FormField label="Slider with select">
-        <SpaceBetween size="m">
-          <Grid
-            gridDefinition={[
-              { colspan: { default: 4, xs: 2 } },
-              { colspan: { default: 4, xs: 2 }, offset: { xs: 8, default: 4 } },
-            ]}
-          >
-            <Select
-              selectedOption={minValue}
-              onChange={({ detail }) => setMinValue(detail.selectedOption)}
-              options={rangeOptions}
-            />
-          </Grid>
-          <Slider
-            value={minValue.value}
+        <Grid
+          gridDefinition={[
+            { colspan: { default: 4, xs: 2 } },
+            { colspan: { default: 4, xs: 2 }, offset: { xs: 8, default: 4 } },
+          ]}
+        >
+          <Select
+            selectedOption={minValue}
+            onChange={({ detail }) => setMinValue(detail.selectedOption)}
+            options={rangeOptions}
+          />
+        </Grid>
+        <Slider
+          value={minValue.value}
+          onChange={({ detail }) => {
+            setMinValue({ value: detail.value });
+          }}
+          min={5}
+          max={25}
+          step={5}
+        />
+      </FormField>
+      <FormField label="Range slider">
+        <Grid
+          gridDefinition={[
+            { colspan: { default: 4, xs: 2 } },
+            { colspan: { default: 4, xs: 2 }, offset: { xs: 8, default: 4 } },
+          ]}
+        >
+          <Input
+            type="number"
+            inputMode="numeric"
+            value={`${rangeMin}`}
             onChange={({ detail }) => {
-              console.log(detail);
-              setMinValue({ value: detail.value });
+              setRangeMin(Number(detail.value));
+            }}
+          />
+          <Input
+            type="number"
+            inputMode="numeric"
+            value={`${rangeMax}`}
+            onBlur={() => setRangeMax(rangeMax)}
+            onChange={({ detail }) => setRangeMax(Number(detail.value))}
+          />
+        </Grid>
+        <Slider
+          variant="range"
+          rangeValue={[rangeMin, rangeMax]}
+          onRangeChange={({ detail }) => {
+            setRangeMin(detail.value[0]);
+            setRangeMax(detail.value[1]);
+          }}
+          min={0}
+          max={100}
+        />
+      </FormField>
+      <FormField
+        constraintText="Units must be a value between 0 and 10."
+        label="Default slider"
+        errorText={
+          error ? (value % 1 !== 0 ? 'Unit must be a whole number' : 'Unit must be between 0 and 10.') : undefined
+        }
+      >
+        <Grid gridDefinition={[{ colspan: { default: 8, xs: 4 } }, { colspan: { default: 3, xs: 3 } }]}>
+          <Slider
+            value={value}
+            onChange={({ detail }) => {
+              setValue(detail.value);
             }}
             min={0}
-            max={20}
-            step={5}
+            max={10}
+            step={0.5}
           />
-          <FormField
-            label="Range slider"
-            errorText={rangeMin < 0 || (rangeMin > 100 && 'Values must be between 0 and 100')}
-          >
-            <Grid
-              gridDefinition={[
-                { colspan: { default: 4, xs: 2 } },
-                { colspan: { default: 4, xs: 2 }, offset: { xs: 8, default: 4 } },
-              ]}
-            >
-              <Input
-                invalid={rangeMin < 0 || rangeMin > 100}
-                type="number"
-                inputMode="numeric"
-                value={`${rangeMin}`}
-                onChange={({ detail }) => {
-                  setRangeMin(Number(detail.value));
-                }}
-              />
-              <Input
-                invalid={rangeMin < 0 || rangeMin > 100}
-                type="number"
-                inputMode="numeric"
-                value={`${rangeMax}`}
-                onChange={({ detail }) => setRangeMax(Number(detail.value))}
-              />
-            </Grid>
-            <Slider
-              variant="range"
-              rangeValue={[rangeMin, rangeMax]}
-              onRangeChange={({ detail }) => {
-                setRangeMin(detail.value[0]);
-                setRangeMax(detail.value[1]);
+          <SpaceBetween size="m" alignItems="center" direction="horizontal">
+            <Input
+              type="number"
+              inputMode="numeric"
+              invalid={error}
+              onBlur={() => {
+                setError(value < 0 || value > 10 || value % 1 !== 0);
               }}
-              min={0}
-              max={100}
+              onKeyDown={({ detail }) => {
+                if (detail.keyCode === 13) {
+                  setError(value < 0 || value > 10 || value % 1 !== 0);
+                }
+              }}
+              value={`${value}`}
+              onChange={({ detail }) => {
+                setValue(Number(detail.value));
+              }}
             />
-          </FormField>
-          <FormField label="Default slider">
-            <Grid
-              gridDefinition={[
-                { colspan: { default: 8, xs: 4 } },
-                { colspan: { default: 2, xs: 1 } },
-                { colspan: { default: 2, xs: 1 } },
-              ]}
-            >
-              <Slider
-                value={value}
-                onChange={({ detail }) => {
-                  setValue(detail.value);
-                }}
-                min={0}
-                max={100}
-              />
-              <Input value={`${value}`} onChange={({ detail }) => setValue(Number(detail.value))} />
-              <Box>Units</Box>
-            </Grid>
-          </FormField>
-        </SpaceBetween>
+            <Box>Units</Box>
+          </SpaceBetween>
+        </Grid>
       </FormField>
     </SpaceBetween>
   );
--- src/slider/index.tsx
+++ src/slider/index.tsx
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useLayoutEffect, useRef } from 'react';
 import clsx from 'clsx';
 
 import styles from './styles.css.js';
@@ -31,27 +31,48 @@ export default function Slider({
   const rv0 = rangeValue ? rangeValue[0] : 0;
   const rv1 = rangeValue ? rangeValue[1] : 1;
   const range = useRef<HTMLDivElement>(null);
+  const tooltip = useRef<HTMLDivElement>(null);
+  const [tooltipVisible, setTooltipVisible] = React.useState(false);
+  const [tooltipWidth, setTooltipWidth] = React.useState(0);
 
   // Set width of the range to decrease from the left side
-  useEffect(() => {
-    const getPercent = (value: any) => Math.round(((value - min) / (max - min)) * 100);
-    const minPercent = getPercent(rv0);
-    const maxPercent = getPercent(rv1);
-    const percent = getPercent(value);
+  const getPercent = (value: number) => ((value - min) / (max - min)) * 100;
+  const minPercent = getPercent(Math.max(Math.min(rv0, max), min));
+  const maxPercent = getPercent(Math.max(Math.min(rv1, max), min));
 
+  const percent = value && getPercent(Math.max(Math.min(value, max), min));
+
+  useLayoutEffect(() => {
+    if (tooltip.current) {
+      tooltip.current.style.left = `calc(${percent}% - ${tooltipWidth}px / 2`;
+    }
+
+    setTooltipWidth(tooltip.current?.offsetWidth ?? 0);
+  }, [tooltipVisible, percent, tooltipWidth]);
+
+  useEffect(() => {
     if (range.current && variant === 'range') {
       range.current.style.left = `${minPercent}%`;
-      range.current.style.width = `${maxPercent - minPercent}%`;
+      range.current.style.width = maxPercent > minPercent ? `${maxPercent - minPercent}%` : `1%`;
     }
 
     if (range.current && variant === 'default') {
       range.current.style.width = `${percent}%`;
     }
-  }, [value, variant, rv0, rv1, min, max]);
+  }, [variant, maxPercent, minPercent, percent]);
 
   return (
     <div className={styles['slider-container']}>
       <div className={styles.slider}>
+        {tooltipVisible && (
+          <div
+            style={{ left: tooltipWidth ? `calc(${percent}% - ${tooltipWidth}px / 2` : `${percent}%` }}
+            className={styles['slider-thumb-label']}
+            ref={tooltip}
+          >
+            {value}
+          </div>
+        )}
         <div className={styles['slider-track']} />
         <div
           ref={range}
@@ -80,14 +101,20 @@ export default function Slider({
         min={min}
         max={max}
         disabled={disabled}
+        onFocus={() => setTooltipVisible(true)}
+        onBlur={() => setTooltipVisible(false)}
+        // onMouseOver={() => setTooltipVisible(true)}
+        // onMouseOut={() => setTooltipVisible(false)}
         step={step}
-        value={value ? value : rangeValue ? rangeValue[1] : ''}
+        value={variant === 'default' ? value ?? '' : rangeValue ? Math.max(rangeValue[1], rv0 + 1) : ''}
         onChange={event => {
           onChange && fireNonCancelableEvent(onChange, { value: Number(event.target.value) });
           onRangeChange &&
             fireNonCancelableEvent(onRangeChange, { value: [rv0, Math.max(Number(event.target.value), rv0 + 1)] });
         }}
-        className={clsx(styles.thumb, styles['thumb-right'])}
+        className={clsx(styles.thumb, {
+          [styles['thumb-right']]: variant === 'range',
+        })}
         {...baseProps}
       />
 
