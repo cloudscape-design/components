39dff0b089e0576c8e9dc24d0f153df132dd8cc8
Joan Perals Tresserra <jotresse@amazon.com>
2024-02-01 09:38:17 +0100

    Cover all test range formatting cases for date only

--- src/internal/utils/date-time/__tests__/format-date-range.test.ts
+++ src/internal/utils/date-time/__tests__/format-date-range.test.ts
@@ -9,13 +9,46 @@ const newYork = { startDate: -240, endDate: -240 };
 const regional = { startDate: 0, endDate: 60 };
 
 describe('formatDateRange', () => {
-  test.each([
-    ['2020-01-01', '2020-01-02', browser, '2020-01-01 — 2020-01-02'],
-    ['2020-01-01', '2020-01-02', berlin, '2020-01-01 — 2020-01-02'],
-    ['2020-01-01T00:00:00', '2020-01-01T12:00:00', berlin, '2020-01-01T00:00:00+02:00 — 2020-01-01T12:00:00+02:00'],
-    ['2020-01-01T00:00:00', '2020-01-01T12:00:00', newYork, '2020-01-01T00:00:00-04:00 — 2020-01-01T12:00:00-04:00'],
-    ['2020-01-01T00:00:00', '2020-01-01T12:00:00', regional, '2020-01-01T00:00:00+00:00 — 2020-01-01T12:00:00+01:00'],
-  ])('formats date correctly [%s, %s, %s]', (startDate, endDate, timeOffset, expected) => {
-    expect(formatDateRange({ startDate, endDate, timeOffset })).toBe(expected);
+  describe('Only date', () => {
+    const cases = [
+      {
+        startDate: '2020-01-01',
+        endDate: '2020-01-02',
+        timeOffset: browser,
+        expected: {
+          iso: '2020-01-01 — 2020-01-02',
+          absolute: { 'en-US': 'January 1, 2020 — January 2, 2020' },
+        },
+      },
+      {
+        startDate: '2020-01-01',
+        endDate: '2020-01-02',
+        timeOffset: berlin,
+        expected: { iso: '2020-01-01 — 2020-01-02', absolute: { 'en-US': 'January 1, 2020 — January 2, 2020' } },
+      },
+    ];
+    describe.each(cases)('formats date correctly [%j]', ({ startDate, endDate, timeOffset, expected }) => {
+      test('Default', () => {
+        expect(formatDateRange({ startDate, endDate, timeOffset })).toBe(expected.iso);
+      });
+      test('ISO', () => {
+        expect(formatDateRange({ startDate, endDate, timeOffset, format: 'iso' })).toBe(expected.iso);
+      });
+      test('Absolute', () => {
+        expect(formatDateRange({ startDate, endDate, timeOffset, format: 'absolute', locale: 'en-US' })).toBe(
+          expected.absolute['en-US']
+        );
+      });
+    });
+  });
+
+  describe('Date and time', () => {
+    test.each([
+      ['2020-01-01T00:00:00', '2020-01-01T12:00:00', berlin, '2020-01-01T00:00:00+02:00 — 2020-01-01T12:00:00+02:00'],
+      ['2020-01-01T00:00:00', '2020-01-01T12:00:00', newYork, '2020-01-01T00:00:00-04:00 — 2020-01-01T12:00:00-04:00'],
+      ['2020-01-01T00:00:00', '2020-01-01T12:00:00', regional, '2020-01-01T00:00:00+00:00 — 2020-01-01T12:00:00+01:00'],
+    ])('formats date correctly [%s, %s, %s]', (startDate, endDate, timeOffset, expected) => {
+      expect(formatDateRange({ startDate, endDate, timeOffset })).toBe(expected);
+    });
   });
 });
