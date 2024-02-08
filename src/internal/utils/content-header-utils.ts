8d84b3aeefe8749f0b174b74491e40b7da6b43cb
Atsushi Ienaka <atsuboy@gmail.com>
2024-02-08 10:02:05 +0100

    Update content-header-utils

--- src/internal/utils/content-header-utils.ts
+++ src/internal/utils/content-header-utils.ts
@@ -1,7 +1,9 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import { getGlobalFlag } from './global-flags';
+//import { useAppLayoutInternals } from '../../app-layout/visual-refresh/context';
 
 export const shouldRemoveHighContrastHeader = (): boolean => !!getGlobalFlag('removeHighContrastHeader');
-export const getContentHeaderClassName = (): string =>
-  shouldRemoveHighContrastHeader() ? '' : 'awsui-context-content-header';
+export const getContentHeaderClassName = (isDarkHeader?: boolean): string => {
+  return shouldRemoveHighContrastHeader() && !isDarkHeader ? '' : 'awsui-context-content-header';
+};
