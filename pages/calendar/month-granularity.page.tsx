6bace8fd78a42855634065a2b5f7c46794963075
Joan Perals Tresserra <jotresse@amazon.com>
2024-02-09 15:40:12 +0100

    Add coverage for keyboard navigation in calendar with month granularity

---
+++ pages/calendar/month-granularity.page.tsx
@@ -0,0 +1,29 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React, { useState } from 'react';
+import { Box, Link } from '~components';
+import Calendar from '~components/calendar';
+import i18nStrings from './i18n-strings';
+
+export default function DatePickerEditorScenario() {
+  const [value, setValue] = useState('2022-01-01');
+
+  return (
+    <Box padding="s">
+      <h1>Calendar with month granularity</h1>
+      <Link id="focusable-element-before-date-picker">Focusable element before the date picker</Link>
+      <br />
+      <Calendar
+        value={value}
+        locale="en-GB"
+        i18nStrings={i18nStrings}
+        onChange={event => setValue(event.detail.value)}
+        isDateEnabled={date => date <= new Date()}
+        granularity="month"
+      />
+      <br />
+      <br />
+      <Link id="focusable-element-after-date-picker">Focusable element after the date picker</Link>
+    </Box>
+  );
+}
--- src/calendar/__integ__/calendar.test.ts
+++ src/calendar/__integ__/calendar.test.ts
@@ -7,42 +7,46 @@ import { BasePageObject } from '@cloudscape-design/browser-test-tools/page-objec
 const calendarWrapper = createWrapper().findCalendar();
 
 describe('Date picker calendar interactions', () => {
-  const setupTest = (testFn: (page: BasePageObject) => Promise<void>) => {
-    return useBrowser(async browser => {
-      const page = new BasePageObject(browser);
+  describe.each(['day', 'month'])('granularity=%s', (granularity: string) => {
+    const setupTest = (testFn: (page: BasePageObject) => Promise<void>) => {
+      return useBrowser(async browser => {
+        const page = new BasePageObject(browser);
 
-      await browser.url('#/light/calendar/integ');
-      await testFn(page);
-    });
-  };
+        const url = `#/light/calendar/${granularity === 'month' ? 'month-granularity' : 'integ'}`;
+        await browser.url(url);
+        await testFn(page);
+      });
+    };
 
-  test(
-    'should focus the element after the date picker',
-    setupTest(async page => {
-      await page.click('#focusable-element-before-date-picker');
-      await page.keys(['Tab', 'Tab', 'Tab', 'Tab']);
-      await expect(page.isFocused('#focusable-element-after-date-picker')).resolves.toBeTruthy();
-    })
-  );
+    test(
+      'should focus the element after the date picker',
+      setupTest(async page => {
+        await page.click('#focusable-element-before-date-picker');
+        await page.keys(['Tab', 'Tab', 'Tab', 'Tab']);
+        await expect(page.isFocused('#focusable-element-after-date-picker')).resolves.toBeTruthy();
+      })
+    );
 
-  test(
-    'should focus the element before the date picker when shift tabing',
-    setupTest(async page => {
-      await page.click('#focusable-element-after-date-picker');
-      await page.keys(['Shift', 'Tab', 'Tab', 'Tab', 'Tab', 'Null']);
-      await expect(page.isFocused('#focusable-element-before-date-picker')).resolves.toBeTruthy();
-    })
-  );
+    test(
+      'should focus the element before the date picker when shift-tabbing',
+      setupTest(async page => {
+        await page.click('#focusable-element-after-date-picker');
+        await page.keys(['Shift', 'Tab', 'Tab', 'Tab', 'Tab', 'Null']);
+        await expect(page.isFocused('#focusable-element-before-date-picker')).resolves.toBeTruthy();
+      })
+    );
 
-  test(
-    'should navigate dates within a month',
-    setupTest(async page => {
-      await page.click('#focusable-element-before-date-picker');
-      await page.keys(['Tab', 'Tab', 'Tab']);
-      await page.keys(['ArrowRight', 'ArrowRight', 'ArrowRight']);
-      await page.keys('Enter');
+    test(
+      'should navigate dates',
+      setupTest(async page => {
+        await page.click('#focusable-element-before-date-picker');
+        await page.keys(['Tab', 'Tab', 'Tab']);
+        await page.keys(['ArrowRight', 'ArrowRight', 'ArrowRight']);
+        await page.keys('Enter');
 
-      await expect(page.getText(calendarWrapper.findSelectedDate().toSelector())).resolves.toBe('4');
-    })
-  );
+        const expectedDateContent = granularity === 'month' ? 'Apr' : '4';
+        await expect(page.getText(calendarWrapper.findSelectedDate().toSelector())).resolves.toBe(expectedDateContent);
+      })
+    );
+  });
 });
