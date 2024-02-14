9e84e3b98005c6643e56d5b702c2d9a05e145f53
Joan Perals Tresserra <jotresse@amazon.com>
2024-02-14 18:06:25 +0100

    Use useCallback in keyboard navigation hook

--- src/calendar/use-calendar-grid-keyboard-navigation.ts
+++ src/calendar/use-calendar-grid-keyboard-navigation.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import React from 'react';
+import React, { useCallback } from 'react';
 import { isSameMonth } from 'date-fns';
 import { CalendarProps } from './interfaces';
 import { moveNextDay, movePrevDay, moveNextWeek, movePrevWeek } from './utils/navigation';
@@ -22,47 +22,50 @@ export default function useCalendarGridKeyboardNavigation({
   onFocusDate: (date: null | Date) => void;
   onSelectDate: (date: Date) => void;
 }) {
-  const onGridKeyDownHandler = (event: React.KeyboardEvent) => {
-    let updatedFocusDate;
+  const onGridKeyDownHandler = useCallback(
+    (event: React.KeyboardEvent) => {
+      let updatedFocusDate;
 
-    if (focusableDate === null) {
-      return;
-    }
-
-    switch (event.keyCode) {
-      case KeyCode.space:
-      case KeyCode.enter:
-        event.preventDefault();
-        if (focusableDate) {
-          onFocusDate(null);
-          onSelectDate(focusableDate);
-        }
+      if (focusableDate === null) {
         return;
-      case KeyCode.right:
-        event.preventDefault();
-        updatedFocusDate = moveNextDay(focusableDate, isDateEnabled);
-        break;
-      case KeyCode.left:
-        event.preventDefault();
-        updatedFocusDate = movePrevDay(focusableDate, isDateEnabled);
-        break;
-      case KeyCode.up:
-        event.preventDefault();
-        updatedFocusDate = movePrevWeek(focusableDate, isDateEnabled);
-        break;
-      case KeyCode.down:
-        event.preventDefault();
-        updatedFocusDate = moveNextWeek(focusableDate, isDateEnabled);
-        break;
-      default:
-        return;
-    }
+      }
 
-    if (!isSameMonth(updatedFocusDate, baseDate)) {
-      onChangePage(updatedFocusDate);
-    }
-    onFocusDate(updatedFocusDate);
-  };
+      switch (event.keyCode) {
+        case KeyCode.space:
+        case KeyCode.enter:
+          event.preventDefault();
+          if (focusableDate) {
+            onFocusDate(null);
+            onSelectDate(focusableDate);
+          }
+          return;
+        case KeyCode.right:
+          event.preventDefault();
+          updatedFocusDate = moveNextDay(focusableDate, isDateEnabled);
+          break;
+        case KeyCode.left:
+          event.preventDefault();
+          updatedFocusDate = movePrevDay(focusableDate, isDateEnabled);
+          break;
+        case KeyCode.up:
+          event.preventDefault();
+          updatedFocusDate = movePrevWeek(focusableDate, isDateEnabled);
+          break;
+        case KeyCode.down:
+          event.preventDefault();
+          updatedFocusDate = moveNextWeek(focusableDate, isDateEnabled);
+          break;
+        default:
+          return;
+      }
+
+      if (!isSameMonth(updatedFocusDate, baseDate)) {
+        onChangePage(updatedFocusDate);
+      }
+      onFocusDate(updatedFocusDate);
+    },
+    [baseDate, focusableDate, isDateEnabled, onChangePage, onFocusDate, onSelectDate]
+  );
 
   return onGridKeyDownHandler;
 }
