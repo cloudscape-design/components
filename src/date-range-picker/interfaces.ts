bd2c450aa835d86112ad84238173b5fd1b88ab68
Joan Perals Tresserra <jotresse@amazon.com>
2024-02-15 22:45:30 +0100

    Fix API description

--- src/date-range-picker/interfaces.ts
+++ src/date-range-picker/interfaces.ts
@@ -161,6 +161,7 @@ export interface DateRangePickerProps
 
   /**
    * Specifies the time format to use for displaying the absolute time range.
+   *
    * It can take the following values:
    * * `iso`: ISO 8601 format, e.g.: 2024-01-30T13:32:32+01:00 (or 2024-01-30 when `dateOnly` is true)
    * * `long-localized`: a more human-readable, localized format, e.g.: January 30, 2024, 13:32:32 (UTC+1) (or January 30, 2024 when `dateOnly` is true)
