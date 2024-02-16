e67f62b86ed5a3c39c73150bf9bbfe2a844b960a
Joan Perals Tresserra <jotresse@amazon.com>
2024-02-16 10:14:04 +0100

    Use initial datetimes for each month

--- src/calendar/grid/use-calendar-grid-rows.ts
+++ src/calendar/grid/use-calendar-grid-rows.ts
@@ -22,16 +22,21 @@ export default function useCalendarGridRows({
   const rows = useMemo<Date[][]>(
     () =>
       isMonthPicker
-        ? new Array(4).fill(0).map((_, i: number) =>
-            new Array(3).fill(0).map((_, j: number) => {
-              const d = new Date(baseDate);
-              d.setMonth(i * 3 + j);
-              return d;
-            })
-          )
+        ? getCalendarYear(baseDate)
         : getCalendarMonth(baseDate, { firstDayOfWeek: normalizeStartOfWeek(startOfWeek, locale) }),
     [baseDate, isMonthPicker, startOfWeek, locale]
   );
 
   return rows;
+}
+
+// Returns a 3-by-4 matrix with dates corresponding to the initial date-time of each month of the year for a given date.
+function getCalendarYear(date: Date): Date[][] {
+  const year = date.getFullYear();
+  return new Array(4).fill(0).map((_, i: number) =>
+    new Array(3).fill(0).map((_, j: number) => {
+      const d = new Date(year, i * 3 + j);
+      return d;
+    })
+  );
 }
