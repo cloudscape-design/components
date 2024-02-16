78caa04b160a0fba9070349de51a77d6ed52d9f9
Joan Perals Tresserra <jotresse@amazon.com>
2024-02-16 19:21:12 +0100

    Fine tune dev page to better support visual regression tests

--- pages/date-picker/month-picker.page.tsx
+++ pages/date-picker/month-picker.page.tsx
@@ -1,10 +1,15 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import React, { useState } from 'react';
+import MockDate from 'mockdate';
 import { Box, DatePicker, Link } from '~components';
+import ScreenshotArea from '../utils/screenshot-area';
+
+// Mock the date in order to have the current day styling in place for screenshot testing.
+MockDate.set(new Date(2024, 1, 16));
 
 export default function () {
-  const [value, setValue] = useState('2023-02');
+  const [value, setValue] = useState('2024-01');
 
   return (
     <Box padding="s">
@@ -12,22 +17,24 @@ export default function () {
       <Link id="focus-dismiss-helper">Focusable element</Link>
       <br />
       <br />
-      <DatePicker
-        value={value}
-        locale="en-GB"
-        placeholder={'YYYY/MM'}
-        onChange={event => setValue(event.detail.value)}
-        openCalendarAriaLabel={selectedDate =>
-          'Choose Date' + (selectedDate ? `, selected date is ${selectedDate}` : '')
-        }
-        isDateEnabled={date => date <= new Date('2023-05')}
-        granularity="month"
-        i18nStrings={{
-          previousYearAriaLabel: 'Previous year',
-          nextYearAriaLabel: 'Next year',
-          currentMonthAriaLabel: 'Current month',
-        }}
-      />
+      <ScreenshotArea style={{ height: 300 }}>
+        <DatePicker
+          value={value}
+          locale="en-GB"
+          placeholder={'YYYY/MM'}
+          onChange={event => setValue(event.detail.value)}
+          openCalendarAriaLabel={selectedDate =>
+            'Choose Date' + (selectedDate ? `, selected date is ${selectedDate}` : '')
+          }
+          isDateEnabled={date => date <= new Date('2024-06')}
+          granularity="month"
+          i18nStrings={{
+            previousYearAriaLabel: 'Previous year',
+            nextYearAriaLabel: 'Next year',
+            currentMonthAriaLabel: 'Current month',
+          }}
+        />
+      </ScreenshotArea>
     </Box>
   );
 }
