b353bbaeb98073144e608614706fa18a7f36fce4
Boris Serdiuk <serdiuk@amazon.com>
2024-03-13 19:23:20 +0100

    refactor: Add wigetized layout utils

---
+++ src/split-panel/internal.tsx
@@ -0,0 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { createWidgetizedSplitPanel } from './widget';
+
+export const SplitPanelInternal = createWidgetizedSplitPanel();
