962ed5dab4c9a1c3962aaddae5fdb1c732bb299c
Katie George <katiegeo@amazon.com>
2024-03-22 14:55:49 -0700

    feat: Adds internal tooltip component

---
+++ src/internal/components/tooltip/__tests__/tooltip.test.tsx
@@ -0,0 +1,45 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React from 'react';
+import { render } from '@testing-library/react';
+import Tooltip, { TooltipProps } from '../../../../../lib/components/internal/components/tooltip';
+import createWrapper, { ElementWrapper, PopoverWrapper } from '../../../../../lib/components/test-utils/dom';
+import styles from '../../../../../lib/components/popover/styles.selectors.js';
+
+class TooltipInternalWrapper extends PopoverWrapper {
+  findContent(): ElementWrapper | null {
+    return createWrapper().findByClassName(styles.content);
+  }
+  findArrow(): ElementWrapper | null {
+    return createWrapper().findByClassName(styles.arrow);
+  }
+  findHeader(): ElementWrapper | null {
+    return createWrapper().findByClassName(styles.header);
+  }
+}
+
+const dummyRef = { current: null };
+function renderTooltip(props: Partial<TooltipProps>) {
+  const { container } = render(<Tooltip trackRef={dummyRef} value={props.value ?? ''} />);
+  return new TooltipInternalWrapper(container);
+}
+
+describe('Tooltip', () => {
+  it('renders text correctly', () => {
+    const wrapper = renderTooltip({ value: 'Value' });
+
+    expect(wrapper.findContent()?.getElement()).toHaveTextContent('Value');
+  });
+
+  it('renders arrow', () => {
+    const wrapper = renderTooltip({ value: 'Value' });
+
+    expect(wrapper.findArrow()).not.toBeNull();
+  });
+
+  it('not does render a header', () => {
+    const wrapper = renderTooltip({ value: 'Value' });
+
+    expect(wrapper.findHeader()).toBeNull();
+  });
+});
---
+++ src/internal/components/tooltip/index.tsx
@@ -0,0 +1,43 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React from 'react';
+import clsx from 'clsx';
+
+import { Transition } from '../transition';
+import PopoverContainer from '../../../popover/container';
+import PopoverBody from '../../../popover/body';
+import Portal from '../portal';
+import popoverStyles from '../../../popover/styles.css.js';
+
+export interface TooltipProps {
+  value: number | string;
+  trackRef: React.RefObject<Element>;
+}
+
+export default function Tooltip({ value, trackRef }: TooltipProps) {
+  return (
+    <Portal>
+      <Transition in={true}>
+        {() => (
+          <PopoverContainer
+            trackRef={trackRef}
+            trackKey={value}
+            size="small"
+            fixedWidth={false}
+            position="top"
+            arrow={position => (
+              <div className={clsx(popoverStyles.arrow, popoverStyles[`arrow-position-${position}`])}>
+                <div className={popoverStyles['arrow-outer']} />
+                <div className={popoverStyles['arrow-inner']} />
+              </div>
+            )}
+          >
+            <PopoverBody dismissButton={false} dismissAriaLabel={undefined} onDismiss={() => {}} header={undefined}>
+              {value}
+            </PopoverBody>
+          </PopoverContainer>
+        )}
+      </Transition>
+    </Portal>
+  );
+}
