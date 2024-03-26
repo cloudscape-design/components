02904761a641ace373802f95b15fa4e5f5b876d5
Andrei Zhaleznichenka <zhalea@amazon.de>
2024-03-26 15:41:24 +0100

    remove unnecessary placeholder component

--- src/test-utils/dom/table/index.ts
+++ src/test-utils/dom/table/index.ts
@@ -174,9 +174,7 @@ export default class TableWrapper extends ComponentWrapper {
    * @param rowIndex 1-based index of the row.
    */
   findExpandToggle(rowIndex: number): ElementWrapper | null {
-    return this.findNativeTable().find(
-      `tbody tr:nth-child(${rowIndex}) .${expandableRowsStyles['expand-toggle']}:not(.${expandableRowsStyles['expand-toggle-hidden']})`
-    );
+    return this.findNativeTable().find(`tbody tr:nth-child(${rowIndex}) .${expandableRowsStyles['expand-toggle']}`);
   }
 
   /**
