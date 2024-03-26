d033c1e25f43353ad4a29c8692b69822b31ce6a9
Scott O'Brien <scottob@amazon.com>
2024-03-26 17:46:58 -0400

    Minor cleanup.

--- src/segmented-control/internal-segmented-control.tsx
+++ src/segmented-control/internal-segmented-control.tsx
@@ -25,12 +25,10 @@ export default function InternalSegmentedControl({
   const moveHighlight = (event: React.KeyboardEvent<HTMLButtonElement>, activeIndex: number) => {
     let nextIndex = activeIndex;
 
-    const onKeyDown = handleKeyDown({
+    handleKeyDown({
       onInlineStart: () => (nextIndex = activeIndex === 0 ? enabledSegments.length - 1 : activeIndex - 1),
       onInlineEnd: () => (nextIndex = activeIndex + 1 === enabledSegments.length ? 0 : activeIndex + 1),
-    });
-
-    onKeyDown(event);
+    })(event);
 
     const nextSegmentId = enabledSegments[nextIndex].id;
     segmentByIdRef.current[nextSegmentId]?.focus();
