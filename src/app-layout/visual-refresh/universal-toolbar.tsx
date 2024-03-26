ee48350aa6841788795b2de502fd506ebcc7baa8
Jessica Kuelz <15003460+jkuelz@users.noreply.github.com>
2024-03-26 23:58:10 +0000

    chore: Remove unnecessary code related to layout gaps and sticky offset values

--- src/app-layout/visual-refresh/universal-toolbar.tsx
+++ src/app-layout/visual-refresh/universal-toolbar.tsx
@@ -12,10 +12,10 @@ export default function UniversalToolbar() {
   const {
     ariaLabels,
     breadcrumbs,
+    disableBodyScroll,
     drawers,
     __embeddedViewMode,
     handleNavigationClick,
-    // handleSplitPanelClick,
     handleToolsClick,
     hasDrawerViewportOverlay,
     isMobile,
@@ -25,15 +25,13 @@ export default function UniversalToolbar() {
     navigationHide,
     navigationRefs,
     splitPanel,
-    // splitPanelControlId,
     splitPanelDisplayed,
     splitPanelPosition,
-    // splitPanelRefs,
-    // splitPanelToggle,
     toolsControlId,
     toolsHide,
     toolsRefs,
     toolbarRef,
+    toolbarHeight,
     pinnedToolbar,
   } = useAppLayoutInternals();
 
@@ -83,18 +81,20 @@ export default function UniversalToolbar() {
     const [scrollDirection, setScrollDirection] = useState('show');
 
     useEffect(() => {
-      let lastScrollY = window.pageYOffset;
+      let lastScrollY = window.scrollY;
 
       const updateScrollDirection = () => {
-        const scrollY = window.pageYOffset;
+        const scrollY = window.scrollY;
         // 80 is an arbitrary number to have a pause before the toolbar scrolls out of view at the top of the page
         const direction = scrollY > lastScrollY && scrollY > 80 ? 'hide' : 'show';
-        // 3 as a buffer to avoid mistaking minor accidental mouse moves as scroll
+        // 2 as a buffer to avoid mistaking minor accidental mouse moves as scroll
         if (direction !== scrollDirection && (scrollY - lastScrollY > 2 || scrollY - lastScrollY < -2)) {
           setScrollDirection(direction);
         }
         lastScrollY = scrollY > 0 ? scrollY : 0;
+        console.log(lastScrollY, scrollY, direction, toolbarHeight);
       };
+
       window.addEventListener('scroll', updateScrollDirection);
       return () => {
         window.removeEventListener('scroll', updateScrollDirection);
@@ -114,19 +114,19 @@ export default function UniversalToolbar() {
     <section
       id="toolbar"
       ref={toolbarRef}
-      className={clsx(
-        styles['universal-toolbar'],
-        {
-          [styles['has-breadcrumbs']]: breadcrumbs,
-          [styles.unfocusable]: hasDrawerViewportOverlay,
-          [testutilStyles['mobile-bar']]: isMobile,
-          [styles['toolbar-hidden']]:
-            (scrollDirection === 'hide' && !pinnedToolbar) ||
-            // when the toolbar does not have anything to show inside, don't show it at all, but leave it rendered in case triggers come back
-            ((navigationHide || isNavigationOpen) && !breadcrumbs && (toolsHide || isToolsOpen) && !drawers),
-        },
-        testutilStyles['mobile-bar']
-      )}
+      className={clsx(styles['universal-toolbar'], {
+        [styles['has-breadcrumbs']]: breadcrumbs,
+        [styles.unfocusable]: hasDrawerViewportOverlay,
+        [testutilStyles['mobile-bar']]: isMobile,
+        [styles['disable-body-scroll']]: disableBodyScroll,
+        [styles['toolbar-hidden']]:
+          (scrollDirection === 'hide' && !pinnedToolbar) ||
+          // when the toolbar does not have anything to show inside, don't show it at all, but leave it rendered in case triggers come back
+          ((navigationHide || isNavigationOpen) &&
+            !breadcrumbs &&
+            (toolsHide || isToolsOpen) &&
+            (!drawers || drawers.length === 0)),
+      })}
     >
       <div className={styles['toolbar-container']}>
         {!navigationHide && !isNavigationOpen && (
@@ -166,22 +166,8 @@ export default function UniversalToolbar() {
                 ref={toolsRefs.toggle}
                 selected={hasSplitPanel && isToolsOpen}
               />
-
-              {/* {hasSplitPanel && splitPanelToggle.displayed && (
-                <TriggerButton
-                  ariaLabel={splitPanelToggle.ariaLabel}
-                  ariaControls={splitPanelControlId}
-                  ariaExpanded={!!isSplitPanelOpen}
-                  iconName={splitPanelPosition === 'side' ? 'view-vertical' : 'view-horizontal'}
-                  onClick={() => handleSplitPanelClick()}
-                  selected={hasSplitPanel && isSplitPanelOpen}
-                  // className={testutilStyles['open-button']}
-                  ref={splitPanelRefs.toggle}
-                />
-              )} */}
             </aside>
           )}
-
           {<DrawersToolbarTriggers />}
         </span>
       </div>
