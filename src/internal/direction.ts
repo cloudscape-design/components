d937aa9516df9d7ee9435bbd07e0df7bf5f34881
Scott O'Brien <scottob@amazon.com>
2024-04-04 11:24:54 -0400

    Added comments.

--- src/internal/direction.ts
+++ src/internal/direction.ts
@@ -1,6 +1,11 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+/**
+ * Determine if the direction of a given element is left-to-right
+ * or right-to-left. The is the JavaScript equivalent of the
+ * with-direction mixin that implements the dir() pseudoclass.
+ */
 export function isRtl(element: HTMLElement | EventTarget) {
   let isRtl = false;
 
@@ -11,8 +16,12 @@ export function isRtl(element: HTMLElement | EventTarget) {
   return isRtl;
 }
 
+/**
+ * The scrollLeft value will be a negative number if the direction is RTL and
+ * needs to be converted to a positive value for direction independent scroll
+ * computations. Additionally, the scrollLeft value can be a decimal value on
+ * systems using display scaling requiring the floor and ceiling calls.
+ */
 export function getScrollInlineStart(element: HTMLElement) {
-  // scrollLeft can be a decimal value on systems using display scaling
-  // scrollLeft will be a negative number if the direction is RTL
   return isRtl(element) ? Math.floor(element.scrollLeft) * -1 : Math.ceil(element.scrollLeft);
 }
