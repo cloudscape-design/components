48049bdb9bdeee7509d69cd6e0ba27ab75784b87
Scott O'Brien <scottob@amazon.com>
2024-04-04 16:40:34 -0400

    Update direction utilities.

--- src/internal/direction.ts
+++ src/internal/direction.ts
@@ -14,3 +14,21 @@ export function isRtl(element: HTMLElement | SVGElement) {
 export function getScrollInlineStart(element: HTMLElement) {
   return isRtl(element) ? Math.floor(element.scrollLeft) * -1 : Math.ceil(element.scrollLeft);
 }
+
+/**
+ * The getBoundingClientRect() function returns values relative to the top left
+ * corner of the document regardless of document direction. The left/right position
+ * will be transformed to insetInlineStart based on element direction in order to
+ * support direction agnostic position computation.
+ */
+export function getLogicalBoundingClientRect(element: Element) {
+  const boundingClientRect = element.getBoundingClientRect();
+  const insetInlineStart = isRtl(element) ? window.innerWidth - boundingClientRect.right : boundingClientRect.left;
+
+  return {
+    blockSize: boundingClientRect.height,
+    inlineSize: boundingClientRect.width,
+    insetBlockStart: boundingClientRect.top,
+    insetInlineStart,
+  };
+}
