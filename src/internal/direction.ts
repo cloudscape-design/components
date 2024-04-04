cb55311eef8a765734a2e632a447f615b939dfc1
Scott O'Brien <scottob@amazon.com>
2024-04-04 11:11:22 -0400

    Adjust types for isRtl function.

--- src/internal/direction.ts
+++ src/internal/direction.ts
@@ -1,17 +1,17 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-export function isRtl(element: Element) {
+export function isRtl(element: HTMLElement | EventTarget) {
   let isRtl = false;
 
-  if (element instanceof Element) {
+  if (element instanceof HTMLElement) {
     isRtl = getComputedStyle(element).direction === 'rtl';
   }
 
   return isRtl;
 }
 
-export function getScrollInlineStart(element: Element) {
+export function getScrollInlineStart(element: HTMLElement) {
   // scrollLeft can be a decimal value on systems using display scaling
   // scrollLeft will be a negative number if the direction is RTL
   return isRtl(element) ? Math.floor(element.scrollLeft) * -1 : Math.ceil(element.scrollLeft);
