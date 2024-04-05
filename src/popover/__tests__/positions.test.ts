c4e9ef04b6c3d31b3c563eb5c969614ff38b02a4
Scott O'Brien <scottob@amazon.com>
2024-04-05 15:44:29 -0400

    Resolved popover test type errors.

--- src/popover/__tests__/positions.test.ts
+++ src/popover/__tests__/positions.test.ts
@@ -6,17 +6,17 @@ import {
   PRIORITY_MAPPING,
 } from '../../../lib/components/popover/utils/positions';
 
-const arrow = { width: 15, height: 15 };
-const body = { width: 250, height: 250 };
-const viewport = { top: 0, left: 0, height: 1000, width: 1000 };
+const arrow = { inlineSize: 15, blockSize: 15 };
+const body = { inlineSize: 250, blockSize: 250 };
+const viewport = { insetBlockStart: 0, insetInlineStart: 0, blockSize: 1000, inlineSize: 1000 };
 
 describe('calculatePosition', () => {
   (
     [
-      ['top', { top: 500, left: 500, height: 25, width: 25 }],
-      ['right', { top: 500, left: 500, height: 25, width: 25 }],
-      ['bottom', { top: 500, left: 500, height: 25, width: 25 }],
-      ['left', { top: 500, left: 500, height: 25, width: 25 }],
+      ['top', { insetBlockStart: 500, insetInlineStart: 500, blockSize: 25, inlineSize: 25 }],
+      ['right', { insetBlockStart: 500, insetInlineStart: 500, blockSize: 25, inlineSize: 25 }],
+      ['bottom', { insetBlockStart: 500, insetInlineStart: 500, blockSize: 25, inlineSize: 25 }],
+      ['left', { insetBlockStart: 500, insetInlineStart: 500, blockSize: 25, inlineSize: 25 }],
     ] as const
   ).forEach(([preferredPosition, trigger]) => {
     test(`takes first position for preferredPosition="${preferredPosition}" from priority mapping when enough space for it`, () => {
@@ -34,10 +34,10 @@ describe('calculatePosition', () => {
 
   (
     [
-      ['top', { top: 500, left: 100, height: 25, width: 25 }],
-      ['right', { top: 800, left: 500, height: 25, width: 25 }],
-      ['bottom', { top: 500, left: 100, height: 25, width: 25 }],
-      ['left', { top: 800, left: 500, height: 25, width: 25 }],
+      ['top', { insetBlockStart: 500, insetInlineStart: 100, blockSize: 25, inlineSize: 25 }],
+      ['right', { insetBlockStart: 800, insetInlineStart: 500, blockSize: 25, inlineSize: 25 }],
+      ['bottom', { insetBlockStart: 500, insetInlineStart: 100, blockSize: 25, inlineSize: 25 }],
+      ['left', { insetBlockStart: 800, insetInlineStart: 500, blockSize: 25, inlineSize: 25 }],
     ] as const
   ).forEach(([preferredPosition, trigger]) => {
     test(`takes second position for preferredPosition="${preferredPosition}" from priority mapping when not enough space for first`, () => {
@@ -56,10 +56,10 @@ describe('calculatePosition', () => {
   describe('disregards preferredPosition in favor of fixedInternalPosition when defined', () => {
     (
       [
-        ['top', { top: 500, left: 500, height: 25, width: 25 }],
-        ['right', { top: 500, left: 500, height: 25, width: 25 }],
-        ['bottom', { top: 500, left: 500, height: 25, width: 25 }],
-        ['left', { top: 500, left: 500, height: 25, width: 25 }],
+        ['top', { insetBlockStart: 500, insetInlineStart: 500, blockSize: 25, inlineSize: 25 }],
+        ['right', { insetBlockStart: 500, insetInlineStart: 500, blockSize: 25, inlineSize: 25 }],
+        ['bottom', { insetBlockStart: 500, insetInlineStart: 500, blockSize: 25, inlineSize: 25 }],
+        ['left', { insetBlockStart: 500, insetInlineStart: 500, blockSize: 25, inlineSize: 25 }],
       ] as const
     ).forEach(([preferredPosition, trigger]) => {
       test(`preferredPosition="${preferredPosition}"`, () => {
@@ -79,8 +79,8 @@ describe('calculatePosition', () => {
 
   (
     [
-      ['top', { top: 200, left: 500, height: 25, width: 25 }],
-      ['bottom', { top: 800, left: 500, height: 25, width: 25 }],
+      ['top', { insetBlockStart: 200, insetInlineStart: 500, blockSize: 25, inlineSize: 25 }],
+      ['bottom', { insetBlockStart: 800, insetInlineStart: 500, blockSize: 25, inlineSize: 25 }],
     ] as const
   ).forEach(([preferredPosition, trigger]) => {
     test(`flips position for preferredPosition="${preferredPosition}" from priority mapping when not enough space above/below`, () => {
@@ -98,22 +98,22 @@ describe('calculatePosition', () => {
 
   (
     [
-      [{ top: 0, left: 0, height: 1000, width: 1000 }, 'top-center'],
-      [{ top: 0, left: 0, height: 1000, width: 600 }, 'top-left'],
-      [{ top: 450, left: 250, height: 1000, width: 350 }, 'bottom-left'],
+      [{ insetBlockStart: 0, insetInlineStart: 0, blockSize: 1000, inlineSize: 1000 }, 'top-center'],
+      [{ insetBlockStart: 0, insetInlineStart: 0, blockSize: 1000, inlineSize: 600 }, 'top-left'],
+      [{ insetBlockStart: 450, insetInlineStart: 250, blockSize: 1000, inlineSize: 350 }, 'bottom-left'],
     ] as const
   ).forEach(([container, expected], index) => {
     test(`for container #${index} finds position ${expected} that fits into it`, () => {
-      const trigger = { top: 500, left: 500, height: 25, width: 25 };
+      const trigger = { insetBlockStart: 500, insetInlineStart: 500, blockSize: 25, inlineSize: 25 };
       const position = calculatePosition({ preferredPosition: 'top', trigger, arrow, body, container, viewport });
       expect(position.internalPosition).toBe(expected);
     });
   });
 
   test("returns the position with the largest availabe area, if can't fit inside viewport", () => {
-    const trigger = { top: 0, left: 0, height: 25, width: 25 };
-    const container = { top: 0, left: 0, height: 600, width: 1000 };
-    const viewport = { top: 0, left: 0, height: 100, width: 100 };
+    const trigger = { insetBlockStart: 0, insetInlineStart: 0, blockSize: 25, inlineSize: 25 };
+    const container = { insetBlockStart: 0, insetInlineStart: 0, blockSize: 600, inlineSize: 1000 };
+    const viewport = { insetBlockStart: 0, insetInlineStart: 0, blockSize: 100, inlineSize: 100 };
     const position = calculatePosition({ preferredPosition: 'bottom', trigger, arrow, body, container, viewport });
     expect(position.internalPosition).toBe('bottom-center');
   });
@@ -121,9 +121,9 @@ describe('calculatePosition', () => {
   test('disregards the container bound, when rendered inside a portal', () => {
     // trigger is in the right bottom corner of the container.
     // Normally, the popover would open top-left, to try to fit inside it
-    const trigger = { top: 175, left: 175, height: 25, width: 25 };
-    const container = { top: 100, left: 100, height: 100, width: 100 };
-    const viewport = { top: 0, left: 0, height: 1000, width: 1000 };
+    const trigger = { insetBlockStart: 175, insetInlineStart: 175, blockSize: 25, inlineSize: 25 };
+    const container = { insetBlockStart: 100, insetInlineStart: 100, blockSize: 100, inlineSize: 100 };
+    const viewport = { insetBlockStart: 0, insetInlineStart: 0, blockSize: 1000, inlineSize: 1000 };
     const position = calculatePosition({
       preferredPosition: 'bottom',
       trigger,
@@ -140,29 +140,29 @@ describe('calculatePosition', () => {
     [
       // bottom-right
       [
-        { left: 200, top: 200, height: 25, width: 25 },
-        { width: 250, height: 1000 },
+        { insetInlineStart: 200, insetBlockStart: 200, blockSize: 25, inlineSize: 25 },
+        { inlineSize: 250, blockSize: 1000 },
       ],
       // bottom-left
       [
-        { left: 800, top: 200, height: 25, width: 25 },
-        { width: 250, height: 1000 },
+        { insetInlineStart: 800, insetBlockStart: 200, blockSize: 25, inlineSize: 25 },
+        { inlineSize: 250, blockSize: 1000 },
       ],
       // top-right
       [
-        { left: 200, top: 800, height: 25, width: 25 },
-        { width: 250, height: 1000 },
+        { insetInlineStart: 200, insetBlockStart: 800, blockSize: 25, inlineSize: 25 },
+        { inlineSize: 250, blockSize: 1000 },
       ],
       // top-left
       [
-        { left: 800, top: 800, height: 25, width: 25 },
-        { width: 250, height: 1000 },
+        { insetInlineStart: 800, insetBlockStart: 800, blockSize: 25, inlineSize: 25 },
+        { inlineSize: 250, blockSize: 1000 },
       ],
     ] as const
   ).forEach(([trigger, body], index) => {
     describe(`index=${index} returns scrollable=true if can't fit popover into viewport`, () => {
       test.each([false, true])('renderWithPortal=%s', renderWithPortal => {
-        const container = { ...viewport, height: viewport.height * 2 };
+        const container = { ...viewport, blockSize: viewport.blockSize * 2 };
         const position = calculatePosition({
           preferredPosition: 'top',
           trigger,
@@ -173,8 +173,8 @@ describe('calculatePosition', () => {
           renderWithPortal,
         });
         expect(position.scrollable).toBe(true);
-        expect(position.rect.width).toBe(250);
-        expect(position.rect.height).toBeLessThan(900);
+        expect(position.rect.inlineSize).toBe(250);
+        expect(position.rect.blockSize).toBeLessThan(900);
       });
     });
   });
@@ -183,15 +183,15 @@ describe('calculatePosition', () => {
 describe('intersectRectangles', () => {
   it('returns the area of the intersection of passed in rectangles', () => {
     const rects = [
-      { top: 0, left: 0, width: 10, height: 10 },
-      { top: 5, left: 5, width: 10, height: 10 },
+      { insetBlockStart: 0, insetInlineStart: 0, inlineSize: 10, blockSize: 10 },
+      { insetBlockStart: 5, insetInlineStart: 5, inlineSize: 10, blockSize: 10 },
     ];
     expect(intersectRectangles(rects)).toEqual(25);
   });
   it('returns null, if there is no intersection', () => {
     const rects = [
-      { top: 0, left: 0, width: 5, height: 5 },
-      { top: 10, left: 10, width: 5, height: 5 },
+      { insetBlockStart: 0, insetInlineStart: 0, inlineSize: 5, blockSize: 5 },
+      { insetBlockStart: 10, insetInlineStart: 10, inlineSize: 5, blockSize: 5 },
     ];
     expect(intersectRectangles(rects)).toEqual(null);
   });
