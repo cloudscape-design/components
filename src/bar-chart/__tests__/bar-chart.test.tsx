383a3c62e6498fa24ab5d45b2a4230fdc5aa5a8b
Andrei Zhaleznichenka <zhalea@amazon.de>
2024-04-18 15:50:47 +0200

    fix: In stacked bar chart threshold series can be added at any position in the series array (#2172)

--- src/bar-chart/__tests__/bar-chart.test.tsx
+++ src/bar-chart/__tests__/bar-chart.test.tsx
@@ -15,7 +15,7 @@ function renderBarChart(jsx: React.ReactElement) {
   };
 }
 
-const barSeries: MixedLineBarChartProps.BarDataSeries<string> = {
+const barSeries1: MixedLineBarChartProps.BarDataSeries<string> = {
   type: 'bar',
   title: 'Bar Series 1',
   data: [
@@ -25,20 +25,41 @@ const barSeries: MixedLineBarChartProps.BarDataSeries<string> = {
     { x: 'Group 4', y: 9 },
   ],
 };
+const barSeries2: MixedLineBarChartProps.BarDataSeries<string> = {
+  type: 'bar',
+  title: 'Bar Series 2',
+  data: [
+    { x: 'Group 1', y: -2 },
+    { x: 'Group 2', y: 0 },
+    { x: 'Group 3', y: 0 },
+    { x: 'Group 4', y: 5 },
+  ],
+};
+const thresholdSeries: MixedLineBarChartProps.ThresholdSeries = {
+  type: 'threshold',
+  title: 'Threshold 1',
+  y: 5,
+};
 
 // Main rendering and testing is done in the MixedChart component. We just make sure that everything is passed down correctly.
 describe('Bar chart', () => {
-  test('can render bar series', () => {
-    const { wrapper } = renderBarChart(
-      <BarChart
-        series={[barSeries]}
-        xScaleType="categorical"
-        xDomain={['Group 1', 'Group 2', 'Group 3', 'Group 4']}
-        yDomain={[0, 10]}
-      />
-    );
+  test.each([{ stackedBars: false }, { stackedBars: true }])(
+    'can render bar series with stackedBars=$stackedBars',
+    ({ stackedBars }) => {
+      const { wrapper } = renderBarChart(
+        <BarChart
+          series={[barSeries1, thresholdSeries, barSeries2]}
+          stackedBars={stackedBars}
+          xScaleType="categorical"
+          xDomain={['Group 1', 'Group 2', 'Group 3', 'Group 4']}
+          yDomain={[0, 10]}
+        />
+      );
 
-    expect(wrapper.findSeries()).toHaveLength(1);
-    expect(wrapper.findSeries()[0].getElement()).toHaveAttribute('aria-label', barSeries.title);
-  });
+      expect(wrapper.findSeries()).toHaveLength(3);
+      expect(wrapper.findSeries()[0].getElement()).toHaveAttribute('aria-label', barSeries1.title);
+      expect(wrapper.findSeries()[1].getElement()).toHaveAttribute('aria-label', thresholdSeries.title);
+      expect(wrapper.findSeries()[2].getElement()).toHaveAttribute('aria-label', barSeries2.title);
+    }
+  );
 });
