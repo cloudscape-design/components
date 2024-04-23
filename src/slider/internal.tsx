37d1601ad75d5adff12c227dc5e664a5f3522469
Katie George <katiegeo@amazon.com>
2024-04-23 14:49:23 -0700

    fix: Fixes step of 0 appearing on slider

--- src/slider/internal.tsx
+++ src/slider/internal.tsx
@@ -112,7 +112,7 @@ export default function InternalSlider({
           />
         )}
       </div>
-      {step && tickMarks && (
+      {!!step && tickMarks && (
         <div
           className={clsx(styles.ticks)}
           style={{
