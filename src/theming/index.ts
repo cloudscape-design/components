ff71d3713ad04240a1f215ee8a98beef2513eda1
Adam Smith <afsmith@amazon.com>
2024-04-25 12:13:42 -0700

    Add generateThemeStylesheet API

--- src/theming/index.ts
+++ src/theming/index.ts
@@ -1,6 +1,9 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { applyTheme as coreApplyTheme } from '@cloudscape-design/theming-runtime';
+import {
+  applyTheme as coreApplyTheme,
+  generateThemeStylesheet as coreGenerateThemeStylesheet,
+} from '@cloudscape-design/theming-runtime';
 import { preset, TypedOverride } from '../internal/generated/theming';
 
 export type Theme = TypedOverride;
@@ -15,6 +18,19 @@ export interface ApplyThemeResult {
 
 export function applyTheme({ theme, baseThemeId }: ApplyThemeParams): ApplyThemeResult {
   return coreApplyTheme({
+    override: theme,
+    preset,
+    baseThemeId,
+  });
+}
+
+export interface GenerateThemeStylesheetParams {
+  theme: Theme;
+  baseThemeId?: string;
+}
+
+export function generateThemeStylesheet({ theme, baseThemeId }: GenerateThemeStylesheetParams): string {
+  return coreGenerateThemeStylesheet({
     override: theme,
     preset,
     baseThemeId,
