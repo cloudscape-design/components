26ac9518a28d957158a68bc1d44838270eed7d39
Avinash Dwarapu <dwaraa@amazon.com>
2024-05-16 13:27:03 +0200

    fix: Make split panel resizer icon aria-hidden

--- src/split-panel/icons/resize-handler.tsx
+++ src/split-panel/icons/resize-handler.tsx
@@ -10,6 +10,7 @@ const ResizeHandler = ({ className }: { className?: string }) => (
     width="16"
     height="16"
     viewBox="0 0 16 16"
+    aria-hidden={true}
   >
     <line strokeWidth="2" x1="2" y1="5" x2="14" y2="5" />
     <line strokeWidth="2" x1="14" y1="10" x2="2" y2="10" />
