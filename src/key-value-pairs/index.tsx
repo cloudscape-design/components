229bc15c7a74a76220569ff3e666125a96dfaa8c
Georgii Lobko <globko@amazon.de>
2024-05-16 17:42:23 +0200

    fix: rename KeyValuePair to KeyValuePairs

---
+++ src/key-value-pairs/index.tsx
@@ -0,0 +1,19 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React from 'react';
+import { getBaseProps } from '../internal/base-component';
+import { applyDisplayName } from '../internal/utils/apply-display-name';
+import useBaseComponent from '../internal/hooks/use-base-component';
+import { KeyValuePairsProps } from './interfaces';
+import InternalKeyValuePairs from './internal';
+
+export { KeyValuePairsProps };
+
+export default function KeyValuePairs({ columns, ...rest }: KeyValuePairsProps) {
+  const { __internalRootRef } = useBaseComponent('KeyValuePairs');
+  const baseProps = getBaseProps(rest);
+
+  return <InternalKeyValuePairs columns={columns} {...baseProps} ref={__internalRootRef} />;
+}
+
+applyDisplayName(KeyValuePairs, 'KeyValuePairs');
