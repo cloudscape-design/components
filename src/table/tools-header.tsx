125c598fa0a67c50ff5ad5956014c8a724ba14e8
Connor Lanigan <connlani@amazon.com>
2024-05-17 16:07:11 +0200

    chore: Use native container queries in Table's Tools Header

--- src/table/tools-header.tsx
+++ src/table/tools-header.tsx
@@ -1,8 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import clsx from 'clsx';
 import React, { useContext } from 'react';
-import { useContainerBreakpoints } from '../internal/hooks/container-queries';
 import styles from './styles.css.js';
 import { CollectionLabelContext } from '../internal/context/collection-label-context';
 import { useUniqueId } from '../internal/hooks/use-unique-id';
@@ -15,20 +13,18 @@ interface ToolsHeaderProps {
 }
 
 export default function ToolsHeader({ header, filter, pagination, preferences }: ToolsHeaderProps) {
-  const [breakpoint, ref] = useContainerBreakpoints(['xs']);
   const isHeaderString = typeof header === 'string';
   const assignHeaderId = useContext(CollectionLabelContext).assignId;
   const headingId = useUniqueId('heading');
   if (assignHeaderId !== undefined && isHeaderString) {
     assignHeaderId(headingId);
   }
-  const isSmall = breakpoint === 'default';
   const hasTools = filter || pagination || preferences;
   return (
     <>
       {isHeaderString ? <span id={headingId}>{header}</span> : header}
       {hasTools && (
-        <div ref={ref} className={clsx(styles.tools, isSmall && styles['tools-small'])}>
+        <div className={styles.tools}>
           {filter && <div className={styles['tools-filtering']}>{filter}</div>}
           <div className={styles['tools-align-right']}>
             {pagination && <div className={styles['tools-pagination']}>{pagination}</div>}
