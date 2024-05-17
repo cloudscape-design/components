1e56573cc86f7785e100f1daa8f8eb5ebbbea1fa
Georgii Lobko <globko@amazon.de>
2024-05-17 11:55:09 +0200

    fix: Introduce render condition for KeyValuePairs component when columns list is empty

--- src/key-value-pairs/internal.tsx
+++ src/key-value-pairs/internal.tsx
@@ -14,30 +14,32 @@ const InternalKeyValuePairs = React.forwardRef(
   ({ columns, className, ...rest }: KeyValuePairsProps, ref: React.Ref<HTMLDivElement>) => {
     return (
       <div {...rest} className={clsx(styles['key-value-pairs'], className)} ref={ref}>
-        <ColumnLayout columns={columns.length} variant="text-grid">
-          {columns.map((column, columnIndex) => (
-            <div className={styles.column} key={columnIndex}>
-              {column.title && (
-                <Box variant="h3" padding="n">
-                  {column.title}
-                </Box>
-              )}
-              <dl className={styles.list}>
-                {column.items.map((item, itemIndex) => (
-                  <div key={columnIndex + itemIndex}>
-                    <dt className={styles.key}>
-                      <label className={styles['key-label']}>{item.key}</label>
-                      <InfoLinkLabelContext.Provider value={item.key}>
-                        {item.info && <span className={styles.info}>{item.info}</span>}
-                      </InfoLinkLabelContext.Provider>
-                    </dt>
-                    <dd className={styles.description}>{item.value}</dd>
-                  </div>
-                ))}
-              </dl>
-            </div>
-          ))}
-        </ColumnLayout>
+        {!!columns?.length && (
+          <ColumnLayout columns={columns.length} variant="text-grid">
+            {columns.map((column, columnIndex) => (
+              <div className={styles.column} key={columnIndex}>
+                {column.title && (
+                  <Box variant="h3" padding="n">
+                    {column.title}
+                  </Box>
+                )}
+                <dl className={styles.list}>
+                  {column.items.map((item, itemIndex) => (
+                    <div key={columnIndex + itemIndex}>
+                      <dt className={styles.key}>
+                        <label className={styles['key-label']}>{item.key}</label>
+                        <InfoLinkLabelContext.Provider value={item.key}>
+                          {item.info && <span className={styles.info}>{item.info}</span>}
+                        </InfoLinkLabelContext.Provider>
+                      </dt>
+                      <dd className={styles.description}>{item.value}</dd>
+                    </div>
+                  ))}
+                </dl>
+              </div>
+            ))}
+          </ColumnLayout>
+        )}
       </div>
     );
   }
