2d9502c6e3f98282ee41d010c3758bd16c281df8
Connor Lanigan <dev@connorlanigan.com>
2024-05-24 14:08:55 +0200

    chore: Do not emit performance marks for buttons inside modals (#2287)

---
+++ pages/button/performance-marks-in-modal.page.tsx
@@ -0,0 +1,57 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React, { useEffect, useState } from 'react';
+import Button from '~components/button';
+import Box from '~components/box';
+import Modal from '~components/modal';
+
+export default function ButtonsPerformanceMarkPage() {
+  const marks = usePerformanceMarks();
+
+  return (
+    <Box padding="xxl">
+      <h1>Performance marks in a Modal</h1>
+
+      <Modal
+        visible={true}
+        footer={<Button variant="primary">Button INSIDE modal</Button>}
+        header="Performance marks on this page"
+      >
+        <pre>
+          {marks.map((m, index) => (
+            <React.Fragment key={index}>
+              <b>{m.name}</b>
+              <pre>Details: {JSON.stringify(m.detail, null, 2)}</pre>
+            </React.Fragment>
+          ))}
+        </pre>
+      </Modal>
+
+      <Button variant="primary">Button OUTSIDE modal</Button>
+    </Box>
+  );
+}
+
+function usePerformanceMarks() {
+  const [marks, setMarks] = useState<PerformanceMark[]>([]);
+
+  useEffect(() => {
+    const existingEntries = (performance.getEntriesByType('mark') as PerformanceMark[]).filter(isNotReactDevTools);
+    setMarks(existingEntries);
+
+    const observer = new PerformanceObserver(list => {
+      const newEntries = (list.getEntries() as PerformanceMark[]).filter(isNotReactDevTools);
+      if (newEntries.length > 0) {
+        setMarks(existing => [...existing, ...newEntries]);
+      }
+    });
+
+    observer.observe({ type: 'mark' });
+    return () => observer.disconnect();
+  }, [setMarks]);
+
+  return marks;
+}
+
+// This prevents an infinite rerendering loop.
+const isNotReactDevTools = (m: PerformanceMark) => !m.name.includes('⚛') && m.name !== '__v3';
