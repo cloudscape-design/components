907a2302ebfe60f00bcbb3c856cf764a3964253a
Ruben Carvalho <rucarva@amazon.de>
2024-05-27 15:32:36 +0200

    chore: Rename some files

--- pages/prompt-input/simple.page.tsx
+++ pages/prompt-input/simple.page.tsx
@@ -1,19 +1,20 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import React, { useState } from 'react';
-import OmniBox from '~components/omni-box';
+import PromptInput from '~components/prompt-input';
 
-export default function OmniBoxPage() {
+export default function PromptInputPage() {
   const [textareaValue, setTextareaValue] = useState('');
 
   return (
     <div style={{ padding: 10 }}>
-      <h1>OmniBox demo</h1>
-      <OmniBox
+      <h1>PromptInput demo</h1>
+      <PromptInput
         ariaLabel="textarea"
         value={textareaValue}
         onChange={(event: any) => setTextareaValue(event.detail.value)}
         placeholder="Ask a question"
+        maxRows={6}
       />
     </div>
   );
