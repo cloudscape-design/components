4f45ddb72be938c7585d6364db183f714ac66aef
Georgii Lobko <globko@amazon.de>
2024-05-31 15:18:18 +0200

    Turn renderKeyValuePairs arg into jsx instead of props

--- src/key-value-pairs/__tests__/key-value-pairs.test.tsx
+++ src/key-value-pairs/__tests__/key-value-pairs.test.tsx
@@ -3,107 +3,115 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import createWrapper from '../../../lib/components/test-utils/dom';
-import KeyValuePairs, { KeyValuePairsProps } from '../../../lib/components/key-value-pairs';
+import KeyValuePairs from '../../../lib/components/key-value-pairs';
 
-function renderKeyValuePairs(props: KeyValuePairsProps) {
-  const { container } = render(<KeyValuePairs {...props} />);
+function renderKeyValuePairs(jsx: React.ReactElement) {
+  const { container } = render(jsx);
   return { wrapper: createWrapper(container).findKeyValuePairs()! };
 }
 
 describe('KeyValuePairs', () => {
   describe('columns', () => {
     test(`renders 1 column correctly`, () => {
-      const { wrapper } = renderKeyValuePairs({
-        columns: [
-          {
-            items: [
-              {
-                label: 'Label for key',
-                value: 'Value',
-              },
-            ],
-          },
-        ],
-      });
+      const { wrapper } = renderKeyValuePairs(
+        <KeyValuePairs
+          columns={[
+            {
+              items: [
+                {
+                  label: 'Label for key',
+                  value: 'Value',
+                },
+              ],
+            },
+          ]}
+        />
+      );
 
       expect(wrapper.findColumns()).toHaveLength(1);
     });
 
     test(`renders multiple columns correctly`, () => {
-      const { wrapper } = renderKeyValuePairs({
-        columns: [
-          {
-            items: [
-              {
-                label: 'Label for key',
-                value: 'Value',
-              },
-            ],
-          },
-          {
-            items: [
-              {
-                label: 'Label for key',
-                value: 'Value',
-              },
-            ],
-          },
-        ],
-      });
+      const { wrapper } = renderKeyValuePairs(
+        <KeyValuePairs
+          columns={[
+            {
+              items: [
+                {
+                  label: 'Label for key',
+                  value: 'Value',
+                },
+              ],
+            },
+            {
+              items: [
+                {
+                  label: 'Label for key',
+                  value: 'Value',
+                },
+              ],
+            },
+          ]}
+        />
+      );
 
       expect(wrapper.findColumns()).toHaveLength(2);
     });
   });
 
   test('renders title correctly', () => {
-    const { wrapper } = renderKeyValuePairs({
-      columns: [
-        {
-          title: 'Title',
-          items: [
-            {
-              label: 'Label for key',
-              value: 'Value',
-            },
-          ],
-        },
-      ],
-    });
+    const { wrapper } = renderKeyValuePairs(
+      <KeyValuePairs
+        columns={[
+          {
+            title: 'Title',
+            items: [
+              {
+                label: 'Label for key',
+                value: 'Value',
+              },
+            ],
+          },
+        ]}
+      />
+    );
 
     expect(wrapper.findColumns()[0].findTitle()!.getElement()).toHaveTextContent('Title');
   });
 
   test('renders pairs correctly', () => {
-    const { wrapper } = renderKeyValuePairs({
-      columns: [
-        {
-          title: 'Column title 1',
-          items: [
-            {
-              label: 'Label for key 1',
-              value: 'Value 1',
-            },
-            {
-              label: 'Label for key 2',
-              value: 'Value 2',
-            },
-          ],
-        },
-        {
-          title: 'Column title 2',
-          items: [
-            {
-              label: 'Label for key 3',
-              value: 'Value 3',
-            },
-            {
-              label: 'Label for key 4',
-              value: 'Value 4',
-            },
-          ],
-        },
-      ],
-    });
+    const { wrapper } = renderKeyValuePairs(
+      <KeyValuePairs
+        columns={[
+          {
+            title: 'Column title 1',
+            items: [
+              {
+                label: 'Label for key 1',
+                value: 'Value 1',
+              },
+              {
+                label: 'Label for key 2',
+                value: 'Value 2',
+              },
+            ],
+          },
+          {
+            title: 'Column title 2',
+            items: [
+              {
+                label: 'Label for key 3',
+                value: 'Value 3',
+              },
+              {
+                label: 'Label for key 4',
+                value: 'Value 4',
+              },
+            ],
+          },
+        ]}
+      />
+    );
 
     expect(wrapper.findColumns()[0].findTitle()!.getElement().textContent).toBe('Column title 1');
     expect(wrapper.findColumns()[0].findPairs()[0].findLabel()!.getElement().textContent).toBe('Label for key 1');
@@ -117,19 +125,21 @@ describe('KeyValuePairs', () => {
   });
 
   test('renders label with info correctly', () => {
-    const { wrapper } = renderKeyValuePairs({
-      columns: [
-        {
-          items: [
-            {
-              label: 'Label for key',
-              value: 'Value',
-              info: 'info',
-            },
-          ],
-        },
-      ],
-    });
+    const { wrapper } = renderKeyValuePairs(
+      <KeyValuePairs
+        columns={[
+          {
+            items: [
+              {
+                label: 'Label for key',
+                value: 'Value',
+                info: 'info',
+              },
+            ],
+          },
+        ]}
+      />
+    );
 
     expect(wrapper.findColumns()[0].findPairs()[0].findInfo()!.getElement()).toHaveTextContent('info');
   });
