84e1f5e78972ee8fbda6055ab30ceedb382a8cf4
Boris Serdiuk <serdiuk@amazon.com>
2024-06-06 16:05:28 +0200

    chore: Update id prop documentation text (#2337)

--- src/internal/base-component/index.ts
+++ src/internal/base-component/index.ts
@@ -13,12 +13,14 @@ initAwsUiVersions(PACKAGE_SOURCE, PACKAGE_VERSION);
 export interface BaseComponentProps {
   /**
    * Adds the specified classes to the root element of the component.
-   * @deprecated Custom CSS is not supported. For other use cases, use [data attributes](https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes).
+   * @deprecated Custom CSS is not supported. For testing and other use cases, use [data attributes](https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes).
    */
   className?: string;
   /**
    * Adds the specified ID to the root element of the component.
-   * @deprecated Custom CSS is not supported. For other use cases, use [data attributes](https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes).
+   * @deprecated The usage of the `id` attribute is reserved for internal use cases. For testing and other use cases,
+   * use [data attributes](https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes). If you must
+   * use the `id` attribute, consider setting it on a parent element instead.
    */
   id?: string;
   // we also support data-* attributes, but they are always implicitly allowed by typescript
