b09b5b165c51e31b446b6cac48ae263c68fc060c
Ruben Carvalho <rucarva@amazon.de>
2024-06-06 17:34:20 +0200

    feat: Add customizable icon and clean up interface

--- src/prompt-input/interfaces.ts
+++ src/prompt-input/interfaces.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import { BaseComponentProps } from '../internal/base-component';
-import { BaseKeyDetail, NonCancelableEventHandler } from '../internal/events';
+import { BaseKeyDetail } from '../internal/events';
 import {
   BaseInputProps,
   InputAutoCorrect,
@@ -10,6 +10,7 @@ import {
   InputSpellcheck,
 } from '../input/interfaces';
 import { FormFieldValidationControlProps } from '../internal/context/form-field-context';
+import { IconProps } from '../icon/interfaces';
 
 export interface PromptInputProps
   extends BaseInputProps,
@@ -20,17 +21,23 @@ export interface PromptInputProps
     BaseComponentProps,
     FormFieldValidationControlProps {
   /**
-   * Called whenever a user clicks the send button or presses the "Enter" key.
+   * Called whenever a user clicks the action button or presses the "Enter" key.
    * The event `detail` contains the current value of the field.
    */
-  onSend?: NonCancelableEventHandler<PromptInputProps.ChangeDetail>;
+  onAction?: () => void;
+
+  actionButtonIconName: IconProps.Name;
 
   /**
-   * Specifies whether to disable the send button.
+   * Adds an aria-label to the action button.
+   * @i18n
    */
-  disableSendButton?: boolean;
+  actionButtonAriaLabel?: string;
 
-  onHeightChange?: NonCancelableEventHandler<HeightChangeDetail>;
+  /**
+   * Specifies whether to disable the action button.
+   */
+  disableActionButton?: boolean;
 
   /**
    * Specifies the minimum number of lines of text to set the height to.
@@ -41,33 +48,10 @@ export interface PromptInputProps
    * Specifies the maximum number of lines of text the textarea will expand to.
    */
   maxRows?: number;
-
-  /**
-   * Specifies whether to disable browser spellcheck feature.
-   * If you set this to `true`, it disables native browser capability
-   * that checks for spelling/grammar errors.
-   * If you don't set it, the behavior follows the default behavior
-   * of the user's browser.
-   *
-   * @deprecated Use the `spellcheck` property instead.
-   */
-  disableBrowserSpellcheck?: boolean;
-}
-
-export interface HeightChangeDetail {
-  height: number;
-  rowHeight: number;
 }
 
 export namespace PromptInputProps {
   export type KeyDetail = BaseKeyDetail;
-
-  export interface ChangeDetail {
-    /**
-     * The new value of this textarea.
-     */
-    value: string;
-  }
   export interface Ref {
     /**
      * Sets input focus on the textarea control.
