ffcd64525db108e362e034c407cf0b52b56a7fa1
Connor Lanigan <dev@connorlanigan.com>
2024-06-07 16:06:58 +0200

    chore: Connect performance marks to DOM elements (#2345)

--- src/internal/hooks/use-performance-marks/index.ts
+++ src/internal/hooks/use-performance-marks/index.ts
@@ -1,11 +1,35 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { useEffect } from 'react';
-import { useUniqueId } from './use-unique-id';
-import { useEffectOnUpdate } from './use-effect-on-update';
-import { useModalContext } from '../context/modal-context';
+import { useEffect, useRef } from 'react';
+import { useRandomId } from '../use-unique-id';
+import { useEffectOnUpdate } from '../use-effect-on-update';
+import { useModalContext } from '../../context/modal-context';
 
+/*
+This hook allows setting an HTML attribute after the first render, without rerendering the component.
+*/
+function usePerformanceMarkAttribute(elementRef: React.RefObject<HTMLElement>, value: string) {
+  const attributeName = 'data-analytics-performance-mark';
+
+  const attributeValueRef = useRef<string | undefined>();
+
+  useEffect(() => {
+    // With this effect, we apply the attribute only on the client, to avoid hydration errors.
+    attributeValueRef.current = value;
+    elementRef.current?.setAttribute(attributeName, value);
+  }, [value, elementRef]);
+
+  return {
+    [attributeName]: attributeValueRef.current,
+  };
+}
+
+/**
+ * This function returns an object that needs to be spread onto the same
+ * element as the `elementRef`, so that the data attribute is applied
+ * correctly.
+ */
 export function usePerformanceMarks(
   name: string,
   enabled: boolean,
@@ -13,8 +37,9 @@ export function usePerformanceMarks(
   getDetails: () => Record<string, string | boolean | number | undefined>,
   dependencies: React.DependencyList
 ) {
-  const id = useUniqueId();
+  const id = useRandomId();
   const { isInModal } = useModalContext();
+  const attributes = usePerformanceMarkAttribute(elementRef, id);
 
   useEffect(() => {
     if (!enabled || !elementRef.current || isInModal) {
@@ -66,4 +91,6 @@ export function usePerformanceMarks(
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, dependencies);
+
+  return attributes;
 }
