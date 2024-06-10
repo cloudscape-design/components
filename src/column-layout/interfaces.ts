9042e98e581ffd16792dabfe1f37535b89ff95a4
Georgii Lobko <globko@amazon.de>
2024-06-10 14:16:50 +0200

    Change tagOverride?: string to tagOverride?: 'dl'

--- src/column-layout/interfaces.ts
+++ src/column-layout/interfaces.ts
@@ -53,5 +53,5 @@ export interface InternalColumnLayoutProps extends ColumnLayoutProps, InternalBa
   /**
    * Overrides the default wrapper HTML tag.
    */
-  tagOverride?: string;
+  tagOverride?: 'dl';
 }
