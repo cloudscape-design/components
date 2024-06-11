261fadca2c2b03eac919c0d2cbe1d0dcf0bbe4d0
Timo <gasda@amazon.com>
2024-06-11 11:37:03 +0200

    Update demo

--- pages/prompt-input/simple.page.tsx
+++ pages/prompt-input/simple.page.tsx
@@ -1,7 +1,11 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import React, { useState } from 'react';
+import ColumnLayout from '~components/column-layout';
+import FormField from '~components/form-field';
 import PromptInput from '~components/prompt-input';
+
+const MAX_CHARS = 200;
 
 export default function PromptInputPage() {
   const [textareaValue, setTextareaValue] = useState('');
@@ -9,15 +13,27 @@ export default function PromptInputPage() {
   return (
     <div style={{ padding: 10 }}>
       <h1>PromptInput demo</h1>
-      <PromptInput
-        ariaLabel="textarea"
-        actionButtonIconName="send"
-        actionButtonAriaLabel="Submit prompt"
-        value={textareaValue}
-        onChange={(event: any) => setTextareaValue(event.detail.value)}
-        placeholder="Ask a question"
-        maxRows={6}
-      />
+      <ColumnLayout columns={2}>
+        <FormField
+          errorText={textareaValue.length > MAX_CHARS && 'The query has too many characters.'}
+          constraintText={
+            <>
+              This service is subject to some policy. Character count: {textareaValue.length}/{MAX_CHARS}
+            </>
+          }
+          label={<span>User prompt</span>}
+        >
+          <PromptInput
+            actionButtonIconName="send"
+            actionButtonAriaLabel="Submit prompt"
+            value={textareaValue}
+            onChange={(event: any) => setTextareaValue(event.detail.value)}
+            placeholder="Ask a question"
+            maxRows={6}
+          />
+        </FormField>
+        <div />
+      </ColumnLayout>
     </div>
   );
 }
