67cc8549cc5909b8fdffce9692271add71dc3d6b
Georgii Lobko <globko@amazon.de>
2024-06-11 15:55:05 +0200

    Align test-utils and u tests for a11y compatible KVP structure

--- src/key-value-pairs/internal.tsx
+++ src/key-value-pairs/internal.tsx
@@ -12,20 +12,20 @@ export { KeyValuePairsProps };
 
 const InternalKeyValuePair = ({ label, info, value }: KeyValuePairsProps.KeyValuePair) => (
   <>
-    <dt className={styles.label}>
+    <dt className={styles.term}>
       <label className={styles['key-label']}>{label}</label>
       <InfoLinkLabelContext.Provider value={label}>
         {info && <span className={styles.info}>{info}</span>}
       </InfoLinkLabelContext.Provider>
     </dt>
-    <dd className={styles.description}>{value}</dd>
+    <dd className={styles.detail}>{value}</dd>
   </>
 );
 
 const InternalKeyValuePairGroup = ({ label, value }: { label?: React.ReactNode; value: React.ReactNode }) => (
   <>
     {label && <dt className={styles.groupTitle}>{label}</dt>}
-    <dd className={styles.description}>{value}</dd>
+    <dd className={styles.detail}>{value}</dd>
   </>
 );
 
@@ -35,14 +35,15 @@ const InternalKeyValuePairs = React.forwardRef(
       <div {...rest} className={clsx(styles['key-value-pairs'], className)} ref={ref}>
         {/*
           minColumnWidth={150} is set to use FlexibleColumnLayout which has only 1 nested div wrapper for column items,
-          otherwise GridColumnLayout will be used which has 2 nested div, which is not a11y compatible for dl -> dt/dd relationship
+          otherwise GridColumnLayout will be used, which has 2 nested div, therefore it is not a11y compatible for dl -> dt/dd relationship
         */}
         <ColumnLayout tagOverride="dl" columns={Math.min(columns, 4)} variant="text-grid" minColumnWidth={150}>
           {items.map((pair, index) => {
             if ('items' in pair) {
               return (
-                /* InternalKeyValuePairGroup tells react to treat the dt-dd pair inside as an individual item.
+                /* InternalKeyValuePairGroup tells react to treat the dt-dd pair as an individual layout item.
                  * Otherwise, without this component, they will be rendered as a list, which ruins the html structure.
+                 * InternalKeyValuePairGroup is not wrapped by div tag, because it ruins a11y compatibility for dl -> dt/dd
                  *  */
                 <InternalKeyValuePairGroup
                   key={index}
@@ -54,9 +55,9 @@ const InternalKeyValuePairs = React.forwardRef(
                     )
                   }
                   value={
-                    <dl className={styles.list}>
+                    <dl className={styles.groupList}>
                       {pair.items.map((item, itemIndex) => (
-                        <div key={itemIndex}>
+                        <div key={itemIndex} className={styles.groupListItem}>
                           <InternalKeyValuePair {...item} />
                         </div>
                       ))}
