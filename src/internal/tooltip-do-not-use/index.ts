3338bd5584e70b4daa1346d515f74ffa2318b7ca
Cansu Aksu <cansua@amazon.com>
2024-06-17 12:30:15 +0200

    renamed component

--- src/internal/tooltip-do-not-use/index.ts
+++ src/internal/tooltip-do-not-use/index.ts
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import InternalTooltipDoNotUse, { TooltipProps } from '../components/tooltip/index.js';
+import InternalTooltip_DO_NOT_USE, { TooltipProps } from '../components/tooltip/index.js';
 
 export type InternalTooltipProps = TooltipProps;
-export default InternalTooltipDoNotUse;
+export default InternalTooltip_DO_NOT_USE;
