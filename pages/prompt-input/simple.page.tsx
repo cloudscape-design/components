3318603875e0ceea1a8a23a59056cf710908af6a
Katie George <katiegeo@amazon.com>
2024-06-24 11:55:23 -0700

    feat: Adds checkboxes for different states

--- pages/prompt-input/simple.page.tsx
+++ pages/prompt-input/simple.page.tsx
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import React, { useState } from 'react';
+import { Checkbox } from '~components';
 import ColumnLayout from '~components/column-layout';
 import FormField from '~components/form-field';
 import PromptInput from '~components/prompt-input';
@@ -9,10 +10,28 @@ const MAX_CHARS = 200;
 
 export default function PromptInputPage() {
   const [textareaValue, setTextareaValue] = useState('');
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [isReadOnly, setIsReadOnly] = useState(false);
+  const [isInvalid, setIsInvalid] = useState(false);
+  const [hasWarning, setHasWarning] = useState(false);
 
   return (
     <div style={{ padding: 10 }}>
       <h1>PromptInput demo</h1>
+      <FormField label="Settings">
+        <Checkbox checked={isDisabled} onChange={() => setIsDisabled(!isDisabled)}>
+          Disabled
+        </Checkbox>
+        <Checkbox checked={isReadOnly} onChange={() => setIsReadOnly(!isReadOnly)}>
+          Read-only
+        </Checkbox>
+        <Checkbox checked={isInvalid} onChange={() => setIsInvalid(!isInvalid)}>
+          Invalid
+        </Checkbox>
+        <Checkbox checked={hasWarning} onChange={() => setHasWarning(!hasWarning)}>
+          Warning
+        </Checkbox>
+      </FormField>
       <ColumnLayout columns={2}>
         <FormField
           errorText={textareaValue.length > MAX_CHARS && 'The query has too many characters.'}
@@ -28,8 +47,13 @@ export default function PromptInputPage() {
             actionButtonAriaLabel="Submit prompt"
             value={textareaValue}
             onChange={(event: any) => setTextareaValue(event.detail.value)}
+            onAction={event => window.alert(`Submitted the following: ${event.detail.value}`)}
             placeholder="Ask a question"
             maxRows={6}
+            disabled={isDisabled}
+            readOnly={isReadOnly}
+            invalid={isInvalid}
+            warning={hasWarning}
           />
         </FormField>
         <div />
