3a5d939a82c0085dd705894577762146184815f2
Georgii Lobko <globko@amazon.de>
2024-06-24 13:40:06 +0200

    feat: Introduce key-value-pairs component
    feat: Introduced key-value-pairs component
    
    feat: Introduced key-value-pairs styles

---
+++ src/key-value-pairs/interfaces.ts
@@ -0,0 +1,38 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { BaseComponentProps } from '../internal/base-component';
+import React from 'react';
+
+export interface KeyValuePairsProps extends BaseComponentProps {
+  /**
+   * Specifies the number of columns in each grid row.
+   * Valid values are any integer between 1 and 4. It defaults to 1.
+   */
+  columns?: number;
+  /**
+   * An array of either key-value pairs individual items or groups.
+   * They could be combined.
+   * Each key-value pair definition has the following properties:
+   *   * `label` (string) - The key label.
+   *   * `info` (React.ReactNode) - (Optional) Area next to the key to display an info link.
+   *   * `value` (React.ReactNode) - The corresponding value for the key.
+   * Each group definition has the following properties:
+   * * `title` (string) - (Optional) An optional title for this column.
+   * * `items` (ReadonlyArray<KeyValuePairProps.KeyValuePair>) - An array of
+   *     key-value pair items.
+   */
+  items: ReadonlyArray<KeyValuePairsProps.KeyValuePair | KeyValuePairsProps.Group>;
+}
+
+export namespace KeyValuePairsProps {
+  export interface Group {
+    title?: string;
+    items: ReadonlyArray<KeyValuePair>;
+  }
+
+  export interface KeyValuePair {
+    label: string;
+    value: React.ReactNode;
+    info?: React.ReactNode;
+  }
+}
---
+++ src/key-value-pairs/internal.tsx
@@ -0,0 +1,78 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.css.js';
+import { KeyValuePairsProps } from './interfaces';
+import { InfoLinkLabelContext } from '../internal/context/info-link-label-context';
+import ColumnLayout from '../column-layout/internal';
+import Box from '../box/internal';
+
+export { KeyValuePairsProps };
+
+const InternalKeyValuePair = ({ label, info, value }: KeyValuePairsProps.KeyValuePair) => (
+  <>
+    <dt className={styles.term}>
+      <label className={styles['key-label']}>{label}</label>
+      <InfoLinkLabelContext.Provider value={label}>
+        {info && <span className={styles.info}>{info}</span>}
+      </InfoLinkLabelContext.Provider>
+    </dt>
+    <dd className={styles.detail}>{value}</dd>
+  </>
+);
+
+const InternalKeyValuePairGroup = ({ label, value }: { label?: React.ReactNode; value: React.ReactNode }) => (
+  <>
+    {label && <dt className={styles['group-title']}>{label}</dt>}
+    <dd className={styles.detail}>{value}</dd>
+  </>
+);
+
+const InternalKeyValuePairs = React.forwardRef(
+  ({ columns = 1, items, className, ...rest }: KeyValuePairsProps, ref: React.Ref<HTMLDivElement>) => {
+    return (
+      <div {...rest} className={clsx(styles['key-value-pairs'], className)} ref={ref}>
+        {/*
+          minColumnWidth={150} is set to use FlexibleColumnLayout which has only 1 nested div wrapper for column items,
+          otherwise GridColumnLayout will be used, which has 2 nested div, therefore it is not a11y compatible for dl -> dt/dd relationship
+        */}
+        <ColumnLayout tagOverride="dl" columns={Math.min(columns, 4)} variant="text-grid" minColumnWidth={150}>
+          {items.map((pair, index) => {
+            if ('items' in pair) {
+              return (
+                /* InternalKeyValuePairGroup tells react to treat the dt-dd pair as an individual layout item.
+                 * Otherwise, without this component, they will be rendered as a list, which ruins the html structure.
+                 * InternalKeyValuePairGroup is not wrapped by div tag, because it ruins a11y compatibility for dl -> dt/dd
+                 *  */
+                <InternalKeyValuePairGroup
+                  key={index}
+                  label={
+                    pair.title && (
+                      <Box variant="h3" padding="n">
+                        {pair.title}
+                      </Box>
+                    )
+                  }
+                  value={
+                    <dl className={styles['group-list']}>
+                      {pair.items.map((item, itemIndex) => (
+                        <div key={itemIndex} className={styles['group-list-item']}>
+                          <InternalKeyValuePair {...item} />
+                        </div>
+                      ))}
+                    </dl>
+                  }
+                />
+              );
+            }
+
+            return <InternalKeyValuePair key={index} {...pair} />;
+          })}
+        </ColumnLayout>
+      </div>
+    );
+  }
+);
+
+export default InternalKeyValuePairs;
