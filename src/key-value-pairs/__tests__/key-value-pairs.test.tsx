c18cea930f39b8a819d6e523860ce13add2ba851
Georgii Lobko <globko@amazon.de>
2024-06-24 13:41:21 +0200

    feat: Introduced key-value-pairs unit tests

---
+++ src/key-value-pairs/__tests__/key-value-pairs.test.tsx
@@ -0,0 +1,146 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import createWrapper from '../../../lib/components/test-utils/dom';
+import KeyValuePairs from '../../../lib/components/key-value-pairs';
+
+function renderKeyValuePairs(jsx: React.ReactElement) {
+  const { container } = render(jsx);
+  return { wrapper: createWrapper(container).findKeyValuePairs()! };
+}
+
+describe('KeyValuePairs', () => {
+  describe('item rendering', () => {
+    test('renders correctly', () => {
+      const { wrapper } = renderKeyValuePairs(
+        <KeyValuePairs
+          items={[
+            {
+              label: 'Label for key',
+              value: 'Value',
+            },
+          ]}
+        />
+      );
+
+      expect(wrapper.findItems()[0]!.findLabel()!.getElement()).toHaveTextContent('Label for key');
+      expect(wrapper.findItems()[0]!.findValue()!.getElement()).toHaveTextContent('Value');
+    });
+
+    test('renders label with info correctly', () => {
+      const { wrapper } = renderKeyValuePairs(
+        <KeyValuePairs
+          items={[
+            {
+              label: 'Label for key',
+              value: 'Value',
+              info: 'Info',
+            },
+          ]}
+        />
+      );
+
+      expect(wrapper.findItems()[0]!.findInfo()!.getElement()).toHaveTextContent('Info');
+    });
+  });
+
+  describe('column layout', () => {
+    test('renders correctly', () => {
+      const { wrapper } = renderKeyValuePairs(
+        <KeyValuePairs
+          items={[
+            {
+              title: 'Title',
+              items: [
+                {
+                  label: 'Label for key',
+                  value: 'Value',
+                },
+                {
+                  label: 'Label for key',
+                  value: 'Value',
+                },
+              ],
+            },
+            {
+              items: [
+                {
+                  label: 'Label for key',
+                  value: 'Value',
+                },
+              ],
+            },
+          ]}
+        />
+      );
+
+      expect(wrapper.findItems()).toHaveLength(2);
+      expect(wrapper.findItems()[0].findGroupTitle()!.getElement()).toHaveTextContent('Title');
+      expect(wrapper.findItems()[0].findGroupPairs()).toHaveLength(2);
+      expect(wrapper.findItems()[1].findGroupPairs()).toHaveLength(1);
+    });
+  });
+
+  describe('flat layout', () => {
+    test('renders correctly', () => {
+      const { wrapper } = renderKeyValuePairs(
+        <KeyValuePairs
+          items={[
+            {
+              label: 'Label for key',
+              value: 'Value',
+            },
+            {
+              label: 'Label for key',
+              value: 'Value',
+            },
+            {
+              label: 'Label for key',
+              value: 'Value',
+            },
+          ]}
+        />
+      );
+
+      expect(wrapper.findItems()).toHaveLength(3);
+    });
+  });
+
+  describe('combined layout', () => {
+    test('renders correctly', () => {
+      const { wrapper } = renderKeyValuePairs(
+        <KeyValuePairs
+          items={[
+            {
+              title: 'Column title',
+              items: [
+                { label: 'Column label 1', value: 'Column value 1' },
+                { label: 'Column label 2', value: 'Column value 2' },
+              ],
+            },
+            {
+              label: 'Label for key',
+              value: 'Value',
+            },
+            {
+              label: 'Label for key',
+              value: 'Value',
+            },
+            {
+              label: 'Label for key',
+              value: 'Value',
+            },
+          ]}
+        />
+      );
+
+      expect(wrapper.findItems()).toHaveLength(4);
+      expect(wrapper.findItems()[0].findGroupPairs()).toHaveLength(2);
+      expect(wrapper.findItems()[0].findGroupPairs()![0].findLabel()!.getElement()).toHaveTextContent('Column label 1');
+      expect(wrapper.findItems()[0].findGroupPairs()![0].findValue()!.getElement()).toHaveTextContent('Column value 1');
+      expect(wrapper.findItems()[1]!.findLabel()!.getElement()).toHaveTextContent('Label for key');
+      expect(wrapper.findItems()[1]!.findValue()!.getElement()).toHaveTextContent('Value');
+    });
+  });
+});
