8c5c3055b08f749c1b66fb2864b61a16ed5120b8
Georgii Lobko <globko@amazon.de>
2024-06-25 10:36:17 +0200

    Add disabled reason examples for select and multiselect
    Add disabled reason examples for select/multiselect
    
    Fixe typo in disabled reason multiselect page

---
+++ pages/select/disabled-reason.page.tsx
@@ -0,0 +1,34 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import * as React from 'react';
+import Select from '~components/select';
+import Box from '~components/box';
+import ScreenshotArea from '../utils/screenshot-area';
+
+const options = [
+  {
+    value: '1',
+    label: 'Option 1',
+    disabled: true,
+    disabledReason: 'disabled reason',
+  },
+  {
+    value: '2',
+    label: 'Option 2',
+  },
+];
+
+export default function SelectPage() {
+  return (
+    <article>
+      <ScreenshotArea>
+        <Box variant="h1">Select with disabled reason</Box>
+        <Box padding="l">
+          <div style={{ width: '400px' }}>
+            <Select placeholder="Choose option" selectedOption={null} options={options} />
+          </div>
+        </Box>
+      </ScreenshotArea>
+    </article>
+  );
+}
