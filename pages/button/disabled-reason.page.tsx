e3e96538f1e4f908744bf94fc94414eb395bd455
Georgii Lobko <globko@amazon.de>
2024-06-25 10:07:14 +0200

    Add disabled reason page for button component

---
+++ pages/button/disabled-reason.page.tsx
@@ -0,0 +1,23 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React from 'react';
+import Button from '~components/button';
+import ScreenshotArea from '../utils/screenshot-area';
+import styles from './styles.scss';
+
+export default function ButtonsScenario() {
+  return (
+    <article>
+      <ScreenshotArea>
+        <h1 className={styles.styledWrapper}>Buttons with disabled reason</h1>
+        <Button variant="primary" disabled={true} disabledReason="disabled reason">
+          Primary
+        </Button>
+        <Button disabled={true} disabledReason="disabled reason">
+          Default
+        </Button>
+        <Button iconName="settings" disabled={true} disabledReason="disabled reason" />
+      </ScreenshotArea>
+    </article>
+  );
+}
