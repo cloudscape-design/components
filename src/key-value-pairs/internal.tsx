eee2721b191b44d57543195b8f8fab61aed303b9
Georgii Lobko <globko@amazon.de>
2024-07-01 10:53:44 +0200

    chore: Default all links to primary variant in KVP component

--- src/key-value-pairs/internal.tsx
+++ src/key-value-pairs/internal.tsx
@@ -6,6 +6,7 @@ import styles from './styles.css.js';
 import { KeyValuePairsProps } from './interfaces';
 import { InfoLinkLabelContext } from '../internal/context/info-link-label-context';
 import ColumnLayout from '../column-layout/internal';
+import { LinkDefaultVariantContext } from '../internal/context/link-default-variant-context';
 import Box from '../box/internal';
 
 const InternalKeyValuePair = ({ label, info, value }: KeyValuePairsProps.Pair) => (
@@ -33,45 +34,47 @@ const InternalKeyValuePairs = React.forwardRef(
     ref: React.Ref<HTMLDivElement>
   ) => {
     return (
-      <div {...rest} className={clsx(styles['key-value-pairs'], className)} ref={ref}>
-        {/*
+      <LinkDefaultVariantContext.Provider value={{ defaultVariant: 'primary' }}>
+        <div {...rest} className={clsx(styles['key-value-pairs'], className)} ref={ref}>
+          {/*
           minColumnWidth={150} is set to use FlexibleColumnLayout which has only 1 nested div wrapper for column items,
           otherwise GridColumnLayout will be used, which has 2 nested div, therefore it is not a11y compatible for dl -> dt/dd relationship
         */}
-        <ColumnLayout tagOverride="dl" columns={Math.min(columns, 4)} variant="text-grid" minColumnWidth={150}>
-          {items.map((pair, index) => {
-            if (pair.type === 'group') {
-              return (
-                /* InternalKeyValuePairGroup tells react to treat the dt-dd pair as an individual layout item.
-                 * Otherwise, without this component, they will be rendered as a list, which ruins the html structure.
-                 * InternalKeyValuePairGroup is not wrapped by div tag, because it ruins a11y compatibility for dl -> dt/dd
-                 *  */
-                <InternalKeyValuePairGroup
-                  key={index}
-                  label={
-                    pair.title && (
-                      <Box variant="h3" padding="n">
-                        {pair.title}
-                      </Box>
-                    )
-                  }
-                  value={
-                    <dl className={styles['group-list']}>
-                      {pair.items.map((item, itemIndex) => (
-                        <div key={itemIndex} className={styles['group-list-item']}>
-                          <InternalKeyValuePair {...item} />
-                        </div>
-                      ))}
-                    </dl>
-                  }
-                />
-              );
-            }
+          <ColumnLayout tagOverride="dl" columns={Math.min(columns, 4)} variant="text-grid" minColumnWidth={150}>
+            {items.map((pair, index) => {
+              if (pair.type === 'group') {
+                return (
+                  /* InternalKeyValuePairGroup tells react to treat the dt-dd pair as an individual layout item.
+                   * Otherwise, without this component, they will be rendered as a list, which ruins the html structure.
+                   * InternalKeyValuePairGroup is not wrapped by div tag, because it ruins a11y compatibility for dl -> dt/dd
+                   *  */
+                  <InternalKeyValuePairGroup
+                    key={index}
+                    label={
+                      pair.title && (
+                        <Box variant="h3" padding="n">
+                          {pair.title}
+                        </Box>
+                      )
+                    }
+                    value={
+                      <dl className={styles['group-list']}>
+                        {pair.items.map((item, itemIndex) => (
+                          <div key={itemIndex} className={styles['group-list-item']}>
+                            <InternalKeyValuePair {...item} />
+                          </div>
+                        ))}
+                      </dl>
+                    }
+                  />
+                );
+              }
 
-            return <InternalKeyValuePair key={index} {...pair} />;
-          })}
-        </ColumnLayout>
-      </div>
+              return <InternalKeyValuePair key={index} {...pair} />;
+            })}
+          </ColumnLayout>
+        </div>
+      </LinkDefaultVariantContext.Provider>
     );
   }
 );
