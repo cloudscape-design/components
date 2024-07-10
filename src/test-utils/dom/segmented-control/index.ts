84f284fccd3048b641317807654d864129e999ae
Georgii Lobko <47106899+georgylobko@users.noreply.github.com>
2024-07-10 14:45:55 +0200

    feat: Disabled reason for SegmentedControl (#2414)

--- src/test-utils/dom/segmented-control/index.ts
+++ src/test-utils/dom/segmented-control/index.ts
@@ -1,7 +1,13 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { ComponentWrapper, ElementWrapper } from '@cloudscape-design/test-utils-core/dom';
+import { ComponentWrapper, ElementWrapper, createWrapper } from '@cloudscape-design/test-utils-core/dom';
 import styles from '../../../segmented-control/styles.selectors.js';
+
+export class SegmentWrapper extends ComponentWrapper {
+  findDisabledReason(): ElementWrapper | null {
+    return createWrapper().find(`.${styles['disabled-reason-tooltip']}`);
+  }
+}
 
 export default class SegmentedControlWrapper extends ComponentWrapper {
   static rootSelector: string = styles.root;
@@ -12,5 +18,14 @@ export default class SegmentedControlWrapper extends ComponentWrapper {
 
   findSelectedSegment(): ElementWrapper | null {
     return this.findByClassName(styles.selected);
+  }
+
+  /**
+   * Finds the segment with the given ID.
+   *
+   * @param id ID of the element to return.
+   */
+  findSegmentById(id: string): SegmentWrapper | null {
+    return this.findComponent(`.${styles.segment}[data-testid="${id}"]`, SegmentWrapper);
   }
 }
