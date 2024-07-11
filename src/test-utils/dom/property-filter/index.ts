1aded8f261124f4cbe31a30c1c30ddff58f8c253
Andrei Zhaleznichenka <zhalea@amazon.de>
2024-07-11 11:40:49 +0200

    wip: Add new draft test-utils to property filter

--- src/test-utils/dom/property-filter/index.ts
+++ src/test-utils/dom/property-filter/index.ts
@@ -1,14 +1,14 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { ElementWrapper } from '@cloudscape-design/test-utils-core/dom';
+import { ComponentWrapper, ElementWrapper } from '@cloudscape-design/test-utils-core/dom';
 
 import styles from '../../../property-filter/styles.selectors.js';
 import tokenListSelectors from '../../../internal/components/token-list/styles.selectors.js';
 import textFilterStyles from '../../../text-filter/styles.selectors.js';
-
 import AutosuggestWrapper from '../autosuggest';
-
 import FilteringTokenWrapper from '../internal/filtering-token';
+import ButtonWrapper from '../button';
+import FormFieldWrapper from '../form-field';
 
 export default class PropertyFilterWrapper extends AutosuggestWrapper {
   static rootSelector = styles.root;
@@ -22,12 +22,14 @@ export default class PropertyFilterWrapper extends AutosuggestWrapper {
       (elementWrapper: ElementWrapper) => new FilteringTokenWrapper(elementWrapper.getElement())
     );
   }
+
   /**
    * Returns the button that toggles if the tokens above `tokenLimit` are visible.
    */
   findTokenToggle(): ElementWrapper | null {
     return this.findByClassName(tokenListSelectors.toggle);
   }
+
   /**
    * Returns the button that removes all current tokens.
    */
@@ -54,5 +56,33 @@ export default class PropertyFilterWrapper extends AutosuggestWrapper {
    */
   findConstraint(): ElementWrapper | null {
     return this.findByClassName(styles.constraint);
+  }
+
+  findEditorDropdown(): null | PropertyFilterEditorDropdownWrapper {
+    return this.findComponent(`.${styles['token-editor']}`, PropertyFilterEditorDropdownWrapper);
+  }
+}
+
+export class PropertyFilterEditorDropdownWrapper extends ComponentWrapper {
+  static rootSelector = styles['token-editor'];
+
+  findPropertyField(): FormFieldWrapper {
+    return this.findComponent(`.${styles['token-editor-field-property']}`, FormFieldWrapper)!;
+  }
+
+  findOperatorField(): FormFieldWrapper {
+    return this.findComponent(`.${styles['token-editor-field-operator']}`, FormFieldWrapper)!;
+  }
+
+  findValueField(): FormFieldWrapper {
+    return this.findComponent(`.${styles['token-editor-field-operator']}`, FormFieldWrapper)!;
+  }
+
+  findCancelButton(): ButtonWrapper {
+    return this.findComponent(`.${styles['token-editor-cancel']}`, ButtonWrapper)!;
+  }
+
+  findSubmitButton(): ButtonWrapper {
+    return this.findComponent(`.${styles['token-editor-submit']}`, ButtonWrapper)!;
   }
 }
