1dcf65791464d1c3ed0e0058de49c0f5953e7f23
Sergey Pugachev <1313155+spugachev@users.noreply.github.com>
2024-07-11 06:49:32 +0100

    feat: support for React.ReactNode values in the internal tooltip (#2455)

--- src/internal/components/tooltip/__tests__/tooltip.test.tsx
+++ src/internal/components/tooltip/__tests__/tooltip.test.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Tooltip, { TooltipProps } from '../../../../../lib/components/internal/components/tooltip';
+import StatusIndicator from '../../../../../lib/components/status-indicator';
 import createWrapper, { ElementWrapper, PopoverWrapper } from '../../../../../lib/components/test-utils/dom';
 import styles from '../../../../../lib/components/popover/styles.selectors.js';
 import tooltipStyles from '../../../../../lib/components/internal/components/tooltip/styles.selectors.js';
@@ -25,7 +26,12 @@ class TooltipInternalWrapper extends PopoverWrapper {
 const dummyRef = { current: null };
 function renderTooltip(props: Partial<TooltipProps>) {
   const { container } = render(
-    <Tooltip trackRef={dummyRef} value={props.value ?? ''} contentAttributes={props.contentAttributes} />
+    <Tooltip
+      trackRef={dummyRef}
+      trackKey={props.trackKey}
+      value={props.value ?? ''}
+      contentAttributes={props.contentAttributes}
+    />
   );
   return new TooltipInternalWrapper(container);
 }
@@ -35,6 +41,13 @@ describe('Tooltip', () => {
     const wrapper = renderTooltip({ value: 'Value' });
 
     expect(wrapper.findContent()!.getElement()).toHaveTextContent('Value');
+  });
+
+  it('renders node correctly', () => {
+    const wrapper = renderTooltip({ value: <StatusIndicator type="success">Success</StatusIndicator> });
+    const statusIndicatorWrapper = createWrapper(wrapper.findContent()!.getElement()).findStatusIndicator()!;
+
+    expect(statusIndicatorWrapper.getElement()).toHaveTextContent('Success');
   });
 
   it('renders arrow', () => {
@@ -53,5 +66,18 @@ describe('Tooltip', () => {
     const wrapper = renderTooltip({ value: 'Value', contentAttributes: { title: 'test' } });
 
     expect(wrapper.findTooltip()?.getElement()).toHaveAttribute('title', 'test');
+  });
+
+  it('trackKey is set correctly for strings', () => {
+    const wrapper = renderTooltip({ value: 'Value' });
+
+    expect(wrapper.findTooltip()?.getElement()).toHaveAttribute('data-testid', 'Value');
+  });
+
+  it('trackKey is set correctly for explicit value', () => {
+    const trackKey = 'test-track-key';
+    const wrapper = renderTooltip({ value: 'Value', trackKey });
+
+    expect(wrapper.findTooltip()?.getElement()).toHaveAttribute('data-testid', trackKey);
   });
 });
