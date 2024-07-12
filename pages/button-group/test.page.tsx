756cd2beec41fb09dd0bce63e8639df21675c564
Sergey Pugachev <1313155+spugachev@users.noreply.github.com>
2024-07-12 15:02:43 +0100

    button-group component

---
+++ pages/button-group/test.page.tsx
@@ -0,0 +1,117 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import * as React from 'react';
+import ButtonGroup, { ButtonGroupProps } from '~components/button-group';
+import ScreenshotArea from '../utils/screenshot-area';
+import { Button, StatusIndicator } from '~components';
+
+const items: ButtonGroupProps.Item[] = [
+  {
+    type: 'group',
+    text: 'Vote',
+    items: [
+      {
+        type: 'icon-button',
+        id: 'like',
+        iconName: 'thumbs-up',
+        text: 'Like',
+        feedbackPopover: <StatusIndicator type="success">Liked</StatusIndicator>,
+      },
+      {
+        type: 'icon-button',
+        id: 'dislike',
+        iconName: 'thumbs-down',
+        text: 'Dislike',
+        feedbackPopover: <StatusIndicator type="error">Disliked</StatusIndicator>,
+      },
+    ],
+  },
+  {
+    type: 'icon-button',
+    id: 'copy',
+    iconName: 'copy',
+    text: 'Copy',
+    feedbackPopover: <StatusIndicator type="success">Copied</StatusIndicator>,
+  },
+  {
+    type: 'group',
+    text: 'Actions',
+    items: [
+      {
+        type: 'icon-button',
+        id: 'add',
+        iconName: 'add-plus',
+        text: 'Add',
+        feedbackPopover: 'Added',
+        disabled: true,
+      },
+      {
+        type: 'icon-button',
+        id: 'remove',
+        iconName: 'remove',
+        text: 'Remove',
+        feedbackPopover: 'Removed',
+      },
+    ],
+  },
+  {
+    type: 'menu-dropdown',
+    id: 'more-actions',
+    text: 'More actions',
+    items: [
+      {
+        id: 'cut',
+        iconName: 'delete-marker',
+        text: 'Cut',
+      },
+      {
+        id: 'paste',
+        iconName: 'add-plus',
+        text: 'Paste',
+        disabled: true,
+        disabledReason: 'No content to paste',
+      },
+      {
+        text: 'Misc',
+        items: [
+          { id: 'edit', iconName: 'edit', text: 'Edit' },
+          { id: 'open', iconName: 'file-open', text: 'Open' },
+          { id: 'search', iconName: 'search', text: 'Search' },
+        ],
+      },
+    ],
+  },
+];
+
+export default function ButtonGroupPage() {
+  const ref = React.useRef<ButtonGroupProps.Ref>(null);
+
+  const onItemClick: ButtonGroupProps['onItemClick'] = event => {
+    document.querySelector('#last-clicked')!.textContent = event.detail.id;
+  };
+
+  const onFocusOnCopyButtonClick = () => {
+    ref.current?.focus('copy');
+  };
+
+  const onFocusOnSearchButtonClick = () => {
+    ref.current?.focus('search');
+  };
+
+  return (
+    <>
+      <ScreenshotArea disableAnimations={true}>
+        <article>
+          <h1>Button Group test page</h1>
+          <ButtonGroup ariaLabel="Chat actions" variant="icon" items={items} onItemClick={onItemClick} ref={ref} />
+          <br />
+          <Button onClick={onFocusOnCopyButtonClick}>Focus on copy</Button>&nbsp;
+          <Button onClick={onFocusOnSearchButtonClick}>Focus on item in the menu</Button>
+          <br />
+          <br />
+          <div id="last-clicked"></div>
+        </article>
+      </ScreenshotArea>
+    </>
+  );
+}
---
+++ src/button-group/icon-button-item.tsx
@@ -0,0 +1,44 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React, { forwardRef } from 'react';
+import { ButtonGroupProps } from './interfaces.js';
+import { InternalButton } from '../button/internal.js';
+import { ButtonProps } from '../button/interfaces.js';
+import { ClickDetail } from '../internal/events/index.js';
+import styles from './styles.css.js';
+
+const IconButtonItem = forwardRef(
+  (
+    {
+      item,
+      onItemClick,
+    }: {
+      item: ButtonGroupProps.IconButton;
+      onItemClick?: (event: CustomEvent<ClickDetail>) => void;
+    },
+    ref: React.Ref<ButtonProps.Ref>
+  ) => {
+    const hasIcon = item.iconName || item.iconUrl || item.iconSvg;
+
+    return (
+      <InternalButton
+        variant="icon"
+        loading={item.loading}
+        loadingText={item.loadingText}
+        disabled={item.disabled}
+        iconName={hasIcon ? item.iconName : 'close'}
+        iconAlt={item.text}
+        iconSvg={item.iconSvg}
+        ariaLabel={item.text}
+        onClick={onItemClick}
+        ref={ref}
+        data-testid={item.id}
+        className={styles.item}
+      >
+        {item.text}
+      </InternalButton>
+    );
+  }
+);
+
+export default IconButtonItem;
