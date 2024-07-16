2adf08ef1a2fd89b4de83a579eb64cf8713364d4
Sergey Pugachev <1313155+spugachev@users.noreply.github.com>
2024-07-16 17:13:13 +0100

    feat(button-group) tests

--- src/button-group/__tests__/button-group.test.tsx
+++ src/button-group/__tests__/button-group.test.tsx
@@ -1,12 +1,10 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-test('true', () => {
-  expect(true).toBeTruthy();
-});
-/*
-import React, { useRef } from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import { KeyCode } from '@cloudscape-design/component-toolkit/internal';
 import ButtonGroup, { ButtonGroupProps } from '../../../lib/components/button-group';
+import buttonStyles from '../../../lib/components/button/styles.css.js';
 import createWrapper from '../../../lib/components/test-utils/dom';
 
 const renderButtonGroup = (props: ButtonGroupProps, ref?: React.Ref<ButtonGroupProps.Ref>) => {
@@ -14,20 +12,32 @@ const renderButtonGroup = (props: ButtonGroupProps, ref?: React.Ref<ButtonGroupP
   return createWrapper(renderResult.container).findButtonGroup()!;
 };
 
-
 const items1: ButtonGroupProps.Item[] = [
   {
     type: 'group',
     text: 'Feedback',
     items: [
       { type: 'icon-button', id: 'like', text: 'Like', iconName: 'thumbs-up', popoverFeedback: 'Liked' },
-      { type: 'icon-button', id: 'dislike', text: 'dislike', iconName: 'thumbs-down', popoverFeedback: 'Disliked' },
+      {
+        type: 'icon-button',
+        id: 'dislike',
+        disabled: true,
+        text: 'dislike',
+        iconName: 'thumbs-down',
+        popoverFeedback: 'Disliked',
+      },
     ],
   },
   { type: 'icon-button', id: 'copy', iconName: 'copy', text: 'Copy', popoverFeedback: 'Copied' },
   { type: 'icon-button', id: 'edit', iconName: 'edit', text: 'Edit' },
-  { type: 'icon-button', id: 'open', iconName: 'file-open', text: 'Open' },
-  { type: 'icon-button', id: 'search', iconName: 'search', text: 'Search' },
+  {
+    type: 'icon-button',
+    id: 'test-button',
+    iconName: 'file-open',
+    text: 'Test Button',
+    popoverFeedback: 'Action Popover',
+  },
+  { type: 'icon-button', id: 'search', text: 'Search' },
   {
     type: 'menu-dropdown',
     id: 'misc',
@@ -48,44 +58,133 @@ test('renders all items', () => {
   expect(wrapper.findMenuById('misc')).not.toBe(null);
 });
 
+test('renders stub icon when no icon specified', () => {
+  const wrapper = renderButtonGroup({ variant: 'icon', items: items1, ariaLabel: 'Chat actions' });
+
+  expect(wrapper.findMenuById('search')?.findAll(`.${buttonStyles.icon}`)).toHaveLength(1);
+});
+
 describe('focus', () => {
   test('focuses the correct item', () => {
-    const TestComponent = () => {
-      const ref = useRef<ButtonGroupProps.Ref>(null);
+    const ref: { current: ButtonGroupProps.Ref | null } = { current: null };
+    const wrapper = renderButtonGroup({ variant: 'icon', items: items1, ariaLabel: 'Chat actions' }, ref);
+    ref.current?.focus('copy');
 
-      return (
-        <div>
-          <button onClick={() => ref.current?.focus('copy')}>Focus on copy</button>
-          <ButtonGroup ref={ref} variant="icon" items={items1} ariaLabel="Chat actions" />
-        </div>
-      );
-    };
-
-    const { container } = render(<TestComponent />);
-    const buttonGroup = createWrapper(container).findButtonGroup()!;
-    fireEvent.click(screen.getByText('Focus on copy'));
-
-    expect(buttonGroup.findButtonById('copy')!.getElement()).toHaveFocus();
+    expect(wrapper.findButtonById('copy')!.getElement()).toHaveFocus();
   });
 
   test('focuses on show more button', () => {
-    const TestComponent = () => {
-      const ref = useRef<ButtonGroupProps.Ref>(null);
+    const ref: { current: ButtonGroupProps.Ref | null } = { current: null };
+    const wrapper = renderButtonGroup({ variant: 'icon', items: items1, ariaLabel: 'Chat actions' }, ref);
+    ref.current?.focus('misc');
 
-      return (
-        <div>
-          <button onClick={() => ref.current?.focus('misc')}>Focus on misc</button>
-          <ButtonGroup ref={ref} variant="icon" items={items1} ariaLabel="Chat actions" />
-        </div>
-      );
-    };
+    expect(wrapper.findMenuById('misc')!.getElement().getElementsByTagName('button')[0]).toHaveFocus();
+  });
 
-    const { container } = render(<TestComponent />);
-    const buttonGroup = createWrapper(container).findButtonGroup()!;
+  test('focuses the correct item', () => {
+    const ref: { current: ButtonGroupProps.Ref | null } = { current: null };
+    const wrapper = renderButtonGroup({ variant: 'icon', items: items1, ariaLabel: 'Chat actions' }, ref);
+    ref.current?.focus('copy');
+    console.log(wrapper.getElement());
 
-    fireEvent.click(screen.getByText('Focus on misc'));
-    const showMoreButton = buttonGroup.findMenuById('misc')!.getElement();
-    expect(showMoreButton.getElementsByTagName('button')[0]).toHaveFocus();
+    fireEvent.keyDown(wrapper.getElement(), { keyCode: KeyCode.right });
+    expect(wrapper.findButtonById('edit')!.getElement()).toHaveFocus();
+    fireEvent.keyDown(wrapper.getElement(), { keyCode: KeyCode.left });
+    expect(wrapper.findButtonById('copy')!.getElement()).toHaveFocus();
+    fireEvent.keyDown(wrapper.getElement(), { keyCode: KeyCode.home });
+    expect(wrapper.findButtonById('like')!.getElement()).toHaveFocus();
+    fireEvent.keyDown(wrapper.getElement(), { keyCode: KeyCode.end });
+    expect(wrapper.findMenuById('misc')!.getElement().getElementsByTagName('button')[0]).toHaveFocus();
+    ref.current?.focus('like');
+    fireEvent.keyDown(wrapper.getElement(), { keyCode: KeyCode.right });
+    expect(wrapper.findButtonById('copy')!.getElement()).toHaveFocus();
+    fireEvent.keyDown(wrapper.getElement(), { keyCode: KeyCode.space });
+    expect(wrapper.findButtonById('copy')!.getElement()).toHaveFocus();
+    fireEvent.keyDown(wrapper.getElement(), { keyCode: KeyCode.left, ctrlKey: true });
+    expect(wrapper.findButtonById('copy')!.getElement()).toHaveFocus();
   });
 });
-*/
+
+describe('tooltips', () => {
+  test('tooltip not shown by default', () => {
+    const wrapper = renderButtonGroup({ variant: 'icon', items: items1, ariaLabel: 'Chat actions' });
+
+    expect(wrapper.findTooltip()).toBeNull();
+  });
+
+  test('shows the tooltip on pointer enter', () => {
+    const wrapper = renderButtonGroup({ variant: 'icon', items: items1, ariaLabel: 'Chat actions' });
+    const button = wrapper.findButtonById('test-button')!;
+    fireEvent.pointerEnter(button.getElement());
+
+    expect(wrapper.findTooltip()).not.toBeNull();
+    expect(wrapper.findTooltip()!.getElement()).toHaveTextContent('Test Button');
+  });
+
+  test('hides the tooltip on pointer leave', () => {
+    const wrapper = renderButtonGroup({ variant: 'icon', items: items1, ariaLabel: 'Chat actions' });
+    const button = wrapper.findButtonById('test-button')!;
+    fireEvent.pointerEnter(button.getElement());
+    fireEvent.pointerLeave(button.getElement());
+
+    expect(wrapper.findTooltip()).toBeNull();
+  });
+
+  test('shows popover on click', () => {
+    const wrapper = renderButtonGroup({ variant: 'icon', items: items1, ariaLabel: 'Chat actions' });
+    const button = wrapper.findButtonById('test-button')!;
+    fireEvent.click(button.getElement());
+    fireEvent.pointerLeave(button.getElement());
+
+    expect(wrapper.findTooltip()).not.toBeNull();
+    expect(wrapper.findTooltip()!.getElement()).toHaveTextContent('Action Popover');
+  });
+
+  test('shows no popover on click if popover not defined', () => {
+    const wrapper = renderButtonGroup({ variant: 'icon', items: items1, ariaLabel: 'Chat actions' });
+    const button = wrapper.findButtonById('search')!;
+    fireEvent.click(button.getElement());
+
+    expect(wrapper.findTooltip()).toBeNull();
+  });
+
+  test('closes popover on pointer down', () => {
+    const wrapper = renderButtonGroup({ variant: 'icon', items: items1, ariaLabel: 'Chat actions' });
+    const button = wrapper.findButtonById('test-button')!;
+    fireEvent.click(button.getElement());
+
+    expect(wrapper.findTooltip()).not.toBeNull();
+    fireEvent.pointerDown(document);
+    expect(wrapper.findTooltip()).toBeNull();
+  });
+
+  test('not closes popover on pointer down on the button', () => {
+    const wrapper = renderButtonGroup({ variant: 'icon', items: items1, ariaLabel: 'Chat actions' });
+    const button = wrapper.findButtonById('test-button')!;
+    fireEvent.click(button.getElement());
+
+    expect(wrapper.findTooltip()).not.toBeNull();
+    fireEvent.pointerDown(button.getElement());
+    expect(wrapper.findTooltip()).not.toBeNull();
+  });
+
+  test('closes popover on toggle event', () => {
+    const wrapper = renderButtonGroup({ variant: 'icon', items: items1, ariaLabel: 'Chat actions' });
+    const button = wrapper.findButtonById('test-button')!;
+    fireEvent.click(button.getElement());
+
+    expect(wrapper.findTooltip()).not.toBeNull();
+    window.dispatchEvent(new CustomEvent('btngroup-tooltip:toggle', { detail: { open: true, trackKey: '123' } }));
+    expect(wrapper.findTooltip()).toBeNull();
+  });
+
+  test('closes popover on esc key', () => {
+    const wrapper = renderButtonGroup({ variant: 'icon', items: items1, ariaLabel: 'Chat actions' });
+    const button = wrapper.findButtonById('test-button')!;
+    fireEvent.click(button.getElement());
+
+    expect(wrapper.findTooltip()).not.toBeNull();
+    fireEvent.keyDown(window, { key: 'Escape' });
+    expect(wrapper.findTooltip()).toBeNull();
+  });
+});
