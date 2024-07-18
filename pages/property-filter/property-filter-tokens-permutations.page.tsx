e6c9cd2cc239a7cc4752c8e23672843050d1e7c2
Andrei Zhaleznichenka <andrei.zhaleznichenka@gmail.com>
2024-07-18 10:49:34 +0200

    token list permutations

--- pages/property-filter/property-filter-tokens-permutations.page.tsx
+++ pages/property-filter/property-filter-tokens-permutations.page.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import { Popover } from '~components';
 import FilteringToken, { FilteringTokenProps } from '~components/property-filter/filtering-token';
+import TokenList, { TokenListProps } from '~components/internal/components/token-list';
 import ScreenshotArea from '../utils/screenshot-area';
 import createPermutations from '../utils/permutations';
 import PermutationsView from '../utils/permutations-view';
@@ -25,7 +26,7 @@ const token3 = {
   dismissAriaLabel: 'remove filter property3 = value',
 } as const;
 
-const defaultProps: FilteringTokenProps = {
+const defaultTokenProps: FilteringTokenProps = {
   tokens: [],
   operation: 'and',
   groupOperation: 'or',
@@ -40,7 +41,7 @@ const defaultProps: FilteringTokenProps = {
   disabled: false,
 };
 
-const permutations = createPermutations<Partial<FilteringTokenProps>>([
+const tokenPermutations = createPermutations<Partial<FilteringTokenProps>>([
   // Single token with and show operation
   {
     tokens: [[token1]],
@@ -82,14 +83,67 @@ const permutations = createPermutations<Partial<FilteringTokenProps>>([
   },
 ]);
 
+const defaultTokenListProps: TokenListProps<React.ReactNode> = {
+  items: [],
+  alignment: 'inline',
+  renderItem: item => item,
+  limit: 3,
+  after: null,
+  limitShowFewerAriaLabel: 'show more',
+  limitShowMoreAriaLabel: 'show fewer',
+};
+
+const tokenListPermutations = createPermutations<Partial<TokenListProps<React.ReactNode>>>([
+  // List of single tokens with show more
+  {
+    items: [
+      [
+        <FilteringToken key="1" {...defaultTokenProps} tokens={[token1]} />,
+        <FilteringToken key="2" {...defaultTokenProps} showOperation={true} tokens={[token2]} />,
+        <FilteringToken key="3" {...defaultTokenProps} showOperation={true} tokens={[token3]} />,
+      ],
+    ],
+    limit: [3, 2],
+  },
+  // List of single and grouped tokens
+  {
+    items: [
+      [
+        <FilteringToken key="2" {...defaultTokenProps} tokens={[token1, token2]} />,
+        <FilteringToken key="1" {...defaultTokenProps} showOperation={true} tokens={[token1]} />,
+        <FilteringToken key="3" {...defaultTokenProps} showOperation={true} tokens={[token2]} />,
+      ],
+      [
+        <FilteringToken key="1" {...defaultTokenProps} tokens={[token1]} />,
+        <FilteringToken key="2" {...defaultTokenProps} showOperation={true} tokens={[token1, token2]} />,
+        <FilteringToken key="3" {...defaultTokenProps} showOperation={true} tokens={[token2]} />,
+      ],
+      [
+        <FilteringToken key="1" {...defaultTokenProps} tokens={[token1]} />,
+        <FilteringToken key="3" {...defaultTokenProps} showOperation={true} tokens={[token2]} />,
+        <FilteringToken key="2" {...defaultTokenProps} showOperation={true} tokens={[token1, token2]} />,
+      ],
+    ],
+  },
+]);
+
 export default function () {
   return (
     <>
       <h1>Property filter tokens permutations</h1>
       <ScreenshotArea disableAnimations={true}>
         <PermutationsView
-          permutations={permutations}
-          render={permutation => <FilteringToken {...defaultProps} {...permutation} />}
+          permutations={tokenPermutations}
+          render={permutation => <FilteringToken {...defaultTokenProps} {...permutation} />}
+        />
+
+        <br />
+        <hr />
+        <br />
+
+        <PermutationsView
+          permutations={tokenListPermutations}
+          render={permutation => <TokenList {...defaultTokenListProps} {...permutation} />}
         />
       </ScreenshotArea>
     </>
