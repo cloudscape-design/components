538cbdb5df45b50fb927c404ae00da8e67545935
Sergey Pugachev <1313155+spugachev@users.noreply.github.com>
2024-07-19 19:32:57 +0100

    Moving code closer to component

--- src/button-group/item-element.tsx
+++ src/button-group/item-element.tsx
@@ -1,10 +1,11 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useEffect } from 'react';
 import { ButtonGroupProps } from './interfaces.js';
-import { CancelableEventHandler, ClickDetail } from '../internal/events/index.js';
+import { ClickDetail, fireCancelableEvent, NonCancelableEventHandler } from '../internal/events/index.js';
 import IconButtonItem from './icon-button-item.js';
 import MenuDropdownItem from './menu-dropdown-item.js';
+import styles from './styles.css.js';
 
 const ItemElement = forwardRef(
   (
@@ -13,40 +14,126 @@ const ItemElement = forwardRef(
       tooltipItemId,
       isFeedbackTooltip,
       dropdownExpandToViewport,
+      setTooltipItemId,
+      setIsFeedbackTooltip,
       onItemClick,
     }: {
       item: ButtonGroupProps.Item;
       tooltipItemId: string | null;
       isFeedbackTooltip: boolean;
       dropdownExpandToViewport?: boolean;
-      onItemClick?: CancelableEventHandler<ButtonGroupProps.ItemClickDetails | ClickDetail>;
+      setTooltipItemId: (id: string | null) => void;
+      setIsFeedbackTooltip: (isFeedbackTooltip: boolean) => void;
+      onItemClick?: NonCancelableEventHandler<ButtonGroupProps.ItemClickDetails> | undefined;
     },
     ref: React.Ref<HTMLDivElement>
   ) => {
-    switch (item.type) {
-      case 'icon-button':
-        return (
+    const containerRef = React.useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      return () => {
+        if (item.id === tooltipItemId) {
+          setTooltipItemId(null);
+          setIsFeedbackTooltip(false);
+        }
+      };
+    }, [item.id, setIsFeedbackTooltip, setTooltipItemId, tooltipItemId]);
+
+    useEffect(() => {
+      if (!tooltipItemId || tooltipItemId !== item.id) {
+        return;
+      }
+
+      const close = () => {
+        setTooltipItemId(null);
+        setIsFeedbackTooltip(false);
+      };
+
+      const handlePointerDownEvent = (event: PointerEvent) => {
+        if (event.target && containerRef.current?.contains(event.target as HTMLElement)) {
+          return;
+        }
+
+        close();
+      };
+
+      const handleKeyDownEvent = (event: KeyboardEvent) => {
+        if (event.key === 'Escape') {
+          close();
+        }
+      };
+
+      window.addEventListener('pointerdown', handlePointerDownEvent);
+      window.addEventListener('keydown', handleKeyDownEvent);
+
+      return () => {
+        window.removeEventListener('pointerdown', handlePointerDownEvent);
+        window.removeEventListener('keydown', handleKeyDownEvent);
+      };
+    }, [item.id, setIsFeedbackTooltip, setTooltipItemId, tooltipItemId]);
+
+    const onShowTooltip = (show: boolean) => {
+      if (isFeedbackTooltip && tooltipItemId) {
+        return;
+      }
+
+      if (tooltipItemId !== item.id) {
+        if (show) {
+          setTooltipItemId(item.id);
+        }
+      } else {
+        if (!show) {
+          setTooltipItemId(null);
+        }
+      }
+
+      setIsFeedbackTooltip(false);
+    };
+
+    const onClickHandler = (event: CustomEvent<ButtonGroupProps.ItemClickDetails | ClickDetail>) => {
+      const popoverFeedback = 'popoverFeedback' in item && item.popoverFeedback;
+
+      if (popoverFeedback) {
+        setTooltipItemId(item.id);
+        setIsFeedbackTooltip(true);
+      } else {
+        setTooltipItemId(null);
+        setIsFeedbackTooltip(false);
+      }
+
+      fireCancelableEvent(onItemClick, { id: 'id' in event.detail ? event.detail.id : item.id }, event);
+    };
+
+    return (
+      <div
+        key={item.id}
+        className={styles['item-wrapper']}
+        ref={containerRef}
+        onPointerEnter={() => onShowTooltip(true)}
+        onPointerLeave={() => onShowTooltip(false)}
+        onFocus={() => onShowTooltip(true)}
+        onBlur={() => onShowTooltip(false)}
+      >
+        {item.type === 'icon-button' && (
           <IconButtonItem
             ref={ref}
             item={item}
-            onItemClick={onItemClick}
+            onItemClick={onClickHandler}
             tooltipItemId={tooltipItemId}
             isFeedbackTooltip={isFeedbackTooltip}
           />
-        );
-      case 'menu-dropdown':
-        return (
+        )}
+        {item.type === 'menu-dropdown' && (
           <MenuDropdownItem
             ref={ref}
             item={item}
             tooltipItemId={tooltipItemId}
-            onItemClick={onItemClick}
+            onItemClick={onClickHandler}
             expandToViewport={dropdownExpandToViewport}
           />
-        );
-      default:
-        return null;
-    }
+        )}
+      </div>
+    );
   }
 );
 
