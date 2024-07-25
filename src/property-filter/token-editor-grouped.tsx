28d246822b3299c1d4598a6ddb95e57e90f521fc
Andrei Zhaleznichenka <zhalea@amazon.de>
2024-07-25 15:00:57 +0200

    update selectors snapshot and reuse code with token editor

--- src/property-filter/token-editor-grouped.tsx
+++ src/property-filter/token-editor-grouped.tsx
@@ -4,7 +4,6 @@
 import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 
-import InternalAutosuggest from '../autosuggest/internal';
 import InternalButton from '../button/internal';
 import InternalButtonDropdown from '../button-dropdown/internal';
 import InternalFormField from '../form-field/internal';
@@ -12,9 +11,7 @@ import { DropdownStatusProps } from '../internal/components/dropdown-status/inte
 import { FormFieldContext } from '../internal/context/form-field-context';
 import { NonCancelableEventHandler } from '../internal/events';
 import { useUniqueId } from '../internal/hooks/use-unique-id';
-import { SelectProps } from '../select/interfaces';
-import InternalSelect from '../select/internal';
-import { getAllowedOperators, getPropertySuggestions, operatorToDescription } from './controller';
+import { getAllowedOperators } from './controller';
 import {
   ComparisonOperator,
   FormattedToken,
@@ -26,152 +23,11 @@ import {
   InternalToken,
   LoadItemsDetail,
 } from './interfaces';
-import { useLoadItems } from './use-load-items';
+import { OperatorInput, PropertyInput, ValueInput } from './token-editor';
 import { getFormattedToken } from './utils';
 
 import styles from './styles.css.js';
 import testUtilStyles from './test-classes/styles.css.js';
-
-interface PropertyInputProps {
-  asyncProps: null | DropdownStatusProps;
-  customGroupsText: readonly GroupText[];
-  freeTextFiltering: InternalFreeTextFiltering;
-  filteringProperties: readonly InternalFilteringProperty[];
-  i18nStrings: I18nStrings;
-  onChangePropertyKey: (propertyKey: undefined | string) => void;
-  onLoadItems?: NonCancelableEventHandler<LoadItemsDetail>;
-  property: null | InternalFilteringProperty;
-}
-
-function PropertyInput({
-  property,
-  onChangePropertyKey,
-  asyncProps,
-  filteringProperties,
-  onLoadItems,
-  customGroupsText,
-  i18nStrings,
-  freeTextFiltering,
-}: PropertyInputProps) {
-  const propertySelectHandlers = useLoadItems(onLoadItems);
-  const asyncPropertySelectProps = asyncProps ? { ...asyncProps, ...propertySelectHandlers } : {};
-  const propertyOptions: (SelectProps.Option | SelectProps.OptionGroup)[] = getPropertySuggestions(
-    filteringProperties,
-    customGroupsText,
-    i18nStrings,
-    ({ propertyKey, propertyLabel }) => ({
-      value: propertyKey,
-      label: propertyLabel,
-      dontCloseOnSelect: true,
-    })
-  );
-
-  const allPropertiesOption = {
-    label: i18nStrings.allPropertiesLabel,
-    value: undefined,
-  };
-  if (!freeTextFiltering.disabled) {
-    propertyOptions.unshift(allPropertiesOption);
-  }
-  return (
-    <InternalSelect
-      options={propertyOptions}
-      selectedOption={
-        property
-          ? {
-              value: property.propertyKey ?? undefined,
-              label: property.propertyLabel,
-            }
-          : allPropertiesOption
-      }
-      onChange={e => onChangePropertyKey(e.detail.selectedOption.value)}
-      {...asyncPropertySelectProps}
-    />
-  );
-}
-
-interface OperatorInputProps {
-  i18nStrings: I18nStrings;
-  onChangeOperator: (operator: ComparisonOperator) => void;
-  operator: undefined | ComparisonOperator;
-  property: null | InternalFilteringProperty;
-  freeTextFiltering: InternalFreeTextFiltering;
-}
-
-function OperatorInput({ property, operator, onChangeOperator, i18nStrings, freeTextFiltering }: OperatorInputProps) {
-  const operatorOptions = (property ? getAllowedOperators(property) : freeTextFiltering.operators).map(operator => ({
-    value: operator,
-    label: operator,
-    description: operatorToDescription(operator, i18nStrings),
-  }));
-  return (
-    <InternalSelect
-      options={operatorOptions}
-      triggerVariant="label"
-      selectedOption={
-        operator
-          ? {
-              value: operator,
-              label: operator,
-              description: operatorToDescription(operator, i18nStrings),
-            }
-          : null
-      }
-      onChange={e => onChangeOperator(e.detail.selectedOption.value as ComparisonOperator)}
-    />
-  );
-}
-
-interface ValueInputProps {
-  asyncProps: DropdownStatusProps;
-  filteringOptions: readonly InternalFilteringOption[];
-  i18nStrings: I18nStrings;
-  onChangeValue: (value: string) => void;
-  onLoadItems?: NonCancelableEventHandler<LoadItemsDetail>;
-  operator: undefined | ComparisonOperator;
-  property: null | InternalFilteringProperty;
-  value: undefined | string;
-}
-
-function ValueInput({
-  property,
-  operator,
-  value,
-  onChangeValue,
-  asyncProps,
-  filteringOptions,
-  onLoadItems,
-  i18nStrings,
-}: ValueInputProps) {
-  const valueOptions = property
-    ? filteringOptions
-        .filter(option => option.property?.propertyKey === property.propertyKey)
-        .map(({ label, value }) => ({ label, value }))
-    : [];
-
-  const valueAutosuggestHandlers = useLoadItems(onLoadItems, '', property?.externalProperty);
-  const asyncValueAutosuggestProps = property?.propertyKey
-    ? { ...valueAutosuggestHandlers, ...asyncProps }
-    : { empty: asyncProps.empty };
-  const [matchedOption] = valueOptions.filter(option => option.value === value);
-
-  const OperatorForm = property?.propertyKey && operator && property?.getValueFormRenderer(operator);
-
-  return OperatorForm ? (
-    <OperatorForm value={value} onChange={onChangeValue} operator={operator} />
-  ) : (
-    <InternalAutosuggest
-      enteredTextLabel={i18nStrings.enteredTextLabel ?? (value => value)}
-      value={matchedOption?.label ?? value ?? ''}
-      clearAriaLabel={i18nStrings.clearAriaLabel}
-      onChange={e => onChangeValue(e.detail.value)}
-      disabled={!operator}
-      options={valueOptions}
-      {...asyncValueAutosuggestProps}
-      virtualScroll={true}
-    />
-  );
-}
 
 export interface TokenEditorProps {
   asyncProperties?: boolean;
@@ -295,6 +151,7 @@ export function TokenEditor({
               onChangeOperator={groups[index].onChangeOperator}
               i18nStrings={i18nStrings}
               freeTextFiltering={freeTextFiltering}
+              triggerVariant="label"
             />
           ),
         }}
