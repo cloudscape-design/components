83bc5a37202003048950c8d38140c35af8bcac3d
Andrei Zhaleznichenka <zhalea@amazon.de>
2024-07-25 15:20:26 +0200

    test both layouts

--- src/property-filter/__tests__/property-filter-token-editor-grouped.test.tsx
+++ src/property-filter/__tests__/property-filter-token-editor-grouped.test.tsx
@@ -91,193 +91,210 @@ function renderComponent(props?: Partial<TokenEditorProps>) {
   return new InternalPropertyFilterEditorDropdownWrapper(container);
 }
 
-test('renders token editor with a single property', () => {
-  const wrapper = renderComponent({});
+describe.each([500, 1000])('window size = %spx', width => {
+  function findRemoveAction(wrapper: InternalPropertyFilterEditorDropdownWrapper, index: number) {
+    wrapper.findTokenRemoveActions(index).openDropdown();
+    return width === 500
+      ? wrapper.findTokenRemoveActions(index).findMainAction()!
+      : wrapper.findTokenRemoveActions(index).findItems()[0];
+  }
+  function findRemoveFromGroupAction(wrapper: InternalPropertyFilterEditorDropdownWrapper, index: number) {
+    wrapper.findTokenRemoveActions(index).openDropdown();
+    return wrapper.findTokenRemoveActions(index).findItems()[width === 500 ? 0 : 1];
+  }
 
-  // Not supported yet because the standalone editor does not include the popover
-  // This can be uncommented when defining the test against the property filter
-  // which is not possible for the internal-only token-editor-grouped yet.
-  // expect(wrapper.findHeader().getElement()).toHaveTextContent('Edit token');
-  // expect(wrapper.findDismissButton().getElement()).toHaveAccessibleName('Close token editor');
-
-  // Token inputs
-  expect(wrapper.findPropertyField().findControl()!.find('button')!.getElement()).toHaveAccessibleName('Property Name');
-  expect(wrapper.findOperatorField().findControl()!.find('button')!.getElement()).toHaveAccessibleName('Operator =');
-  expect(wrapper.findValueField().findControl()!.find('input')!.getElement()).toHaveAccessibleName('Value');
-  expect(wrapper.findValueField().findControl()!.find('input')!.getElement()).toHaveValue('John');
-
-  // Remove actions
-  expect(wrapper.findTokenRemoveActions().findNativeButton().getElement()).toHaveAccessibleName(
-    'Filter remove actions for Name = John'
-  );
-  expect(wrapper.findTokenRemoveActions().findNativeButton().getElement()).toBeDisabled();
-
-  // Add actions
-  expect(wrapper.findTokenAddActions().findNativeButton().getElement()).toHaveAccessibleName('Add filter actions');
-  expect(wrapper.findTokenAddActions().findNativeButton().getElement()).toBeDisabled();
-  expect(wrapper.findTokenAddActions().findMainAction()!.getElement()).toHaveTextContent('Add new filter');
-
-  // Form actions
-  expect(wrapper.findCancelButton().getElement()).toHaveTextContent('Cancel');
-  expect(wrapper.findSubmitButton().getElement()).toHaveTextContent('Apply');
-});
-
-test('changes property name', () => {
-  const onChangeTempGroup = jest.fn();
-  const wrapper = renderComponent({ onChangeTempGroup });
-  const propertySelect = createWrapper(wrapper.findPropertyField().getElement()).findSelect()!;
-
-  propertySelect.openDropdown();
-  propertySelect.selectOptionByValue('date');
-  wrapper.findSubmitButton().click();
-
-  expect(onChangeTempGroup).toHaveBeenCalledTimes(1);
-  expect(onChangeTempGroup).toHaveBeenCalledWith([
-    expect.objectContaining({ property: dateProperty, operator: '=', value: null }),
-  ]);
-});
-
-test('changes property operator', () => {
-  const onChangeTempGroup = jest.fn();
-  const wrapper = renderComponent({ onChangeTempGroup });
-  const operatorSelect = createWrapper(wrapper.findOperatorField().getElement()).findSelect()!;
-
-  operatorSelect.openDropdown();
-  operatorSelect.selectOptionByValue('!=');
-  wrapper.findSubmitButton().click();
-
-  expect(onChangeTempGroup).toHaveBeenCalledTimes(1);
-  expect(onChangeTempGroup).toHaveBeenCalledWith([
-    expect.objectContaining({ property: nameProperty, operator: '!=', value: 'John' }),
-  ]);
-});
-
-test('changes property value', () => {
-  const onChangeTempGroup = jest.fn();
-  const wrapper = renderComponent({ onChangeTempGroup });
-  const valueAutosuggest = createWrapper(wrapper.findValueField().getElement()).findAutosuggest()!;
-
-  valueAutosuggest.setInputValue('Jane');
-  wrapper.findSubmitButton().click();
-
-  expect(onChangeTempGroup).toHaveBeenCalledTimes(1);
-  expect(onChangeTempGroup).toHaveBeenCalledWith([
-    expect.objectContaining({ property: nameProperty, operator: '=', value: 'Jane' }),
-  ]);
-});
-
-test('removes first property', () => {
-  const onChangeTempGroup = jest.fn();
-  const wrapper = renderComponent({
-    tempGroup: [
-      { property: nameProperty, operator: '=', value: 'John' },
-      { property: nameProperty, operator: '=', value: 'Jane' },
-    ],
-    onChangeTempGroup,
+  beforeEach(() => {
+    Object.defineProperty(window, 'innerWidth', { value: width });
+    window.dispatchEvent(new CustomEvent('resize'));
   });
 
-  wrapper.findTokenRemoveActions(1).openDropdown();
-  wrapper.findTokenRemoveActions(1).findItems()[0].click();
-  wrapper.findSubmitButton().click();
+  test('renders token editor with a single property', () => {
+    const wrapper = renderComponent({});
 
-  expect(onChangeTempGroup).toHaveBeenCalledTimes(1);
-  expect(onChangeTempGroup).toHaveBeenCalledWith([
-    expect.objectContaining({ property: nameProperty, operator: '=', value: 'Jane' }),
-  ]);
-});
+    // Not supported yet because the standalone editor does not include the popover
+    // This can be uncommented when defining the test against the property filter
+    // which is not possible for the internal-only token-editor-grouped yet.
+    // expect(wrapper.findHeader().getElement()).toHaveTextContent('Edit token');
+    // expect(wrapper.findDismissButton().getElement()).toHaveAccessibleName('Close token editor');
 
-test('removes second property', () => {
-  const onChangeTempGroup = jest.fn();
-  const wrapper = renderComponent({
-    tempGroup: [
-      { property: nameProperty, operator: '=', value: 'John' },
-      { property: nameProperty, operator: '=', value: 'Jane' },
-    ],
-    onChangeTempGroup,
+    // Token inputs
+    expect(wrapper.findPropertyField().findControl()!.find('button')!.getElement()).toHaveAccessibleName(
+      'Property Name'
+    );
+    expect(wrapper.findOperatorField().findControl()!.find('button')!.getElement()).toHaveAccessibleName('Operator =');
+    expect(wrapper.findValueField().findControl()!.find('input')!.getElement()).toHaveAccessibleName('Value');
+    expect(wrapper.findValueField().findControl()!.find('input')!.getElement()).toHaveValue('John');
+
+    // Remove actions
+    expect(wrapper.findTokenRemoveActions().findNativeButton().getElement()).toHaveAccessibleName(
+      'Filter remove actions for Name = John'
+    );
+    expect(wrapper.findTokenRemoveActions().findNativeButton().getElement()).toBeDisabled();
+
+    // Add actions
+    expect(wrapper.findTokenAddActions().findNativeButton().getElement()).toHaveAccessibleName('Add filter actions');
+    expect(wrapper.findTokenAddActions().findNativeButton().getElement()).toBeDisabled();
+    expect(wrapper.findTokenAddActions().findMainAction()!.getElement()).toHaveTextContent('Add new filter');
+
+    // Form actions
+    expect(wrapper.findCancelButton().getElement()).toHaveTextContent('Cancel');
+    expect(wrapper.findSubmitButton().getElement()).toHaveTextContent('Apply');
   });
 
-  wrapper.findTokenRemoveActions(2).openDropdown();
-  wrapper.findTokenRemoveActions(2).findItems()[0].click();
-  wrapper.findSubmitButton().click();
+  test('changes property name', () => {
+    const onChangeTempGroup = jest.fn();
+    const wrapper = renderComponent({ onChangeTempGroup });
+    const propertySelect = createWrapper(wrapper.findPropertyField().getElement()).findSelect()!;
 
-  expect(onChangeTempGroup).toHaveBeenCalledTimes(1);
-  expect(onChangeTempGroup).toHaveBeenCalledWith([
-    expect.objectContaining({ property: nameProperty, operator: '=', value: 'John' }),
-  ]);
-});
+    propertySelect.openDropdown();
+    propertySelect.selectOptionByValue('date');
+    wrapper.findSubmitButton().click();
 
-test('removes first property from group', () => {
-  const onChangeTempGroup = jest.fn();
-  const onChangeStandalone = jest.fn();
-  const wrapper = renderComponent({
-    tempGroup: [
-      { property: nameProperty, operator: '=', value: 'John' },
-      { property: nameProperty, operator: '=', value: 'Jane' },
-    ],
-    onChangeTempGroup,
-    onChangeStandalone,
+    expect(onChangeTempGroup).toHaveBeenCalledTimes(1);
+    expect(onChangeTempGroup).toHaveBeenCalledWith([
+      expect.objectContaining({ property: dateProperty, operator: '=', value: null }),
+    ]);
   });
 
-  wrapper.findTokenRemoveActions(1).openDropdown();
-  wrapper.findTokenRemoveActions(1).findItems()[1].click();
-  wrapper.findSubmitButton().click();
+  test('changes property operator', () => {
+    const onChangeTempGroup = jest.fn();
+    const wrapper = renderComponent({ onChangeTempGroup });
+    const operatorSelect = createWrapper(wrapper.findOperatorField().getElement()).findSelect()!;
 
-  expect(onChangeTempGroup).toHaveBeenCalledTimes(1);
-  expect(onChangeTempGroup).toHaveBeenCalledWith([
-    expect.objectContaining({ property: nameProperty, operator: '=', value: 'Jane' }),
-  ]);
+    operatorSelect.openDropdown();
+    operatorSelect.selectOptionByValue('!=');
+    wrapper.findSubmitButton().click();
 
-  expect(onChangeStandalone).toHaveBeenCalledTimes(1);
-  expect(onChangeStandalone).toHaveBeenCalledWith([
-    expect.objectContaining({ property: nameProperty, operator: '=', value: 'John' }),
-  ]);
-});
-
-test('adds new property', () => {
-  const onChangeTempGroup = jest.fn();
-  const wrapper = renderComponent({
-    tempGroup: [
-      { property: nameProperty, operator: '=', value: 'John' },
-      { property: nameProperty, operator: '=', value: 'Jane' },
-    ],
-    onChangeTempGroup,
+    expect(onChangeTempGroup).toHaveBeenCalledTimes(1);
+    expect(onChangeTempGroup).toHaveBeenCalledWith([
+      expect.objectContaining({ property: nameProperty, operator: '!=', value: 'John' }),
+    ]);
   });
 
-  wrapper.findTokenAddActions().findMainAction()!.click();
-  wrapper.findSubmitButton().click();
+  test('changes property value', () => {
+    const onChangeTempGroup = jest.fn();
+    const wrapper = renderComponent({ onChangeTempGroup });
+    const valueAutosuggest = createWrapper(wrapper.findValueField().getElement()).findAutosuggest()!;
 
-  expect(onChangeTempGroup).toHaveBeenCalledTimes(1);
-  expect(onChangeTempGroup).toHaveBeenCalledWith([
-    expect.objectContaining({ property: nameProperty, operator: '=', value: 'John' }),
-    expect.objectContaining({ property: nameProperty, operator: '=', value: 'Jane' }),
-    expect.objectContaining({ property: null, operator: ':', value: null }),
-  ]);
-});
+    valueAutosuggest.setInputValue('Jane');
+    wrapper.findSubmitButton().click();
 
-test('adds new property from standalone', () => {
-  const onChangeTempGroup = jest.fn();
-  const onChangeStandalone = jest.fn();
-  const wrapper = renderComponent({
-    tempGroup: [
-      { property: nameProperty, operator: '=', value: 'John' },
-      { property: nameProperty, operator: '=', value: 'Jane' },
-    ],
-    standaloneTokens: [{ property: dateProperty, operator: '=', value: null }],
-    onChangeTempGroup,
-    onChangeStandalone,
+    expect(onChangeTempGroup).toHaveBeenCalledTimes(1);
+    expect(onChangeTempGroup).toHaveBeenCalledWith([
+      expect.objectContaining({ property: nameProperty, operator: '=', value: 'Jane' }),
+    ]);
   });
 
-  wrapper.findTokenAddActions().openDropdown();
-  wrapper.findTokenAddActions().findItems()[0].click();
-  wrapper.findSubmitButton().click();
+  test('removes first property', () => {
+    const onChangeTempGroup = jest.fn();
+    const wrapper = renderComponent({
+      tempGroup: [
+        { property: nameProperty, operator: '=', value: 'John' },
+        { property: nameProperty, operator: '=', value: 'Jane' },
+      ],
+      onChangeTempGroup,
+    });
 
-  expect(onChangeTempGroup).toHaveBeenCalledTimes(1);
-  expect(onChangeTempGroup).toHaveBeenCalledWith([
-    expect.objectContaining({ property: nameProperty, operator: '=', value: 'John' }),
-    expect.objectContaining({ property: nameProperty, operator: '=', value: 'Jane' }),
-    expect.objectContaining({ property: dateProperty, operator: '=', value: null }),
-  ]);
+    findRemoveAction(wrapper, 1).click();
+    wrapper.findSubmitButton().click();
 
-  expect(onChangeStandalone).toHaveBeenCalledTimes(1);
-  expect(onChangeStandalone).toHaveBeenCalledWith([]);
+    expect(onChangeTempGroup).toHaveBeenCalledTimes(1);
+    expect(onChangeTempGroup).toHaveBeenCalledWith([
+      expect.objectContaining({ property: nameProperty, operator: '=', value: 'Jane' }),
+    ]);
+  });
+
+  test('removes second property', () => {
+    const onChangeTempGroup = jest.fn();
+    const wrapper = renderComponent({
+      tempGroup: [
+        { property: nameProperty, operator: '=', value: 'John' },
+        { property: nameProperty, operator: '=', value: 'Jane' },
+      ],
+      onChangeTempGroup,
+    });
+
+    findRemoveAction(wrapper, 2).click();
+    wrapper.findSubmitButton().click();
+
+    expect(onChangeTempGroup).toHaveBeenCalledTimes(1);
+    expect(onChangeTempGroup).toHaveBeenCalledWith([
+      expect.objectContaining({ property: nameProperty, operator: '=', value: 'John' }),
+    ]);
+  });
+
+  test('removes first property from group', () => {
+    const onChangeTempGroup = jest.fn();
+    const onChangeStandalone = jest.fn();
+    const wrapper = renderComponent({
+      tempGroup: [
+        { property: nameProperty, operator: '=', value: 'John' },
+        { property: nameProperty, operator: '=', value: 'Jane' },
+      ],
+      onChangeTempGroup,
+      onChangeStandalone,
+    });
+
+    findRemoveFromGroupAction(wrapper, 1).click();
+    wrapper.findSubmitButton().click();
+
+    expect(onChangeTempGroup).toHaveBeenCalledTimes(1);
+    expect(onChangeTempGroup).toHaveBeenCalledWith([
+      expect.objectContaining({ property: nameProperty, operator: '=', value: 'Jane' }),
+    ]);
+
+    expect(onChangeStandalone).toHaveBeenCalledTimes(1);
+    expect(onChangeStandalone).toHaveBeenCalledWith([
+      expect.objectContaining({ property: nameProperty, operator: '=', value: 'John' }),
+    ]);
+  });
+
+  test('adds new property', () => {
+    const onChangeTempGroup = jest.fn();
+    const wrapper = renderComponent({
+      tempGroup: [
+        { property: nameProperty, operator: '=', value: 'John' },
+        { property: nameProperty, operator: '=', value: 'Jane' },
+      ],
+      onChangeTempGroup,
+    });
+
+    wrapper.findTokenAddActions().findMainAction()!.click();
+    wrapper.findSubmitButton().click();
+
+    expect(onChangeTempGroup).toHaveBeenCalledTimes(1);
+    expect(onChangeTempGroup).toHaveBeenCalledWith([
+      expect.objectContaining({ property: nameProperty, operator: '=', value: 'John' }),
+      expect.objectContaining({ property: nameProperty, operator: '=', value: 'Jane' }),
+      expect.objectContaining({ property: null, operator: ':', value: null }),
+    ]);
+  });
+
+  test('adds new property from standalone', () => {
+    const onChangeTempGroup = jest.fn();
+    const onChangeStandalone = jest.fn();
+    const wrapper = renderComponent({
+      tempGroup: [
+        { property: nameProperty, operator: '=', value: 'John' },
+        { property: nameProperty, operator: '=', value: 'Jane' },
+      ],
+      standaloneTokens: [{ property: dateProperty, operator: '=', value: null }],
+      onChangeTempGroup,
+      onChangeStandalone,
+    });
+
+    wrapper.findTokenAddActions().openDropdown();
+    wrapper.findTokenAddActions().findItems()[0].click();
+    wrapper.findSubmitButton().click();
+
+    expect(onChangeTempGroup).toHaveBeenCalledTimes(1);
+    expect(onChangeTempGroup).toHaveBeenCalledWith([
+      expect.objectContaining({ property: nameProperty, operator: '=', value: 'John' }),
+      expect.objectContaining({ property: nameProperty, operator: '=', value: 'Jane' }),
+      expect.objectContaining({ property: dateProperty, operator: '=', value: null }),
+    ]);
+
+    expect(onChangeStandalone).toHaveBeenCalledTimes(1);
+    expect(onChangeStandalone).toHaveBeenCalledWith([]);
+  });
 });
