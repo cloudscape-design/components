bdc4b4ad1a80715bfaf97b15f99f64ba57dcc1a7
Georgii Lobko <globko@amazon.de>
2024-07-31 15:39:26 +0200

    fix: Remove a metadata entry for classic colorTextToggleButtonIconPressed token

--- style-dictionary/classic/metadata.ts
+++ style-dictionary/classic/metadata.ts
@@ -13,7 +13,6 @@ const updatedDescriptions: StyleDictionary.TokenIndex<string> = {
     'The color of clickable elements in their default state. *For example: expandable sections, tabs, and icons.*',
   colorTextInteractiveHover:
     'The color of clickable elements on hover. *For example: expandable sections, and icons on hover.*',
-  colorTextToggleButtonIconPressed: 'The pressed text color of icon toggle buttons.',
   spaceScaledM: 'The M spacing unit which scales between modes. For example: top padding of content inside a container',
   spaceScaledXxl:
     'The XXL spacing unit which scales between modes. For example: horizontal padding for side navigation and help panel content.',
