4f825761dd8ba4af91d3d005417a43dbb5f5baac
Andrei Zhaleznichenka <zhalea@amazon.de>
2024-08-08 12:30:25 +0200

    replace all instanceof checks

--- src/internal/utils/node-belongs.ts
+++ src/internal/utils/node-belongs.ts
@@ -5,6 +5,10 @@ import { nodeContains } from '@cloudscape-design/component-toolkit/dom';
 
 import { findUpUntil } from './dom';
 
+// We avoid instanceof Node/HTMLElement checks as the interfaces are tied to the window they are created in.
+// If an element was moved to an iframe after it was created, then element instanceof Node/HTMLElement
+// will be false since the interface has a different window.
+
 /**
  * Checks whether the given node (target) belongs to the container.
  * The function is similar to nodeContains but also accounts for dropdowns with expandToViewport=true.
@@ -13,20 +17,17 @@ import { findUpUntil } from './dom';
  * @param target Node that is checked to be a descendant of the container
  */
 export function nodeBelongs(container: Node | null, target: Node | EventTarget | null): boolean {
-  // ('nodeType' in target) is a workaround to check if target is a node
-  //  Node interface is tied to the window it's created in, if the target was moved to an iframe after it was created,
-  //  target instanceof Node will be false since Node has a different window
   if (!target || !('nodeType' in target)) {
     return false;
   }
   const portal = findUpUntil(
     target as HTMLElement,
-    node => node === container || (node instanceof HTMLElement && !!node.dataset.awsuiReferrerId)
+    node => node === container || ('dataset' in node && !!node.dataset.awsuiReferrerId)
   );
   if (portal && portal === container) {
     // We found the container as a direct ancestor without a portal
     return true;
   }
-  const referrer = portal instanceof HTMLElement ? document.getElementById(portal.dataset.awsuiReferrerId ?? '') : null;
+  const referrer = portal && 'dataset' in portal ? document.getElementById(portal.dataset.awsuiReferrerId ?? '') : null;
   return referrer ? nodeContains(container, referrer) : nodeContains(container, target);
 }
