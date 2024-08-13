27a5608893d2fcaeb8b3d3c8dac0f1d9b9913bab
Boris Serdiuk <serdiuk@amazon.com>
2024-08-13 10:05:12 +0200

    fix: Consistently fire onSelect even when closing the dropdown (#2563)

--- src/autosuggest/options-controller.ts
+++ src/autosuggest/options-controller.ts
@@ -87,11 +87,19 @@ export const useAutosuggestItems = ({
   });
 
   const selectHighlightedOptionWithKeyboard = () => {
-    if (highlightedOptionState.highlightedOption && isInteractive(highlightedOptionState.highlightedOption)) {
-      onSelectItem(highlightedOptionState.highlightedOption);
-      return true;
+    if (highlightedOptionState.highlightedOption && !isInteractive(highlightedOptionState.highlightedOption)) {
+      // skip selection when a non-interactive item is active
+      return false;
     }
-    return false;
+    onSelectItem(
+      highlightedOptionState.highlightedOption ?? {
+        // put use-entered item as a fallback
+        value: filterValue,
+        type: 'use-entered',
+        option: { value: filterValue },
+      }
+    );
+    return true;
   };
 
   const highlightVisibleOptionWithMouse = (index: number) => {
