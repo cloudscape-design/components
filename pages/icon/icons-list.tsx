1edebf9d5194165fc305eaae1931039963bcc503
at-susie <atsuboy@gmail.com>
2024-09-12 15:07:15 +0200

    Refine svg

--- pages/icon/icons-list.tsx
+++ pages/icon/icons-list.tsx
@@ -17,11 +17,17 @@ export default function IconsList({ variant }: { variant: IconProps['variant'] }
     <ScreenshotArea key={variant} className={className}>
       <h1 className={styles.header}>{variant}</h1>
       {sizes.map(size => (
-        <div key={size} style={{ padding: 8, display: 'flex', flexWrap: 'wrap', gap: 4 }}>
-          {Object.keys(icons).map(icon => (
-            <Icon key={icon} name={icon as IconProps['name']} variant={variant} size={size} />
-          ))}
-        </div>
+        <>
+          <h2 style={{ marginBlockEnd: 8 }}>Icon {size}</h2>
+          <div
+            key={size}
+            style={{ paddingBlockEnd: 8, paddingBlockStart: 2, display: 'flex', flexWrap: 'wrap', gap: 4 }}
+          >
+            {Object.keys(icons).map(icon => (
+              <Icon key={icon} name={icon as IconProps['name']} variant={variant} size={size} />
+            ))}
+          </div>
+        </>
       ))}
     </ScreenshotArea>
   );
