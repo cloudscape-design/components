020c5ae35b713d24a57152854f046eeb2c295b06
Andrei Zhaleznichenka <zhalea@amazon.de>
2024-09-16 21:21:47 +0200

    fix table tests

--- src/table/__tests__/columns-auto-resize.test.tsx
+++ src/table/__tests__/columns-auto-resize.test.tsx
@@ -31,7 +31,7 @@ function renderTable(jsx: React.ReactElement) {
   const { container, rerender } = render(jsx);
   const wrapper = createWrapper(container).findTable()!;
   // stub all timers from this moment
-  jest.useFakeTimers('modern');
+  jest.useFakeTimers({ legacyFakeTimers: true });
   // override getBoundingClientRect method we use for position calculation
   wrapper.findColumnHeaders().forEach(header => {
     header.getElement().getBoundingClientRect = fakeBoundingClientRect;
