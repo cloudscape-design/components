c83fda4bc0eae9c5563fe72b46417bdc71c9bc00
Avinash Dwarapu <dwaraa@amazon.com>
2024-09-18 17:00:11 +0200

    Fix tests to allow for multiple isValidRange calls.

--- src/date-range-picker/__tests__/date-range-picker.test.tsx
+++ src/date-range-picker/__tests__/date-range-picker.test.tsx
@@ -391,15 +391,13 @@ describe('Date range picker', () => {
     // When endDate hasn't been selected
     wrapper.findDropdown()!.findDateAt('left', 3, 4).click();
     wrapper.findDropdown()!.findApplyButton().click();
-    expect(isValidRange).toHaveBeenNthCalledWith(
-      1,
+    expect(isValidRange).toHaveBeenLastCalledWith(
       expect.objectContaining({ type: 'absolute', startDate: '2023-08-16', endDate: '' })
     );
     // When full range has been selected
     wrapper.findDropdown()!.findDateAt('right', 3, 4).click();
     wrapper.findDropdown()!.findApplyButton().click();
-    expect(isValidRange).toHaveBeenNthCalledWith(
-      1,
+    expect(isValidRange).toHaveBeenLastCalledWith(
       expect.objectContaining({ type: 'absolute', startDate: '2023-08-16', endDate: '2023-09-13' })
     );
   });
