2a3f5ab51679cf881e5dceaac380ce5e9127720a
Jessica Kuelz <15003460+jkuelz@users.noreply.github.com>
2024-09-25 03:48:45 -0700

    test: Enable more integration tests for toolbar (#2710)

--- src/app-layout/__integ__/utils.ts
+++ src/app-layout/__integ__/utils.ts
@@ -5,10 +5,11 @@ export const testIf = (condition: boolean) => (condition ? test : test.skip);
 
 export type Theme = 'classic' | 'refresh' | 'refresh-toolbar';
 
-export function getUrlParams(theme: Theme) {
+export function getUrlParams(theme: Theme, other?: Record<string, string>) {
   const params = new URLSearchParams({
     visualRefresh: `${theme.startsWith('refresh')}`,
     appLayoutToolbar: `${theme === 'refresh-toolbar'}`,
+    ...other,
   });
   return params.toString();
 }
