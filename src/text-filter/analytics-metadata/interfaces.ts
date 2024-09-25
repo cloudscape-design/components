1de3bdabf55cf7378cc80f4eb0611924b5c47274
fralongo <88311273+fralongo@users.noreply.github.com>
2024-09-25 14:13:31 +0200

    feat: Add analytics metadata to Text filter (#2764)
    Co-authored-by: Francesco Longo <flongo@amazon.de>

---
+++ src/text-filter/analytics-metadata/interfaces.ts
@@ -0,0 +1,13 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { GeneratedAnalyticsMetadataInputClearInput } from '../../input/analytics-metadata/interfaces';
+export type GeneratedAnalyticsMetadataTextFilterClearInput = GeneratedAnalyticsMetadataInputClearInput;
+
+export interface GeneratedAnalyticsMetadataTextFilterComponent {
+  name: 'awsui.TextFilter';
+  label: string;
+  properties: {
+    disabled: string;
+  };
+}
