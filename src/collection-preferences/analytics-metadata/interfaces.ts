5c593a71240d96ec04db8d96c6481d276252c179
Francesco Longo <flongo@amazon.de>
2024-09-26 17:24:24 +0200

    feat: Add analytics metadata to Collection preferences

---
+++ src/collection-preferences/analytics-metadata/interfaces.ts
@@ -0,0 +1,48 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+export interface GeneratedAnalyticsMetadataCollectionPreferencesOpen {
+  action: 'open';
+  detail: {
+    label: string;
+  };
+}
+
+export interface GeneratedAnalyticsMetadataCollectionPreferencesDismiss {
+  action: 'dismiss';
+  detail: {
+    label: string;
+  };
+}
+
+export interface GeneratedAnalyticsMetadataCollectionPreferencesConfirm {
+  action: 'confirm';
+  detail: {
+    label: string;
+  };
+}
+
+export interface GeneratedAnalyticsMetadataCollectionPreferencesCancel {
+  action: 'cancel';
+  detail: {
+    label: string;
+  };
+}
+
+export interface GeneratedAnalyticsMetadataCollectionPreferencesComponent {
+  name: 'awsui.CollectionPreferences';
+  label: string;
+  properties: {
+    disabled: string;
+    pageSize?: string;
+    wrapLines?: string;
+    stripedRows?: string;
+    contentDensity?: string;
+    visibleContentCount?: string;
+    stickyColumns?: string;
+    contentDisplayVisibleCount?: string;
+  };
+  innerContext?: {
+    preference: string;
+  };
+}
