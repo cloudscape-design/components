585df9309039848b5f1290f743d42b229b11ca45
Michael Dowse <dowsem@amazon.com>
2024-10-10 11:58:23 +0200

    chore: Use @cloudscape-design/build-tools stylelint rules

--- build-tools/stylelint/__tests__/common.js
+++
@@ -1,5 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-import { fileURLToPath } from 'node:url';
-
-export const configBasedir = fileURLToPath(new URL('.', import.meta.url));
