7ac139885b3b74fc837c046f68994e12693c3098
Katie George <katiegeo@amazon.com>
2024-10-11 16:18:49 -0700

    chore: Adds file token group integ test

---
+++ src/file-token-group/__integ__/file-token-group.test.ts
@@ -0,0 +1,90 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { BasePageObject } from '@cloudscape-design/browser-test-tools/page-objects';
+import useBrowser from '@cloudscape-design/browser-test-tools/use-browser';
+
+import createWrapper from '../../../lib/components/test-utils/selectors';
+
+const fileTokenGroupWrapper = createWrapper().findFileTokenGroup('#test');
+
+class FileTokenGroupPage extends BasePageObject {
+  getActiveElementAttribute(attributeName: string) {
+    return this.browser.execute(function (attributeName) {
+      return document.activeElement!.getAttribute(attributeName);
+    }, attributeName);
+  }
+
+  clickDismissButton(itemIndex: number) {
+    return this.click(fileTokenGroupWrapper.findFileToken(itemIndex).findRemoveButton().getElement());
+  }
+
+  countItems() {
+    return this.getElementsCount(fileTokenGroupWrapper.findFileTokens().toSelector());
+  }
+
+  async resetFocus() {
+    await this.click('.focus-element');
+  }
+}
+
+const setupTest = (testFn: (page: FileTokenGroupPage) => Promise<void>) => {
+  return useBrowser(async browser => {
+    const page = new FileTokenGroupPage(browser);
+    await browser.url('#/light/file-token-group/index');
+    await page.waitForVisible(fileTokenGroupWrapper.findFileToken(1).toSelector());
+    await testFn(page);
+  });
+};
+
+test(
+  'should fire the dismiss event when clicking on the dismiss button',
+  setupTest(async page => {
+    await page.clickDismissButton(3);
+    await expect(page.countItems()).resolves.toBe(2);
+  })
+);
+
+describe('Keyboard interaction', () => {
+  ['Space', 'Enter'].forEach(confirmKey => {
+    test(
+      `should fire the dismiss event on ${confirmKey} key`,
+      setupTest(async page => {
+        await page.resetFocus();
+        await page.keys(['Tab', confirmKey]);
+        await expect(page.countItems()).resolves.toBe(2);
+      })
+    );
+  });
+  test(
+    'should focus a dismiss button of a disabled token when tabbing',
+    setupTest(async page => {
+      await page.resetFocus();
+      await page.keys(['Tab', 'Tab']);
+      await expect(page.getActiveElementAttribute('aria-label')).resolves.toBe('Remove item 2');
+    })
+  );
+
+  test(
+    'should focus on a dismiss button of a token when tabbing over a disabled token',
+    setupTest(async page => {
+      await page.resetFocus();
+      await page.keys(['Tab', 'Tab', 'Tab']);
+      await expect(page.getActiveElementAttribute('aria-label')).resolves.toBe('Remove item 3');
+    })
+  );
+
+  test(
+    'should not fire the dismiss event when pressing the dismiss button of a disabled token',
+    setupTest(async page => {
+      await page.resetFocus();
+      await page.keys(['Tab', 'Tab']);
+      await expect(page.getActiveElementAttribute('aria-label')).resolves.toBe('Remove item 2');
+
+      // Click the disabled token's removal button and verify token did not get removed:
+      await expect(page.countItems()).resolves.toBe(3);
+      await page.keys(['Enter']);
+      await expect(page.countItems()).resolves.toBe(3);
+    })
+  );
+});
