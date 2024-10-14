c07c3ef9bf9ea09583c27cc9623e1121256db700
Boris Serdiuk <serdiuk@amazon.com>
2024-10-14 14:42:35 +0200

    refactor integration test to avoid hard-coded values

--- pages/app-layout/with-notifications.page.tsx
+++ pages/app-layout/with-notifications.page.tsx
@@ -1,23 +1,31 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React from 'react';
+import React, { useContext } from 'react';
 
 import AppLayout from '~components/app-layout';
 
+import AppContext, { AppContextType } from '../app/app-context';
 import ScreenshotArea from '../utils/screenshot-area';
 import { Notifications } from './utils/content-blocks';
 import labels from './utils/labels';
 
 import styles from './styles.scss';
 
+type DemoPageContext = React.Context<
+  AppContextType<{
+    disableNotifications: boolean;
+  }>
+>;
+
 export default function () {
+  const { urlParams } = useContext(AppContext as DemoPageContext);
   return (
     <ScreenshotArea gutters={false}>
       <AppLayout
         ariaLabels={labels}
         // Note: Rendering flashbar here directly does not work. It has to be a separate component to reproduce
         // the case when `notifications` slot is not falsy, but eventually it renders no HTML
-        notifications={<Notifications />}
+        notifications={!urlParams.disableNotifications && <Notifications />}
         content={
           <div data-testid="content-root" className={styles.highlightBorder}>
             <h1>Distribution details</h1>
