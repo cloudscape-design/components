0c2aa4459edd40fd137ab9a5862c3c09382b8a64
Andrei Zhaleznichenka <zhalea@amazon.de>
2024-10-15 11:00:21 +0200

    scoped tests

--- build-tools/tasks/integ.js
+++ build-tools/tasks/integ.js
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 const execa = require('execa');
-const glob = require('glob');
+// const glob = require('glob');
 const waitOn = require('wait-on');
 const { task } = require('../utils/gulp-utils.js');
 
@@ -13,7 +13,10 @@ module.exports = task('test:integ', async () => {
   });
   await waitOn({ resources: ['http://localhost:8080'] });
 
-  const files = glob.sync('src/**/__integ__/**/*.test.ts');
+  const files = ['src/table/__integ__/resizable-columns.test.ts', 'src/table/__integ__/expandable-rows.test.ts'];
+
+  // const files = glob.sync('src/**/__integ__/**/*.test.ts');
+
   await execa('jest', ['-c', 'jest.integ.config.js', ...files], {
     stdio: 'inherit',
     env: { ...process.env, NODE_OPTIONS: '--experimental-vm-modules' },
