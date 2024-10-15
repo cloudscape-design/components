e8aa200c893af6931c206ae117af2ead2826ddaf
Andrei Zhaleznichenka <zhalea@amazon.de>
2024-10-15 15:25:26 +0200

    experiment

--- build-tools/tasks/integ.js
+++ build-tools/tasks/integ.js
@@ -1,18 +1,18 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 const execa = require('execa');
-// const glob = require('glob');
+const glob = require('glob');
 const waitOn = require('wait-on');
 const { task } = require('../utils/gulp-utils.js');
-// const { parseArgs } = require('node:util');
+const { parseArgs } = require('node:util');
 
 module.exports = task('test:integ', async () => {
-  // const options = {
-  //   shard: {
-  //     type: 'string',
-  //   },
-  // };
-  // const shard = parseArgs({ options, strict: false }).values.shard;
+  const options = {
+    shard: {
+      type: 'string',
+    },
+  };
+  const shard = parseArgs({ options, strict: false }).values.shard;
   const devServer = execa('webpack', ['serve', '--config', 'pages/webpack.config.integ.js'], {
     env: {
       NODE_ENV: 'development',
@@ -20,12 +20,11 @@ module.exports = task('test:integ', async () => {
   });
   await waitOn({ resources: ['http://localhost:8080'] });
 
-  // const files = glob.sync('src/**/__integ__/**/*.test.ts');
-  const files = ['src/table/__integ__/resizable-columns.test.ts', 'src/table/__integ__/expandable-rows.test.ts'];
+  const files = glob.sync('src/**/__integ__/**/*.test.ts');
   const commands = ['-c', 'jest.integ.config.js', ...files];
-  // if (shard) {
-  //   commands.push(`--shard=${shard}`);
-  // }
+  if (shard) {
+    commands.push(`--shard=${shard}`);
+  }
   await execa('jest', commands, {
     stdio: 'inherit',
     env: { ...process.env, NODE_OPTIONS: '--experimental-vm-modules' },
