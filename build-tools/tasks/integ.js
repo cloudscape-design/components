09ebe693c69095efd0bfe46ebcccfbafedaa6f3d
Andrei Zhaleznichenka <zhalea@amazon.de>
2024-10-15 11:59:08 +0200

    disable shards

--- build-tools/tasks/integ.js
+++ build-tools/tasks/integ.js
@@ -4,15 +4,15 @@ const execa = require('execa');
 // const glob = require('glob');
 const waitOn = require('wait-on');
 const { task } = require('../utils/gulp-utils.js');
-const { parseArgs } = require('node:util');
+// const { parseArgs } = require('node:util');
 
 module.exports = task('test:integ', async () => {
-  const options = {
-    shard: {
-      type: 'string',
-    },
-  };
-  const shard = parseArgs({ options, strict: false }).values.shard;
+  // const options = {
+  //   shard: {
+  //     type: 'string',
+  //   },
+  // };
+  // const shard = parseArgs({ options, strict: false }).values.shard;
   const devServer = execa('webpack', ['serve', '--config', 'pages/webpack.config.integ.js'], {
     env: {
       NODE_ENV: 'development',
@@ -23,9 +23,9 @@ module.exports = task('test:integ', async () => {
   // const files = glob.sync('src/**/__integ__/**/*.test.ts');
   const files = ['src/table/__integ__/resizable-columns.test.ts', 'src/table/__integ__/expandable-rows.test.ts'];
   const commands = ['-c', 'jest.integ.config.js', ...files];
-  if (shard) {
-    commands.push(`--shard=${shard}`);
-  }
+  // if (shard) {
+  //   commands.push(`--shard=${shard}`);
+  // }
   await execa('jest', commands, {
     stdio: 'inherit',
     env: { ...process.env, NODE_OPTIONS: '--experimental-vm-modules' },
