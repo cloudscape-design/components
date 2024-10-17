ac5eaa732d69d322275e72baec67adeb8485de4c
Dennis Pitcock <denpitco@amazon.de>
2024-10-17 12:17:46 +0200

    fixing/adding unit tests

--- src/app-layout/__tests__/multi-layout.test.tsx
+++ src/app-layout/__tests__/multi-layout.test.tsx
@@ -116,6 +116,7 @@ describe('mergeMultiAppLayoutProps', () => {
     const result = mergeMultiAppLayoutProps(ownProps, additionalPropsBase);
 
     expect(result).toEqual({
+      //asserting new aria labels overwrite existing yet preserve others
       ariaLabels: {
         navigation: 'New Navigation',
         drawers: 'Drawers',
@@ -135,22 +136,13 @@ describe('mergeMultiAppLayoutProps', () => {
       },
       splitPanelFocusRef: ownProps.splitPanelFocusRef,
       onSplitPanelToggle: mockParentSplitPanelToggle,
-      activeDrawerId: 'drawer2',
-      drawers: [
-        {
-          id: 'drawer2',
-          ariaLabels: { drawerName: 'Drawer 2' },
-          content: <div>Drawer 2 Content</div>,
-        },
-      ],
+      //asserting the ownProps drawer is not overwritten
+      activeDrawerId: ownProps.activeDrawerId,
+      drawers: ownProps.drawers,
       drawersFocusRef: ownProps.drawersFocusRef,
       onActiveDrawerChange: mockParentActiveDrawerChange,
     });
   });
-
-  //other assertions
-  //additional props arial labels overrite initial
-  //hasNavigation vales
 
   it('should return null if no fields are defined, except ariaLabels', () => {
     const result = mergeMultiAppLayoutProps({ ariaLabels: {} } as SharedMultiAppLayoutProps, []);
