41139bec16fec45740d47c3351fff6d41d6b5758
Katie George <katiegeo@amazon.com>
2024-10-21 12:47:59 -0700

    feat: Adds internal file input

---
+++ src/test-utils/dom/file-input/index.ts
@@ -0,0 +1,19 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { ComponentWrapper, ElementWrapper } from '@cloudscape-design/test-utils-core/dom';
+
+import ButtonWrapper from '../button';
+
+import selectors from '../../../internal/components/file-input/styles.selectors.js';
+
+export default class FileInputWrapper extends ComponentWrapper<HTMLElement> {
+  static rootSelector: string = selectors['file-input-container'];
+
+  findTrigger(): ButtonWrapper {
+    return this.findComponent(`.${selectors['upload-button']}`, ButtonWrapper)!;
+  }
+
+  findNativeInput(): ElementWrapper<HTMLInputElement> {
+    return this.findByClassName(selectors['upload-input'])!;
+  }
+}
