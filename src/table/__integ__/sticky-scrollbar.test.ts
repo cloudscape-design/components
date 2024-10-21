1de412218e1eaf831cce5fe2ad977c824b245240
Andrei Zhaleznichenka <zhalea@amazon.de>
2024-10-21 14:22:54 +0200

    keep integ test changes

--- src/table/__integ__/sticky-scrollbar.test.ts
+++ src/table/__integ__/sticky-scrollbar.test.ts
@@ -4,23 +4,18 @@ import { BasePageObject } from '@cloudscape-design/browser-test-tools/page-objec
 import useBrowser from '@cloudscape-design/browser-test-tools/use-browser';
 
 import createWrapper from '../../../lib/components/test-utils/selectors';
-const tableWrapper = createWrapper().findTable();
+
 import scrollbarStyles from '../../../lib/components/table/sticky-scrollbar/styles.selectors.js';
 
 const scrollbarSelector = `.${scrollbarStyles['sticky-scrollbar-visible']}`;
 
-class StickyScrollbarPage extends BasePageObject {
-  findVisibleScrollbar() {
-    return tableWrapper.find(scrollbarSelector).toSelector();
-  }
-  findTable() {
-    return tableWrapper.toSelector();
-  }
-}
+const tableWrapper = createWrapper().findTable();
+const containerWrapper = createWrapper().findContainer();
+const scrollbarWrapper = tableWrapper.find(scrollbarSelector);
 
-const setupTest = (testFn: (page: StickyScrollbarPage) => Promise<void>, isVisualRefresh?: boolean) => {
+const setupTest = (testFn: (page: BasePageObject) => Promise<void>, isVisualRefresh?: boolean) => {
   return useBrowser(async browser => {
-    const page = new StickyScrollbarPage(browser);
+    const page = new BasePageObject(browser);
     await page.setWindowSize({ width: 600, height: 400 });
     await browser.url(
       `#/light/table/sticky-scrollbar?${isVisualRefresh ? 'visualRefresh=true' : 'visualRefresh=false'}`
@@ -34,49 +29,63 @@ describe('Sticky scrollbar', () => {
   test(
     `is visible, when the table is in view, but it's bottom is not`,
     setupTest(async page => {
-      await expect(page.isExisting(await page.findVisibleScrollbar())).resolves.toEqual(true);
+      await expect(page.isExisting(scrollbarWrapper.toSelector())).resolves.toEqual(true);
     })
   );
 
   [false, true].forEach(visualRefresh =>
     describe(`visualRefresh=${visualRefresh}`, () => {
       test(
-        `scrollbarWidth is equal to tableWidth`,
+        'scrollbarWidth is equal to tableWidth',
         setupTest(async page => {
-          const { width: scrollbarWidth } = await page.getBoundingBox(await page.findVisibleScrollbar());
-          const { width: tableWidth } = await page.getBoundingBox(await page.findTable());
+          const { width: scrollbarWidth } = await page.getBoundingBox(scrollbarWrapper.toSelector());
+          const { width: tableWidth } = await page.getBoundingBox(tableWrapper.toSelector());
           const borderOffset = visualRefresh ? 2 : 0;
           expect(scrollbarWidth).toEqual(tableWidth - borderOffset);
         }, visualRefresh)
+      );
+
+      // Skipped because #2864 is reverted.
+      test.skip(
+        'sticky scrollbar is at the bottom when rendered inside container with fit-height',
+        useBrowser(async browser => {
+          const page = new BasePageObject(browser);
+          await browser.url(
+            `#/light/table/sticky-scrollbar-in-container?visualRefresh=${visualRefresh}&fitHeight=true`
+          );
+          const { bottom: containerBottom } = await page.getBoundingBox(containerWrapper.findContent().toSelector());
+          const { bottom: scrollbarBottom } = await page.getBoundingBox(scrollbarWrapper.toSelector());
+          expect(scrollbarBottom).toBe(containerBottom);
+        })
       );
     })
   );
 
   test(
-    `is hidden, when page is resized and table fits into the screen`,
+    'is hidden, when page is resized and table fits into the screen',
     setupTest(async page => {
       await page.setWindowSize({ width: 1600, height: 400 });
-      await expect(page.isExisting(await page.findVisibleScrollbar())).resolves.toEqual(false);
+      await expect(page.isExisting(scrollbarWrapper.toSelector())).resolves.toEqual(false);
     })
   );
   test(
-    `appears when screen is resized`,
+    'appears when screen is resized',
     setupTest(async page => {
       await page.setWindowSize({ width: 1600, height: 400 });
-      await expect(page.isExisting(await page.findVisibleScrollbar())).resolves.toEqual(false);
+      await expect(page.isExisting(scrollbarWrapper.toSelector())).resolves.toEqual(false);
       await page.setWindowSize({ width: 600, height: 400 });
-      await expect(page.isExisting(await page.findVisibleScrollbar())).resolves.toEqual(true);
+      await expect(page.isExisting(scrollbarWrapper.toSelector())).resolves.toEqual(true);
     })
   );
   test(
-    `scrollbar position updates when window resizes`,
+    'scrollbar position updates when window resizes',
     setupTest(async page => {
       await page.setWindowSize({ width: 600, height: 600 });
-      const { bottom: bottom1 } = await page.getBoundingBox(page.findVisibleScrollbar());
+      const { bottom: bottom1 } = await page.getBoundingBox(scrollbarWrapper.toSelector());
       expect(bottom1).toEqual((await page.getViewportSize()).height);
 
       await page.setWindowSize({ width: 600, height: 400 });
-      const { bottom: bottom2 } = await page.getBoundingBox(page.findVisibleScrollbar());
+      const { bottom: bottom2 } = await page.getBoundingBox(scrollbarWrapper.toSelector());
       expect(bottom1 - bottom2).toBe(200);
     })
   );
