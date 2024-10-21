ae386a457133c89910a93bcacf5a8f7aa0937f44
Andrei Zhaleznichenka <zhalea@amazon.de>
2024-10-21 17:11:25 +0200

    chore: Adds missing screenshot area to app layout test page

--- pages/app-layout/disable-paddings-with-split-panel.page.tsx
+++ pages/app-layout/disable-paddings-with-split-panel.page.tsx
@@ -7,6 +7,7 @@ import Box from '~components/box';
 import SplitPanel from '~components/split-panel';
 
 import AppContext, { AppContextType } from '../app/app-context';
+import ScreenshotArea from '../utils/screenshot-area';
 import { Breadcrumbs, Navigation, Tools } from './utils/content-blocks';
 import labels from './utils/labels';
 import * as toolsContent from './utils/tools-content';
@@ -25,45 +26,47 @@ export default function () {
   } = useContext(AppContext as SplitPanelDemoContext);
 
   return (
-    <AppLayout
-      disableContentPaddings={true}
-      ariaLabels={labels}
-      breadcrumbs={<Breadcrumbs />}
-      navigation={<Navigation />}
-      tools={<Tools>{toolsContent.long}</Tools>}
-      splitPanelPreferences={{ position: splitPanelPosition }}
-      onSplitPanelPreferencesChange={event => {
-        const { position } = event.detail;
-        setUrlParams({ splitPanelPosition: position === 'side' ? position : undefined });
-      }}
-      splitPanelOpen={splitPanelOpen}
-      onSplitPanelToggle={event => setUrlParams({ splitPanelOpen: event.detail.open })}
-      splitPanel={
-        <SplitPanel
-          header="Split panel header"
-          i18nStrings={{
-            preferencesTitle: 'Preferences',
-            preferencesPositionLabel: 'Split panel position',
-            preferencesPositionDescription: 'Choose the default split panel position for the service.',
-            preferencesPositionSide: 'Side',
-            preferencesPositionBottom: 'Bottom',
-            preferencesConfirm: 'Confirm',
-            preferencesCancel: 'Cancel',
-            closeButtonAriaLabel: 'Close panel',
-            openButtonAriaLabel: 'Open panel',
-            resizeHandleAriaLabel: 'Slider',
-          }}
-        >
-          <Box>Content</Box>
-        </SplitPanel>
-      }
-      content={
-        <Box padding="m">
-          <div style={{ border: '1px solid magenta' }}>
-            <Box variant="h1">Content</Box>
-          </div>
-        </Box>
-      }
-    />
+    <ScreenshotArea gutters={false}>
+      <AppLayout
+        disableContentPaddings={true}
+        ariaLabels={labels}
+        breadcrumbs={<Breadcrumbs />}
+        navigation={<Navigation />}
+        tools={<Tools>{toolsContent.long}</Tools>}
+        splitPanelPreferences={{ position: splitPanelPosition }}
+        onSplitPanelPreferencesChange={event => {
+          const { position } = event.detail;
+          setUrlParams({ splitPanelPosition: position === 'side' ? position : undefined });
+        }}
+        splitPanelOpen={splitPanelOpen}
+        onSplitPanelToggle={event => setUrlParams({ splitPanelOpen: event.detail.open })}
+        splitPanel={
+          <SplitPanel
+            header="Split panel header"
+            i18nStrings={{
+              preferencesTitle: 'Preferences',
+              preferencesPositionLabel: 'Split panel position',
+              preferencesPositionDescription: 'Choose the default split panel position for the service.',
+              preferencesPositionSide: 'Side',
+              preferencesPositionBottom: 'Bottom',
+              preferencesConfirm: 'Confirm',
+              preferencesCancel: 'Cancel',
+              closeButtonAriaLabel: 'Close panel',
+              openButtonAriaLabel: 'Open panel',
+              resizeHandleAriaLabel: 'Slider',
+            }}
+          >
+            <Box>Content</Box>
+          </SplitPanel>
+        }
+        content={
+          <Box padding="m">
+            <div style={{ border: '1px solid magenta' }}>
+              <Box variant="h1">Content</Box>
+            </div>
+          </Box>
+        }
+      />
+    </ScreenshotArea>
   );
 }
