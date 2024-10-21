d57a87378b104ca1f15bbcb34ba50c2e0e6766bb
Andrei Zhaleznichenka <zhalea@amazon.de>
2024-10-21 14:18:16 +0200

    Revert "fix: Container overflow constrained to the content slot (#2864)"
    This reverts commit 11df6c3e9a129851193a39d46fa02357ad35c54c.

--- pages/table/sticky-scrollbar-in-container.page.tsx
+++ pages/table/sticky-scrollbar-in-container.page.tsx
@@ -1,62 +1,37 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React, { useContext } from 'react';
+import React from 'react';
 
 import { useCollection } from '@cloudscape-design/collection-hooks';
 
-import { Checkbox, Container, Header, Pagination, SpaceBetween, Table } from '~components';
+import Header from '~components/header';
+import Table from '~components/table';
 
-import AppContext, { AppContextType } from '../app/app-context';
 import ScreenshotArea from '../utils/screenshot-area';
-import { generateItems } from './generate-data';
-import { columnsConfig, paginationLabels } from './shared-configs';
-
-type PageContext = React.Context<
-  AppContextType<{
-    fitHeight?: boolean;
-    hasFooter?: boolean;
-  }>
->;
+import { generateItems, Instance } from './generate-data';
+import { columnsConfig } from './shared-configs';
 
 const allItems = generateItems();
 const PAGE_SIZE = 50;
 
 export default function App() {
-  const {
-    urlParams: { fitHeight = true, hasFooter = false },
-    setUrlParams,
-  } = useContext(AppContext as PageContext);
-
-  const { items, paginationProps } = useCollection(allItems, { pagination: { pageSize: PAGE_SIZE }, sorting: {} });
-
+  const { items } = useCollection(allItems, {
+    pagination: { pageSize: PAGE_SIZE },
+    sorting: {},
+  });
   return (
     <ScreenshotArea>
-      <SpaceBetween size="m" direction="horizontal">
-        <div style={{ blockSize: '500px', inlineSize: '500px', overflow: 'scroll' }}>
-          <Container fitHeight={fitHeight}>
-            <Table
-              variant="borderless"
-              header={
-                <Header headingTagOverride="h1" counter={`(${allItems.length})`}>
-                  Sticky Scrollbar Example
-                </Header>
-              }
-              footer={hasFooter ? <Pagination {...paginationProps} ariaLabels={paginationLabels} /> : undefined}
-              columnDefinitions={columnsConfig}
-              items={items}
-            />
-          </Container>
-        </div>
-
-        <SpaceBetween size="s">
-          <Checkbox checked={fitHeight} onChange={({ detail }) => setUrlParams({ fitHeight: detail.checked })}>
-            fitHeight
-          </Checkbox>
-          <Checkbox checked={hasFooter} onChange={({ detail }) => setUrlParams({ hasFooter: detail.checked })}>
-            hasFooter
-          </Checkbox>
-        </SpaceBetween>
-      </SpaceBetween>
+      <div style={{ blockSize: '400px', overflow: 'scroll' }}>
+        <Table<Instance>
+          header={
+            <Header headingTagOverride="h1" counter={`(${allItems.length})`}>
+              Sticky Scrollbar Example
+            </Header>
+          }
+          columnDefinitions={columnsConfig}
+          items={items}
+        />
+      </div>
     </ScreenshotArea>
   );
 }
--- src/table/__integ__/sticky-scrollbar.test.ts
+++ src/table/__integ__/sticky-scrollbar.test.ts
@@ -4,18 +4,23 @@ import { BasePageObject } from '@cloudscape-design/browser-test-tools/page-objec
 import useBrowser from '@cloudscape-design/browser-test-tools/use-browser';
 
 import createWrapper from '../../../lib/components/test-utils/selectors';
-
+const tableWrapper = createWrapper().findTable();
 import scrollbarStyles from '../../../lib/components/table/sticky-scrollbar/styles.selectors.js';
 
 const scrollbarSelector = `.${scrollbarStyles['sticky-scrollbar-visible']}`;
 
-const tableWrapper = createWrapper().findTable();
-const containerWrapper = createWrapper().findContainer();
-const scrollbarWrapper = tableWrapper.find(scrollbarSelector);
+class StickyScrollbarPage extends BasePageObject {
+  findVisibleScrollbar() {
+    return tableWrapper.find(scrollbarSelector).toSelector();
+  }
+  findTable() {
+    return tableWrapper.toSelector();
+  }
+}
 
-const setupTest = (testFn: (page: BasePageObject) => Promise<void>, isVisualRefresh?: boolean) => {
+const setupTest = (testFn: (page: StickyScrollbarPage) => Promise<void>, isVisualRefresh?: boolean) => {
   return useBrowser(async browser => {
-    const page = new BasePageObject(browser);
+    const page = new StickyScrollbarPage(browser);
     await page.setWindowSize({ width: 600, height: 400 });
     await browser.url(
       `#/light/table/sticky-scrollbar?${isVisualRefresh ? 'visualRefresh=true' : 'visualRefresh=false'}`
@@ -29,62 +34,49 @@ describe('Sticky scrollbar', () => {
   test(
     `is visible, when the table is in view, but it's bottom is not`,
     setupTest(async page => {
-      await expect(page.isExisting(scrollbarWrapper.toSelector())).resolves.toEqual(true);
+      await expect(page.isExisting(await page.findVisibleScrollbar())).resolves.toEqual(true);
     })
   );
 
   [false, true].forEach(visualRefresh =>
     describe(`visualRefresh=${visualRefresh}`, () => {
       test(
-        'scrollbarWidth is equal to tableWidth',
+        `scrollbarWidth is equal to tableWidth`,
         setupTest(async page => {
-          const { width: scrollbarWidth } = await page.getBoundingBox(scrollbarWrapper.toSelector());
-          const { width: tableWidth } = await page.getBoundingBox(tableWrapper.toSelector());
+          const { width: scrollbarWidth } = await page.getBoundingBox(await page.findVisibleScrollbar());
+          const { width: tableWidth } = await page.getBoundingBox(await page.findTable());
           const borderOffset = visualRefresh ? 2 : 0;
           expect(scrollbarWidth).toEqual(tableWidth - borderOffset);
         }, visualRefresh)
       );
-
-      test(
-        'sticky scrollbar is at the bottom when rendered inside container with fit-height',
-        useBrowser(async browser => {
-          const page = new BasePageObject(browser);
-          await browser.url(
-            `#/light/table/sticky-scrollbar-in-container?visualRefresh=${visualRefresh}&fitHeight=true`
-          );
-          const { bottom: containerBottom } = await page.getBoundingBox(containerWrapper.findContent().toSelector());
-          const { bottom: scrollbarBottom } = await page.getBoundingBox(scrollbarWrapper.toSelector());
-          expect(scrollbarBottom).toBe(containerBottom);
-        })
-      );
     })
   );
 
   test(
-    'is hidden, when page is resized and table fits into the screen',
+    `is hidden, when page is resized and table fits into the screen`,
     setupTest(async page => {
       await page.setWindowSize({ width: 1600, height: 400 });
-      await expect(page.isExisting(scrollbarWrapper.toSelector())).resolves.toEqual(false);
+      await expect(page.isExisting(await page.findVisibleScrollbar())).resolves.toEqual(false);
     })
   );
   test(
-    'appears when screen is resized',
+    `appears when screen is resized`,
     setupTest(async page => {
       await page.setWindowSize({ width: 1600, height: 400 });
-      await expect(page.isExisting(scrollbarWrapper.toSelector())).resolves.toEqual(false);
+      await expect(page.isExisting(await page.findVisibleScrollbar())).resolves.toEqual(false);
       await page.setWindowSize({ width: 600, height: 400 });
-      await expect(page.isExisting(scrollbarWrapper.toSelector())).resolves.toEqual(true);
+      await expect(page.isExisting(await page.findVisibleScrollbar())).resolves.toEqual(true);
     })
   );
   test(
-    'scrollbar position updates when window resizes',
+    `scrollbar position updates when window resizes`,
     setupTest(async page => {
       await page.setWindowSize({ width: 600, height: 600 });
-      const { bottom: bottom1 } = await page.getBoundingBox(scrollbarWrapper.toSelector());
+      const { bottom: bottom1 } = await page.getBoundingBox(page.findVisibleScrollbar());
       expect(bottom1).toEqual((await page.getViewportSize()).height);
 
       await page.setWindowSize({ width: 600, height: 400 });
-      const { bottom: bottom2 } = await page.getBoundingBox(scrollbarWrapper.toSelector());
+      const { bottom: bottom2 } = await page.getBoundingBox(page.findVisibleScrollbar());
       expect(bottom1 - bottom2).toBe(200);
     })
   );
