fb7155a3f5c1e693a19ea30606947819daf1b01c
Katie George <katiegeo@amazon.com>
2024-10-24 13:26:28 -0700

    chore: Add test util and test

--- src/test-utils/dom/internal/file-dropzone.ts
+++ src/test-utils/dom/internal/file-dropzone.ts
@@ -1,9 +1,13 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { ElementWrapper } from '@cloudscape-design/test-utils-core/dom';
+import { ComponentWrapper, ElementWrapper } from '@cloudscape-design/test-utils-core/dom';
 
 import styles from '../../../internal/components/file-dropzone/styles.selectors.js';
 
-export default class FileDropzoneWrapper extends ElementWrapper {
+export default class FileDropzoneWrapper extends ComponentWrapper {
   static rootSelector: string = styles.root;
+
+  findContent(): ElementWrapper {
+    return this.findByClassName(styles.root)!;
+  }
 }
