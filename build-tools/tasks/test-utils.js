4406433ee4d6f026ff890e766afdb5d1a22732bc
Amir Alami <alamiami@amazon.de>
2024-10-24 10:02:24 +0200

    feat: Adds findXAll and findXByTestId selectors

--- build-tools/tasks/test-utils.js
+++ build-tools/tasks/test-utils.js
@@ -7,6 +7,7 @@ const { pascalCase } = require('change-case');
 const { default: convertToSelectorUtil } = require('@cloudscape-design/test-utils-converter');
 const { through, task } = require('../utils/gulp-utils');
 const { writeFile, listPublicItems } = require('../utils/files');
+const { pluralizeComponentName } = require('../utils/pluralize');
 const themes = require('../utils/themes');
 
 function toWrapper(componentClass) {
@@ -15,24 +16,132 @@ function toWrapper(componentClass) {
 
 const testUtilsSrcDir = path.resolve('src/test-utils');
 const configs = {
+  common: {
+    // These components are not meant to be present in multiple instances in a single app.
+    // For this reason no findAll and findByTestId finders will be generated for them.
+    noExtraFinders: ['AppLayout', 'TopNavigation'],
+    buildFinder: ({ componentName }) => `
+       ElementWrapper.prototype.find${componentName} = function(selector) {
+         const rootSelector = \`.$\{${toWrapper(componentName)}.rootSelector}\`;
+         // casting to 'any' is needed to avoid this issue with generics
+         // https://github.com/microsoft/TypeScript/issues/29132
+         return (this as any).findComponent(selector ? appendSelector(selector, rootSelector) : rootSelector, ${toWrapper(componentName)});
+       };`,
+    buildExtraFinders: ({ componentName, componentNamePlural }) => `
+      ElementWrapper.prototype.findAll${componentNamePlural} = function(selector) {
+        return this.findAllComponents(${toWrapper(componentName)}, selector);
+      };
+
+      ElementWrapper.prototype.find${componentName}ByTestId = function(testId) {
+        const selector = \`.\${${toWrapper(componentName)}.rootSelector}[data-testid="\${CSS.escape(testId)}"]\`;
+        return this.findComponent(selector, ${toWrapper(componentName)});
+      };`,
+  },
   dom: {
     defaultExport: `export default function wrapper(root: Element = document.body) { if (document && document.body && !document.body.contains(root)) { console.warn('[AwsUi] [test-utils] provided element is not part of the document body, interactions may work incorrectly')}; return new ElementWrapper(root); }`,
-    buildFinderInterface: ({ componentName }) =>
-      `find${componentName}(selector?: string): ${toWrapper(componentName)} | null;`,
+    buildFinderInterface: ({ componentName }) => `
+       /**
+        * Returns the wrapper of the first ${componentName} that matches the specified CSS selector.
+        * If no CSS selector is specified, returns the wrapper of the first ${componentName}.
+        * If no matching ${componentName} is found, returns \`null\`.
+        *
+        * @param {string} [selector] CSS Selector
+        * @returns {${toWrapper(componentName)} | null}
+        */
+       find${componentName}(selector?: string): ${toWrapper(componentName)} | null;`,
+    buildExtraFinderInterfaces: ({ componentName, componentNamePlural }) => `
+       /**
+        * Returns the wrappers of all ${componentNamePlural} that match the specified CSS selector.
+        * If no CSS selector is specified, returns all of the ${componentNamePlural} inside the current wrapper.
+        * If no matching ${componentName} is found, returns an empty array.
+        *
+        * @param {string} [selector] CSS Selector
+        * @returns {Array<${toWrapper(componentName)}>}
+        */
+       findAll${componentNamePlural}(selector?: string): Array<${toWrapper(componentName)}>;
+       /**
+        * Returns the first ${componentName} that matches the specified test ID.
+        * Looks for the \`data-testid\` attribute assigned to the component.
+        * If no matching ${componentName} is found, returns \`null\`.
+        *
+        * @param {string} testId
+        * @returns {${toWrapper(componentName)} | null}
+        */
+       find${componentName}ByTestId(testId: string): ${toWrapper(componentName)} | null;`,
   },
   selectors: {
     defaultExport: `export default function wrapper(root: string = 'body') { return new ElementWrapper(root); }`,
-    buildFinderInterface: ({ componentName }) =>
-      `find${componentName}(selector?: string): ${toWrapper(componentName)};`,
+    buildFinderInterface: ({ componentName, componentNamePlural }) => `
+       /**
+        * Returns a wrapper that matches the ${componentNamePlural} with the specified CSS selector.
+        * If no CSS selector is specified, returns a wrapper that matches ${componentNamePlural}.
+        *
+        * @param {string} [selector] CSS Selector
+        * @returns {${toWrapper(componentName)}}
+        */
+       find${componentName}(selector?: string): ${toWrapper(componentName)};`,
+    buildExtraFinderInterfaces: ({ componentName, componentNamePlural }) => `
+       /**
+        * Returns a multi-element wrapper that matches ${componentNamePlural} with the specified CSS selector.
+        * If no CSS selector is specified, returns a multi-element wrapper that matches ${componentNamePlural}.
+        *
+        * @param {string} [selector] CSS Selector
+        * @returns {MultiElementWrapper<${toWrapper(componentName)}>}
+        */
+       findAll${componentNamePlural}(selector?: string): MultiElementWrapper<${toWrapper(componentName)}>;
+       /**
+        * Returns a wrapper that matches the first ${componentName} with the specified test ID.
+        * Looks for the \`data-testid\` attribute assigned to the component.
+        *
+        * @param {string} testId
+        * @returns {${toWrapper(componentName)}}
+        */
+       find${componentName}ByTestId(testId: string): ${toWrapper(componentName)};`,
   },
 };
+
+function generateFindersInterfaces({ testUtilMetaData, testUtilType, configs }) {
+  const { buildFinderInterface, buildExtraFinderInterfaces } = configs[testUtilType];
+  const { noExtraFinders } = configs.common;
+
+  const findersInterfaces = testUtilMetaData.map(metadata => {
+    if (noExtraFinders.includes(metadata.componentName)) {
+      return buildFinderInterface(metadata);
+    }
+
+    return [buildFinderInterface(metadata), buildExtraFinderInterfaces(metadata)].join('\n');
+  });
+
+  // we need to redeclare the interface in its original definition, extending a re-export will not work
+  // https://github.com/microsoft/TypeScript/issues/12607
+  const interfaces = `declare module '@cloudscape-design/test-utils-core/dist/${testUtilType}' {
+      interface ElementWrapper {
+        ${findersInterfaces.join('\n')}
+      }
+  }`;
+
+  return interfaces;
+}
+
+function generateFindersImplementations({ testUtilMetaData, configs }) {
+  const { noExtraFinders, buildFinder, buildExtraFinders } = configs.common;
+
+  const findersImplementations = testUtilMetaData.map(metadata => {
+    if (noExtraFinders.includes(metadata.componentName)) {
+      return buildFinder(metadata);
+    }
+
+    return [buildFinder(metadata), buildExtraFinders(metadata)].join('\n');
+  });
+
+  return findersImplementations.join('\n');
+}
 
 function generateIndexFileContent(testUtilType, testUtilMetaData) {
   const config = configs[testUtilType];
   if (config === undefined) {
     throw new Error('Unknown test util type');
   }
-  const { defaultExport, buildFinderInterface } = config;
 
   return [
     // language=TypeScript
@@ -47,24 +156,9 @@ function generateIndexFileContent(testUtilType, testUtilMetaData) {
         export { ${componentName}Wrapper };
       `;
     }),
-    // we need to redeclare the interface in its original definition, extending a re-export will not work
-    // https://github.com/microsoft/TypeScript/issues/12607
-    `declare module '@cloudscape-design/test-utils-core/dist/${testUtilType}' {
-      interface ElementWrapper {
-        ${testUtilMetaData.map(metaData => buildFinderInterface(metaData)).join('\n')}
-      }
-    }`,
-    ...testUtilMetaData.map(({ componentName }) => {
-      const wrapperName = toWrapper(componentName);
-      // language=TypeScript
-      return `ElementWrapper.prototype.find${componentName} = function(selector) {
-          const rootSelector = \`.$\{${wrapperName}.rootSelector}\`;
-          // casting to 'any' is needed to avoid this issue with generics
-          // https://github.com/microsoft/TypeScript/issues/29132
-          return (this as any).findComponent(selector ? appendSelector(selector, rootSelector) : rootSelector, ${wrapperName});
-      };`;
-    }),
-    defaultExport,
+    generateFindersInterfaces({ testUtilMetaData, testUtilType, configs }),
+    generateFindersImplementations({ testUtilMetaData, configs }),
+    config.defaultExport,
   ].join('\n');
 }
 
@@ -77,9 +171,11 @@ function generateTestUtilMetaData(testUtilType) {
 
     const componentNameKebab = componentFolderName;
     const componentName = pascalCase(componentNameKebab);
+    const componentNamePlural = pluralizeComponentName(componentName);
 
     const componentMetaData = {
       componentName,
+      componentNamePlural,
       relPathtestUtilFile,
     };
 
--- src/__tests__/functional-tests/test-utils.test.tsx
+++ src/__tests__/functional-tests/test-utils.test.tsx
@@ -7,9 +7,79 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { render as renderTestingLibrary } from '@testing-library/react';
+import { pascalCase } from 'change-case';
 
+import { Modal } from '../../../lib/components';
 import Button from '../../../lib/components/button';
-import createWrapper from '../../../lib/components/test-utils/dom';
+import * as dom from '../../../lib/components/test-utils/dom';
+import * as selectors from '../../../lib/components/test-utils/selectors';
+import { getRequiredPropsForComponent } from '../required-props-for-components';
+import { getAllComponents, requireComponent } from '../utils';
+
+const createWrapperDom = dom.default;
+const createWrapperSelectors = selectors.default;
+
+const componentsWithNoExtraFinders = ['top-navigation', 'app-layout'];
+const componentsWithExceptions = ['annotation-context', ...componentsWithNoExtraFinders];
+const components = getAllComponents().filter(component => !componentsWithExceptions.includes(component));
+
+const RENDER_COMPONENTS_DEFAULT_PROPS: Record<string, unknown>[] = [
+  {
+    'data-testid': 'first-item',
+    'data-name': 'first item',
+  },
+  {
+    'data-testid': 'second-item',
+    'data-name': 'second item',
+  },
+];
+
+function renderModal(props = RENDER_COMPONENTS_DEFAULT_PROPS) {
+  const root = document.createElement('div');
+  document.body.appendChild(root);
+
+  return renderTestingLibrary(
+    <div>
+      {props.map(({ ...customProps }, index) => (
+        <Modal key={index} visible={true} modalRoot={root} {...customProps} />
+      ))}
+    </div>,
+    { container: root }
+  );
+}
+
+function renderComponents(componentName: string, props = RENDER_COMPONENTS_DEFAULT_PROPS) {
+  if (componentName === 'modal') {
+    return renderModal(props);
+  }
+
+  const { default: Component } = requireComponent(componentName);
+  const defaultProps = getRequiredPropsForComponent(componentName);
+  return renderTestingLibrary(
+    <div>
+      {props.map(({ ...customProps }, index) => (
+        <Component key={index} {...defaultProps} {...customProps} />
+      ))}
+    </div>
+  );
+}
+
+function getComponentSelectors(componentName: string) {
+  const componentNamePascalCase = pascalCase(componentName);
+  const findAllRegex = new RegExp(`findAll${componentNamePascalCase}.*`);
+
+  // The same set of selector functions are present in both dom and selectors.
+  // For this reason, looking into DOM is representative of both groups.
+  const wrapperPropsList = Object.keys(dom.ElementWrapper.prototype);
+
+  // Every component has the same set of selector functions.
+  // For this reason, casting the function names into the Alert component.
+  const findName = `find${componentNamePascalCase}` as 'findAlert';
+  const findAllName = wrapperPropsList.find(selector => findAllRegex.test(selector)) as 'findAllAlerts';
+  const findByTestIdName = `find${componentNamePascalCase}ByTestId` as 'findAlertByTestId';
+
+  return { findName, findAllName, findByTestIdName };
+}
 
 describe('createWrapper', () => {
   let spy: jest.SpyInstance;
@@ -27,23 +97,169 @@ describe('createWrapper', () => {
     document.body.appendChild(container);
     render(React.createElement(Button), container);
 
-    createWrapper(container);
+    createWrapperDom(container);
 
     expect(spy).not.toBeCalled();
   });
+
   test('given detached node when creating a wrapper then a warning is printed', () => {
     const container = document.createElement('div');
     render(React.createElement(Button), container);
 
-    createWrapper(container);
+    createWrapperDom(container);
 
     expect(spy).toBeCalledTimes(1);
   });
+
   test('given node rendered with "@testing-library/react" when creating a wrapper then no warning is printed', () => {
     const { container } = renderTestingLibrary(React.createElement(Button));
 
-    createWrapper(container);
+    createWrapperDom(container);
 
     expect(spy).not.toBeCalled();
+  });
+});
+
+describe.each(components)('ElementWrapper selectors for %s component', componentName => {
+  const { findName, findAllName, findByTestIdName } = getComponentSelectors(componentName);
+
+  describe('dom wrapper', () => {
+    test(`${findName} returns the first ${componentName}`, () => {
+      const { container } = renderComponents(componentName);
+      const wrapper = createWrapperDom(container);
+      const element = wrapper[findName]()!.getElement();
+
+      expect(element).toHaveAttribute('data-name', 'first item');
+    });
+
+    test(`${findAllName} returns all of the ${componentName} components`, () => {
+      const { container } = renderComponents(componentName);
+      const wrapper = createWrapperDom(container);
+      const elementNameAttributes = wrapper[findAllName]().map(component =>
+        component!.getElement().getAttribute('data-name')
+      );
+
+      expect(elementNameAttributes).toEqual(['first item', 'second item']);
+    });
+
+    test(`${findAllName} returns only the matching ${componentName} components, when a selector is specified`, () => {
+      const { container } = renderComponents(componentName, [
+        { className: 'first-type', 'data-name': 'first item' },
+        { className: 'second-type', 'data-name': 'second item' },
+        { className: 'second-type', 'data-name': 'third item' },
+      ]);
+      const wrapper = createWrapperDom(container);
+      const elementNameAttributes = wrapper[findAllName]('.second-type').map(component =>
+        component!.getElement().getAttribute('data-name')
+      );
+
+      expect(elementNameAttributes).toEqual(['second item', 'third item']);
+    });
+
+    test(`${findByTestIdName} returns the ${componentName} with matching test id`, () => {
+      const { container } = renderComponents(componentName);
+      const wrapper = createWrapperDom(container);
+      const element = wrapper[findByTestIdName]('second-item')!.getElement();
+
+      expect(element).toHaveAttribute('data-name', 'second item');
+    });
+
+    test(`${findByTestIdName} returns the correct ${componentName}, even when test id contains double quotes`, () => {
+      const { container } = renderComponents(componentName, [{ 'data-testid': '"test-id-with-quotes"' }]);
+      const wrapper = createWrapperDom(container);
+
+      expect(wrapper[findByTestIdName]('"test-id-with-quotes"')).toBeTruthy();
+    });
+  });
+
+  describe('selectors wrapper', () => {
+    test(`${findName} returns a selector that matches the ${componentName}`, () => {
+      const { container } = renderComponents(componentName);
+      const wrapper = createWrapperSelectors();
+      const selector = wrapper[findName]().toSelector();
+      const element = container.querySelector(selector);
+
+      expect(element).toHaveAttribute('data-name', 'first item');
+    });
+
+    test(`${findAllName} returns a selector that matches the ${componentName} with nth-child index`, () => {
+      const { container } = renderComponents(componentName);
+      const wrapper = createWrapperSelectors();
+      const selector = wrapper[findAllName]().get(2).toSelector();
+      const element = container.querySelector(selector);
+
+      expect(element).toHaveAttribute('data-name', 'second item');
+    });
+
+    test(`${findAllName} appends the specified selector to the default ${componentName} selectors`, () => {
+      const { container } = renderComponents(componentName, [
+        { className: 'first-type', 'data-name': 'first item' },
+        { className: 'second-type', 'data-name': 'second item' },
+      ]);
+      const wrapper = createWrapperSelectors();
+      const firstElement = container.querySelector(wrapper[findAllName]('.second-type').get(1).toSelector());
+      const secondElement = container.querySelector(wrapper[findAllName]('.second-type').get(2).toSelector());
+
+      expect(firstElement).toBeFalsy();
+      expect(secondElement).toBeTruthy();
+    });
+
+    test(`${findByTestIdName} returns selector matching the ${componentName} with test id`, () => {
+      const { container } = renderComponents(componentName);
+      const wrapper = createWrapperSelectors();
+      const selector = wrapper[findByTestIdName]('second-item').toSelector();
+      const element = container.querySelector(selector);
+
+      expect(element).toHaveAttribute('data-name', 'second item');
+    });
+
+    test(`${findByTestIdName} returns selector matching the ${componentName}, even when test id contains double quotes`, () => {
+      const { container } = renderComponents(componentName, [{ 'data-testid': '"test-id-with-quotes"' }]);
+      const wrapper = createWrapperSelectors();
+      const selector = wrapper[findByTestIdName]('"test-id-with-quotes"').toSelector();
+      const element = container.querySelector(selector);
+
+      expect(element).toBeTruthy();
+    });
+  });
+});
+
+describe.each(componentsWithNoExtraFinders)('ElementWrapper selectors for %s component', componentName => {
+  const { findName, findAllName, findByTestIdName } = getComponentSelectors(componentName);
+  const renderOnlyOneInstance = () => renderComponents(componentName, [{}]);
+
+  describe('dom wrapper', () => {
+    test(`${findName} returns the first ${componentName}`, () => {
+      const { container } = renderOnlyOneInstance();
+      const wrapper = createWrapperDom(container);
+      const element = wrapper[findName]()!.getElement();
+
+      expect(element).toBeTruthy();
+    });
+
+    test('it does not have findAll or findByTestId finders', () => {
+      const wrapper = createWrapperDom();
+
+      expect(findAllName).toBeUndefined();
+      expect(wrapper[findByTestIdName]).toBeFalsy();
+    });
+  });
+
+  describe('selectors wrapper', () => {
+    test(`${findName} returns a selector that matches the ${componentName}`, () => {
+      const { container } = renderOnlyOneInstance();
+      const wrapper = createWrapperSelectors();
+      const selector = wrapper[findName]().toSelector();
+      const element = container.querySelector(selector);
+
+      expect(element).toBeTruthy();
+    });
+
+    test('it does not have findAll or findByTestId finders', () => {
+      const wrapper = createWrapperDom();
+
+      expect(findAllName).toBeUndefined();
+      expect(wrapper[findByTestIdName]).toBeFalsy();
+    });
   });
 });
