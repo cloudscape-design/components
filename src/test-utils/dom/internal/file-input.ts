1a8e5d97912c66cb359fcf872acd6f44e26e0237
Katie George <katiegeorge94@gmail.com>
2024-10-30 08:54:34 -0700

    feat: FileInput component (internal) (#2912)
    Co-authored-by: Katie George <katiegeo@amazon.com>

---
+++ src/test-utils/dom/internal/file-input.ts
@@ -0,0 +1,19 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { ComponentWrapper, ElementWrapper } from '@cloudscape-design/test-utils-core/dom';
+
+import ButtonWrapper from '../button';
+
+import selectors from '../../../internal/components/file-input/styles.selectors.js';
+
+export default class FileInputWrapper extends ComponentWrapper<HTMLElement> {
+  static rootSelector: string = selectors.root;
+
+  findTrigger(): ButtonWrapper {
+    return this.findComponent(`.${selectors['file-input-button']}`, ButtonWrapper)!;
+  }
+
+  findNativeInput(): ElementWrapper<HTMLInputElement> {
+    return this.findByClassName(selectors['file-input'])!;
+  }
+}
