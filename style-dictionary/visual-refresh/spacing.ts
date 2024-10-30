dcde0959ee8150850f79cda61feab7157bf43541
Dennis Pitcock <denpitco@amazon.de>
2024-10-30 11:14:04 +0100

    updating snapshots for theme integ test

--- style-dictionary/visual-refresh/spacing.ts
+++ style-dictionary/visual-refresh/spacing.ts
@@ -63,7 +63,7 @@ const tokens: StyleDictionary.SpacingDictionary = {
   spaceTableHeaderToolsBottom: '0px',
   spaceTableHeaderToolsFullPageBottom: '4px',
   spaceTableHorizontal: '{spaceContainerHorizontal}',
-  spaceTileGutter: '24px',
+  spaceTileGutter: { comfortable: '{spaceXl}', compact: '{spaceM}' },
 
   spaceScaled2xNone: '{spaceNone}',
   spaceScaled2xXxxs: { comfortable: '{spaceXxxs}', compact: '{spaceNone}' },
