a4785cf594d358ee14d72482e6b593931653c637
Dennis Pitcock <denpitco@amazon.de>
2024-10-31 11:30:10 +0100

    updating navitation test to ensure it does not block content when mobile

--- src/app-layout/__tests__/app-layout-navigation.test.tsx
+++ src/app-layout/__tests__/app-layout-navigation.test.tsx
@@ -2,19 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 /* eslint simple-import-sort/imports: 0 */
 import React from 'react';
-import { waitFor } from '@testing-library/react';
-import { useMobile } from '../../../lib/components/internal/hooks/use-mobile';
 import { describeEachAppLayout, renderComponent } from './utils';
 import AppLayout from '../../../lib/components/app-layout';
 
 import visualRefreshToolbarStyles from '../../../lib/components/app-layout/visual-refresh-toolbar/skeleton/styles.css.js';
 
-jest.mock('../../../lib/components/internal/hooks/use-mobile', () => ({
-  useMobile: jest.fn().mockReturnValue(false),
-}));
-
-describeEachAppLayout({ themes: ['refresh-toolbar'], sizes: ['desktop'] }, () => {
-  test('a resize to mobile does not hide content when navigationOpen and navigationHide is true and onNavigationChange is overwritten', async () => {
+describeEachAppLayout({ themes: ['refresh-toolbar'], sizes: ['desktop', 'mobile'] }, ({ size }) => {
+  test('a does not hide content when navigationOpen and navigationHide is true and onNavigationChange is overwritten', () => {
     const mockOnNavigationChange = jest.fn();
     const { wrapper } = renderComponent(
       <AppLayout
@@ -26,22 +20,12 @@ describeEachAppLayout({ themes: ['refresh-toolbar'], sizes: ['desktop'] }, () =>
       />
     );
 
-    expect(mockOnNavigationChange).toHaveBeenCalledTimes(0);
+    expect(mockOnNavigationChange).toHaveBeenCalledTimes(size === 'mobile' ? 1 : 0);
     expect(wrapper).not.toBeNull();
     expect(wrapper.findNavigation()).toBeFalsy();
     expect(wrapper.findNavigationToggle()).toBeFalsy();
     expect(wrapper.findByClassName(visualRefreshToolbarStyles['main-landmark'])).not.toBeNull();
     expect(wrapper.findByClassName(visualRefreshToolbarStyles['unfocusable-mobile'])).toBeNull();
     expect(wrapper.findByClassName(visualRefreshToolbarStyles.content)?.getElement()).toBeVisible();
-
-    (useMobile as jest.Mock).mockReturnValue(true);
-    await waitFor(() => {
-      expect(mockOnNavigationChange).toHaveBeenCalledTimes(1);
-      expect(wrapper.findNavigation()).toBeFalsy();
-      expect(wrapper.findNavigationToggle()).toBeFalsy();
-      expect(wrapper.findByClassName(visualRefreshToolbarStyles['main-landmark'])).not.toBeNull();
-      expect(wrapper.findByClassName(visualRefreshToolbarStyles['unfocusable-mobile'])).toBeNull();
-      expect(wrapper.findByClassName(visualRefreshToolbarStyles.content)?.getElement()).toBeVisible();
-    });
   });
 });
