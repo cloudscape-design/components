60475d04ba78f95688caa0ecb74af5459ef6b95c
Katie George <katiegeo@amazon.com>
2024-11-01 14:21:19 -0700

    feat: Moves dropzone to public folder

--- src/file-dropzone/interfaces.tsx
+++ src/file-dropzone/interfaces.tsx
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { BaseComponentProps } from '../../base-component';
-import { NonCancelableEventHandler } from '../../events';
+import { BaseComponentProps } from '../internal/base-component';
+import { NonCancelableEventHandler } from '../internal/events';
 
 export interface FileDropzoneProps extends BaseComponentProps {
   /**
