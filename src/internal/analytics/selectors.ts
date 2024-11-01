e1b2998df21006c1a0076954231023224e952695
Boris Serdiuk <serdiuk@amazon.com>
2024-11-01 15:11:32 +0100

    fix: Use more reliable funnelName extraction

--- src/internal/analytics/selectors.ts
+++ src/internal/analytics/selectors.ts
@@ -35,4 +35,4 @@ export const getSubStepNameSelector = (subStepId?: string) =>
 export const getFieldSlotSeletor = (id: string | undefined) => (id ? `[id="${id}"]` : undefined);
 
 export const getTextFromSelector = (selector: string | undefined): string | undefined =>
-  selector ? document.querySelector<HTMLElement>(selector)?.innerText?.trim() : undefined;
+  selector ? document.querySelector<HTMLElement>(selector)?.textContent?.trim() : undefined;
