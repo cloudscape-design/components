2cd7bb1a86c79f9eecf3ffbc36a45e7220920b53
Georgii Lobko <globko@amazon.de>
2024-11-11 15:48:06 +0100

    chore: Make the test page a11y compliant

--- pages/app-layout/with-table-in-nested-layout.page.tsx
+++ pages/app-layout/with-table-in-nested-layout.page.tsx
@@ -7,59 +7,45 @@ import Button from '~components/button';
 import Header from '~components/header';
 import Table from '~components/table';
 
+import { generateItems, Instance } from '../table/generate-data';
+import { columnsConfig } from '../table/shared-configs';
+import ScreenshotArea from '../utils/screenshot-area';
+import labels from './utils/labels';
+
+const items = generateItems(100);
+
 function App() {
   return (
-    <AppLayout
-      toolsHide={true}
-      disableContentPaddings={true}
-      content={
-        <AppLayout
-          navigationHide={true}
-          content={
-            <Table
-              header={
-                <Header
-                  variant="awsui-h1-sticky"
-                  description="Demo page with footer"
-                  actions={<Button variant="primary">Button</Button>}
-                >
-                  Sticky Scrollbar Example
-                </Header>
-              }
-              stickyHeader={true}
-              variant="full-page"
-              columnDefinitions={[
-                {
-                  id: 'variable',
-                  header: 'Variable name',
-                  cell: item => item.name,
-                  sortingField: 'name',
-                  isRowHeader: true,
-                },
-                {
-                  id: 'alt',
-                  header: 'Text value',
-                  cell: item => item.alt || '-',
-                  sortingField: 'alt',
-                },
-                {
-                  id: 'description',
-                  header: 'Description',
-                  cell: item => item.description || '-',
-                },
-              ]}
-              items={new Array(100).fill(null).map((_, index) => ({
-                name: `Item ${index}`,
-                alt: index,
-                description: `This is the ${index} item`,
-                type: '1A',
-                size: 'Small',
-              }))}
-            />
-          }
-        />
-      }
-    />
+    <ScreenshotArea gutters={false}>
+      <AppLayout
+        ariaLabels={labels}
+        toolsHide={true}
+        disableContentPaddings={true}
+        content={
+          <AppLayout
+            ariaLabels={labels}
+            navigationHide={true}
+            content={
+              <Table<Instance>
+                header={
+                  <Header
+                    variant="awsui-h1-sticky"
+                    description="Demo page with footer"
+                    actions={<Button variant="primary">Create</Button>}
+                  >
+                    Sticky Scrollbar Example
+                  </Header>
+                }
+                stickyHeader={true}
+                variant="full-page"
+                columnDefinitions={columnsConfig}
+                items={items}
+              />
+            }
+          />
+        }
+      />
+    </ScreenshotArea>
   );
 }
 
