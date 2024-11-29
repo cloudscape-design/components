980ae3055907c875ce0f2dabe317260483737a6a
Georgii Lobko <globko@amazon.de>
2024-11-29 12:22:31 +0100

    chore: cleanup

--- pages/app-layout/multi-layout-iframe.page.tsx
+++ pages/app-layout/multi-layout-iframe.page.tsx
@@ -3,12 +3,9 @@
 import React from 'react';
 
 import AppLayout from '~components/app-layout';
-import Button from '~components/button';
 import Header from '~components/header';
 import ScreenreaderOnly from '~components/internal/components/screenreader-only';
-import KeyValuePairs from '~components/key-value-pairs';
 import Link from '~components/link';
-import Popover from '~components/popover';
 import SpaceBetween from '~components/space-between';
 
 import { IframeWrapper } from '../utils/iframe-wrapper';
@@ -34,59 +31,6 @@ function InnerApp() {
           <Link external={true} href="#">
             External link
           </Link>
-
-          <Popover
-            header="Network interface eth0"
-            size="large"
-            triggerType="custom"
-            content={
-              <KeyValuePairs
-                columns={2}
-                items={[
-                  {
-                    label: 'Interface ID',
-                    value: (
-                      <Link href="#" variant="primary">
-                        eni-055da457bed9bbbe6
-                      </Link>
-                    ),
-                  },
-                  {
-                    label: 'Private IP address',
-                    value: '192.0.2.0',
-                  },
-                  { label: 'VPC ID', value: 'vpc-626163728' },
-                  {
-                    label: 'Private DNS name',
-                    value: 'example.com',
-                  },
-                  {
-                    label: 'Attachment owner',
-                    value: 'vpc-626163728',
-                  },
-                  {
-                    label: 'Public IP address',
-                    value: '198.51.100.0',
-                  },
-                  {
-                    label: 'Attached',
-                    value: 'May 4, 2010, 04:56 (UTC+3:30)',
-                  },
-                  {
-                    label: 'Source/Dest. check',
-                    value: 'true',
-                  },
-                  {
-                    label: 'Delete on terminate',
-                    value: 'true',
-                  },
-                  { label: 'Description', value: '-' },
-                ]}
-              />
-            }
-          >
-            <Button ariaLabel="metricInformation" iconName="status-info" variant="icon" />
-          </Popover>
 
           <Containers />
         </SpaceBetween>
