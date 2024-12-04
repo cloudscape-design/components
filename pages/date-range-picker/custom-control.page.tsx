b486f7354bcd49e6a9622aeda70b1757731c995b
Dennis Pitcock <denpitco@amazon.de>
2024-12-04 11:54:50 +0100

    removal of month-only calander page as no longer needed

--- pages/date-range-picker/custom-control.page.tsx
+++ pages/date-range-picker/custom-control.page.tsx
@@ -1,23 +1,47 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import endOfMonth from 'date-fns/endOfMonth';
 import endOfWeek from 'date-fns/endOfWeek';
 import enLocale from 'date-fns/locale/en-GB';
 import startOfMonth from 'date-fns/startOfMonth';
 import startOfWeek from 'date-fns/startOfWeek';
 
-import { Box, DateRangePicker, DateRangePickerProps, FormField, Link } from '~components';
+import { Box, Checkbox, DateRangePicker, DateRangePickerProps, FormField, Link, SpaceBetween } from '~components';
 import { formatDate } from '~components/internal/utils/date-time';
 
-import { i18nStrings, isValid } from './common';
+import AppContext from '../app/app-context';
+import {
+  applyDisabledIfEven,
+  DateRangePickerDemoContext,
+  dateRangePickerDemoDefaults,
+  evenDisabledMsg,
+  i18nStrings,
+  isValid,
+} from './common';
 
 export default function DatePickerScenario() {
+  const { urlParams, setUrlParams } = useContext(AppContext as DateRangePickerDemoContext);
   const [value, setValue] = useState<DateRangePickerProps['value']>(null);
+
+  const monthOnly = urlParams.monthOnly ?? dateRangePickerDemoDefaults.monthOnly;
+  const dateOnly = urlParams.dateOnly ?? dateRangePickerDemoDefaults.dateOnly;
+  const disableEven = urlParams.disableEven ?? dateRangePickerDemoDefaults.disableEven;
 
   return (
     <Box padding="s">
       <h1>Date range picker with custom control</h1>
+      <SpaceBetween direction="horizontal" size="s">
+        <Checkbox checked={dateOnly} onChange={({ detail }) => setUrlParams({ dateOnly: detail.checked })}>
+          Date-only
+        </Checkbox>
+        <Checkbox checked={monthOnly} onChange={({ detail }) => setUrlParams({ monthOnly: detail.checked })}>
+          Month-only
+        </Checkbox>
+        <Checkbox checked={disableEven} onChange={({ detail }) => setUrlParams({ disableEven: detail.checked })}>
+          Disable even dates
+        </Checkbox>
+      </SpaceBetween>
       <FormField label="Date Range Picker field">
         <DateRangePicker
           value={value}
@@ -28,6 +52,11 @@ export default function DatePickerScenario() {
           placeholder="Filter by a date and time range"
           isValidRange={isValid}
           rangeSelectorMode="absolute-only"
+          granularity={monthOnly ? 'month' : 'day'}
+          isDateEnabled={(date: Date) => applyDisabledIfEven(date, !disableEven, monthOnly)}
+          dateDisabledReason={(date: Date) =>
+            applyDisabledIfEven(date, !disableEven, monthOnly) ? '' : evenDisabledMsg
+          }
           customAbsoluteRangeControl={(selectedDate, setSelectedDate) => (
             <>
               Auto-select:{' '}
