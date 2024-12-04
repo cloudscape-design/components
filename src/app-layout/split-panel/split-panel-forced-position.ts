0aa073d826a6577a40a144a05b944d816a8ef2ef
Joan Perals Tresserra <jotresse@amazon.com>
2024-12-04 10:15:13 +0100

    Fine tune comments

--- src/app-layout/split-panel/split-panel-forced-position.ts
+++ src/app-layout/split-panel/split-panel-forced-position.ts
@@ -5,15 +5,13 @@ import { browserScrollbarSize } from '../../internal/utils/browser-scrollbar-siz
 import { SPLIT_PANEL_MIN_WIDTH } from './constants';
 
 /**
- * The Split Panel will be in forced (bottom) position
- * if the defined minimum width is greater than the available width.
- * If the minimum width exceeds this value,
- * then there is not enough horizontal space and we must force it to the bottom position.
+ * Even if the Split Panel is set to be displayed on the side, it will be forced to the bottom
+ * if the available space is less than 280px.
  *
  * We also have to account for the fact that after the split panel changes its position
  * from bottom to side or vice versa, there is a chance that the content  a scrollbar appears or disappears from the page,
- * which changes the available space again, potentially causing an infinite loop.
- * To prevent that, we give the split panel some "resistance" to change from bottom to side,
+ * which changes the available space again. This could potentially lead to an infinite loop.
+ * To prevent that, we give the split panel some "resistance" to change from bottom to side
  * (but not from side to bottom), requiring in this case some extra space on top of the "nominal" 280px.
  */
 
