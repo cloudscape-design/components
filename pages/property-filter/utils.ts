edf3c81b66dcfeabd8d9d37fa7ab51c052c48134
Georgii Lobko <globko@amazon.de>
2024-12-04 10:42:32 +0100

    chore: encode/decode as uri components

--- pages/property-filter/utils.ts
+++ pages/property-filter/utils.ts
@@ -2,14 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export function encode<T>(queryObject: T): string {
-  return JSON.stringify(queryObject);
+  try {
+    return encodeURIComponent(JSON.stringify(queryObject));
+  } catch (error) {
+    // Thrown if uriComponent contains a lone surrogate (like 0xD800–0xDBFF)
+    return '';
+  }
 }
 
 export function decode<T>(queryString: string, defaultResult: T): T;
 export function decode<T>(queryString: string, defaultResult?: undefined): T | null;
 export function decode<T>(queryString: string, defaultResult?: T): T | null {
   try {
-    return JSON.parse(queryString);
+    return JSON.parse(decodeURIComponent(queryString));
   } catch (error) {
     return defaultResult ?? null;
   }
