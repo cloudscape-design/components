5fd2796e69e3fa1211850a55d819af950bf479b7
Dennis Pitcock <denpitco@amazon.de>
2024-12-05 12:33:01 +0100

    adding granularity to integ tests - structurally only - not working

--- src/date-range-picker/__integ__/events.test.ts
+++ src/date-range-picker/__integ__/events.test.ts
@@ -5,6 +5,9 @@ import useBrowser from '@cloudscape-design/browser-test-tools/use-browser';
 import createWrapper from '../../../lib/components/test-utils/selectors';
 import DateRangePickerPage from './page-objects/date-range-picker-page';
 
+//todo remove when confirmed
+const testOnlyIf = (condition: boolean) => (condition ? test.only : test.skip);
+
 class DateRangePickerEventPage extends DateRangePickerPage {
   async clickPosition(x: number, y: number) {
     await this.browser.performActions([
@@ -24,73 +27,100 @@ class DateRangePickerEventPage extends DateRangePickerPage {
   }
 }
 
-describe.each<boolean>([false, true])('DatePicker blur events (expandTOViewport=%s)', expandToViewport => {
-  const setupTest = (testFn: (page: DateRangePickerEventPage) => Promise<void>) => {
+describe('Date Range Picker', () => {
+  const setupTest = (
+    testFn: (page: DateRangePickerEventPage) => Promise<void>,
+    granularity: 'day' | 'month',
+    expandToViewport: boolean
+  ) => {
     return useBrowser(async browser => {
       const page = new DateRangePickerEventPage(
         createWrapper().findDateRangePicker().getElement(),
         browser,
         expandToViewport
       );
-      await browser.url(`#/light/date-range-picker/with-event-handlers?expandToViewport=${expandToViewport}`);
+      const params = new URLSearchParams({
+        monthOnly: granularity === 'month' ? 'true' : 'false',
+        expandToViewport: `${expandToViewport}`,
+      });
+      await browser.url(`#/light/date-range-picker/with-event-handlers?${params}`);
       await page.waitForLoad();
       await testFn(page);
     });
   };
 
-  test(
-    'onBlur is fired when clicking next to the date range picker',
-    setupTest(async page => {
-      await page.focusTrigger();
-      await expect(page.getText('#onFocusEvent')).resolves.toBe('onFocus event called 1 times.');
-      await page.keys('Enter');
-      await expect(page.isDropdownOpen()).resolves.toBe(true);
-      await expect(page.isDropdownFocused()).resolves.toBe(true);
+  describe.each<boolean>([false, true] as const)('DatePicker blur events (expandTOViewport=%s)', expandToViewport => {
+    describe.each([
+      'day',
+      // 'month'
+    ] as const)('With granularity of %s', granularity => {
+      testOnlyIf(granularity === 'day')(
+        'onBlur is fired when clicking next to the date range picker',
+        setupTest(
+          async page => {
+            await page.focusTrigger();
+            await expect(page.getText('#onFocusEvent')).resolves.toBe('onFocus event called 1 times.');
+            await page.keys('Enter');
+            await expect(page.isDropdownOpen()).resolves.toBe(true);
+            await expect(page.isDropdownFocused()).resolves.toBe(true);
 
-      // Click below everything
-      await page.clickPosition(100, 600);
+            // Click below everything
+            await page.clickPosition(100, 600);
 
-      await expect(page.getText('#onBlurEvent')).resolves.toBe('onBlur event called 1 times.');
-      await expect(page.isDropdownOpen()).resolves.toBe(false);
+            await expect(page.getText('#onBlurEvent')).resolves.toBe('onBlur event called 1 times.');
+            await expect(page.isDropdownOpen()).resolves.toBe(false);
 
-      await page.focusTrigger();
-      await expect(page.getText('#onFocusEvent')).resolves.toBe('onFocus event called 2 times.');
-      await expect(page.isTriggerFocused()).resolves.toBe(true);
+            await page.focusTrigger();
+            await expect(page.getText('#onFocusEvent')).resolves.toBe('onFocus event called 2 times.');
+            await expect(page.isTriggerFocused()).resolves.toBe(true);
 
-      // Click next to the date picker
-      await page.clickPosition(400, 80);
-      await expect(page.getText('#onBlurEvent')).resolves.toBe('onBlur event called 2 times.');
-      await expect(page.isDropdownOpen()).resolves.toBe(false);
-    })
-  );
-
-  test(
-    'onBlur is not fired when still focused',
-    setupTest(async page => {
-      await page.focusTrigger();
-      await page.keys('Enter');
-      await expect(page.isDropdownOpen()).resolves.toBe(true);
-      await expect(page.isDropdownFocused()).resolves.toBe(true);
-      await page.clickRange('previous-5-minutes');
-      await page.clickApplyButton();
-      await expect(page.getText('#onChangeEvent')).resolves.toBe(
-        'onChange Event: 1 times. Latest detail: {"key":"previous-5-minutes","amount":5,"unit":"minute","type":"relative"}'
+            // Click next to the date picker
+            await page.clickPosition(400, 80);
+            await expect(page.getText('#onBlurEvent')).resolves.toBe('onBlur event called 2 times.');
+            await expect(page.isDropdownOpen()).resolves.toBe(false);
+          },
+          granularity,
+          expandToViewport
+        )
       );
-      await expect(page.getText('#onBlurEvent')).resolves.toBe('onBlur event called 0 times.');
-    })
-  );
 
-  test(
-    'onBlur is fired after a date has been selected and clicked outside',
-    setupTest(async page => {
-      await page.focusTrigger();
-      await page.keys('Enter');
-      await expect(page.isDropdownOpen()).resolves.toBe(true);
-      await expect(page.isDropdownFocused()).resolves.toBe(true);
-      await page.clickRange('previous-5-minutes');
-      await page.clickApplyButton();
-      await page.clickPosition(400, 80);
-      await expect(page.getText('#onBlurEvent')).resolves.toBe('onBlur event called 1 times.');
-    })
-  );
+      testOnlyIf(granularity === 'day')(
+        'onBlur is not fired when still focused',
+        setupTest(
+          async page => {
+            await page.focusTrigger();
+            await page.keys('Enter');
+            await expect(page.isDropdownOpen()).resolves.toBe(true);
+            await expect(page.isDropdownFocused()).resolves.toBe(true);
+            await page.clickRange('previous-5-minutes');
+            await page.clickApplyButton();
+            await expect(page.getText('#onChangeEvent')).resolves.toBe(
+              'onChange Event: 1 times. Latest detail: {"key":"previous-5-minutes","amount":5,"unit":"minute","type":"relative"}'
+            );
+            await expect(page.getText('#onBlurEvent')).resolves.toBe('onBlur event called 0 times.');
+          },
+          granularity,
+          expandToViewport
+        )
+      );
+
+      testOnlyIf(granularity === 'day')(
+        'onBlur is fired after a date has been selected and clicked outside',
+        setupTest(
+          async page => {
+            await page.focusTrigger();
+            await page.keys('Enter');
+            await expect(page.isDropdownOpen()).resolves.toBe(true);
+            await expect(page.isDropdownFocused()).resolves.toBe(true);
+            await page.clickRange('previous-5-minutes');
+            await page.clickApplyButton();
+            await page.clickPosition(400, 80);
+            await expect(page.getText('#onBlurEvent')).resolves.toBe('onBlur event called 1 times.');
+          },
+          granularity,
+          expandToViewport
+        )
+      );
+    });
+  });
 });
