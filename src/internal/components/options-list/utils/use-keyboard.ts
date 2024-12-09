1d539ccaf179382b99c3715cd747938f71bddcca
Avinash Dwarapu <dwaraa@amazon.com>
2024-12-09 17:54:58 +0100

    fix: Allow space key to select an option inside select/multiselect with filtering (#3101)

--- src/internal/components/options-list/utils/use-keyboard.ts
+++ src/internal/components/options-list/utils/use-keyboard.ts
@@ -1,7 +1,5 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { useCallback } from 'react';
-
 import { BaseKeyDetail, CancelableEventHandler } from '../../../events';
 import { KeyCode } from '../../../keycode';
 
@@ -29,40 +27,37 @@ export const useMenuKeyboard: UseMenuKeyboard = ({
   closeDropdown,
   preventNativeSpace = false,
 }) => {
-  return useCallback(
-    (e: CustomEvent<BaseKeyDetail>) => {
-      switch (e.detail.keyCode) {
-        case KeyCode.up:
-          e.preventDefault();
-          goUp();
-          break;
-        case KeyCode.down:
-          e.preventDefault();
-          goDown();
-          break;
-        case HOME:
-          goHome();
-          break;
-        case END:
-          goEnd();
-          break;
-        case KeyCode.escape:
-          e.stopPropagation();
-          closeDropdown();
-          break;
-        case KeyCode.enter:
-          e.preventDefault();
+  return (event: CustomEvent<BaseKeyDetail>) => {
+    switch (event.detail.keyCode) {
+      case KeyCode.up:
+        event.preventDefault();
+        goUp();
+        break;
+      case KeyCode.down:
+        event.preventDefault();
+        goDown();
+        break;
+      case HOME:
+        goHome();
+        break;
+      case END:
+        goEnd();
+        break;
+      case KeyCode.escape:
+        event.stopPropagation();
+        closeDropdown();
+        break;
+      case KeyCode.enter:
+        event.preventDefault();
+        selectOption();
+        break;
+      case KeyCode.space:
+        if (preventNativeSpace) {
+          event.preventDefault();
           selectOption();
-          break;
-        case KeyCode.space:
-          if (preventNativeSpace) {
-            e.preventDefault();
-            selectOption();
-          }
-      }
-    },
-    [selectOption, goHome, goEnd, closeDropdown, preventNativeSpace, goUp, goDown]
-  );
+        }
+    }
+  };
 };
 
 interface UseTriggerKeyboard {
@@ -70,22 +65,19 @@ interface UseTriggerKeyboard {
 }
 
 export const useTriggerKeyboard: UseTriggerKeyboard = ({ openDropdown, goHome }) => {
-  return useCallback(
-    (e: CustomEvent<BaseKeyDetail>) => {
-      switch (e.detail.keyCode) {
-        case KeyCode.up:
-        case KeyCode.down:
-          e.preventDefault();
-          goHome();
-          openDropdown();
-          break;
-        case KeyCode.space:
-        case KeyCode.enter:
-          e.preventDefault();
-          openDropdown();
-          break;
-      }
-    },
-    [openDropdown, goHome]
-  );
+  return (event: CustomEvent<BaseKeyDetail>) => {
+    switch (event.detail.keyCode) {
+      case KeyCode.up:
+      case KeyCode.down:
+        event.preventDefault();
+        goHome();
+        openDropdown();
+        break;
+      case KeyCode.space:
+      case KeyCode.enter:
+        event.preventDefault();
+        openDropdown();
+        break;
+    }
+  };
 };
