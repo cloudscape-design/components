9f237e728b09810939ca8bcaa0f9493b5673dc41
Boris Serdiuk <serdiuk@amazon.com>
2024-12-10 12:33:46 +0100

    add comment

--- build-tools/jest/setup.js
+++ build-tools/jest/setup.js
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+// we load this file in both SSR and DOM environment, but these utilities are only needed in DOM
 if (typeof window !== 'undefined') {
   require('@testing-library/jest-dom/extend-expect');
   const { cleanup } = require('@testing-library/react');
