b49b0b7748fd31a03863ded6085e5c354a534e78
Boris Serdiuk <serdiuk@amazon.com>
2024-12-10 12:19:22 +0100

    chore: Do not load testing-library and jest-dom helpers in SSR environment

--- build-tools/jest/setup.js
+++ build-tools/jest/setup.js
@@ -1,5 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-require('@testing-library/jest-dom/extend-expect');
-const { cleanup } = require('@testing-library/react');
-afterEach(cleanup);
+
+if (typeof window !== 'undefined') {
+  require('@testing-library/jest-dom/extend-expect');
+  const { cleanup } = require('@testing-library/react');
+  afterEach(cleanup);
+}
