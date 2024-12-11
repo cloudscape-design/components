4e079bbbe52e26435c5055d0bb1e709e078c9c24
Johannes Weber <jowejowe@amazon.com>
2024-12-11 10:46:00 +0100

    chore: Add more unit test for SearchResults

--- src/text-filter/__tests__/search-results.test.tsx
+++ src/text-filter/__tests__/search-results.test.tsx
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import React, { createRef } from 'react';
-import { act, render } from '@testing-library/react';
+import { act, render, waitFor } from '@testing-library/react';
 
 import { InternalLiveRegionRef } from '../../../lib/components/live-region/internal'; // Adjust path as needed
 import createWrapper from '../../../lib/components/test-utils/dom';
@@ -9,49 +9,55 @@ import { SearchResults } from '../../../lib/components/text-filter/search-result
 
 import styles from '../../../lib/components/text-filter/styles.css.js';
 
-const renderSearchResults = (jsx: React.ReactElement, runAllTimers = true) => {
-  const { container, rerender } = render(jsx);
-  if (runAllTimers) {
-    act(() => void jest.runAllTimers());
-  }
+const renderSearchResults = (renderLiveRegion: boolean) => {
+  const ref = createRef<InternalLiveRegionRef>(); // Create a ref to test the imperative handle
+  const { container } = render(
+    <SearchResults id="dummy-id" ref={ref} renderLiveRegion={renderLiveRegion}>
+      123 results found
+    </SearchResults>
+  );
+  jest.runAllTimers();
 
   return {
-    searchResultsElement: container.querySelector(`.${styles.results}`)!,
+    searchResults: container.querySelector(`.${styles.results}`)!,
+    politeRegion: document.querySelector('[aria-live=polite]')!,
     wrapper: createWrapper(container),
-    rerender,
+    ref,
   };
 };
 
 describe('SearchResults', () => {
-  let searchResultsRef: React.RefObject<InternalLiveRegionRef>;
-  beforeEach(() => {
+  beforeAll(() => {
     jest.useFakeTimers();
-    searchResultsRef = createRef<InternalLiveRegionRef>(); // Create a ref to test the imperative handle
   });
 
-  afterEach(() => {
+  afterAll(() => {
     jest.useRealTimers();
   });
 
-  test('renders the visible representation and live region component when renderLiveRegion set to true', () => {
-    const { searchResultsElement, wrapper } = renderSearchResults(
-      <SearchResults id="search-results-id" ref={searchResultsRef} renderLiveRegion={true}>
-        123 results found
-      </SearchResults>
-    );
-    expect(searchResultsElement.textContent).toBe('123 results found');
+  afterEach(() => {
+    document.body.innerHTML = '';
+  });
+
+  test('renders the content and live region content when renderLiveRegion is set to true', () => {
+    const { searchResults, wrapper } = renderSearchResults(true);
+    expect(searchResults.textContent).toBe('123 results found');
     expect(wrapper.findLiveRegion()!.getElement().textContent).toBe('123 results found');
   });
 
-  test('renders the visible representation and no live region component when renderLiveRegion set to false', () => {
-    const { searchResultsElement, wrapper } = renderSearchResults(
-      <SearchResults id="search-results-id" ref={searchResultsRef} renderLiveRegion={false}>
-        123 results found
-      </SearchResults>
-    );
-    expect(searchResultsElement.textContent).toBe('123 results found');
+  test('renders the content and no live region content when renderLiveRegion set to false', () => {
+    const { searchResults, wrapper } = renderSearchResults(false);
+    expect(searchResults.textContent).toBe('123 results found');
     expect(wrapper.findLiveRegion()).toBeNull();
   });
 
-  // TODO add test if reannounce is called; mock InternalLiveRegion's reannounce and validate if it's called as expected.
+  test('calling `reannounce` on SearchResults re-announces the live region content', async () => {
+    const { politeRegion, ref } = renderSearchResults(true);
+    await waitFor(() => expect(politeRegion).toHaveTextContent('123 results found'));
+    act(() => {
+      ref.current?.reannounce();
+      jest.runAllTimers();
+    });
+    await waitFor(() => expect(politeRegion).toHaveTextContent('123 results found.'));
+  });
 });
