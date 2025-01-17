e62a0745c8920c1479d0dfcaec3a2bf2aca5f79f
Joan Perals <jotresse@amazon.com>
2025-01-17 12:51:01 +0100

    chore: Remove local override to enable scrollbars in integration tests (#3183)

--- src/app-layout/__integ__/runtime-drawers-with-updates.test.ts
+++ src/app-layout/__integ__/runtime-drawers-with-updates.test.ts
@@ -1,9 +1,9 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import { BasePageObject } from '@cloudscape-design/browser-test-tools/page-objects';
+import useBrowser from '@cloudscape-design/browser-test-tools/use-browser';
 
 import createWrapper from '../../../lib/components/test-utils/selectors';
-import useBrowser from '../../__integ__/use-browser-with-scrollbars';
 import { getUrlParams, Theme } from './utils';
 
 const wrapper = createWrapper().findAppLayout();
