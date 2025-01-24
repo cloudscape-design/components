ff8c5d0f57dc9a3e9d8a82ab14312a965aa5273f
Georgii Lobko <globko@amazon.de>
2025-01-24 14:06:18 +0100

    chore: Add u tests

--- src/page-layout/__tests__/page-layout.test.tsx
+++ src/page-layout/__tests__/page-layout.test.tsx
@@ -14,6 +14,14 @@ export function renderComponent(jsx: React.ReactElement) {
 }
 
 describe('PageLayout', () => {
+  test('without toolbar', () => {
+    const { wrapper } = renderComponent(
+      <PageLayout toolbarHide={true} navigation={<>Mock Navigation</>} content={<div>Content</div>} />
+    );
+
+    expect(wrapper.findToolbar()).toBeFalsy();
+  });
+
   test('triggerless navigation', () => {
     const PageLayoutWrapper = () => {
       const [isNavigationOpen, setIsNavigationOpen] = useState(false);
