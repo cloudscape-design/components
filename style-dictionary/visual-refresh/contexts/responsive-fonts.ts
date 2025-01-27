bdb56e8ac4f0e99ab42246b7ada7b9e7ca24772f
Francesco Longo <flongo@amazon.de>
2025-01-27 14:20:02 +0100

    feat: Introduce responsive fonts visual context

---
+++ style-dictionary/visual-refresh/contexts/responsive-fonts.ts
@@ -0,0 +1,24 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { StyleDictionary } from '../../utils/interfaces';
+
+const tokens: StyleDictionary.TypographyDictionary = {
+  fontSizeBodyM: '1.4rem',
+  fontSizeBodyS: '1.2rem',
+  fontSizeDisplayL: '4.2rem',
+  fontSizeHeadingXl: '2.4rem',
+  fontSizeHeadingL: '2.0rem',
+  fontSizeHeadingM: '1.8rem',
+  fontSizeHeadingS: '1.6rem',
+  fontSizeHeadingXs: '1.4rem',
+  lineHeightBodyM: '2rem',
+  lineHeightBodyS: '1.6rem',
+  lineHeightDisplayL: '4.8rem',
+  lineHeightHeadingXl: '3rem',
+  lineHeightHeadingL: '2.4rem',
+  lineHeightHeadingM: '2.2rem',
+  lineHeightHeadingS: '2rem',
+  lineHeightHeadingXs: '1.8rem',
+};
+
+export { tokens };
