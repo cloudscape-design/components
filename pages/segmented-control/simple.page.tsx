9efd108b32909dbd78d29f4ef32cb75017b89f1e
at-susie <ienakaai@amazon.de>
2025-02-07 12:58:46 +0100

    update example

--- pages/segmented-control/simple.page.tsx
+++ pages/segmented-control/simple.page.tsx
@@ -3,29 +3,25 @@
 import React, { useState } from 'react';
 
 import { Box, Button, Select, SpaceBetween } from '~components';
-import Input from '~components/input';
 import SegmentedControl from '~components/segmented-control';
 
 export default function SegmentedControlPage() {
+  const [selectedFirstId, setSelecteFirstdId] = useState<string | null>('seg1-1');
   const [selectedId, setSelectedId] = useState<string | null>('seg-4');
   return (
     <article>
       <h1>SegmentedControl demo</h1>
       <SpaceBetween size="l">
-        <Input ariaLabel="Input Label" value="" readOnly={true} />
-        <Box margin={{ horizontal: 'm' }}>
+        <Box margin={{ horizontal: 'm', vertical: 'xxl' }}>
           <SpaceBetween size="xl" direction="horizontal">
             <SegmentedControl
               options={[
-                { text: 'Segment-1', iconName: 'settings', id: 'seg-1', disabled: false },
-                { text: 'S2', iconName: 'settings', id: 'seg-2', disabled: false },
-                { text: 'S3', iconName: 'settings', id: 'seg-3', disabled: false },
-                { text: 'Segment-4 Logs Insights QL', iconName: 'settings', id: 'seg-4', disabled: false },
-                { text: 'Segment-5', iconName: 'settings', id: 'seg-5', disabled: false },
-                { text: 'Segment-6 Logs Insights QL', iconName: 'settings', id: 'seg-6', disabled: false },
+                { text: 'Logs Insights QL', id: 'seg1-1', disabled: false },
+                { text: 'OpenSearch PPL - new', id: 'seg1-2', disabled: false },
+                { text: 'OpenSearch SQL - new', id: 'seg1-3', disabled: false },
               ]}
-              onChange={event => setSelectedId(event.detail.selectedId)}
-              selectedId={selectedId}
+              onChange={event => setSelecteFirstdId(event.detail.selectedId)}
+              selectedId={selectedFirstId}
               label="Segmented Control Label"
             />
             <Select
@@ -41,6 +37,21 @@ export default function SegmentedControlPage() {
             />
             <Button>Button</Button>
           </SpaceBetween>
+        </Box>
+        <Box margin={{ horizontal: 'm', vertical: 'xxl' }}>
+          <SegmentedControl
+            options={[
+              { text: 'Segment-1', iconName: 'settings', id: 'seg-1', disabled: false },
+              { text: 'S2', iconName: 'settings', id: 'seg-2', disabled: false },
+              { text: 'S3', iconName: 'settings', id: 'seg-3', disabled: false },
+              { text: 'Segment-4 Logs Insights QL', iconName: 'settings', id: 'seg-4', disabled: false },
+              { text: 'Segment-5', iconName: 'settings', id: 'seg-5', disabled: false },
+              { text: 'Segment-6 Logs Insights QL', iconName: 'settings', id: 'seg-6', disabled: false },
+            ]}
+            onChange={event => setSelectedId(event.detail.selectedId)}
+            selectedId={selectedId}
+            label="Segmented Control Label"
+          />
         </Box>
       </SpaceBetween>
     </article>
