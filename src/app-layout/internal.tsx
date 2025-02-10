af7d117d13e2f62b3b525c020789c0b7464ab06d
Michael Dowse <dowsem@amazon.com>
2025-02-10 13:05:56 +0100

    chore: Export only ToolBarAppLayout

--- src/app-layout/internal.tsx
+++ src/app-layout/internal.tsx
@@ -3,15 +3,8 @@
 import React from 'react';
 
 import { AppLayoutProps, AppLayoutPropsWithDefaults } from './interfaces';
-import { useAppLayoutToolbarEnabled } from './utils/feature-flags';
-import RefreshedAppLayout from './visual-refresh';
 import ToolbarAppLayout from './visual-refresh-toolbar';
 
 export const AppLayoutInternal = React.forwardRef<AppLayoutProps.Ref, AppLayoutPropsWithDefaults>((props, ref) => {
-  const isToolbar = useAppLayoutToolbarEnabled();
-  if (isToolbar) {
-    return <ToolbarAppLayout ref={ref} {...props} />;
-  } else {
-    return <RefreshedAppLayout ref={ref} {...props} />;
-  }
+  return <ToolbarAppLayout ref={ref} {...props} />;
 });
