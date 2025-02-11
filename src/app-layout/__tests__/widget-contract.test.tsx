1aaf0837d3d6590e2d235bb32abf02b896bd5f4b
Georgii Lobko <globko@amazon.de>
2025-02-11 11:25:57 +0100

    chore: Replace back real split panel with a dummy version

--- src/app-layout/__tests__/widget-contract.test.tsx
+++ src/app-layout/__tests__/widget-contract.test.tsx
@@ -6,7 +6,6 @@ import { render } from '@testing-library/react';
 
 import { describeEachAppLayout, testDrawer } from './utils';
 import AppLayout from '../../../lib/components/app-layout';
-import SplitPanel from '../../../lib/components/split-panel';
 
 const renderedProps = new Map();
 function createWidgetizedComponentMock(Implementation: React.ComponentType) {
@@ -46,58 +45,7 @@ describeEachAppLayout({ themes: ['refresh-toolbar'], sizes: ['desktop'] }, () =>
         navigation={<div>navigation</div>}
         tools={<div>tools</div>}
         content={<div>content</div>}
-        splitPanel={
-          <SplitPanel
-            header="Split panel header"
-            i18nStrings={{
-              preferencesTitle: 'Preferences',
-              preferencesPositionLabel: 'Split panel position',
-              preferencesPositionDescription: 'Choose the default split panel position for the service.',
-              preferencesPositionSide: 'Side',
-              preferencesPositionBottom: 'Bottom',
-              preferencesConfirm: 'Confirm',
-              preferencesCancel: 'Cancel',
-              closeButtonAriaLabel: 'Close panel',
-              openButtonAriaLabel: 'Open panel',
-              resizeHandleAriaLabel: 'Slider',
-            }}
-          >
-            <div>split panel content</div>
-          </SplitPanel>
-        }
-      />
-    );
-    expect(renderedProps).toMatchSnapshot();
-  });
-
-  test('contract with provided but hidden split panel', () => {
-    render(
-      <AppLayout
-        breadcrumbs={<div>breadcrumbs</div>}
-        notifications={<div>notifications</div>}
-        navigation={<div>navigation</div>}
-        tools={<div>tools</div>}
-        content={<div>content</div>}
-        splitPanel={
-          <SplitPanel
-            header="Split panel header"
-            closeBehavior="hide"
-            i18nStrings={{
-              preferencesTitle: 'Preferences',
-              preferencesPositionLabel: 'Split panel position',
-              preferencesPositionDescription: 'Choose the default split panel position for the service.',
-              preferencesPositionSide: 'Side',
-              preferencesPositionBottom: 'Bottom',
-              preferencesConfirm: 'Confirm',
-              preferencesCancel: 'Cancel',
-              closeButtonAriaLabel: 'Close panel',
-              openButtonAriaLabel: 'Open panel',
-              resizeHandleAriaLabel: 'Slider',
-            }}
-          >
-            <div>split panel content</div>
-          </SplitPanel>
-        }
+        splitPanel={<div>split panel</div>}
       />
     );
     expect(renderedProps).toMatchSnapshot();
