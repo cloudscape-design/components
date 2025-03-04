1bee751d4eccb47d3825794fd7ba3d69161be55e
Joan Perals Tresserra <jotresse@amazon.com>
2025-03-04 18:23:05 +0100

    Keep open state buttons when side split panel is closed

--- src/split-panel/side.tsx
+++ src/split-panel/side.tsx
@@ -8,13 +8,14 @@ import { ButtonProps } from '../button/interfaces';
 import InternalButton from '../button/internal';
 import { useSplitPanelContext } from '../internal/context/split-panel-context';
 import { useVisualRefresh } from '../internal/hooks/use-visual-mode';
-import { SplitPanelContentProps } from './interfaces';
+import { SplitPanelContentProps, SplitPanelProps } from './interfaces';
 
 import sharedStyles from '../app-layout/resize/styles.css.js';
 import styles from './styles.css.js';
 import testUtilStyles from './test-classes/styles.css.js';
 
 interface SplitPanelContentSideProps extends SplitPanelContentProps {
+  closeBehavior: SplitPanelProps['closeBehavior'];
   openButtonAriaLabel?: string;
   toggleRef: React.RefObject<ButtonProps.Ref>;
 }
@@ -32,6 +33,7 @@ export function SplitPanelContentSide({
   openButtonAriaLabel,
   panelHeaderId,
   onToggle,
+  closeBehavior,
 }: SplitPanelContentSideProps) {
   const { topOffset, bottomOffset, animationDisabled } = useSplitPanelContext();
   const isRefresh = useVisualRefresh();
@@ -63,7 +65,7 @@ export function SplitPanelContentSide({
         aria-labelledby={panelHeaderId}
         role="region"
       >
-        {isOpen ? (
+        {isOpen || closeBehavior === 'hide' ? (
           <div className={clsx(styles['slider-wrapper-side'], isToolbar && styles['with-toolbar'])}>{resizeHandle}</div>
         ) : (
           <InternalButton
