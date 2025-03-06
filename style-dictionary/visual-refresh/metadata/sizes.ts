7d30dfa9232445281457eb547b0c66f4b34614ce
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-03-06 14:14:59 +0100

    chore: Exposes more design tokens to be used by charts

--- style-dictionary/visual-refresh/metadata/sizes.ts
+++ style-dictionary/visual-refresh/metadata/sizes.ts
@@ -2,6 +2,27 @@
 // SPDX-License-Identifier: Apache-2.0
 import { StyleDictionary } from '../../utils/interfaces.js';
 
-const metadata: StyleDictionary.MetadataIndex = {};
+const metadata: StyleDictionary.MetadataIndex = {
+  sizeIconNormal: {
+    description: 'Normal icon size',
+    public: true,
+    themeable: false,
+  },
+  sizeIconMedium: {
+    description: 'Medium icon size',
+    public: true,
+    themeable: false,
+  },
+  sizeIconBig: {
+    description: 'Big icon size',
+    public: true,
+    themeable: false,
+  },
+  sizeIconLarge: {
+    description: 'Large icon size',
+    public: true,
+    themeable: false,
+  },
+};
 
 export default metadata;
