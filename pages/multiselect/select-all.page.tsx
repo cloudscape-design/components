3e7209479b9df267cd65b2743cd71ee6316678e9
Joan Perals Tresserra <jotresse@amazon.com>
2025-03-10 18:13:38 +0100

    Refactor dev page

--- pages/multiselect/select-all.page.tsx
+++ pages/multiselect/select-all.page.tsx
@@ -7,7 +7,7 @@ import Multiselect, { MultiselectProps } from '~components/multiselect';
 
 import { deselectAriaLabel, getInlineAriaLabel, i18nStrings } from './constants';
 
-const _selectedOptions1 = [
+const _selectedOptions = [
   {
     value: 'option3',
     label: 'option3',
@@ -21,22 +21,7 @@ const _selectedOptions1 = [
     tags: ['2-CPU', '2Gb RAM'],
   },
 ];
-const _selectedOptions2 = [
-  {
-    value: 'option4',
-    label: 'option4',
-    description: 'option4',
-    tags: ['2-CPU', '2Gb RAM'],
-  },
-  {
-    value: 'option5',
-    label: 'option5',
-    description: 'option5',
-    tags: ['2-CPU', '2Gb RAM', 'Stuff', 'More stuff', 'A lot'],
-    disabled: true,
-  },
-];
-const options1 = [
+const options = [
   {
     label: 'First category',
     options: [
@@ -84,175 +69,64 @@ const options1 = [
     ],
   },
 ];
-const options2 = [
-  ...options1,
-  {
-    label: 'Third category',
-    options: [
-      {
-        value: 'option7',
-        label: 'option7',
-        description: 'option7',
-        tags: ['2-CPU', '2Gb RAM'],
-        disabled: true,
-      },
-      {
-        value: 'option8',
-        label: 'option8',
-        description: 'option8',
-        tags: ['2-CPU', '2Gb RAM', 'Stuff', 'More stuff', 'A lot'],
-        disabled: true,
-      },
-      {
-        value: 'option9',
-        label: 'option9',
-        description: 'option9',
-        tags: ['2-CPU', '2Gb RAM'],
-        disabled: true,
-      },
-    ],
-  },
-];
+
+function InternalMultiselect(props: Partial<MultiselectProps>) {
+  const [selectedOptions, setSelectedOptions] = React.useState<MultiselectProps.Options>(_selectedOptions);
+  return (
+    <Multiselect
+      selectedOptions={selectedOptions}
+      deselectAriaLabel={deselectAriaLabel}
+      statusType="pending"
+      filteringType="none"
+      options={options}
+      i18nStrings={i18nStrings}
+      enableSelectAll={true}
+      placeholder={'Choose option'}
+      onChange={event => {
+        setSelectedOptions(event.detail.selectedOptions);
+      }}
+      ariaLabel={props.inlineTokens ? getInlineAriaLabel(selectedOptions) : undefined}
+      {...props}
+    />
+  );
+}
 
 export default function MultiselectPage() {
-  const [selectedOptions1, setSelectedOptions1] = React.useState<MultiselectProps.Options>(_selectedOptions1);
-  const [selectedOptions2, setSelectedOptions2] = React.useState<MultiselectProps.Options>(_selectedOptions1);
-  const [selectedOptions3, setSelectedOptions3] = React.useState<MultiselectProps.Options>([]);
-  const [selectedOptions4, setSelectedOptions4] = React.useState<MultiselectProps.Options>(_selectedOptions1);
-  const [selectedOptions5, setSelectedOptions5] = React.useState<MultiselectProps.Options>(_selectedOptions2);
-  const [selectedOptions6, setSelectedOptions6] = React.useState<MultiselectProps.Options>(_selectedOptions1);
-  const [selectedOptions7, setSelectedOptions7] = React.useState<MultiselectProps.Options>(_selectedOptions1);
-
   return (
     <article>
       <Box padding="l">
         <Box padding="s">
           <Box variant="h1">Test: Close after</Box>
-          <Multiselect
-            id="close_after"
-            statusType="pending"
-            filteringType="none"
-            options={options1}
-            placeholder={'Choose option'}
-            tokenLimit={3}
-            keepOpen={false}
-            selectedOptions={selectedOptions1}
-            deselectAriaLabel={deselectAriaLabel}
-            i18nStrings={i18nStrings}
-            enableSelectAll={true}
-            onChange={event => {
-              setSelectedOptions1(event.detail.selectedOptions);
-            }}
-          />
+          <InternalMultiselect id="close_after" tokenLimit={3} keepOpen={false} enableSelectAll={true} />
         </Box>
         <Box padding="s">
           <Box variant="h1">Test: Keep open</Box>
-          <Multiselect
-            id="keep_open"
-            statusType="pending"
-            filteringType="none"
-            options={options1}
-            placeholder={'Choose option'}
-            tokenLimit={3}
-            selectedOptions={selectedOptions2}
-            deselectAriaLabel={deselectAriaLabel}
-            i18nStrings={i18nStrings}
-            enableSelectAll={true}
-            onChange={event => {
-              setSelectedOptions2(event.detail.selectedOptions);
-            }}
-          />
+          <InternalMultiselect id="keep_open" tokenLimit={3} enableSelectAll={true} />
         </Box>
         <Box padding="s">
           <Box variant="h1">Test: With Filtering</Box>
-          <Multiselect
-            id="with_filtering"
-            statusType="pending"
-            filteringType="auto"
-            options={options1}
-            placeholder={'Choose option'}
-            selectedOptions={selectedOptions3}
-            deselectAriaLabel={deselectAriaLabel}
-            i18nStrings={i18nStrings}
-            enableSelectAll={true}
-            onChange={event => {
-              setSelectedOptions3(event.detail.selectedOptions);
-            }}
-          />
+          <InternalMultiselect id="with_filtering" filteringType="auto" />
         </Box>
 
         <Box padding="s">
           <Box variant="h1">Test: With token limit</Box>
-          <Multiselect
-            id="with_token_limit"
-            statusType="pending"
-            filteringType="none"
-            options={options1}
-            placeholder={'Choose option'}
-            tokenLimit={1}
-            selectedOptions={selectedOptions4}
-            deselectAriaLabel={deselectAriaLabel}
-            i18nStrings={i18nStrings}
-            onChange={event => {
-              setSelectedOptions4(event.detail.selectedOptions);
-            }}
-          />
+          <InternalMultiselect id="with_token_limit" placeholder={'Choose option'} tokenLimit={1} />
         </Box>
 
         <Box padding="s">
           <Box variant="h1">Test: Group selection</Box>
-          <Multiselect
-            id="group_selection"
-            statusType="pending"
-            filteringType="auto"
-            options={options2}
-            placeholder={'Choose option'}
-            selectedOptions={selectedOptions5}
-            deselectAriaLabel={deselectAriaLabel}
-            i18nStrings={i18nStrings}
-            onChange={event => {
-              setSelectedOptions5(event.detail.selectedOptions);
-            }}
-          />
+          <InternalMultiselect id="group_selection" />
         </Box>
 
         <Box padding="s">
           <Box variant="h1">Test: Expand to viewport</Box>
-          <Multiselect
-            id="expand_to_viewport"
-            statusType="pending"
-            filteringType="none"
-            options={selectedOptions6}
-            placeholder={'Choose option'}
-            tokenLimit={3}
-            selectedOptions={selectedOptions1}
-            deselectAriaLabel={deselectAriaLabel}
-            i18nStrings={i18nStrings}
-            expandToViewport={true}
-            enableSelectAll={true}
-            onChange={event => {
-              setSelectedOptions6(event.detail.selectedOptions);
-            }}
-          />
+          <InternalMultiselect id="expand_to_viewport" expandToViewport={true} />
         </Box>
 
         <Box padding="s">
           <Box variant="h1">Test: Inline tokens</Box>
           <div style={{ width: 200 }}>
-            <Multiselect
-              inlineTokens={true}
-              statusType="pending"
-              filteringType="none"
-              options={options1}
-              placeholder={'Choose option'}
-              selectedOptions={selectedOptions7}
-              i18nStrings={i18nStrings}
-              ariaLabel={getInlineAriaLabel(selectedOptions7)}
-              enableSelectAll={true}
-              onChange={event => {
-                setSelectedOptions7(event.detail.selectedOptions);
-              }}
-            />
+            <InternalMultiselect inlineTokens={true} enableSelectAll={true} />
           </div>
         </Box>
       </Box>
