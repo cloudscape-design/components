383d72ce183653bb28e04b06ff2316ebf0dc4e52
Joan Perals Tresserra <jotresse@amazon.com>
2025-03-19 14:07:03 +0100

    Add test for disabled state

--- src/multiselect/__tests__/select-all.test.tsx
+++ src/multiselect/__tests__/select-all.test.tsx
@@ -3,13 +3,18 @@
 
 import { KeyCode } from '@cloudscape-design/test-utils-core/utils';
 
+import { MultiselectProps } from '../interfaces';
 import { optionsWithoutGroups, renderMultiselect } from './common';
 
+const renderMultiselectWithSelectAll = (props?: Partial<MultiselectProps>) => {
+  const { wrapper } = renderMultiselect({ ...(props || {}), enableSelectAll: true });
+  return wrapper;
+};
 describe('Multiselect with "Select all" control', () => {
   describe('Mouse interactions', () => {
     test('selects all options when none are selected', () => {
       const onChange = jest.fn();
-      const { wrapper } = renderMultiselect({ enableSelectAll: true, onChange });
+      const wrapper = renderMultiselectWithSelectAll({ onChange });
       wrapper.openDropdown();
       wrapper.selectOption(1);
       expect(onChange).toHaveBeenCalledWith(
@@ -18,13 +23,13 @@ describe('Multiselect with "Select all" control', () => {
     });
     test('deselects all options when all are selected', () => {
       const onChange = jest.fn();
-      const { wrapper } = renderMultiselect({ enableSelectAll: true, selectedOptions: optionsWithoutGroups, onChange });
+      const wrapper = renderMultiselectWithSelectAll({ selectedOptions: optionsWithoutGroups, onChange });
       wrapper.openDropdown();
       wrapper.selectOption(1);
       expect(onChange).toHaveBeenCalledWith(expect.objectContaining({ detail: { selectedOptions: [] } }));
     });
     test('closes the dropdown after clicking when `keepOpen` is false', () => {
-      const { wrapper } = renderMultiselect({ enableSelectAll: true, keepOpen: false });
+      const wrapper = renderMultiselectWithSelectAll({ keepOpen: false });
       wrapper.openDropdown();
       const dropdown = wrapper.findDropdown();
       expect(dropdown.findOptionByValue('1')).not.toBeNull();
@@ -36,7 +41,7 @@ describe('Multiselect with "Select all" control', () => {
   describe('Keyboard interactions', () => {
     test('selects all options when none are selected', () => {
       const onChange = jest.fn();
-      const { wrapper } = renderMultiselect({ enableSelectAll: true, onChange });
+      const wrapper = renderMultiselectWithSelectAll({ onChange });
       wrapper.openDropdown();
       const optionsContainer = wrapper.findDropdown().findOptionsContainer()!;
       // When opening the dropdown no option gets highlighted if none is selected. Move one position down to highlight the "Select all" control.
@@ -48,7 +53,7 @@ describe('Multiselect with "Select all" control', () => {
     });
     test('deselects all options when all are selected', () => {
       const onChange = jest.fn();
-      const { wrapper } = renderMultiselect({ enableSelectAll: true, selectedOptions: optionsWithoutGroups, onChange });
+      const wrapper = renderMultiselectWithSelectAll({ selectedOptions: optionsWithoutGroups, onChange });
       wrapper.openDropdown();
       const optionsContainer = wrapper.findDropdown().findOptionsContainer()!;
       // When opening the dropdown the first selected option is highlighted. Move one position up to highlight the "Select all" control.
@@ -58,7 +63,7 @@ describe('Multiselect with "Select all" control', () => {
     });
     test('closes the dropdown after clicking when `keepOpen` is false', () => {
       const onChange = jest.fn();
-      const { wrapper } = renderMultiselect({ enableSelectAll: true, keepOpen: false, onChange });
+      const wrapper = renderMultiselectWithSelectAll({ keepOpen: false, onChange });
       wrapper.openDropdown();
       const dropdown = wrapper.findDropdown();
       const optionsContainer = dropdown.findOptionsContainer()!;
@@ -67,5 +72,13 @@ describe('Multiselect with "Select all" control', () => {
       optionsContainer.keydown(KeyCode.space);
       expect(dropdown.findOptionByValue('1')).toBeNull();
     });
+  });
+
+  test('is disabled when there are no options to select', () => {
+    const wrapper = renderMultiselectWithSelectAll({ options: [] });
+    wrapper.openDropdown();
+    const options = wrapper.findDropdown().findOptions();
+    expect(options.length).toBe(1);
+    expect(options[0].getElement().getAttribute('aria-disabled')).toBe('true');
   });
 });
