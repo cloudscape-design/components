3a1180581405a40f5eb2c7954d2e53d36270fefe
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-03-19 13:25:58 +0100

    exposes internal components

---
+++ src/internal/button-dropdown-do-not-use/index.ts
@@ -0,0 +1,7 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import InternalButtonDropdown_DO_NOT_USE from '../../button-dropdown/internal';
+export type { InternalButtonDropdownProps } from '../../button-dropdown/interfaces';
+
+export default InternalButtonDropdown_DO_NOT_USE;
