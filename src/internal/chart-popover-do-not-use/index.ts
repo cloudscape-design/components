1271512b04dac1b0d607f85f7e20b91e463691dc
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-03-19 13:25:58 +0100

    exposes internal components

---
+++ src/internal/chart-popover-do-not-use/index.ts
@@ -0,0 +1,7 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import ChartPopover_DO_NOT_USE, { ChartPopoverProps } from '../components/chart-popover/index.js';
+
+export type InternalChartPopoverProps = ChartPopoverProps;
+export default ChartPopover_DO_NOT_USE;
