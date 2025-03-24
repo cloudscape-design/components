4392b9f4c4af460c62d99a78156775a55bd0a312
Joan Perals Tresserra <jotresse@amazon.com>
2025-03-24 18:32:44 +0100

    Add unit test coverage for virtual scroll case

--- src/multiselect/__tests__/select-all.test.tsx
+++ src/multiselect/__tests__/select-all.test.tsx
@@ -15,99 +15,123 @@ const renderMultiselectWithSelectAll = (props?: Partial<MultiselectProps>) => {
   const { wrapper } = renderMultiselect({ ...(props || {}), enableSelectAll: true });
   return wrapper;
 };
-describe('Multiselect with "Select all" control', () => {
-  describe('Mouse interactions', () => {
-    test('selects all options when none are selected', () => {
-      const onChange = jest.fn();
-      const wrapper = renderMultiselectWithSelectAll({ onChange });
-      wrapper.openDropdown();
+describe('Multiselect with "select all" control', () => {
+  describe.each([false, true])('virtualScroll=%s', virtualScroll => {
+    describe('Mouse interactions', () => {
+      test('selects all options when none are selected', () => {
+        const onChange = jest.fn();
+        const wrapper = renderMultiselectWithSelectAll({ onChange, virtualScroll });
+        wrapper.openDropdown();
 
-      wrapper.selectAll();
-      expect(onChange).toHaveBeenCalledWith(
-        expect.objectContaining({ detail: { selectedOptions: optionsWithoutGroups } })
-      );
+        wrapper.selectAll();
+        expect(onChange).toHaveBeenCalledWith(
+          expect.objectContaining({ detail: { selectedOptions: optionsWithoutGroups } })
+        );
+      });
+
+      test('selects all options when some but not all are selected', () => {
+        const onChange = jest.fn();
+        const wrapper = renderMultiselectWithSelectAll({
+          onChange,
+          selectedOptions: [optionsWithoutGroups[0]],
+          virtualScroll,
+        });
+        wrapper.openDropdown();
+        wrapper.selectAll();
+        expect(onChange).toHaveBeenCalledWith(
+          expect.objectContaining({ detail: { selectedOptions: optionsWithoutGroups } })
+        );
+      });
+
+      test('deselects all options when all are selected', () => {
+        const onChange = jest.fn();
+        const wrapper = renderMultiselectWithSelectAll({
+          selectedOptions: optionsWithoutGroups,
+          onChange,
+          virtualScroll,
+        });
+        wrapper.openDropdown();
+        wrapper.selectAll();
+        expect(onChange).toHaveBeenCalledWith(expect.objectContaining({ detail: { selectedOptions: [] } }));
+      });
+
+      test('closes the dropdown after clicking when `keepOpen` is false', () => {
+        const wrapper = renderMultiselectWithSelectAll({ keepOpen: false, virtualScroll });
+        wrapper.openDropdown();
+        const dropdown = wrapper.findDropdown();
+        expect(dropdown.findOptionByValue('1')).not.toBeNull();
+        wrapper.selectAll();
+        expect(dropdown.findOptionByValue('1')).toBeNull();
+      });
     });
-    test('selects all options when some but not all are selected', () => {
-      const onChange = jest.fn();
-      const wrapper = renderMultiselectWithSelectAll({ onChange, selectedOptions: [optionsWithoutGroups[0]] });
-      wrapper.openDropdown();
-      wrapper.selectAll();
-      expect(onChange).toHaveBeenCalledWith(
-        expect.objectContaining({ detail: { selectedOptions: optionsWithoutGroups } })
-      );
+
+    describe('Keyboard interactions', () => {
+      test('selects all options when none are selected', () => {
+        const onChange = jest.fn();
+        const wrapper = renderMultiselectWithSelectAll({ onChange, virtualScroll });
+        wrapper.openDropdown();
+        const optionsContainer = wrapper.findDropdown().findOptionsContainer()!;
+        // When opening the dropdown no option gets highlighted if none is selected. Move one position down to highlight the "Select all" control.
+        optionsContainer.keydown(KeyCode.down);
+        optionsContainer.keydown(KeyCode.space);
+        expect(onChange).toHaveBeenCalledWith(
+          expect.objectContaining({ detail: { selectedOptions: optionsWithoutGroups } })
+        );
+      });
+
+      test('selects all options when some but not all are selected', () => {
+        const onChange = jest.fn();
+        const wrapper = renderMultiselectWithSelectAll({
+          onChange,
+          selectedOptions: [optionsWithoutGroups[0]],
+          virtualScroll,
+        });
+        wrapper.openDropdown();
+        const optionsContainer = wrapper.findDropdown().findOptionsContainer()!;
+        // When opening the dropdown the first selected option is highlighted. Move one position up to highlight the "Select all" control.
+        optionsContainer.keydown(KeyCode.up);
+        optionsContainer.keydown(KeyCode.space);
+        expect(onChange).toHaveBeenCalledWith(
+          expect.objectContaining({ detail: { selectedOptions: optionsWithoutGroups } })
+        );
+      });
+
+      test('deselects all options when all are selected', () => {
+        const onChange = jest.fn();
+        const wrapper = renderMultiselectWithSelectAll({
+          selectedOptions: optionsWithoutGroups,
+          onChange,
+          virtualScroll,
+        });
+        wrapper.openDropdown();
+        const optionsContainer = wrapper.findDropdown().findOptionsContainer()!;
+        // When opening the dropdown the first selected option is highlighted. Move one position up to highlight the "Select all" control.
+        optionsContainer.keydown(KeyCode.up);
+        optionsContainer.keydown(KeyCode.space);
+        expect(onChange).toHaveBeenCalledWith(expect.objectContaining({ detail: { selectedOptions: [] } }));
+      });
+
+      test('closes the dropdown after clicking when `keepOpen` is false', () => {
+        const onChange = jest.fn();
+        const wrapper = renderMultiselectWithSelectAll({ keepOpen: false, onChange, virtualScroll });
+        wrapper.openDropdown();
+        const dropdown = wrapper.findDropdown();
+        const optionsContainer = dropdown.findOptionsContainer()!;
+        // When opening the dropdown no option gets highlighted if none is selected. Move one position down to highlight the "Select all" control.
+        optionsContainer.keydown(KeyCode.down);
+        optionsContainer.keydown(KeyCode.space);
+        expect(dropdown.findOptionByValue('1')).toBeNull();
+      });
     });
-    test('deselects all options when all are selected', () => {
-      const onChange = jest.fn();
-      const wrapper = renderMultiselectWithSelectAll({ selectedOptions: optionsWithoutGroups, onChange });
-      wrapper.openDropdown();
-      wrapper.selectAll();
-      expect(onChange).toHaveBeenCalledWith(expect.objectContaining({ detail: { selectedOptions: [] } }));
-    });
-    test('closes the dropdown after clicking when `keepOpen` is false', () => {
-      const wrapper = renderMultiselectWithSelectAll({ keepOpen: false });
+
+    test('is disabled when there are no options to select', () => {
+      const wrapper = renderMultiselectWithSelectAll({ options: [], virtualScroll });
       wrapper.openDropdown();
       const dropdown = wrapper.findDropdown();
-      expect(dropdown.findOptionByValue('1')).not.toBeNull();
-      wrapper.selectAll();
-      expect(dropdown.findOptionByValue('1')).toBeNull();
+      const options = dropdown.findOptions();
+      expect(options.length).toBe(0);
+      expect(dropdown.findSelectAll()!.getElement().getAttribute('aria-disabled')).toBe('true');
     });
-  });
-
-  describe('Keyboard interactions', () => {
-    test('selects all options when none are selected', () => {
-      const onChange = jest.fn();
-      const wrapper = renderMultiselectWithSelectAll({ onChange });
-      wrapper.openDropdown();
-      const optionsContainer = wrapper.findDropdown().findOptionsContainer()!;
-      // When opening the dropdown no option gets highlighted if none is selected. Move one position down to highlight the "Select all" control.
-      optionsContainer.keydown(KeyCode.down);
-      optionsContainer.keydown(KeyCode.space);
-      expect(onChange).toHaveBeenCalledWith(
-        expect.objectContaining({ detail: { selectedOptions: optionsWithoutGroups } })
-      );
-    });
-    test('selects all options when some but not all are selected', () => {
-      const onChange = jest.fn();
-      const wrapper = renderMultiselectWithSelectAll({ onChange, selectedOptions: [optionsWithoutGroups[0]] });
-      wrapper.openDropdown();
-      const optionsContainer = wrapper.findDropdown().findOptionsContainer()!;
-      // When opening the dropdown the first selected option is highlighted. Move one position up to highlight the "Select all" control.
-      optionsContainer.keydown(KeyCode.up);
-      optionsContainer.keydown(KeyCode.space);
-      expect(onChange).toHaveBeenCalledWith(
-        expect.objectContaining({ detail: { selectedOptions: optionsWithoutGroups } })
-      );
-    });
-    test('deselects all options when all are selected', () => {
-      const onChange = jest.fn();
-      const wrapper = renderMultiselectWithSelectAll({ selectedOptions: optionsWithoutGroups, onChange });
-      wrapper.openDropdown();
-      const optionsContainer = wrapper.findDropdown().findOptionsContainer()!;
-      // When opening the dropdown the first selected option is highlighted. Move one position up to highlight the "Select all" control.
-      optionsContainer.keydown(KeyCode.up);
-      optionsContainer.keydown(KeyCode.space);
-      expect(onChange).toHaveBeenCalledWith(expect.objectContaining({ detail: { selectedOptions: [] } }));
-    });
-    test('closes the dropdown after clicking when `keepOpen` is false', () => {
-      const onChange = jest.fn();
-      const wrapper = renderMultiselectWithSelectAll({ keepOpen: false, onChange });
-      wrapper.openDropdown();
-      const dropdown = wrapper.findDropdown();
-      const optionsContainer = dropdown.findOptionsContainer()!;
-      // When opening the dropdown no option gets highlighted if none is selected. Move one position down to highlight the "Select all" control.
-      optionsContainer.keydown(KeyCode.down);
-      optionsContainer.keydown(KeyCode.space);
-      expect(dropdown.findOptionByValue('1')).toBeNull();
-    });
-  });
-
-  test('is disabled when there are no options to select', () => {
-    const wrapper = renderMultiselectWithSelectAll({ options: [] });
-    wrapper.openDropdown();
-    const dropdown = wrapper.findDropdown();
-    const options = dropdown.findOptions();
-    expect(options.length).toBe(0);
-    expect(dropdown.findSelectAll()!.getElement().getAttribute('aria-disabled')).toBe('true');
   });
 
   describe('i18n', () => {
