c3c17d5019263b9baefe48b20e1192ecf710cb3e
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-03-25 07:45:57 +0100

    max permutations back to 276

--- pages/utils/permutations-view.tsx
+++ pages/utils/permutations-view.tsx
@@ -21,7 +21,7 @@ function formatValue(key: string, value: any) {
   return value;
 }
 
-const maximumPermutations = 250;
+const maximumPermutations = 276;
 
 export default function PermutationsView<T>({ permutations, render }: PermutationsViewProps<T>) {
   if (permutations.length > maximumPermutations) {
