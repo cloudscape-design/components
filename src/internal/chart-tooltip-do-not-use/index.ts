65859db566754e3f4560f3750e74af890924c9cb
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-03-30 15:40:40 +0200

    chart tooltip

---
+++ src/internal/chart-tooltip-do-not-use/index.ts
@@ -0,0 +1,7 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import ChartTooltip_DO_NOT_USE, { ChartPopoverProps } from '../components/chart-popover/index.js';
+
+export type InternalChartTooltipProps = ChartPopoverProps;
+export default ChartTooltip_DO_NOT_USE;
