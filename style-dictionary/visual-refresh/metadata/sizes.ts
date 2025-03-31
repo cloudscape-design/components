56f86160e3f22451cbcf232373513f35306e238a
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-03-31 12:18:23 +0200

    fix popover selectors

--- style-dictionary/visual-refresh/metadata/sizes.ts
+++ style-dictionary/visual-refresh/metadata/sizes.ts
@@ -3,26 +3,26 @@
 import { StyleDictionary } from '../../utils/interfaces.js';
 
 const metadata: StyleDictionary.MetadataIndex = {
-  sizeIconNormal: {
-    description: 'Normal icon size',
-    public: true,
-    themeable: false,
-  },
-  sizeIconMedium: {
-    description: 'Medium icon size',
-    public: true,
-    themeable: false,
-  },
-  sizeIconBig: {
-    description: 'Big icon size',
-    public: true,
-    themeable: false,
-  },
-  sizeIconLarge: {
-    description: 'Large icon size',
-    public: true,
-    themeable: false,
-  },
+  // sizeIconNormal: {
+  //   description: 'Normal icon size',
+  //   public: true,
+  //   themeable: false,
+  // },
+  // sizeIconMedium: {
+  //   description: 'Medium icon size',
+  //   public: true,
+  //   themeable: false,
+  // },
+  // sizeIconBig: {
+  //   description: 'Big icon size',
+  //   public: true,
+  //   themeable: false,
+  // },
+  // sizeIconLarge: {
+  //   description: 'Large icon size',
+  //   public: true,
+  //   themeable: false,
+  // },
 };
 
 export default metadata;
