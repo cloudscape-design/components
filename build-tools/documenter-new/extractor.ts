561c8db8c605c846cc614ff79d1dd9b5d7a724f0
Boris Serdiuk <serdiuk@amazon.com>
2025-04-01 21:22:27 +0200

    wip

--- build-tools/documenter-new/extractor.ts
+++ build-tools/documenter-new/extractor.ts
@@ -2,6 +2,58 @@
 // SPDX-License-Identifier: Apache-2.0
 import ts from 'typescript';
 
+export function extractDefaultValues(exportSymbol: ts.Symbol, checker: ts.TypeChecker) {
+  // Find the component function
+  let declaration: ts.Node = exportSymbol.getDeclarations()[0];
+  if (ts.isExportAssignment(declaration)) {
+    // Traverse from "export default Something;" to the actual "Something"
+    const symbol = checker.getSymbolAtLocation(declaration.expression);
+    if (!symbol) {
+      throw new Error('Cannot resolve symbol');
+    }
+    declaration = symbol.getDeclarations()[0];
+  }
+  // Extract "Something" from "const Component = Something"
+  if (ts.isVariableDeclaration(declaration) && declaration.initializer) {
+    declaration = declaration.initializer;
+  }
+  // Extract "Something" from "Something as MyComponentType"
+  if (ts.isAsExpression(declaration)) {
+    declaration = declaration.expression;
+  }
+  // Extract "Something from React.forwardRef(Something)"
+  if (
+    ts.isCallExpression(declaration) &&
+    (declaration.expression.getText() === 'React.forwardRef' || declaration.expression.getText() === 'forwardRef')
+  ) {
+    declaration = declaration.arguments[0];
+  }
+
+  // In the component function, find arguments destructuring
+  let argument: ts.Node | undefined;
+  if (
+    ts.isFunctionDeclaration(declaration) ||
+    ts.isFunctionExpression(declaration) ||
+    ts.isArrowFunction(declaration)
+  ) {
+    argument = declaration.parameters[0].name;
+  }
+  if (!argument) {
+    throw new Error(`Unsupported component declaration type ${ts.SyntaxKind[declaration.kind]}`);
+  }
+  if (!ts.isObjectBindingPattern(argument)) {
+    // if a component does not use props de-structuring, we do not detect default values
+    return {};
+  }
+  const values: Record<string, string> = {};
+  for (const element of argument.elements) {
+    if (ts.isIdentifier(element.name) && element.initializer) {
+      values[element.name.escapedText as string] = element.initializer.getText();
+    }
+  }
+  return values;
+}
+
 export function validateExports(componentName: string, exportSymbols: Array<ts.Symbol>, checker: ts.TypeChecker) {
   for (const exportSymbol of exportSymbols) {
     if (exportSymbol.name === 'default') {
