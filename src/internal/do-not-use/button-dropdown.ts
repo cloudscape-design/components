864878225dc10987d5375bbc770e8cbf0edf930f
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-04-03 11:36:46 +0200

    restructured do-not-use exports and i18n export

--- src/internal/do-not-use/button-dropdown.ts
+++ src/internal/do-not-use/button-dropdown.ts
@@ -1,7 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import InternalButtonDropdown_DO_NOT_USE from '../../button-dropdown/internal';
+import InternalButtonDropdown from '../../button-dropdown/internal';
 export type { InternalButtonDropdownProps } from '../../button-dropdown/interfaces';
-
-export default InternalButtonDropdown_DO_NOT_USE;
+export { InternalButtonDropdown };
--- src/internal/do-not-use/chart-tooltip.ts
+++ src/internal/do-not-use/chart-tooltip.ts
@@ -1,7 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import ChartTooltip_DO_NOT_USE, { ChartPopoverProps } from '../components/chart-popover/index.js';
-
+import InternalChartTooltip, { ChartPopoverProps } from '../components/chart-popover/index.js';
 export type InternalChartTooltipProps = ChartPopoverProps;
-export default ChartTooltip_DO_NOT_USE;
+export { InternalChartTooltip };
