4cd489d5436f1c0d435ec0969d41533481f04787
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-04-03 11:36:46 +0200

    restructured do-not-use exports and i18n export

--- src/internal/do-not-use/tooltip.ts
+++ src/internal/do-not-use/tooltip.ts
@@ -1,7 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import InternalTooltip_DO_NOT_USE, { TooltipProps } from '../components/tooltip/index.js';
-
+import InternalTooltip, { TooltipProps } from '../components/tooltip/index.js';
 export type InternalTooltipProps = TooltipProps;
-export default InternalTooltip_DO_NOT_USE;
+export { InternalTooltip };
