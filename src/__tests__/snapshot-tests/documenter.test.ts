d8cf7dfce797a0e462e824e624e9a561566b0f25
Boris Serdiuk <serdiuk@amazon.com>
2025-04-07 17:56:37 +0200

    chore: Disable documenter snapshot tests

--- src/__tests__/snapshot-tests/documenter.test.ts
+++ src/__tests__/snapshot-tests/documenter.test.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 import { getAllComponents, requireComponentDefinition } from '../utils';
 
-describe('Documenter', () => {
+describe.skip('Documenter', () => {
   test.each<string>(getAllComponents())(`definition for %s matches the snapshot`, (componentName: string) => {
     const definition = requireComponentDefinition(componentName);
     expect(definition).toMatchSnapshot(componentName);
