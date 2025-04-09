3684f3a2cc2d3504acf786a8b1bf9161fd5e92c6
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-04-09 19:03:55 +0200

    chart filter mini

---
+++ src/internal/do-not-use/chart-filter.ts
@@ -0,0 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import InternalChartFilter, { ChartFilterProps } from '../components/chart-filter-mini/index.js';
+export type InternalChartFilterProps<T> = ChartFilterProps<T>;
+export { InternalChartFilter };
