5c309c40311cea95687d057d5b5f8c10d2a75822
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-04-15 12:17:27 +0200

    add sortable area

---
+++ src/internal/do-not-use/sortable-area.ts
@@ -0,0 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import InternalSortableArea from '../../internal/components/sortable-area';
+export type { SortableAreaProps as InternalSortableAreaProps } from '../../internal/components/sortable-area';
+export { InternalSortableArea };
