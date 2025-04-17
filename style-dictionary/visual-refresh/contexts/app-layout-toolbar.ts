197287f4136e7b2309bf51865d1d108f3ef8580c
at-susie <ienakaai@amazon.de>
2025-04-17 15:22:07 +0200

    remove parentTokens from toolbar visual context definition and adjust min height of prompt-input

--- style-dictionary/visual-refresh/contexts/app-layout-toolbar.ts
+++ style-dictionary/visual-refresh/contexts/app-layout-toolbar.ts
@@ -4,14 +4,11 @@ import merge from 'lodash/merge.js';
 
 import { expandColorDictionary } from '../../utils/index.js';
 import { StyleDictionary } from '../../utils/interfaces.js';
-import { tokens as parentTokens } from '../colors.js';
 
 const tokens = {
   colorBackgroundLayoutMain: { light: '{colorGrey50}', dark: '{colorGrey850}' },
 };
 
-const expandedTokens: StyleDictionary.ExpandedColorScopeDictionary = expandColorDictionary(
-  merge({}, parentTokens, { ...tokens })
-);
+const expandedTokens: StyleDictionary.ExpandedColorScopeDictionary = expandColorDictionary(merge({}, { ...tokens }));
 
 export { expandedTokens as tokens };
