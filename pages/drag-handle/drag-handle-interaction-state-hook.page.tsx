3835a20302345f62444750140b57e577ef1351a9
Johannes Weber <jowejowe@amazon.com>
2025-05-13 14:09:08 +0200

    chore: Fix a11y on test page

--- pages/drag-handle/drag-handle-interaction-state-hook.page.tsx
+++ pages/drag-handle/drag-handle-interaction-state-hook.page.tsx
@@ -1,15 +1,31 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
+import clsx from 'clsx';
 
-import InternalDragHandle from '~components/internal/components/drag-handle';
+import Box from '~components/box';
+import Checkbox from '~components/checkbox';
+import Container from '~components/container';
+import Header from '~components/header';
 import useDragHandleInteractionState, {
   UseDragHandleInteractionStateProps,
 } from '~components/internal/components/drag-handle/hooks/use-drag-handle-interaction-state';
+import SpaceBetween from '~components/space-between';
+
+import AppContext, { AppContextType } from '../app/app-context';
+
+import styles from './styles.scss';
+
+type PageContext = React.Context<
+  AppContextType<{
+    renderInPortal: boolean;
+  }>
+>;
 
 const TestBoardItemButton: React.FC = () => {
-  const [shouldRenderInPortal, setShouldRenderInPortal] = useState(true);
+  const { urlParams, setUrlParams } = useContext(AppContext as PageContext);
+  const [renderInPortal, setRenderInPortal] = useState(false);
 
   interface TestMetadata {
     operation: 'drag' | 'resize';
@@ -36,84 +52,79 @@ const TestBoardItemButton: React.FC = () => {
   const hook = useDragHandleInteractionState<TestMetadata>(hookProps);
   const buttonRef = useRef<HTMLButtonElement>(null);
 
-  useEffect(() => {
-    const currentButton = buttonRef.current;
+  const handleGlobalPointerMove = (event: PointerEvent) => {
+    hook.processPointerMove(event);
+  };
 
-    const handleGlobalPointerMove = (event: PointerEvent) => {
-      hook.processPointerMove(event);
-    };
+  const handleGlobalPointerUp = (event: PointerEvent) => {
+    hook.processPointerUp(event);
+    // Clean up global listeners after interaction ends
+    window.removeEventListener('pointermove', handleGlobalPointerMove);
+    window.removeEventListener('pointerup', handleGlobalPointerUp);
+  };
 
-    const handleGlobalPointerUp = (event: PointerEvent) => {
-      hook.processPointerUp(event);
-      // Clean up global listeners after interaction ends
-      window.removeEventListener('pointermove', handleGlobalPointerMove);
-      window.removeEventListener('pointerup', handleGlobalPointerUp);
-    };
+  const handleOnPointerDown = (event: PointerEvent) => {
+    hook.processPointerDown(event, { operation: 'drag' });
+    if (urlParams.renderInPortal) {
+      setRenderInPortal(true);
+    }
 
-    const handleWindowPointerDown = (event: PointerEvent) => {
-      // Is the event originated from our button?
-      if (currentButton && currentButton.contains(event.target as Node)) {
-        hook.processPointerDown(event, { operation: 'drag' }); // event is already native PointerEvent from window listener
+    window.addEventListener('pointermove', handleGlobalPointerMove);
+    window.addEventListener('pointerup', handleGlobalPointerUp);
+  };
 
-        // Start listening for global move and up
-        window.addEventListener('pointermove', handleGlobalPointerMove);
-        window.addEventListener('pointerup', handleGlobalPointerUp);
-      }
-    };
-    window.addEventListener('pointerdown', handleWindowPointerDown);
-
-    return () => {
-      window.removeEventListener('pointerdown', handleWindowPointerDown);
-      window.removeEventListener('pointermove', handleGlobalPointerMove);
-      window.removeEventListener('pointerup', handleGlobalPointerUp);
-    };
-    // Depend on the specific (stable) hook properties, as the hook itself changes on every state change.
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [hook.processPointerDown, shouldRenderInPortal]);
-
+  // Using a native button for testing because the system's button does not expose all native properties
   const buttonComp = (
-    <>
-      <InternalDragHandle />
-      <button
-        ref={buttonRef}
-        onKeyDown={e => hook.processKeyDown(e, { operation: 'resize' })}
-        onFocus={hook.processFocus}
-        onBlur={hook.processBlur}
-        style={{
-          padding: '20px',
-          border: '2px solid blue',
-          touchAction: 'none',
-        }}
-        tabIndex={0}
-      >
-        Interact with Me (Global Pointer Listeners)
-      </button>
-    </>
+    <button
+      ref={buttonRef}
+      onPointerDown={e => handleOnPointerDown(e.nativeEvent)}
+      onKeyDown={e => hook.processKeyDown(e, { operation: 'resize' })}
+      onFocus={hook.processFocus}
+      onBlur={hook.processBlur}
+      className={clsx(styles['test-button'])}
+      tabIndex={0}
+    >
+      Test button to interact with
+    </button>
   );
 
   return (
-    <div>
-      <button onClick={() => setShouldRenderInPortal(prevState => !prevState)}>
-        RenderInPortal: {shouldRenderInPortal.toString()}
-      </button>
-      <hr />
-      <div style={{ marginTop: '20px', fontFamily: 'monospace' }}>
-        <p>Current State: {String(hook.interaction.state) || 'null'}</p>
-        <p>Metadata: {JSON.stringify(hook.interaction.metadata)}</p>
-        <p>
-          DnD Event Coords (last):{' '}
-          {hook.interaction.eventData ? (
-            <>
-              ({hook.interaction.eventData?.clientX}, {hook.interaction.eventData?.clientY})
-            </>
-          ) : (
-            '-'
-          )}
-        </p>
-      </div>
-      <hr />
-      {shouldRenderInPortal ? <div>{createPortal(buttonComp, document.body)}</div> : buttonComp}
-    </div>
+    <>
+      <Header
+        variant="h1"
+        description="Page to test the board-item behaviour which is rendered in a portal on onPointerDown when adding from the pallete to the board"
+      >
+        Drag handle hook
+      </Header>
+      <SpaceBetween size="m">
+        <Checkbox
+          checked={urlParams.renderInPortal}
+          onChange={event => {
+            setUrlParams({ renderInPortal: event.detail.checked });
+          }}
+        >
+          Render Test Button in a portal on click
+        </Checkbox>
+
+        <Container>
+          <Box variant="pre">
+            <p>Current State: {String(hook.interaction.state) || 'null'}</p>
+            <p>Metadata: {JSON.stringify(hook.interaction.metadata)}</p>
+            <p>
+              DnD Event Coords:{' '}
+              {hook.interaction.eventData ? (
+                <>
+                  ({hook.interaction.eventData?.clientX}, {hook.interaction.eventData?.clientY})
+                </>
+              ) : (
+                '-'
+              )}
+            </p>
+          </Box>
+        </Container>
+        {renderInPortal ? <div>{createPortal(buttonComp, document.body)}</div> : buttonComp}
+      </SpaceBetween>
+    </>
   );
 };
 
