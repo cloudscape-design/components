9ffc2e8aaf61f9e5c782d33c90439f323fec881f
Dennis Pitcock <denpitco@amazon.de>
2025-05-14 11:01:14 +0200

    chore: enhancing and moving Date Input masking to a seperate &  tested util file

--- src/date-input/internal.tsx
+++ src/date-input/internal.tsx
@@ -3,15 +3,14 @@
 
 import React, { Ref } from 'react';
 import clsx from 'clsx';
-import { getDaysInMonth } from 'date-fns';
 
 import { CalendarProps } from '../calendar/interfaces';
 import MaskedInput from '../internal/components/masked-input';
-import { MaskArgs } from '../internal/components/masked-input/utils/mask-format';
 import { fireNonCancelableEvent } from '../internal/events';
 import { InternalBaseComponentProps } from '../internal/hooks/use-base-component';
-import { displayToIso, isoToDisplay, parseDate } from '../internal/utils/date-time';
+import { displayToIso, isoToDisplay } from '../internal/utils/date-time';
 import { DateInputProps } from './interfaces';
+import { generateMaskArgs } from './utils';
 
 import styles from './styles.css.js';
 
@@ -20,27 +19,11 @@ type InternalDateInputProps = DateInputProps &
     granularity?: CalendarProps.Granularity;
   };
 
-function daysMax(value: string): number {
-  // force to first day in month, as new Date('2018-02-30') -> March 2nd 2018
-  const baseDate = displayToIso(value).substring(0, 7);
-  return getDaysInMonth(parseDate(baseDate));
-}
-
-const yearMask = { min: 0, max: 9999, default: 2000, length: 4 };
-const monthMask = { min: 1, max: 12, length: 2 };
-const dayMask = { min: 1, max: daysMax, length: 2 };
-
 const InternalDateInput = React.forwardRef(
   (
     { value, onChange, granularity, __internalRootRef = null, ...props }: InternalDateInputProps,
     ref: Ref<HTMLInputElement>
   ) => {
-    const maskArgs: MaskArgs = {
-      separator: '/',
-      inputSeparators: ['-', '.', ' '],
-      segments: granularity === 'month' ? [yearMask, monthMask] : [yearMask, monthMask, dayMask],
-    };
-
     return (
       <MaskedInput
         ref={ref}
@@ -48,7 +31,7 @@ const InternalDateInput = React.forwardRef(
         value={isoToDisplay(value || '')}
         onChange={event => fireNonCancelableEvent(onChange, { value: displayToIso(event.detail.value) })}
         className={clsx(styles.root, props.className)}
-        mask={maskArgs}
+        mask={generateMaskArgs({ granularity })}
         autofix={true}
         autoComplete={false}
         disableAutocompleteOnBlur={false}
