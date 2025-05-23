50922ce3ad5fbf9b43629416fa13a847e8025778
Michael <michaeldowseza@users.noreply.github.com>
2025-05-23 12:06:49 +0200

    chore: Change webpack config extension to .cjs (#3525)

--- pages/webpack.config.integ.cjs
+++ pages/webpack.config.integ.cjs
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-const baseConfig = require('./webpack.config');
 const merge = require('lodash/merge');
+const baseConfig = require('./webpack.config.cjs');
 
 module.exports = (...args) => {
   const webConfig = baseConfig(...args);
