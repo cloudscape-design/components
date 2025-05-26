0296f43432b19af7e50f73487385020f2f95b837
at-susie <ienakaai@amazon.de>
2025-05-26 18:10:43 +0200

    chore: Fix issues for first time open

--- src/app-layout/visual-refresh-toolbar/drawer/global-drawer.tsx
+++ src/app-layout/visual-refresh-toolbar/drawer/global-drawer.tsx
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import clsx from 'clsx';
 
 import { InternalButton } from '../../../button/internal';
@@ -70,119 +70,120 @@ function AppLayoutGlobalDrawerImplementation({
   const hasTriggerButton = !!activeGlobalDrawer?.trigger;
   const isExpanded = activeGlobalDrawer?.isExpandable && expandedDrawerId === activeDrawerId;
   const wasExpanded = usePrevious(isExpanded);
-  const animationDisabled =
-    (activeGlobalDrawer?.defaultActive && !drawersOpenQueue.includes(activeGlobalDrawer.id)) ||
-    (wasExpanded && !isExpanded);
+  const animationDisabled = wasExpanded && !isExpanded;
+  const [hasOpened, setHasOpened] = React.useState(false);
+
+  useEffect(() => {
+    if (show) {
+      setHasOpened(true);
+    }
+  }, [show]);
 
   return (
-    <Transition in={show} exit={!show}>
-      {(state, transitionRef) => {
-        console.log('Transition state:', state);
-        return (
-          <aside
-            id={activeDrawerId}
-            aria-hidden={!show}
-            aria-label={computedAriaLabels.content}
-            className={clsx(
-              styles.drawer,
-              styles['drawer-global'],
-              styles[state],
-              !animationDisabled && sharedStyles['with-motion-horizontal'],
-              !animationDisabled && isExpanded && styles['with-expanded-motion'],
-              {
-                [styles['drawer-hidden']]: !show && state === 'exited',
-                [styles['last-opened']]: lastOpenedDrawerId === activeDrawerId || isExpanded,
-                [testutilStyles['active-drawer']]: show,
-                [styles['drawer-expanded']]: isExpanded,
-                [styles['has-next-siblings']]:
-                  activeGlobalDrawers.findIndex(drawer => drawer.id === activeDrawerId) + 1 <
-                  activeGlobalDrawers.length,
-              }
+    <Transition in={hasOpened && show} exit={!show}>
+      {(state, transitionRef) => (
+        <aside
+          id={activeDrawerId}
+          aria-hidden={!show}
+          aria-label={computedAriaLabels.content}
+          className={clsx(
+            styles.drawer,
+            styles['drawer-global'],
+            styles[state],
+            !animationDisabled && sharedStyles['with-motion-horizontal'],
+            !animationDisabled && isExpanded && styles['with-expanded-motion'],
+            {
+              [styles['drawer-hidden']]: !show && state === 'exited',
+              [styles['last-opened']]: lastOpenedDrawerId === activeDrawerId || isExpanded,
+              [testutilStyles['active-drawer']]: show,
+              [styles['drawer-expanded']]: isExpanded,
+              [styles['has-next-siblings']]:
+                activeGlobalDrawers.findIndex(drawer => drawer.id === activeDrawerId) + 1 < activeGlobalDrawers.length,
+            }
+          )}
+          ref={transitionRef}
+          onBlur={e => {
+            // Drawers with trigger buttons follow this restore focus logic:
+            // If a previously focused element exists, restore focus on it; otherwise, focus on the associated trigger button.
+            // This function resets the previously focused element.
+            // If the drawer has no trigger button and loses focus on the previously focused element, it defaults to document.body,
+            // which ideally should never happen.
+            if (!hasTriggerButton) {
+              return;
+            }
+
+            if (!e.relatedTarget || !e.currentTarget.contains(e.relatedTarget)) {
+              globalDrawersFocusControl.loseFocus();
+            }
+          }}
+          style={{
+            blockSize: drawerHeight,
+            insetBlockStart: drawerTopOffset,
+            ...(!isMobile && {
+              [customCssProps.drawerSize]: `${['entering', 'entered'].includes(state) ? (isExpanded ? '100%' : size + 'px') : 0}`,
+            }),
+          }}
+          data-testid={`awsui-app-layout-drawer-${activeDrawerId}`}
+        >
+          <div ref={drawerRef} className={clsx(styles['global-drawer-wrapper'])}>
+            {!isMobile && <div className={styles['drawer-gap']}></div>}
+            {!isMobile && activeGlobalDrawer?.resizable && !isExpanded && (
+              <div className={styles['drawer-slider']}>
+                <PanelResizeHandle
+                  ref={refs?.slider}
+                  position="side"
+                  className={testutilStyles['drawers-slider']}
+                  ariaLabel={activeGlobalDrawer?.ariaLabels?.resizeHandle}
+                  tooltipText={activeGlobalDrawer?.ariaLabels?.resizeHandleTooltipText}
+                  ariaValuenow={resizeProps.relativeSize}
+                  onKeyDown={resizeProps.onKeyDown}
+                  onDirectionClick={resizeProps.onDirectionClick}
+                  onPointerDown={resizeProps.onPointerDown}
+                />
+              </div>
             )}
-            ref={transitionRef}
-            onBlur={e => {
-              // Drawers with trigger buttons follow this restore focus logic:
-              // If a previously focused element exists, restore focus on it; otherwise, focus on the associated trigger button.
-              // This function resets the previously focused element.
-              // If the drawer has no trigger button and loses focus on the previously focused element, it defaults to document.body,
-              // which ideally should never happen.
-              if (!hasTriggerButton) {
-                return;
-              }
 
-              if (!e.relatedTarget || !e.currentTarget.contains(e.relatedTarget)) {
-                globalDrawersFocusControl.loseFocus();
-              }
-            }}
-            style={{
-              blockSize: drawerHeight,
-              insetBlockStart: drawerTopOffset,
-              ...(!isMobile && {
-                [customCssProps.drawerSize]: `${['entering', 'entered'].includes(state) ? (isExpanded ? '100%' : size + 'px') : 0}`,
-              }),
-            }}
-            data-testid={`awsui-app-layout-drawer-${activeDrawerId}`}
-          >
-            <div ref={drawerRef} className={clsx(styles['global-drawer-wrapper'])}>
-              {!isMobile && <div className={styles['drawer-gap']}></div>}
-              {!isMobile && activeGlobalDrawer?.resizable && !isExpanded && (
-                <div className={styles['drawer-slider']}>
-                  <PanelResizeHandle
-                    ref={refs?.slider}
-                    position="side"
-                    className={testutilStyles['drawers-slider']}
-                    ariaLabel={activeGlobalDrawer?.ariaLabels?.resizeHandle}
-                    tooltipText={activeGlobalDrawer?.ariaLabels?.resizeHandleTooltipText}
-                    ariaValuenow={resizeProps.relativeSize}
-                    onKeyDown={resizeProps.onKeyDown}
-                    onDirectionClick={resizeProps.onDirectionClick}
-                    onPointerDown={resizeProps.onPointerDown}
-                  />
-                </div>
-              )}
-
-              <div
-                className={clsx(styles['drawer-content-container'], sharedStyles['with-motion-horizontal'])}
-                data-testid={`awsui-app-layout-drawer-content-${activeDrawerId}`}
-              >
-                <div className={styles['drawer-actions']}>
-                  {!isMobile && activeGlobalDrawer?.isExpandable && (
-                    <div className={styles['drawer-expanded-mode-button']}>
-                      <InternalButton
-                        ariaLabel={activeGlobalDrawer?.ariaLabels?.expandedModeButton}
-                        className={testutilStyles['active-drawer-expanded-mode-button']}
-                        formAction="none"
-                        ariaExpanded={isExpanded}
-                        iconName={isExpanded ? 'shrink' : 'expand'}
-                        onClick={() => setExpandedDrawerId(isExpanded ? null : activeDrawerId)}
-                        variant="icon"
-                        analyticsAction={isExpanded ? 'expand' : 'collapse'}
-                      />
-                    </div>
-                  )}
-                  <div className={clsx(styles['drawer-close-button'])}>
+            <div
+              className={clsx(styles['drawer-content-container'], sharedStyles['with-motion-horizontal'])}
+              data-testid={`awsui-app-layout-drawer-content-${activeDrawerId}`}
+            >
+              <div className={styles['drawer-actions']}>
+                {!isMobile && activeGlobalDrawer?.isExpandable && (
+                  <div className={styles['drawer-expanded-mode-button']}>
                     <InternalButton
-                      ariaLabel={computedAriaLabels.closeButton}
-                      className={clsx({
-                        [testutilStyles['active-drawer-close-button']]: activeDrawerId,
-                      })}
+                      ariaLabel={activeGlobalDrawer?.ariaLabels?.expandedModeButton}
+                      className={testutilStyles['active-drawer-expanded-mode-button']}
                       formAction="none"
-                      iconName={isMobile ? 'close' : 'angle-right'}
-                      onClick={() => onActiveGlobalDrawersChange(activeDrawerId, { initiatedByUserAction: true })}
-                      ref={refs?.close}
+                      ariaExpanded={isExpanded}
+                      iconName={isExpanded ? 'shrink' : 'expand'}
+                      onClick={() => setExpandedDrawerId(isExpanded ? null : activeDrawerId)}
                       variant="icon"
-                      analyticsAction="close"
+                      analyticsAction={isExpanded ? 'expand' : 'collapse'}
                     />
                   </div>
-                </div>
-                <div className={styles['drawer-content']} style={{ blockSize: drawerHeight }}>
-                  {activeGlobalDrawer?.content}
+                )}
+                <div className={clsx(styles['drawer-close-button'])}>
+                  <InternalButton
+                    ariaLabel={computedAriaLabels.closeButton}
+                    className={clsx({
+                      [testutilStyles['active-drawer-close-button']]: activeDrawerId,
+                    })}
+                    formAction="none"
+                    iconName={isMobile ? 'close' : 'angle-right'}
+                    onClick={() => onActiveGlobalDrawersChange(activeDrawerId, { initiatedByUserAction: true })}
+                    ref={refs?.close}
+                    variant="icon"
+                    analyticsAction="close"
+                  />
                 </div>
               </div>
+              <div className={styles['drawer-content']} style={{ blockSize: drawerHeight }}>
+                {activeGlobalDrawer?.content}
+              </div>
             </div>
-          </aside>
-        );
-      }}
+          </div>
+        </aside>
+      )}
     </Transition>
   );
 }
