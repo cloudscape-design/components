af0da129d04f337e0b98ce25e52bae38e4eaeb16
Francesco Longo <flongo@amazon.de>
2025-06-06 16:39:16 +0200

    feat: Add analytics metadata to FileInput component

---
+++ src/file-input/analytics-metadata/interfaces.ts
@@ -0,0 +1,14 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+export interface GeneratedAnalyticsMetadataFileInputClick {
+  action: string;
+  detail: {
+    label: string;
+  };
+}
+
+export interface GeneratedAnalyticsMetadataFileInputComponent {
+  name: 'awsui.FileInput';
+  label: string;
+}
