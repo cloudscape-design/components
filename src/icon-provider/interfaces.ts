b4dd005e8f2db975e0187a6c956dbfb971bd1a7b
Ernst Kaese <ernstka@amazon.de>
2025-06-18 14:58:14 +0200

    General clean-up

--- src/icon-provider/interfaces.ts
+++ src/icon-provider/interfaces.ts
@@ -12,17 +12,23 @@ export interface IconProviderProps extends BaseComponentProps {
   /**
    * Specifies icon overrides using existing icon names, e.g. `{'add-plus': <svg>...</svg>}`.
    *
-   * Context is shared with child instances of IconProvider.
+   * These icon overrides will automatically be applied to any component that is a descendant of this provider, including nested providers.
    *
    * Set to `null` to reset the icons to the default set.
    *
-   * e.g. Override `AppLayout` icons but not content icons by wrapping content with an `IconProvider` with this property set to `null`.
+   * For example, override `AppLayout` icons but not icons in the content slot by wrapping content with an `IconProvider` with this property set to `null`.
+   *
+   * `<Icon ... />` component can be used as an override (e.g. `{'close': <Icon name='arrow-left' />}`).
+   * However, if the icon name is the same as the key, e.g. `{'close': <Icon name='close' />}` an infinite loop will be created.
+   * The same applies to switching icons in the same configuration (e.g. `{'close': <Icon name='arrow-left' />, 'arrow-left': <Icon name='close' />}`).
    */
   icons: IconProviderProps.Icons | null;
 }
 
 export namespace IconProviderProps {
-  export type Icons = Partial<{
-    [name in keyof typeof generatedIcons]: JSX.Element;
-  }>;
+  export type Icons = {
+    // Generates an object shape using the current set of available icons.
+    // For example: {'add-plus': JSX.Element, 'anchor-link': ...}
+    [name in keyof typeof generatedIcons]?: JSX.Element;
+  };
 }
