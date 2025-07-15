f741f94702e8b633c40fab176e397291d2299d0e
Ernst Kaese <ernstka@amazon.de>
2025-07-15 17:20:02 +0200

    Initial Token split-out

---
+++ src/token/index.tsx
@@ -0,0 +1,31 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React from 'react';
+
+// import { getAnalyticsMetadataAttribute } from '@cloudscape-design/component-toolkit/internal/analytics-metadata';
+import useBaseComponent from '../internal/hooks/use-base-component';
+import { applyDisplayName } from '../internal/utils/apply-display-name';
+// import { GeneratedAnalyticsMetadataTokenGroupComponent } from './analytics-metadata/interfaces';
+import { TokenProps } from './interfaces';
+import InternalToken from './internal';
+
+export { TokenProps };
+
+export default function Token(props: TokenProps) {
+  const baseComponentProps = useBaseComponent('TokenGroup');
+
+  // const componentAnalyticsMetadata: GeneratedAnalyticsMetadataTokenGroupComponent = {
+  //   name: 'awsui.Token',
+  //   label: 'invalid',
+  // };
+
+  return (
+    <InternalToken
+      {...props}
+      {...baseComponentProps}
+      // {...getAnalyticsMetadataAttribute({ component: componentAnalyticsMetadata })}
+    />
+  );
+}
+
+applyDisplayName(Token, 'Token');
