ffd58e24f9b063eb12515964407831b0e32496bd
Boris Serdiuk <serdiuk@amazon.com>
2025-07-15 14:37:54 +0200

    chore: apply use-client directives (#3641)

--- src/tutorial-panel/index.tsx
+++ src/tutorial-panel/index.tsx
@@ -1,5 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+'use client';
 import React, { useContext } from 'react';
 import clsx from 'clsx';
 
