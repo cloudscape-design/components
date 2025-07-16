22e3f51987f1ec6a75605123d477dc58906b2916
Mario Savio <marijsav@amazon.de>
2025-07-16 15:50:20 +0200

    Icons for expandable buttons push to my dev branch

--- src/button-dropdown/category-elements/mobile-expandable-category-element.tsx
+++ src/button-dropdown/category-elements/mobile-expandable-category-element.tsx
@@ -83,6 +83,7 @@ const MobileExpandableCategoryElement = ({
             } as GeneratedAnalyticsMetadataButtonDropdownExpand)
       )}
       //Add Rendering of icon image in expandable button
+      //
     >
       {(item.iconName || item.iconUrl || item.iconSvg) && (
         <InternalIcon name={item.iconName} url={item.iconUrl} svg={item.iconSvg} alt={item.iconAlt} />
