54a37e75761b022b3a074694343d8712e1b6bb8b
Amir Alami <alamiami@amazon.de>
2025-07-17 11:19:24 +0200

    feat: Adds disabled state to file input

---
+++ pages/file-input/permutations.page.tsx
@@ -0,0 +1,38 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React from 'react';
+
+import FileInput, { FileInputProps } from '~components/file-input';
+
+import createPermutations from '../utils/permutations';
+import PermutationsView from '../utils/permutations-view';
+import ScreenshotArea from '../utils/screenshot-area';
+
+const permutations = createPermutations<FileInputProps>([
+  {
+    value: [[]],
+    onChange: [() => {}],
+    variant: ['icon', 'button'],
+    children: ['Upload file'],
+    disabled: [false, true],
+  },
+  {
+    value: [[]],
+    onChange: [() => {}],
+    variant: ['icon', 'button'],
+    children: ['Upload file'],
+    disabled: [true],
+    disabledReason: ["You don't have access to upload files."],
+  },
+]);
+
+export default function ExpandableSectionPermutations() {
+  return (
+    <>
+      <h1>File input permutations</h1>
+      <ScreenshotArea disableAnimations={true}>
+        <PermutationsView permutations={permutations} render={permutation => <FileInput {...permutation} />} />
+      </ScreenshotArea>
+    </>
+  );
+}
