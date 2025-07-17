7505c08d6f9ab8dedcfbbc407bd9bd3c3614fbb3
Gethin Webster <gethinw@amazon.de>
2025-07-17 14:24:59 +0200

    feat: Allow button native element attributes to be set (#3552)

--- src/button/__tests__/internal.test.tsx
+++ src/button/__tests__/internal.test.tsx
@@ -12,22 +12,8 @@ import { mockedFunnelInteractionId, mockFunnelMetrics } from '../../internal/ana
 
 import styles from '../../../lib/components/button/styles.css.js';
 
-test('specific properties take precedence over nativeAttributes', () => {
-  const { container } = render(
-    <InternalButton ariaLabel="property" __nativeAttributes={{ 'aria-label': 'native attribute' }} />
-  );
-  expect(container.querySelector('button')).toHaveAttribute('aria-label', 'property');
-});
-
-test('supports providing custom attributes', () => {
-  const { container } = render(<InternalButton __nativeAttributes={{ 'aria-hidden': 'true' }} />);
-  expect(container.querySelector('button')).toHaveAttribute('aria-hidden', 'true');
-});
-
 test('supports __iconClass property', () => {
-  const { container } = render(
-    <InternalButton __iconClass="example-class" iconName="settings" __nativeAttributes={{ 'aria-expanded': 'true' }} />
-  );
+  const { container } = render(<InternalButton __iconClass="example-class" iconName="settings" />);
   expect(container.querySelector(`button .${styles.icon}`)).toHaveClass('example-class');
 });
 
