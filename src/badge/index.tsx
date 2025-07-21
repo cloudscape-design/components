5661333fc276e3176c5c1fc7353f22c334cbe07d
Nemes-Teodora <69592886+teodoranemes@users.noreply.github.com>
2025-07-21 17:53:29 +0200

    feat: Implementation of the Badge component Style API (#3650)

--- src/badge/index.tsx
+++ src/badge/index.tsx
@@ -8,19 +8,20 @@ import { getBaseProps } from '../internal/base-component';
 import useBaseComponent from '../internal/hooks/use-base-component';
 import { applyDisplayName } from '../internal/utils/apply-display-name';
 import { BadgeProps } from './interfaces';
+import { getBadgeStyles } from './style';
 
 import styles from './styles.css.js';
 
 export { BadgeProps };
 
-export default function Badge({ color = 'grey', children, ...rest }: BadgeProps) {
+export default function Badge({ color = 'grey', children, style, ...rest }: BadgeProps) {
   const { __internalRootRef } = useBaseComponent('Badge', { props: { color } });
   const baseProps = getBaseProps(rest);
 
   const className = clsx(baseProps.className, styles.badge, styles[`badge-color-${color}`]);
 
   return (
-    <span {...baseProps} {...{ className }} ref={__internalRootRef}>
+    <span {...baseProps} {...{ className }} ref={__internalRootRef} style={getBadgeStyles(style)}>
       {children}
     </span>
   );
