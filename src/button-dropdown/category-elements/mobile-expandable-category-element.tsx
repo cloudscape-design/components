411fc9ced24e8fa1f4300174cd456bd21c7cb6aa
Mario Savio <marijsav@amazon.de>
2025-07-25 13:10:51 +0200

    Reviewer reequets applied

--- src/button-dropdown/category-elements/mobile-expandable-category-element.tsx
+++ src/button-dropdown/category-elements/mobile-expandable-category-element.tsx
@@ -82,7 +82,6 @@ const MobileExpandableCategoryElement = ({
               },
             } as GeneratedAnalyticsMetadataButtonDropdownExpand)
       )}
-      //Add Rendering of icon image in expandable button
     >
       {(item.iconName || item.iconUrl || item.iconSvg) && (
         <InternalIcon name={item.iconName} url={item.iconUrl} svg={item.iconSvg} alt={item.iconAlt} />
