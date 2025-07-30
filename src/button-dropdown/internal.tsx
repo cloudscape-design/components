cafadff4da7c7cb9ae24f120833cd0e1167f68cd
Aleksandra Danilina <akdanili@amazon.com>
2025-07-30 10:35:03 +0200

    fix: Align styles between disabled input and select form controls

--- src/button-dropdown/internal.tsx
+++ src/button-dropdown/internal.tsx
@@ -258,7 +258,6 @@ const InternalButtonDropdown = React.forwardRef(
               styles['trigger-item'],
               styles['split-trigger'],
               styles[`variant-${variant}`],
-              mainActionProps.disabled && styles.disabled,
               mainActionProps.loading && styles.loading
             )}
             // Close dropdown upon main action click unless event is cancelled.
@@ -282,7 +281,6 @@ const InternalButtonDropdown = React.forwardRef(
                 styles['dropdown-trigger'],
                 isVisualRefresh && styles['visual-refresh'],
                 styles[`variant-${variant}`],
-                baseTriggerProps.disabled && styles.disabled,
                 baseTriggerProps.loading && styles.loading
               )}
               {...getAnalyticsMetadataAttribute(analyticsMetadata)}
