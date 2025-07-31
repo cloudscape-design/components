69c9ab9775dff055e375aad9de3beb904f100ce9
Scott O'Brien <DaemonCahill@users.noreply.github.com>
2025-07-31 14:09:39 -0400

    feat: Implementation of the Container component Style API  (#3695)

---
+++ pages/container/style-custom.page.tsx
@@ -0,0 +1,217 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React, { useRef } from 'react';
+
+import { useCurrentMode } from '@cloudscape-design/component-toolkit/internal';
+
+import { Box, Container, SpaceBetween } from '~components';
+
+import ScreenshotArea from '../utils/screenshot-area';
+import image43 from './images/4-3.png';
+
+export default function CustomContainer() {
+  const mode = useCurrentMode(useRef(document.body));
+  const background = mode === 'light' ? '#fcfcfc' : '#000';
+
+  return (
+    <ScreenshotArea>
+      <h1>Custom Container</h1>
+
+      <SpaceBetween size="m" direction="horizontal">
+        <Container
+          style={{
+            root: {
+              background,
+              borderColor: 'green',
+              borderRadius: '8px',
+              borderWidth: '4px',
+              boxShadow: '0px 5px 5px red',
+            },
+            content: {
+              paddingBlock: '0px',
+              paddingInline: '0px',
+            },
+          }}
+        >
+          Container content
+        </Container>
+
+        <Container
+          header="Container header"
+          style={{
+            root: {
+              background,
+              borderColor: 'magenta',
+              borderRadius: '0px',
+              borderWidth: '1px',
+              boxShadow: '0px 5px 5px green',
+            },
+            content: {
+              paddingBlock: '20px',
+              paddingInline: '40px',
+            },
+            header: {
+              paddingBlock: '20px',
+              paddingInline: '40px',
+            },
+          }}
+        >
+          Container content
+        </Container>
+
+        <Container
+          footer="Container footer"
+          style={{
+            root: {
+              background,
+              borderColor: '#000',
+              borderRadius: '20px',
+              borderWidth: '3px',
+              boxShadow: '0px 5px 5px blue',
+            },
+            content: {
+              paddingBlock: '40px',
+              paddingInline: '40px',
+            },
+            footer: {
+              root: {
+                paddingBlock: '40px',
+                paddingInline: '40px',
+              },
+              divider: {
+                borderColor: '#000',
+                borderWidth: '3px',
+              },
+            },
+          }}
+        >
+          Container content
+        </Container>
+
+        <Container
+          header="Container header"
+          footer="Container footer"
+          style={{
+            root: {
+              background,
+              borderColor: 'blue',
+              borderRadius: '40px',
+              borderWidth: '4px',
+              boxShadow: '5px 5px 5px pink',
+            },
+            header: {
+              paddingBlock: '60px',
+              paddingInline: '10px',
+            },
+            footer: {
+              root: {
+                paddingBlock: '60px',
+                paddingInline: '10px',
+              },
+              divider: {
+                borderWidth: '0px',
+              },
+            },
+          }}
+        />
+
+        <Container
+          header="Container header"
+          footer="Container footer"
+          style={{
+            root: {
+              background,
+              borderColor: 'purple',
+              borderRadius: '240px',
+              borderWidth: '6px',
+              boxShadow: '0px 5px 5px orange',
+            },
+            content: {
+              paddingBlock: '20px',
+              paddingInline: '140px',
+            },
+            header: {
+              paddingBlock: '20px 0px',
+              paddingInline: '140px',
+            },
+            footer: {
+              root: {
+                paddingBlock: '40px',
+                paddingInline: '140px',
+              },
+              divider: {
+                borderColor: 'purple',
+                borderWidth: '6px',
+              },
+            },
+          }}
+        >
+          Container content
+        </Container>
+        <Box>
+          <Container
+            header="Header content"
+            variant="stacked"
+            style={{
+              root: {
+                borderColor: 'green',
+                borderWidth: '2px',
+              },
+            }}
+          >
+            Container content
+          </Container>
+
+          <Container
+            header="Header content"
+            variant="stacked"
+            style={{
+              root: {
+                borderColor: 'purple',
+                borderWidth: '10px',
+              },
+            }}
+          >
+            Container content
+          </Container>
+        </Box>
+
+        <Container
+          header="Header content"
+          media={{
+            content: <img src={image43} alt="placeholder" />,
+            height: 200,
+            position: 'top',
+          }}
+          style={{
+            root: {
+              borderColor: 'green',
+              borderWidth: '1px',
+              borderRadius: '40px',
+            },
+          }}
+        >
+          Container content
+        </Container>
+
+        <Container
+          header="Header content"
+          media={{
+            content: <img src={image43} alt="placeholder" />,
+            width: 200,
+            position: 'side',
+          }}
+          style={{
+            root: {
+              borderColor: 'green',
+              borderWidth: '1px',
+              borderRadius: '40px',
+            },
+          }}
+        >
+          Container content
+        </Container>
+      </SpaceBetween>
+    </ScreenshotArea>
+  );
+}
--- src/container/__tests__/container.test.tsx
+++ src/container/__tests__/container.test.tsx
@@ -6,6 +6,8 @@ import { render } from '@testing-library/react';
 import Container from '../../../lib/components/container';
 import createWrapper from '../../../lib/components/test-utils/dom';
 
+import styles from '../../../lib/components/container/styles.css.js';
+
 function renderContainer(jsx: React.ReactElement) {
   const { container } = render(jsx);
   return createWrapper(container).findContainer()!;
@@ -49,4 +51,108 @@ test('renders everything together', () => {
   expect(wrapper.findHeader()!.getElement()).toHaveTextContent('Test header');
   expect(wrapper.findFooter()!.getElement()).toHaveTextContent('Test footer');
   expect(wrapper.findContent().getElement()).toHaveTextContent('test content');
+});
+
+describe('Style API', () => {
+  test('all style properties', () => {
+    const wrapper = renderContainer(
+      <Container
+        header="Container header"
+        footer="Container footer"
+        style={{
+          root: {
+            background: 'rgb(240, 240, 235)',
+            borderColor: 'purple',
+            borderRadius: '240px',
+            borderWidth: '6px',
+          },
+          content: {
+            paddingBlock: '20px',
+            paddingInline: '140px',
+          },
+          header: {
+            paddingBlock: '20px 0px',
+            paddingInline: '140px',
+          },
+          footer: {
+            root: {
+              paddingBlock: '40px',
+              paddingInline: '140px',
+            },
+            divider: {
+              borderColor: 'purple',
+              borderWidth: '6px',
+            },
+          },
+        }}
+      >
+        Container content
+      </Container>
+    );
+
+    expect(getComputedStyle(wrapper.getElement()).getPropertyValue('background')).toBe('rgb(240, 240, 235)');
+    expect(getComputedStyle(wrapper.getElement()).getPropertyValue('border-color')).toBe('purple');
+    expect(getComputedStyle(wrapper.getElement()).getPropertyValue('border-radius')).toBe('240px');
+    expect(getComputedStyle(wrapper.getElement()).getPropertyValue('border-width')).toBe('6px');
+    expect(
+      getComputedStyle(wrapper.findByClassName(styles['content-inner'])!.getElement()).getPropertyValue('padding-block')
+    ).toBe('20px');
+    expect(
+      getComputedStyle(wrapper.findByClassName(styles['content-inner'])!.getElement()).getPropertyValue(
+        'padding-inline'
+      )
+    ).toBe('140px');
+    expect(getComputedStyle(wrapper.findHeader()!.getElement()).getPropertyValue('padding-block')).toBe('20px 0px');
+    expect(getComputedStyle(wrapper.findHeader()!.getElement()).getPropertyValue('padding-inline')).toBe('140px');
+    expect(getComputedStyle(wrapper.findFooter()!.getElement()).getPropertyValue('border-color')).toBe('purple');
+    expect(getComputedStyle(wrapper.findFooter()!.getElement()).getPropertyValue('border-width')).toBe('6px');
+    expect(getComputedStyle(wrapper.findFooter()!.getElement()).getPropertyValue('padding-block')).toBe('40px');
+    expect(getComputedStyle(wrapper.findFooter()!.getElement()).getPropertyValue('padding-inline')).toBe('140px');
+  });
+
+  test('media top', () => {
+    const url = '/test.png';
+
+    const wrapper = renderContainer(
+      <Container
+        header="Container header"
+        footer="Container footer"
+        media={{ content: <img src={url} />, position: 'top', height: '100px' }}
+        style={{
+          root: {
+            borderRadius: '20px',
+          },
+        }}
+      >
+        Container content
+      </Container>
+    );
+
+    expect(getComputedStyle(wrapper.findMedia()!.getElement()).getPropertyValue('border-radius')).toBe('20px');
+    expect(getComputedStyle(wrapper.findMedia()!.getElement()).getPropertyValue('border-end-start-radius')).toBe('0px');
+    expect(getComputedStyle(wrapper.findMedia()!.getElement()).getPropertyValue('border-end-end-radius')).toBe('0px');
+  });
+
+  test('media side', () => {
+    const url = '/test.png';
+
+    const wrapper = renderContainer(
+      <Container
+        header="Container header"
+        footer="Container footer"
+        media={{ content: <img src={url} />, position: 'side', width: '100px' }}
+        style={{
+          root: {
+            borderRadius: '20px',
+          },
+        }}
+      >
+        Container content
+      </Container>
+    );
+
+    expect(getComputedStyle(wrapper.findMedia()!.getElement()).getPropertyValue('border-radius')).toBe('20px');
+    expect(getComputedStyle(wrapper.findMedia()!.getElement()).getPropertyValue('border-start-end-radius')).toBe('0px');
+    expect(getComputedStyle(wrapper.findMedia()!.getElement()).getPropertyValue('border-end-end-radius')).toBe('0px');
+  });
 });
