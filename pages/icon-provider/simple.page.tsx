22c176018a12b46aa2aeb6a4f821e81d0bc83595
Ernst Kaese <ernstka@amazon.de>
2025-07-31 16:34:57 +0200

    Fix A11Y test failures

--- pages/icon-provider/simple.page.tsx
+++ pages/icon-provider/simple.page.tsx
@@ -5,6 +5,8 @@ import React, { useState } from 'react';
 import Alert from '~components/alert';
 import Button from '~components/button';
 import CollectionPreferences from '~components/collection-preferences';
+import I18nProvider from '~components/i18n';
+import messages from '~components/i18n/messages/all.en';
 import Icon from '~components/icon';
 import IconProvider from '~components/icon-provider/internal';
 import SpaceBetween from '~components/space-between';
@@ -37,98 +39,101 @@ const CUSTOM_SIMPLE_SVG = (
 export default function SimpleContainers() {
   const [filteringText, setFilteringText] = useState('Icon test');
   return (
-    <article>
-      <h1>Simple Icon Provider</h1>
-      <ScreenshotArea>
-        {/* Global icon override */}
-        <IconProvider
-          icons={{
-            close: CUSTOM_SIMPLE_SVG,
-          }}
-        >
-          <SpaceBetween size="m">
-            {/* Basic icon override */}
-            <IconProvider
-              icons={{
-                search: CUSTOM_SVG,
-              }}
-            >
-              <TextFilter
-                filteringText={filteringText}
-                onChange={({ detail }) => setFilteringText(detail.filteringText)}
-              />
-            </IconProvider>
-
-            {/* Multi-component icon override */}
-            <IconProvider
-              icons={{
-                'status-positive': CUSTOM_SVG,
-                'status-negative': CUSTOM_SVG,
-              }}
-            >
-              <SpaceBetween size="m">
-                <SpaceBetween size="s" direction="horizontal" alignItems="center">
-                  {/* Use close override from parent Provider */}
-                  <Button iconName="close">Icon test</Button>
-                  <StatusIndicator type="success">Icon test</StatusIndicator>
-                </SpaceBetween>
-                <Alert type="error" dismissible={true}>
-                  Icon test
-                </Alert>
-              </SpaceBetween>
-            </IconProvider>
-
-            {/* Icon reset */}
-            <IconProvider icons={{ 'add-plus': CUSTOM_SVG, close: CUSTOM_SIMPLE_SVG }}>
-              <SpaceBetween direction="vertical" size="l">
-                <Icon name={'add-plus'} />
-                <Icon name={'close'} />
-                {/* Restore icons in the below Provider back to the built-in set */}
-                <IconProvider icons={null}>
-                  <SpaceBetween direction="horizontal" size="s">
-                    <Icon name={'add-plus'} />
-                    <Icon name={'close'} />
-                  </SpaceBetween>
-                </IconProvider>
-                {/* Restore specific icons in the below Provider back to the built-in icons */}
-                <IconProvider icons={{ close: null }}>
-                  <SpaceBetween direction="horizontal" size="s">
-                    <Icon name={'add-plus'} />
-                    <Icon name={'close'} />
-                  </SpaceBetween>
-                </IconProvider>
-              </SpaceBetween>
-            </IconProvider>
-
-            {/* Modal icon override */}
-            <IconProvider
-              icons={{
-                'drag-indicator': CUSTOM_SVG,
-                close: CUSTOM_SIMPLE_SVG,
-              }}
-            >
-              <CollectionPreferences
-                {...baseProperties}
-                pageSizePreference={pageSizePreference}
-                wrapLinesPreference={wrapLinesPreference}
-                contentDensityPreference={contentDensityPreference}
-                customPreference={customPreference}
-                contentDisplayPreference={{
-                  title: 'Column preferences',
-                  description: 'Customize the columns visibility and order.',
-                  options: [
-                    { id: 'id1', label: 'Item 1' },
-                    { id: 'id2', label: 'Item 2' },
-                    { id: 'id3', label: 'Item 3' },
-                    { id: 'id4', label: 'Item 4' },
-                  ],
-                  ...contentDisplayPreferenceI18nStrings,
+    <I18nProvider messages={[messages]} locale="en">
+      <article>
+        <h1>Simple Icon Provider</h1>
+        <ScreenshotArea>
+          {/* Global icon override */}
+          <IconProvider
+            icons={{
+              close: CUSTOM_SIMPLE_SVG,
+            }}
+          >
+            <SpaceBetween size="m">
+              {/* Basic icon override */}
+              <IconProvider
+                icons={{
+                  search: CUSTOM_SVG,
                 }}
-              />
-            </IconProvider>
-          </SpaceBetween>
-        </IconProvider>
-      </ScreenshotArea>
-    </article>
+              >
+                <TextFilter
+                  filteringText={filteringText}
+                  filteringAriaLabel="Test filter"
+                  onChange={({ detail }) => setFilteringText(detail.filteringText)}
+                />
+              </IconProvider>
+
+              {/* Multi-component icon override */}
+              <IconProvider
+                icons={{
+                  'status-positive': CUSTOM_SVG,
+                  'status-negative': CUSTOM_SVG,
+                }}
+              >
+                <SpaceBetween size="m">
+                  <SpaceBetween size="s" direction="horizontal" alignItems="center">
+                    {/* Use close override from parent Provider */}
+                    <Button iconName="close">Icon test</Button>
+                    <StatusIndicator type="success">Icon test</StatusIndicator>
+                  </SpaceBetween>
+                  <Alert type="error" dismissible={true}>
+                    Icon test
+                  </Alert>
+                </SpaceBetween>
+              </IconProvider>
+
+              {/* Icon reset */}
+              <IconProvider icons={{ 'add-plus': CUSTOM_SVG, close: CUSTOM_SIMPLE_SVG }}>
+                <SpaceBetween direction="vertical" size="l">
+                  <Icon name={'add-plus'} />
+                  <Icon name={'close'} />
+                  {/* Restore icons in the below Provider back to the built-in set */}
+                  <IconProvider icons={null}>
+                    <SpaceBetween direction="horizontal" size="s">
+                      <Icon name={'add-plus'} />
+                      <Icon name={'close'} />
+                    </SpaceBetween>
+                  </IconProvider>
+                  {/* Restore specific icons in the below Provider back to the built-in icons */}
+                  <IconProvider icons={{ close: null }}>
+                    <SpaceBetween direction="horizontal" size="s">
+                      <Icon name={'add-plus'} />
+                      <Icon name={'close'} />
+                    </SpaceBetween>
+                  </IconProvider>
+                </SpaceBetween>
+              </IconProvider>
+
+              {/* Modal icon override */}
+              <IconProvider
+                icons={{
+                  'drag-indicator': CUSTOM_SVG,
+                  close: CUSTOM_SIMPLE_SVG,
+                }}
+              >
+                <CollectionPreferences
+                  {...baseProperties}
+                  pageSizePreference={pageSizePreference}
+                  wrapLinesPreference={wrapLinesPreference}
+                  contentDensityPreference={contentDensityPreference}
+                  customPreference={customPreference}
+                  contentDisplayPreference={{
+                    title: 'Column preferences',
+                    description: 'Customize the columns visibility and order.',
+                    options: [
+                      { id: 'id1', label: 'Item 1' },
+                      { id: 'id2', label: 'Item 2' },
+                      { id: 'id3', label: 'Item 3' },
+                      { id: 'id4', label: 'Item 4' },
+                    ],
+                    ...contentDisplayPreferenceI18nStrings,
+                  }}
+                />
+              </IconProvider>
+            </SpaceBetween>
+          </IconProvider>
+        </ScreenshotArea>
+      </article>
+    </I18nProvider>
   );
 }
