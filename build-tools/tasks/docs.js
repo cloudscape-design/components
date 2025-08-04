b11a976d7284f79c7a32fd398ebbe3d36b270a31
Boris Serdiuk <serdiuk@amazon.com>
2025-08-04 19:13:10 +0200

    Revert "chore: Migrate to new test-utils documenter (#3693)"
    This reverts commit 0cea5f4b36814ef901cbf7df401a320775d37133.

--- build-tools/tasks/docs.js
+++ build-tools/tasks/docs.js
@@ -1,7 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 const path = require('path');
-const { writeComponentsDocumentation, writeTestUtilsDocumentation } = require('@cloudscape-design/documenter');
+const { writeComponentsDocumentation, documentTestUtils } = require('@cloudscape-design/documenter');
+const { writeFile } = require('../utils/files');
 const workspace = require('../utils/workspace');
 
 module.exports = function docs() {
@@ -14,17 +15,27 @@ module.exports = function docs() {
       TagEditor: ['getTagsDiff'],
     },
   });
-  writeTestUtilsDocumentation({
-    outDir: path.join(workspace.apiDocsPath, 'test-utils-doc'),
-    tsconfigPath: require.resolve('../../src/test-utils/tsconfig.json'),
-    domUtils: {
-      root: 'src/test-utils/dom/index.ts',
-      extraExports: ['default', 'ElementWrapper'],
-    },
-    selectorsUtils: {
-      root: 'src/test-utils/selectors/index.ts',
-      extraExports: ['default', 'ElementWrapper'],
-    },
-  });
+  testUtilDocs();
   return Promise.resolve();
 };
+
+function testUtilDocs() {
+  ['dom', 'selectors'].forEach(testUtilType => {
+    const baseWrapperDefinitions = require(`@cloudscape-design/test-utils-core/test-utils-doc/${testUtilType}`);
+    const componentWrapperDefinitions = documentTestUtils(
+      {
+        tsconfig: require.resolve('../../src/test-utils/tsconfig.json'),
+      },
+      `**/{${testUtilType},types}/**/*`
+    );
+
+    const definitions = [...baseWrapperDefinitions, ...componentWrapperDefinitions];
+    const indexContent = `module.exports = {
+      classes: ${JSON.stringify(definitions)}
+    }
+    `;
+
+    const outPath = path.join(workspace.apiDocsPath, 'test-utils-doc', `${testUtilType}.js`);
+    writeFile(outPath, indexContent);
+  });
+}
