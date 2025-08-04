39debd4fe94acc81b6852cc39a83da05826d5b33
Boris Serdiuk <serdiuk@amazon.com>
2025-08-04 19:51:54 +0200

    revert: Migrate to new test-utils documenter (#3693) (#3718)

--- src/__tests__/snapshot-tests/documenter.test.ts
+++ src/__tests__/snapshot-tests/documenter.test.ts
@@ -1,7 +1,9 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import definitions from '../../../lib/components-definitions/components';
+// @ts-expect-error no typings
 import testUtilDomDefinitions from '../../../lib/components-definitions/test-utils-doc/dom';
+// @ts-expect-error no typings
 import testUtilSelectorsDefinitions from '../../../lib/components-definitions/test-utils-doc/selectors';
 import { getAllComponents } from '../utils';
 
