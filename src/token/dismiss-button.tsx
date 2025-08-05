e09f5f4541cfbab706540da322fc520689d5e81f
Ernst Kaese <ernstka@amazon.de>
2025-08-05 13:40:09 +0200

    Initial inline token variant and styling

--- src/token/dismiss-button.tsx
+++ src/token/dismiss-button.tsx
@@ -1,10 +1,12 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import React, { forwardRef, Ref } from 'react';
+import clsx from 'clsx';
 
 import { getAnalyticsMetadataAttribute } from '@cloudscape-design/component-toolkit/internal/analytics-metadata';
 
 import InternalIcon from '../icon/internal';
+import { fireNonCancelableEvent, NonCancelableEventHandler } from '../internal/events';
 import { GeneratedAnalyticsMetadataTokenGroupDismiss } from '../token-group/analytics-metadata/interfaces';
 
 import styles from './styles.css.js';
@@ -12,14 +14,15 @@ import styles from './styles.css.js';
 interface DismissButtonProps {
   disabled?: boolean;
   readOnly?: boolean;
-  onDismiss?: () => void;
+  onDismiss?: NonCancelableEventHandler;
   dismissLabel?: string;
+  inline?: boolean;
 }
 
 export default forwardRef(DismissButton);
 
 function DismissButton(
-  { disabled, dismissLabel, onDismiss, readOnly }: DismissButtonProps,
+  { disabled, dismissLabel, onDismiss, readOnly, inline }: DismissButtonProps,
   ref: Ref<HTMLButtonElement>
 ) {
   const analyticsMetadata: GeneratedAnalyticsMetadataTokenGroupDismiss = {
@@ -32,14 +35,14 @@ function DismissButton(
     <button
       ref={ref}
       type="button"
-      className={styles['dismiss-button']}
+      className={clsx(styles[`dismiss-button`], inline && styles['dismiss-button-inline'])}
       aria-disabled={disabled || readOnly ? true : undefined}
       onClick={() => {
         if (disabled || readOnly || !onDismiss) {
           return;
         }
 
-        onDismiss();
+        fireNonCancelableEvent(onDismiss);
       }}
       aria-label={dismissLabel}
       {...(disabled || readOnly ? {} : getAnalyticsMetadataAttribute(analyticsMetadata))}
