577bfbf63996c6cf9f98780f299ddf6ab8a1f642
Ernst Kaese <ernstka@amazon.de>
2025-08-05 13:40:09 +0200

    Initial inline token variant and styling

--- src/token/interfaces.ts
+++ src/token/interfaces.ts
@@ -1,12 +1,20 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-export interface TokenProps {
+import { BaseComponentProps } from '../internal/base-component';
+import { NonCancelableEventHandler } from '../internal/events';
+
+export interface TokenProps extends BaseComponentProps {
   children: React.ReactNode;
   ariaLabel?: string;
   dismissLabel?: string;
-  onDismiss?: () => void;
+  onDismiss?: NonCancelableEventHandler;
   disabled?: boolean;
   readOnly?: boolean;
   className?: string;
+  variant?: TokenProps.Variant;
+}
+
+export namespace TokenProps {
+  export type Variant = 'normal' | 'inline';
 }
