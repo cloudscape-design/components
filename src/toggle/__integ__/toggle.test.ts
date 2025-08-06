8e6227800605a9a93ceb5503c68add23f4e7bcfd
Scott O'Brien <scottob@amazon.com>
2025-08-06 14:52:40 -0400

    Add test for focus ring.

--- src/toggle/__integ__/toggle.test.ts
+++ src/toggle/__integ__/toggle.test.ts
@@ -9,20 +9,36 @@ const wrapper = createWrapper();
 
 class TogglePage extends BasePageObject {}
 
-const setupTest = (testFn: (page: TogglePage) => Promise<void>) => {
+const setupTest = (url: string, testFn: (page: TogglePage) => Promise<void>) => {
   return useBrowser(async browser => {
     const page = new TogglePage(browser);
-    await browser.url('#/light/toggle/simple');
+    await browser.url(url);
     await testFn(page);
   });
 };
 
 test(
   'Clicking on form field label should not change toggle state',
-  setupTest(async page => {
+  setupTest('#/light/toggle/simple', async page => {
     await page.click(wrapper.findFormField().findLabel().toSelector());
     await expect(
       page.isSelected(wrapper.find('#formfield-with-toggle').findToggle().find('input').toSelector())
     ).resolves.toBe(false);
+  })
+);
+
+test(
+  'style api focus state',
+  useBrowser(async browser => {
+    await browser.url('#/light/toggle/style-custom/');
+    const toggleSelector = '[data-testid="1"]';
+    const page = new TogglePage(browser);
+    const focusRingSelector = `${toggleSelector} > span:first-of-type > span:first-of-type > span:last-of-type`;
+
+    await page.click(toggleSelector);
+    await page.keys('a');
+    await expect((await browser.$(focusRingSelector).getCSSProperty('box-shadow', '::before')).value).toBe(
+      'rgb(4,125,149)0px0px0px3px'
+    );
   })
 );
