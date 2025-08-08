d5170d09039dc3f4731abb6f29320acea698ef9f
Joan Perals Tresserra <jotresse@amazon.com>
2025-08-08 10:56:26 +0200

    Add unit test coverage

---
+++ src/split-panel/__tests__/header.test.tsx
@@ -0,0 +1,50 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React from 'react';
+
+import { describeEachAppLayout } from '../../app-layout/__tests__/utils';
+import { renderSplitPanel } from './common';
+
+describe('Split panel: Header slots', () => {
+  describeEachAppLayout({ sizes: ['desktop', 'mobile'] }, () => {
+    test('renders headerActions', () => {
+      const { wrapper } = renderSplitPanel({
+        props: { headerActions: <button>Action</button> },
+      });
+      expect(wrapper!.findHeaderActions()).not.toBeNull();
+      expect(wrapper!.findHeaderActions()!.getElement()).toHaveTextContent('Action');
+    });
+
+    test('renders headerBefore', () => {
+      const { wrapper } = renderSplitPanel({
+        props: { headerBefore: <span>Before</span> },
+      });
+      expect(wrapper!.findHeaderBefore()).not.toBeNull();
+      expect(wrapper!.findHeaderBefore()!.getElement()).toHaveTextContent('Before');
+    });
+
+    test('renders headerDescription', () => {
+      const { wrapper } = renderSplitPanel({
+        props: { headerDescription: 'Header description' },
+      });
+      expect(wrapper!.findHeaderDescription()).not.toBeNull();
+      expect(wrapper!.findHeaderDescription()!.getElement()).toHaveTextContent('Header description');
+    });
+
+    test('renders headerInfo', () => {
+      const { wrapper } = renderSplitPanel({
+        props: { headerInfo: <span>Info</span> },
+      });
+      expect(wrapper!.findHeaderInfo()).not.toBeNull();
+      expect(wrapper!.findHeaderInfo()!.getElement()).toHaveTextContent('Info');
+    });
+
+    test('does not render header properties when not provided', () => {
+      const { wrapper } = renderSplitPanel();
+      expect(wrapper!.findHeaderActions()).toBeNull();
+      expect(wrapper!.findHeaderBefore()).toBeNull();
+      expect(wrapper!.findHeaderDescription()).toBeNull();
+      expect(wrapper!.findHeaderInfo()).toBeNull();
+    });
+  });
+});
