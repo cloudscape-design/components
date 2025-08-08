0087bf61d00491491b240eb9a6fb948abea417d7
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-08-08 12:57:11 +0200

    poc

--- build-tools/utils/pluralize.js
+++ build-tools/utils/pluralize.js
@@ -28,6 +28,7 @@ const pluralizationMap = {
   DatePicker: 'DatePickers',
   DateRangePicker: 'DateRangePickers',
   Drawer: 'Drawers',
+  ErrorBoundary: 'ErrorBoundaries',
   ExpandableSection: 'ExpandableSections',
   FileDropzone: 'FileDropzones',
   FileInput: 'FileInputs',
