d5d6553d79f31169ddba44df6a69bccf053ff986
Boris Serdiuk <serdiuk@amazon.com>
2025-08-11 23:25:02 +0200

    fix: Handle multiple intersection changes in a single callback

--- src/internal/hooks/use-intersection-observer/__tests__/use-intersection-observer.test.tsx
+++ src/internal/hooks/use-intersection-observer/__tests__/use-intersection-observer.test.tsx
@@ -1,52 +1,111 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import React from 'react';
-import { render } from '@testing-library/react';
+import { act, cleanup, render } from '@testing-library/react';
 
-import { useIntersectionObserver } from '..';
-
-declare global {
-  interface Window {
-    IntersectionObserver: any;
-  }
-}
+import { useIntersectionObserver } from '../../../../../lib/components/internal/hooks/use-intersection-observer';
 
 function TestComponent({ initialState }: { initialState?: boolean }) {
   const { ref, isIntersecting } = useIntersectionObserver({ initialState });
   return <div ref={ref} data-testid="test" data-value={isIntersecting} />;
 }
 
-const mockObserve = jest.fn();
-const mockIntersectionObserver = jest.fn(() => ({
-  observe: mockObserve,
-  disconnect: jest.fn(),
-}));
+type MockIntersectionObserverCallback = (
+  // only fields actually used in our implementation
+  entries: Array<Pick<IntersectionObserverEntry, 'isIntersecting' | 'time'>>
+) => void;
+let mockObserverInstance: (IntersectionObserver & { callback: MockIntersectionObserverCallback }) | undefined;
+
+class MockIntersectionObserver {
+  constructor(callback: MockIntersectionObserverCallback) {
+    if (mockObserverInstance) {
+      throw new Error('only one observer instance expected per test');
+    }
+    const instance = {
+      observe: jest.fn() as IntersectionObserver['observe'],
+      disconnect: jest.fn() as IntersectionObserver['disconnect'],
+    } as IntersectionObserver;
+    mockObserverInstance = {
+      ...instance,
+      callback,
+    };
+    return instance;
+  }
+}
 
 describe('useIntersectionObserver', () => {
   beforeEach(() => {
-    window.IntersectionObserver = mockIntersectionObserver;
+    window.IntersectionObserver = MockIntersectionObserver as unknown as typeof IntersectionObserver;
     jest.clearAllMocks();
   });
 
-  it('should create an observer with the defined target element', async () => {
-    const { findByTestId } = render(<TestComponent />);
-    const target = await findByTestId('test');
-
-    expect(mockIntersectionObserver).toHaveBeenCalled();
-    expect(mockObserve).toHaveBeenCalledWith(target);
+  afterEach(() => {
+    cleanup();
+    expect(mockObserverInstance!.disconnect).toHaveBeenCalled();
+    mockObserverInstance = undefined;
   });
 
-  it('defaults to not intersecting', async () => {
-    const { findByTestId } = render(<TestComponent />);
-    const target = await findByTestId('test');
+  it('should create an observer with the defined target element', () => {
+    const { getByTestId } = render(<TestComponent />);
+    const target = getByTestId('test');
+
+    expect(mockObserverInstance!.observe).toHaveBeenCalledWith(target);
+  });
+
+  it('defaults to not intersecting', () => {
+    const { getByTestId } = render(<TestComponent />);
+    const target = getByTestId('test');
 
     expect(target.dataset.value).toBe('false');
   });
 
-  it('allows overriding the default value', async () => {
-    const { findByTestId } = render(<TestComponent initialState={true} />);
-    const target = await findByTestId('test');
+  it('allows overriding the default value', () => {
+    const { getByTestId } = render(<TestComponent initialState={true} />);
+    const target = getByTestId('test');
 
+    expect(target.dataset.value).toBe('true');
+  });
+
+  it('updates value with a callback', () => {
+    const { getByTestId } = render(<TestComponent />);
+
+    const target = getByTestId('test');
+    expect(target.dataset.value).toBe('false');
+
+    act(() => mockObserverInstance!.callback([{ time: 0, isIntersecting: true }]));
+    expect(target.dataset.value).toBe('true');
+
+    act(() => mockObserverInstance!.callback([{ time: 0, isIntersecting: false }]));
+    expect(target.dataset.value).toBe('false');
+  });
+
+  it('should resolve the final state if multiple observations occurred', () => {
+    const { getByTestId } = render(<TestComponent />);
+
+    act(() =>
+      mockObserverInstance!.callback([
+        { time: 0, isIntersecting: true },
+        { time: 1, isIntersecting: false },
+        { time: 2, isIntersecting: true },
+      ])
+    );
+
+    const target = getByTestId('test');
+    expect(target.dataset.value).toBe('true');
+  });
+
+  it('should sort observations by time', () => {
+    const { getByTestId } = render(<TestComponent />);
+
+    act(() =>
+      mockObserverInstance!.callback([
+        { time: 1, isIntersecting: false },
+        { time: 2, isIntersecting: true },
+        { time: 0, isIntersecting: false },
+      ])
+    );
+
+    const target = getByTestId('test');
     expect(target.dataset.value).toBe('true');
   });
 });
