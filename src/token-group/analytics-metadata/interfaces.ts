451a89ad29a7bebeaa4b9a50df9ec9b154cb8345
Ernst Kaese <ernstka@amazon.de>
2025-08-11 14:40:41 +0200

    Add analytics

--- src/token-group/analytics-metadata/interfaces.ts
+++ src/token-group/analytics-metadata/interfaces.ts
@@ -1,19 +1,9 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { LabelIdentifier } from '@cloudscape-design/component-toolkit/internal/analytics-metadata';
-
 import {
   GeneratedAnalyticsMetadataTokenListShowLess,
   GeneratedAnalyticsMetadataTokenListShowMore,
 } from '../../internal/components/token-list/analytics-metadata/interfaces';
-
-export interface GeneratedAnalyticsMetadataTokenGroupDismiss {
-  action: 'dismiss';
-  detail: {
-    label: LabelIdentifier;
-    position?: string;
-  };
-}
 
 export type GeneratedAnalyticsMetadataTokenGroupShowMore = GeneratedAnalyticsMetadataTokenListShowMore;
 export type GeneratedAnalyticsMetadataTokenGroupShowLess = GeneratedAnalyticsMetadataTokenListShowLess;
--- src/token/index.tsx
+++ src/token/index.tsx
@@ -3,28 +3,34 @@
 'use client';
 import React from 'react';
 
-// import { getAnalyticsMetadataAttribute } from '@cloudscape-design/component-toolkit/internal/analytics-metadata';
+import { getAnalyticsMetadataAttribute } from '@cloudscape-design/component-toolkit/internal/analytics-metadata';
+
 import useBaseComponent from '../internal/hooks/use-base-component';
 import { applyDisplayName } from '../internal/utils/apply-display-name';
-// import { GeneratedAnalyticsMetadataTokenGroupComponent } from './analytics-metadata/interfaces';
+import { GeneratedAnalyticsMetadataTokenComponent } from './analytics-metadata/interfaces';
 import { TokenProps } from './interfaces';
 import InternalToken from './internal';
+
+import analyticsSelectors from './analytics-metadata/styles.css.js';
 
 export { TokenProps };
 
 export default function Token(props: TokenProps) {
   const baseComponentProps = useBaseComponent('Token');
 
-  // const componentAnalyticsMetadata: GeneratedAnalyticsMetadataTokenGroupComponent = {
-  //   name: 'awsui.Token',
-  //   label: 'invalid',
-  // };
+  const componentAnalyticsMetadata: GeneratedAnalyticsMetadataTokenComponent = {
+    name: 'awsui.Token',
+    label: `.${analyticsSelectors.token}`,
+    properties: {
+      hasPopover: String(!!props.popoverProps),
+    },
+  };
 
   return (
     <InternalToken
       {...props}
       {...baseComponentProps}
-      // {...getAnalyticsMetadataAttribute({ component: componentAnalyticsMetadata })}
+      {...getAnalyticsMetadataAttribute({ component: componentAnalyticsMetadata })}
     />
   );
 }
