9e6af190f9e73eb9574d8e355a356b5e6e51ccec
Joan Perals Tresserra <jotresse@amazon.com>
2025-08-11 19:23:10 +0200

    Revert "Remove headerBefore and ariaLabel"
    This reverts commit fca330ffc0f637f5c45999da770ef1b6a5df8479.

--- src/split-panel/implementation.tsx
+++ src/split-panel/implementation.tsx
@@ -36,7 +36,9 @@ export function SplitPanelImplementation({
   hidePreferencesButton,
   closeBehavior,
   i18nStrings = {},
+  ariaLabel,
   headerActions,
+  headerBefore,
   headerDescription,
   headerInfo,
   ...restProps
@@ -91,9 +93,10 @@ export function SplitPanelImplementation({
     [globalVars.stickyVerticalBottomOffset]: bottomOffset,
   };
 
-  const panelHeaderId = useUniqueId('split-panel-header');
+  const panelHeaderUniqueId = useUniqueId('split-panel-header');
+  const panelHeaderId = ariaLabel ? undefined : panelHeaderUniqueId;
 
-  const hasCustomElements = !!headerActions || !!headerInfo;
+  const hasCustomElements = !!headerActions || !!headerBefore || !!headerInfo;
 
   const showDescription = headerDescription && isOpen;
 
@@ -101,16 +104,16 @@ export function SplitPanelImplementation({
     <div className={clsx(styles.header, isToolbar && styles['with-toolbar'])} style={appLayoutMaxWidth}>
       <div className={styles['header-main-row']}>
         <div className={styles['header-main-content']}>
-          <div className={styles['header-text-and-info']}>
-            <h2
-              className={clsx(
-                styles['header-text'],
-                testUtilStyles['header-text'],
-                !!headerInfo && styles['with-info']
+          <div className={styles['header-tag-and-info']}>
+            <h2 className={clsx(styles['header-tag'], !!headerInfo && styles['with-info'])} id={panelHeaderId}>
+              {headerBefore && (
+                <span className={clsx(styles['header-before'], testUtilStyles['header-before'])}>{headerBefore}</span>
               )}
-              id={panelHeaderId}
-            >
-              {header}
+              {header && (
+                <div className={clsx(styles['header-text'], testUtilStyles['header-text'])} id={panelHeaderId}>
+                  {header}
+                </div>
+              )}{' '}
             </h2>
             {headerInfo && (
               <span className={clsx(styles['header-info'], testUtilStyles['header-info'])}>{headerInfo}</span>
@@ -246,6 +249,7 @@ export function SplitPanelImplementation({
           toggleRef={refs.toggle}
           header={wrappedHeader}
           panelHeaderId={panelHeaderId}
+          ariaLabel={ariaLabel}
           closeBehavior={closeBehavior}
         >
           {children}
@@ -264,6 +268,7 @@ export function SplitPanelImplementation({
           header={wrappedHeader}
           panelHeaderId={panelHeaderId}
           appLayoutMaxWidth={appLayoutMaxWidth}
+          ariaLabel={ariaLabel}
           closeBehavior={closeBehavior}
           hasCustomElements={hasCustomElements}
         >
