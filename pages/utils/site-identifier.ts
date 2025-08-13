1df7c6ae755a046eb77c40f776cd7ca7cf518656
Ernst Kaese <ernstka@amazon.de>
2025-08-13 13:16:24 +0200

    Add util for site identifier in dev pages

---
+++ pages/utils/site-identifier.ts
@@ -0,0 +1,7 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { SYSTEM } from '~components/internal/environment';
+
+export const getSiteIdentifier = () => {
+  return SYSTEM;
+};
