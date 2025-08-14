45d3c65326f33493727f74107c5f1b4b0912c430
Boris Serdiuk <serdiuk@amazon.com>
2025-08-14 12:36:26 +0200

    refactor integration test

--- src/app-layout/__integ__/widget-async-loading.test.ts
+++ src/app-layout/__integ__/widget-async-loading.test.ts
@@ -4,53 +4,30 @@ import { BasePageObject } from '@cloudscape-design/browser-test-tools/page-objec
 import useBrowser from '@cloudscape-design/browser-test-tools/use-browser';
 
 import { viewports } from './constants';
+import { getUrlParams } from './utils';
 
-interface SetupTestOptions {
-  splitPanelPosition?: string;
-  screenSize?: (typeof viewports)['desktop' | 'mobile'];
-  disableContentPaddings?: string;
-  theme: string;
-}
+describe.each(['desktop', 'mobile'] as const)('%s', size => {
+  test(
+    'main content area layout should not shift after loading the widget part of the page',
+    useBrowser(size === 'desktop' ? viewports.desktop : viewports.mobile, async browser => {
+      const page = new BasePageObject(browser);
+      await browser.url(
+        `#/light/app-layout/runtime-drawers?${getUrlParams('refresh-toolbar', {
+          appLayoutWidget: 'true',
+          appLayoutDelayedWidget: 'true',
+        })}`
+      );
+      // make sure the widget part has not loaded yet
+      const { top: contentTopBefore, left: contentLeftBefore } = await page.getBoundingBox(
+        `[data-awsui-app-layout-widget-loaded="false"] [data-testid="app-layout-content-area"]`
+      );
 
-const setupTest = (
-  { screenSize = viewports.desktop }: SetupTestOptions,
-  testFn: (page: BasePageObject) => Promise<void>
-) =>
-  useBrowser(screenSize, async browser => {
-    const page = new BasePageObject(browser);
-    const params = new URLSearchParams({
-      appLayoutWidget: 'true',
-      appLayoutDelayedWidget: 'true',
-      visualRefresh: 'true',
-      appLayoutToolbar: 'true',
-    }).toString();
-    await browser.url(`#/light/app-layout/runtime-drawers?${params}`);
-    await testFn(page);
-  });
+      const { top: contentTopAfter, left: contentLeftAfter } = await page.getBoundingBox(
+        `[data-awsui-app-layout-widget-loaded="true"] [data-testid="app-layout-content-area"]`
+      );
 
-describe.each(['refresh-toolbar'] as const)('%s', theme => {
-  describe.each(['desktop', 'mobile'] as const)('%s', size => {
-    test(
-      'main content area layout should not shift after loading the widget part of the page',
-      setupTest(
-        {
-          theme,
-          screenSize: size === 'desktop' ? viewports.desktop : viewports.mobile,
-        },
-        async page => {
-          // make sure the widget part has not loaded yet
-          const { top: contentTopBefore, left: contentLeftBefore } = await page.getBoundingBox(
-            `[data-awsui-app-layout-widget-loaded="false"] [data-testid="app-layout-content-area"]`
-          );
-
-          const { top: contentTopAfter, left: contentLeftAfter } = await page.getBoundingBox(
-            `[data-awsui-app-layout-widget-loaded="true"] [data-testid="app-layout-content-area"]`
-          );
-
-          expect(contentTopBefore).toBe(contentTopAfter);
-          expect(contentLeftBefore).toBe(contentLeftAfter);
-        }
-      )
-    );
-  });
+      expect(contentTopBefore).toBe(contentTopAfter);
+      expect(contentLeftBefore).toBe(contentLeftAfter);
+    })
+  );
 });
