fb17da679837c75e448b1eed2bf7fb726fe0c3e8
ClaudioGSDB <csciotto@amazon.de>
2025-08-27 13:59:59 +0200

    Fixed Unit Tests

--- src/internal/components/option/__tests__/option.test.tsx
+++ src/internal/components/option/__tests__/option.test.tsx
@@ -189,24 +189,6 @@ describe('Option component', () => {
       expect(findIconWithSize(optionWrapper, 'normal')).not.toBeNull();
     });
 
-    test('in big variant if description is set', () => {
-      const optionWrapper = renderOption({
-        option: {
-          ...baseOptionWithIcon,
-          description: 'desc',
-        },
-      });
-      expect(findIconWithSize(optionWrapper, 'big')).not.toBeNull();
-    });
-    test('in big variant if tags are set', () => {
-      const optionWrapper = renderOption({
-        option: {
-          ...baseOptionWithIcon,
-          tags: ['first'],
-        },
-      });
-      expect(findIconWithSize(optionWrapper, 'big')).not.toBeNull();
-    });
     test('with custom icon', () => {
       const __customIcon = <span className="custom-icon" />;
 
