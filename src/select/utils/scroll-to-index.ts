494edbd2af34b221b05adfbf638cd2dfcf21d83e
Gethin Webster <gethinw@amazon.de>
2025-08-27 14:58:46 +0200

    Test coverage

--- src/select/utils/scroll-to-index.ts
+++ src/select/utils/scroll-to-index.ts
@@ -7,20 +7,16 @@ export default function ({ index, menuEl }: { index: number; menuEl: HTMLElement
   if (item) {
     // In edge case dropdown can be very small, scrolling can cause side effect AWSUI-60318
     if (menuEl.clientHeight !== undefined && menuEl.clientHeight > 15) {
-      /* istanbul ignore next: clientHeight always returns 0 in JSDOM, the line is covered by integ tests */
+      // We only want the menu element to scroll, as scrolling any further parent elements can
+      // cause the screen to jump, resulting in a select item being "clicked" while opening the select.
+      // Therefore, we scroll manually rather than using `scrollIntoView`.
       const menuRect = menuEl.getBoundingClientRect();
       const itemRect = item.getBoundingClientRect();
       const offset = parseInt(menuEl.style.scrollPaddingBlockStart) || 0;
-      if (itemRect.top < menuRect.top + offset) {
-        console.log('scroll up', itemRect.top, menuRect.top);
+      if (itemRect.top < menuRect.top + offset || itemRect.height > menuRect.height - offset) {
         menuEl.scrollBy({ top: itemRect.top - menuRect.top - offset });
       } else if (itemRect.bottom > menuRect.bottom) {
-        if (itemRect.height > menuRect.height) {
-          console.log('scroll down partial', itemRect.bottom, menuRect.bottom, itemRect.height, menuRect.height);
-        } else {
-          console.log('scroll down', itemRect.bottom, menuRect.bottom);
-          menuEl.scrollBy({ top: itemRect.bottom - menuRect.bottom });
-        }
+        menuEl.scrollBy({ top: itemRect.bottom - menuRect.bottom });
       }
     }
   }
