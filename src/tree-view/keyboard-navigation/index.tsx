4e35b6c99bfb858c884782df96fc28df2e9c5332
Cansu Aksu <cansua@amazon.com>
2025-09-02 15:39:55 +0200

    import STSN from component-toolkit

--- src/tree-view/keyboard-navigation/index.tsx
+++ src/tree-view/keyboard-navigation/index.tsx
@@ -4,13 +4,13 @@ import React, { useRef } from 'react';
 import { useEffect, useMemo } from 'react';
 
 import { useStableCallback } from '@cloudscape-design/component-toolkit/internal';
-
-import AsyncStore, { useSelector } from '../../area-chart/async-store';
-import { getAllFocusables } from '../../internal/components/focus-lock/utils';
 import {
   SingleTabStopNavigationAPI,
   SingleTabStopNavigationProvider,
-} from '../../internal/context/single-tab-stop-navigation-context';
+} from '@cloudscape-design/component-toolkit/internal';
+
+import AsyncStore, { useSelector } from '../../area-chart/async-store';
+import { getAllFocusables } from '../../internal/components/focus-lock/utils';
 import { KeyCode } from '../../internal/keycode';
 import handleKey, { isEventLike } from '../../internal/utils/handle-key';
 import {
