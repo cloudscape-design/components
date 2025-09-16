7ee756d72363ee83725f335e61a6a0c398dac698
Georgii Lobko <47106899+georgylobko@users.noreply.github.com>
2025-09-16 15:48:30 +0200

    feat: Add a new whenAppLayoutReady to the plugin api (#3863)

--- src/internal/plugins/widget.ts
+++ src/internal/plugins/widget.ts
@@ -1,4 +1,4 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 export * from './widget/interfaces';
-export { registerLeftDrawer, updateDrawer, isAppLayoutReady } from './widget/internal';
+export { registerLeftDrawer, updateDrawer, isAppLayoutReady, whenAppLayoutReady } from './widget/internal';
