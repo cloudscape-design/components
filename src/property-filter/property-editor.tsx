b8af97c0a76a48e729b89a390c137b9f8c95ad62
Le Qin <johnnsqi@amazon.com>
2025-09-21 17:00:00 -0700

    chore: fix lint errors

--- src/property-filter/property-editor.tsx
+++ src/property-filter/property-editor.tsx
@@ -75,7 +75,12 @@ export function PropertyEditorContentEnum({
   filteringOptions: readonly InternalFilteringOption[];
   onLoadItems?: NonCancelableEventHandler<LoadItemsDetail>;
 }) {
-  const valueOptions: readonly { value: string; label: string; tags?: readonly string[]; filteringTags?: readonly string[] }[] = filteringOptions
+  const valueOptions: readonly {
+    value: string;
+    label: string;
+    tags?: readonly string[];
+    filteringTags?: readonly string[];
+  }[] = filteringOptions
     .filter(option => option.property?.propertyKey === property.propertyKey)
     .map(({ label, value, tags, filteringTags }) => ({ label, value, tags, filteringTags }));
   const valueHandlers = useLoadItems(onLoadItems, '', property.externalProperty);
