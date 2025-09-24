39de2bfb49d22da410db9a8acdc2fb24f1004e4a
Cansu Aksu <cansua@amazon.com>
2025-09-24 12:12:21 +0200

    clean the test file

--- src/tree-view/__tests__/keyboard-navigation.test.tsx
+++ src/tree-view/__tests__/keyboard-navigation.test.tsx
@@ -1,13 +1,12 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import React, { useRef } from 'react';
+import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
 
 import createWrapper from '../../../lib/components/test-utils/dom';
 import TreeView, { TreeViewProps } from '../../../lib/components/tree-view';
 import { KeyCode } from '../../internal/keycode';
-import { KeyboardNavigationProcessor, KeyboardNavigationProvider } from '../keyboard-navigation';
 import { itemsWithFocusables as items, ItemWithFocusables as Item } from './items';
 
 const TestTreeView = (props: Partial<TreeViewProps<Item>> = {}) => (
@@ -65,97 +64,6 @@ test('tree-items have flat indices', () => {
   const allItems = wrapper.findItems();
   const lastRootLevelItem = allItems[allItems.length - 1];
   expect(lastRootLevelItem.getElement().getAttribute(treeItemIndexAttribute)).toBe(`${allItems.length - 1}`);
-});
-
-describe('KeyboardNavigationProvider', () => {
-  test('does not throw when not initialized', () => {
-    const navigation = new KeyboardNavigationProcessor({
-      current: {
-        updateFocusTarget: () => {},
-        getFocusTarget: () => null,
-        isRegistered: () => false,
-      },
-    });
-    expect(() => navigation.getNextFocusTarget()).not.toThrow();
-    expect(() => navigation.onUnregisterActive()).not.toThrow();
-    expect(() => navigation.refresh()).not.toThrow();
-    expect(() => navigation.cleanup()).not.toThrow();
-  });
-
-  test('does not throw when tree-view is null', () => {
-    function TestTreeView() {
-      return <KeyboardNavigationProvider getTreeView={() => null}>{null}</KeyboardNavigationProvider>;
-    }
-    expect(() => render(<TestTreeView />)).not.toThrow();
-  });
-
-  test('does not throw when focusing on incorrect target', () => {
-    function TestTreeView() {
-      const treeViewRef = useRef<HTMLUListElement>(null);
-      return (
-        <KeyboardNavigationProvider getTreeView={() => treeViewRef.current}>
-          <ul ref={treeViewRef} data-testid="tree-view">
-            <li>
-              <button tabIndex={0} data-testid="random-button">
-                Random button
-              </button>
-            </li>
-            <li>
-              <svg focusable="false">
-                <g tabIndex={0}>graphic</g>
-              </svg>
-            </li>
-          </ul>
-        </KeyboardNavigationProvider>
-      );
-    }
-
-    const { container } = render(<TestTreeView />);
-
-    const button = container.querySelector('button')!;
-    const g = container.querySelector('g')!;
-
-    button.focus();
-    expect(button).toHaveFocus();
-
-    g.focus();
-    expect(g).toHaveFocus();
-  });
-
-  test('does not focus re-registered element if the focus is not within the tree-view anymore', () => {
-    const { container, rerender } = render(
-      <div>
-        <TestTreeView />
-        <button data-testid="outside-focus-target">outside</button>
-      </div>
-    );
-    const wrapper = createWrapper(container).findTreeView()!;
-
-    const firstToggle = wrapper.findItemById('1-button-actions')!.findItemToggle()!.getElement();
-    const outsideButton = container.querySelector('[data-testid="outside-focus-target"]') as HTMLButtonElement;
-
-    firstToggle.focus();
-    expect(firstToggle).toHaveFocus();
-
-    rerender(
-      <div>
-        <TestTreeView />
-        <button data-testid="outside-focus-target">outside</button>
-      </div>
-    );
-    expect(firstToggle).toHaveFocus();
-
-    outsideButton.focus();
-    expect(outsideButton).toHaveFocus();
-
-    rerender(
-      <div>
-        <TestTreeView />
-        <button data-testid="outside-focus-target">outside</button>
-      </div>
-    );
-    expect(outsideButton).toHaveFocus();
-  });
 });
 
 describe('Keyboard navigation', () => {
