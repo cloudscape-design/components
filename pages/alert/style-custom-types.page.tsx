145e4e3fa88c28c386873841e9c1cf2fcd2f68b4
teonemes <teonemes@amazon.de>
2025-09-25 12:59:26 +0200

    chore: Update custom dev pages to use light-dark method

--- pages/alert/style-custom-types.page.tsx
+++ pages/alert/style-custom-types.page.tsx
@@ -1,8 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React, { useRef } from 'react';
-
-import { useCurrentMode } from '@cloudscape-design/component-toolkit/internal';
+import React from 'react';
 
 import { Alert as CloudscapeAlert, Button, SpaceBetween } from '~components';
 
