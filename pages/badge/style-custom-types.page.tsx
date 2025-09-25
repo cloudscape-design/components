577a9467e073a2ec62b8c4000845e7114bb75c82
teonemes <teonemes@amazon.de>
2025-09-25 12:59:26 +0200

    chore: Update custom dev pages to use light-dark method

--- pages/badge/style-custom-types.page.tsx
+++ pages/badge/style-custom-types.page.tsx
@@ -1,8 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React, { useRef } from 'react';
-
-import { useCurrentMode } from '@cloudscape-design/component-toolkit/internal';
+import React from 'react';
 
 import { Badge as CloudscapeBadge, SpaceBetween } from '~components';
 
@@ -31,20 +29,15 @@ interface CustomBadgeProps {
 }
 
 function CustomBadge({ children, colorTheme }: CustomBadgeProps) {
-  const mode = useCurrentMode(useRef(document.body));
-  const background = backgrounds[mode][colorTheme];
-  const borderColor = borderColors[mode][colorTheme];
-  const borderWidth = borderWidths[colorTheme];
-  const color = colors[mode];
   return (
     <CloudscapeBadge
       style={{
         root: {
-          background,
-          borderColor,
+          background: getBackground(colorTheme),
+          borderColor: getBorderColor(colorTheme),
           borderRadius: '8px',
-          color,
-          borderWidth,
+          color: getColor(),
+          borderWidth: getBorderWidth(colorTheme),
           paddingBlock: '8px',
           paddingInline: '12px',
         },
@@ -55,54 +48,42 @@ function CustomBadge({ children, colorTheme }: CustomBadgeProps) {
   );
 }
 
-const backgrounds = {
-  light: {
-    blue: palette.blue80,
-    critical: palette.red80,
-    high: palette.red60,
-    medium: palette.green80,
-    low: palette.teal90,
-    neutral: palette.neutral80,
-  },
-  dark: {
-    blue: palette.blue40,
-    critical: palette.red30,
-    high: palette.red30,
-    medium: palette.green20,
-    low: palette.teal20,
-    neutral: palette.neutral20,
-  },
-};
+function getBackground(colorTheme: string) {
+  const backgrounds = {
+    blue: `light-dark(${palette.blue80}, ${palette.blue40})`,
+    critical: `light-dark(${palette.red80}, ${palette.red30})`,
+    high: `light-dark(${palette.red60}, ${palette.red30})`,
+    medium: `light-dark(${palette.green80}, ${palette.green20})`,
+    low: `light-dark(${palette.teal90}, ${palette.teal20})`,
+    neutral: `light-dark(${palette.neutral80}, ${palette.neutral20})`,
+  };
+  return backgrounds[colorTheme as keyof typeof backgrounds];
+}
 
-const colors = {
-  light: 'white',
-  dark: 'black',
-};
+function getColor() {
+  return 'light-dark(white, black)';
+}
 
-const borderColors = {
-  light: {
-    blue: palette.neutral80,
-    critical: palette.blue90,
-    high: palette.red80,
-    medium: palette.green80,
-    low: palette.neutral80,
-    neutral: palette.teal80,
-  },
-  dark: {
-    blue: palette.neutral20,
-    critical: palette.red60,
-    high: palette.red10,
-    medium: palette.green30,
-    low: palette.neutral20,
-    neutral: palette.teal40,
-  },
-};
+function getBorderColor(colorTheme: string) {
+  const borderColors = {
+    blue: `light-dark(${palette.neutral80}, ${palette.neutral20})`,
+    critical: `light-dark(${palette.blue90}, ${palette.red60})`,
+    high: `light-dark(${palette.red80}, ${palette.red10})`,
+    medium: `light-dark(${palette.green80}, ${palette.green30})`,
+    low: `light-dark(${palette.neutral80}, ${palette.neutral20})`,
+    neutral: `light-dark(${palette.teal80}, ${palette.teal40})`,
+  };
+  return borderColors[colorTheme as keyof typeof borderColors];
+}
 
-const borderWidths = {
-  blue: '2px',
-  critical: '4px',
-  high: '0px',
-  medium: '0px',
-  low: '0px',
-  neutral: '0px',
-};
+function getBorderWidth(colorTheme: string) {
+  const borderWidths = {
+    blue: '2px',
+    critical: '4px',
+    high: '0px',
+    medium: '0px',
+    low: '0px',
+    neutral: '0px',
+  };
+  return borderWidths[colorTheme as keyof typeof borderWidths];
+}
