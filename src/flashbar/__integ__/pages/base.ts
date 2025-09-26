74732dde1f39c4087d99490b3a09e4aaac209439
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-09-26 11:22:44 +0200

    chore: Skip more integ tests for React 18 (#3888)

--- src/flashbar/__integ__/pages/base.ts
+++ src/flashbar/__integ__/pages/base.ts
@@ -1,14 +1,14 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { BasePageObject } from '@cloudscape-design/browser-test-tools/page-objects';
 
 import createWrapper from '../../../../lib/components/test-utils/selectors';
+import BasePageExtendedObject from '../../../__integ__/page-objects/base-page-ext';
 
 import selectors from '../../../../lib/components/flashbar/styles.selectors.js';
 
 export const flashbar = createWrapper().findFlashbar();
 
-export class FlashbarBasePage extends BasePageObject {
+export class FlashbarBasePage extends BasePageExtendedObject {
   async toggleCollapsedState() {
     const selector = this.getNotificationBar();
     await this.click(selector);
