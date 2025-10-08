50aa87e2bee337de5ff4ec809b22744905523a5b
Boris Serdiuk <serdiuk@amazon.com>
2025-10-08 01:06:10 -0700

    fix: Allow ai drawer to have bigger max width (#3917)

--- src/app-layout/visual-refresh-toolbar/compute-layout.ts
+++ src/app-layout/visual-refresh-toolbar/compute-layout.ts
@@ -19,7 +19,6 @@ interface HorizontalLayoutInput {
 }
 
 export const CONTENT_PADDING = 2 * 24; // space-xl
-const MOBILE_BREAKPOINT = 688;
 
 export function computeHorizontalLayout({
   navigationOpen,
@@ -52,8 +51,7 @@ export function computeHorizontalLayout({
   const maxSplitPanelSize = Math.max(resizableSpaceAvailable - totalActiveGlobalDrawersSize - activeDrawerSize, 0);
   resizableSpaceAvailable -= sideSplitPanelSize;
   const maxDrawerSize = resizableSpaceAvailable - totalActiveGlobalDrawersSize;
-  // let the ai drawer be resized until the "main screen" hits the mobile breakpoint to have consistent UX
-  const maxAiDrawerSize = placement.inlineSize - MOBILE_BREAKPOINT;
+  const maxAiDrawerSize = resizableSpaceAvailable - totalActiveGlobalDrawersSize + activeAiDrawerSize;
   const maxGlobalDrawersSizes: Record<string, number> = Object.keys(activeGlobalDrawersSizes).reduce(
     (acc, drawerId) => {
       return {
