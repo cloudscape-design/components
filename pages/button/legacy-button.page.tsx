0188b2ca8b2d94c68a2a0638c5a52bc15ecaca7f
Gethin Webster <gethinw@amazon.de>
2025-10-15 13:14:29 +0200

    Add heading

--- pages/button/legacy-button.page.tsx
+++ pages/button/legacy-button.page.tsx
@@ -9,6 +9,7 @@ import ScreenshotArea from '../utils/screenshot-area';
 export default function ButtonsScenario() {
   return (
     <article>
+      <h1>Legacy buttons</h1>
       <ScreenshotArea>
         <button className="awsui_button_vjswe_t8nlg_157">Legacy button</button>
         <button className="awsui_button_vjswe_1qz6m_157">Older legacy button</button>
