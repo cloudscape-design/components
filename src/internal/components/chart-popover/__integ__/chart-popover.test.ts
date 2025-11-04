71e792d3931be9a9251b30095dc56d82604356d0
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-11-04 15:24:30 +0100

    fix integ test def

--- src/internal/components/chart-popover/__integ__/chart-popover.test.ts
+++ src/internal/components/chart-popover/__integ__/chart-popover.test.ts
@@ -6,25 +6,26 @@ import useBrowser from '@cloudscape-design/browser-test-tools/use-browser';
 
 import createWrapper from '../../../../../lib/components/test-utils/selectors';
 
-test.each([false, true])('can be unpinned by clicking outside, iframe=%s', async iframe => {
-  await useBrowser(async browser => {
-    const page = new BasePageObject(browser);
-    await browser.url(`#/light/charts.test?iframe=${iframe}`);
-    await page.runInsideIframe('#content-iframe', iframe, async () => {
-      const chart = createWrapper().findLineChart();
-      const popover = chart.findDetailPopover();
-      const popoverDismiss = popover.findDismissButton();
+describe.each([false, true])('iframe=%s', iframe => {
+  test(
+    'can be unpinned by clicking outside',
+    useBrowser(async browser => {
+      const page = new BasePageObject(browser);
+      await browser.url(`#/light/charts.test?iframe=${iframe}`);
+      await page.runInsideIframe('#content-iframe', iframe, async () => {
+        const chart = createWrapper().findLineChart();
+        const popover = chart.findDetailPopover();
+        const popoverDismiss = popover.findDismissButton();
 
-      // Show popover with an offset from chart's center.
-      await page.hoverElement(chart.toSelector(), 100);
-      // Pin the popover by clicking on the left from it to ensure the click lands on the SVG, not popover content.
-      await page.click(chart.toSelector());
-      await page.waitForAssertion(() => expect(page.isDisplayed(popoverDismiss.toSelector())).resolves.toBe(true));
-      await page.waitForAssertion(() => expect(page.isFocused(popoverDismiss.toSelector())).resolves.toBe(true));
+        // Pins popover on the first point.
+        await page.click('h2');
+        await page.keys(['Tab', 'ArrowRight', 'Enter']);
+        await page.waitForVisible(popoverDismiss.toSelector());
 
-      // Unpin by clicking outside the chart.
-      await page.click('h2');
-      await expect(page.isDisplayed(popover.toSelector())).resolves.toBe(false);
-    });
-  })();
+        // Unpin by clicking outside the chart.
+        await page.click('h1');
+        await expect(page.isDisplayed(popover.toSelector())).resolves.toBe(false);
+      });
+    })
+  );
 });
