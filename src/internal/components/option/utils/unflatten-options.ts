90225f7ff7a7c9d8ffb9307275033e83ca4ed21e
Joan Perals Tresserra <jotresse@amazon.com>
2025-11-14 09:20:27 +0100

    Fix: option grouping

--- src/internal/components/option/utils/unflatten-options.ts
+++ src/internal/components/option/utils/unflatten-options.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { DropdownOption } from '../interfaces';
+import { DropdownOption, OptionGroup } from '../interfaces';
 
 interface ParentDropdownOption {
   type: 'parent';
@@ -20,17 +20,21 @@ export type NestedDropdownOption = ParentDropdownOption | ChildDropdownOption;
 
 export function unflattenOptions(options: ReadonlyArray<DropdownOption>): NestedDropdownOption[] {
   const nestedOptions: NestedDropdownOption[] = [];
-  let currentParent: ParentDropdownOption | undefined;
+
+  const attachedChildren = new Set<DropdownOption>();
 
   options.forEach((option, index) => {
     if (option.type === 'parent') {
       const wrapped: ParentDropdownOption = { type: 'parent', option, index, children: [] };
-      currentParent = wrapped;
+      (option.option as OptionGroup).options.forEach(child => {
+        const childOption = options.find(o => o.type === 'child' && o.option.value === child.value);
+        if (childOption) {
+          wrapped.children.push({ type: 'child', option: childOption, index });
+          attachedChildren.add(childOption);
+        }
+      });
       nestedOptions.push(wrapped);
-    } else if (!option.type || option.type === 'child') {
-      (currentParent?.children ?? nestedOptions).push({ type: 'child', option, index });
-    } else {
-      currentParent = undefined;
+    } else if (!attachedChildren.has(option)) {
       nestedOptions.push({ type: 'child', option, index });
     }
   });
