ed47f80b17e395587106937cf7033748b8dc8f6e
Joan Perals Tresserra <jotresse@amazon.com>
2025-11-20 12:42:57 +0100

    fix: Restore legacy test util selectors for Radio Group

--- src/radio-group/internal.tsx
+++ src/radio-group/internal.tsx
@@ -58,6 +58,7 @@ const InternalRadioGroup = React.forwardRef(
         className={clsx(
           baseProps.className,
           testUtilStyles.root,
+          styles.root,
           styles['radio-group'],
           direction === 'horizontal' && styles['horizontal-group']
         )}
