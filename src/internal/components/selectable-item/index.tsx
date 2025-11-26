22e18c38cdf1e79c18e6901da89941e099af9674
Simon Brebeck <sbrebeck@amazon.de>
2025-11-26 16:31:15 +0100

    feat: change disableContentPadding to disableContentStyling

--- src/internal/components/selectable-item/index.tsx
+++ src/internal/components/selectable-item/index.tsx
@@ -29,7 +29,7 @@ const SelectableItem = (
     isSelectAll,
     virtualPosition,
     padBottom,
-    disableContentPaddings,
+    disableContentStyling,
     isNextSelected,
     isPreviousSelected,
     useInteractiveGroups,
@@ -69,7 +69,7 @@ const SelectableItem = (
   });
 
   const contentClassNames = clsx(styles['option-content'], analyticsSelectors['option-content'], {
-    [styles['no-content-padding']]: !!disableContentPaddings,
+    [styles['no-content-styling']]: !!disableContentStyling,
   });
 
   const contentRef = useRef<HTMLDivElement>(null);
