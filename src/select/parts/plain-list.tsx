ae894995515df4e3b33cc02128ea020c03504e23
Simon Brebeck <sbrebeck@amazon.de>
2025-11-27 10:10:21 +0100

    feat: arbitrary content for select, multiselect

--- src/select/parts/plain-list.tsx
+++ src/select/parts/plain-list.tsx
@@ -8,6 +8,7 @@ import { useMergeRefs, useUniqueId } from '@cloudscape-design/component-toolkit/
 import { DropdownOption } from '../../internal/components/option/interfaces';
 import OptionsList from '../../internal/components/options-list';
 import { HighlightType } from '../../internal/components/options-list/utils/use-highlight-option';
+import { MultiselectProps } from '../../multiselect/interfaces';
 import { SelectProps } from '../interfaces';
 import { renderOptions } from '../utils/render-options';
 import scrollToIndex from '../utils/scroll-to-index';
@@ -28,7 +29,7 @@ export interface SelectListProps {
   useInteractiveGroups?: boolean;
   screenReaderContent?: string;
   firstOptionSticky?: boolean;
-  renderOption?: (option: SelectProps.SelectOptionItem) => ReactNode;
+  renderOption?: (option: SelectProps.SelectOptionItem | MultiselectProps.MultiselectOptionItem) => ReactNode;
 }
 
 export namespace SelectListProps {
