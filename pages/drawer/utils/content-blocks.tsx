042a56f53fa1c7b9e850a4137c6c6dc1900a0843
Nathnael Dereje <natidere@amazon.de>
2025-11-28 06:44:06 +0100

    chore: Add test pages for drawer with footer

---
+++ pages/drawer/utils/content-blocks.tsx
@@ -0,0 +1,22 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import React from 'react';
+
+import BreadcrumbGroup from '~components/breadcrumb-group';
+import HelpPanel from '~components/help-panel';
+
+export function Breadcrumbs() {
+  return (
+    <BreadcrumbGroup
+      items={[
+        { text: 'Home', href: '#' },
+        { text: 'Service', href: '#' },
+      ]}
+    />
+  );
+}
+
+export function Tools({ children }: { children: React.ReactNode }) {
+  return <HelpPanel header={<h2>Overview</h2>}>{children}</HelpPanel>;
+}
