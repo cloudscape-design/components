b66bf8ccf373372f884b1f958ac1e51b1c6aaea2
Georgii Lobko <globko@amazon.de>
2025-11-28 14:46:16 +0100

    feat: Extended feature prompt component

--- src/feature-prompt/interfaces.ts
+++ src/feature-prompt/interfaces.ts
@@ -5,9 +5,23 @@ import { NonCancelableEventHandler } from '../internal/events';
 import { PopoverProps } from '../popover/interfaces';
 
 export interface FeaturePromptProps {
+  /**
+   * Determines whether the prompt is displayed.
+   *
+   * When you use ref.show() to display the prompt it will be shown regardless of the value in this field.
+   */
   visible: boolean;
 
-  onDismiss: NonCancelableEventHandler<null>;
+  /**
+   * Called when a user closes the modal by using the close icon button,
+   * clicking outside of the modal, or pressing ESC.
+   */
+  onDismiss?: NonCancelableEventHandler<null>;
+
+  /**
+   * Called when prompt focus is removed from the UI control.
+   */
+  onBlur?: NonCancelableEventHandler<null>;
 
   /**
    * Determines where the popover is displayed when opened, relative to the trigger.
@@ -50,4 +64,21 @@ export interface FeaturePromptProps {
   dismissAriaLabel?: string;
 }
 
-export namespace FeaturePromptProps {}
+export namespace FeaturePromptProps {
+  export interface Ref {
+    /**
+     * Sets focus on the prompt's close button.
+     */
+    focus(): void;
+
+    /**
+     * Dismisses the prompt without focusing the trigger. Use only if an element other than the trigger needs to be focused after dismissing the prompt.
+     */
+    dismiss(): void;
+
+    /**
+     * Shows the prompt and focuses its close button.
+     */
+    show(): void;
+  }
+}
