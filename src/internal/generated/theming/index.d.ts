69c542aca1badd5353defc55766f70d6dbe3803f
Jessica Kuelz <15003460+jkuelz@users.noreply.github.com>
2025-12-03 03:21:25 -0800

    feat: Add reference tokens to theming API in Core

--- src/internal/generated/theming/index.d.ts
+++ src/internal/generated/theming/index.d.ts
@@ -1,11 +1,20 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 /* This file is only for the compiler. The artifacts will contain the generated file instead. */
-import { ReferenceTokens } from '@cloudscape-design/theming-build';
-import { GlobalValue, Override, ThemePreset, TypedModeValueOverride } from '@cloudscape-design/theming-runtime';
+import {
+  GlobalValue,
+  Override,
+  ReferenceTokens,
+  ThemePreset,
+  TypedModeValueOverride,
+} from '@cloudscape-design/theming-runtime';
 
 export interface TypedOverride extends Override {
   tokens: Partial<Record<string, GlobalValue | TypedModeValueOverride>>;
+
+  /**
+   * @awsuiSystem core
+   */
   referenceTokens?: ReferenceTokens;
 }
 export declare const preset: ThemePreset;
