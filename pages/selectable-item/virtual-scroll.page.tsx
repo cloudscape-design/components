9c0a03a54d9552068cadeb3e0a31afa2f86537e8
Maximilian Schoell <schomax@amazon.de>
2025-12-04 11:45:11 +0100

    feat: switch between select types

--- pages/selectable-item/virtual-scroll.page.tsx
+++ pages/selectable-item/virtual-scroll.page.tsx
@@ -3,7 +3,7 @@
 
 import React, { useState } from 'react';
 
-import { Multiselect, MultiselectProps, SpaceBetween } from '~components';
+import { Multiselect, MultiselectProps, SegmentedControl, SpaceBetween } from '~components';
 import Autosuggest, { AutosuggestProps } from '~components/autosuggest';
 import Select, { SelectProps } from '~components/select';
 
@@ -20,13 +20,14 @@ const autosuggestOptions: AutosuggestProps.Options = Array.from({ length: 1000 }
 }));
 
 export default function () {
+  const [selectedType, setSelectedType] = useState('select');
   const [selected, setSelected] = useState<SelectProps['selectedOption']>(null);
   const [selectedMulti, setSelectedMulti] = useState<MultiselectProps.Options>(options.slice(0, 2));
   const [selectedMultiWithSelectAll, setSelectedMultiWithSelectAll] = useState<MultiselectProps.Options>([]);
   const [autosuggestValue, setAutosuggestValue] = useState('');
 
   return (
-    <SimplePage title="Virtual Scroll" i18n={{}}>
+    <SimplePage title="Virtual Scroll" i18n={{}} screenshotArea={{}}>
       <div
         style={{
           height: 500,
@@ -36,61 +37,80 @@ export default function () {
         }}
       >
         <SpaceBetween size="m">
-          <Select
-            placeholder="Select with virtual scroll"
-            selectedOption={selected}
-            options={options}
-            filteringType="auto"
-            finishedText="End of all results"
-            onChange={event => setSelected(event.detail.selectedOption)}
-            virtualScroll={true}
-            expandToViewport={false}
-            ariaLabel="select demo"
-            data-testid="select-demo"
+          <SegmentedControl
+            selectedId={selectedType}
+            onChange={({ detail }) => setSelectedType(detail.selectedId)}
+            options={[
+              { id: 'select', text: 'Select' },
+              { id: 'multiselect', text: 'Multiselect' },
+              { id: 'multiselect-select-all', text: 'Multiselect with Select All' },
+              { id: 'autosuggest', text: 'Autosuggest' },
+            ]}
           />
 
-          <Multiselect
-            placeholder="Multiselect with virtual scroll"
-            selectedOptions={selectedMulti}
-            options={options}
-            filteringType="manual"
-            finishedText="End of all results"
-            errorText="verylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspaces"
-            recoveryText="Retry"
-            onLoadItems={() => {}}
-            onChange={event => setSelectedMulti(event.detail.selectedOptions)}
-            tokenLimit={2}
-            virtualScroll={true}
-            expandToViewport={false}
-            ariaLabel="multiselect demo"
-            data-testid="multiselect-demo"
-          />
+          {selectedType === 'select' && (
+            <Select
+              placeholder="Select with virtual scroll"
+              selectedOption={selected}
+              options={options}
+              filteringType="auto"
+              finishedText="End of all results"
+              onChange={event => setSelected(event.detail.selectedOption)}
+              virtualScroll={true}
+              expandToViewport={false}
+              ariaLabel="select demo"
+              data-testid="select-demo"
+            />
+          )}
 
-          <Multiselect
-            placeholder="Multiselect with virtual scroll and select all"
-            selectedOptions={selectedMultiWithSelectAll}
-            options={options}
-            filteringType="auto"
-            finishedText="End of all results"
-            onChange={event => setSelectedMultiWithSelectAll(event.detail.selectedOptions)}
-            enableSelectAll={true}
-            virtualScroll={true}
-            expandToViewport={false}
-            ariaLabel="multiselect with select all demo"
-            data-testid="multiselect-select-all-demo"
-          />
+          {selectedType === 'multiselect' && (
+            <Multiselect
+              placeholder="Multiselect with virtual scroll"
+              selectedOptions={selectedMulti}
+              options={options}
+              filteringType="manual"
+              finishedText="End of all results"
+              errorText="verylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspacesverylongtextwithoutspaces"
+              recoveryText="Retry"
+              onLoadItems={() => {}}
+              onChange={event => setSelectedMulti(event.detail.selectedOptions)}
+              tokenLimit={2}
+              virtualScroll={true}
+              expandToViewport={false}
+              ariaLabel="multiselect demo"
+              data-testid="multiselect-demo"
+            />
+          )}
 
-          <Autosuggest
-            value={autosuggestValue}
-            options={autosuggestOptions}
-            onChange={event => setAutosuggestValue(event.detail.value)}
-            enteredTextLabel={value => `Use: "${value}"`}
-            placeholder="Autosuggest with virtual scroll"
-            ariaLabel="autosuggest demo"
-            virtualScroll={true}
-            expandToViewport={false}
-            data-testid="autosuggest-demo"
-          />
+          {selectedType === 'multiselect-select-all' && (
+            <Multiselect
+              placeholder="Multiselect with virtual scroll and select all"
+              selectedOptions={selectedMultiWithSelectAll}
+              options={options}
+              filteringType="auto"
+              finishedText="End of all results"
+              onChange={event => setSelectedMultiWithSelectAll(event.detail.selectedOptions)}
+              enableSelectAll={true}
+              virtualScroll={true}
+              expandToViewport={false}
+              ariaLabel="multiselect with select all demo"
+              data-testid="multiselect-select-all-demo"
+            />
+          )}
+
+          {selectedType === 'autosuggest' && (
+            <Autosuggest
+              value={autosuggestValue}
+              options={autosuggestOptions}
+              onChange={event => setAutosuggestValue(event.detail.value)}
+              enteredTextLabel={value => `Use: "${value}"`}
+              placeholder="Autosuggest with virtual scroll"
+              ariaLabel="autosuggest demo"
+              virtualScroll={true}
+              expandToViewport={false}
+              data-testid="autosuggest-demo"
+            />
+          )}
         </SpaceBetween>
       </div>
     </SimplePage>
