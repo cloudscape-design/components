009c20f0382c3d524f4304b5672830e1c47b1e8e
Simon Brebeck <sbrebeck@amazon.de>
2025-12-04 10:29:14 +0100

    test: move disable-content-styling prop to custom permutations page

---
+++ pages/selectable-item/disable-content-styling.permutations.tsx
@@ -0,0 +1,31 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React from 'react';
+
+import SelectableItem, { SelectableItemProps } from '~components/internal/components/selectable-item';
+import SpaceBetween from '~components/space-between';
+
+import createPermutations from '../utils/permutations';
+import PermutationsView from '../utils/permutations-view';
+import ScreenshotArea from '../utils/screenshot-area';
+import { permutationsConfigs } from './common';
+
+// Copy permutations and add selectable-items with no-content-styling.
+const permutations = createPermutations<SelectableItemProps>(
+  permutationsConfigs.map(permutation => ({ ...permutation, disableContentStyling: [true] }))
+);
+
+export default function InputPermutations() {
+  return (
+    <>
+      <h1>Selectable item permutations</h1>
+      <ScreenshotArea>
+        <SpaceBetween size="xs">
+          <ul role="listbox" aria-label="list">
+            <PermutationsView permutations={permutations} render={permutation => <SelectableItem {...permutation} />} />
+          </ul>
+        </SpaceBetween>
+      </ScreenshotArea>
+    </>
+  );
+}
