83500f358e430039483d95d7e511a5133155c5ee
Simon Brebeck <sbrebeck@amazon.de>
2025-12-04 18:54:10 +0100

    refactor: Apply padding to option-content-div instead of selectable-item outer-div (#4070)

--- pages/selectable-item/disable-content-styling.permutations.page.tsx
+++ pages/selectable-item/disable-content-styling.permutations.page.tsx
@@ -3,25 +3,26 @@
 import React from 'react';
 
 import SelectableItem, { SelectableItemProps } from '~components/internal/components/selectable-item';
-import SpaceBetween from '~components/space-between';
 
 import createPermutations from '../utils/permutations';
 import PermutationsView from '../utils/permutations-view';
 import ScreenshotArea from '../utils/screenshot-area';
 import { permutationsConfigs } from './common';
 
+// Copy permutations and add selectable-items with no-content-styling.
 const permutations = createPermutations<SelectableItemProps>(permutationsConfigs);
 
-export default function InputPermutations() {
+export default function SelectableItemPermutations() {
   return (
     <>
-      <h1>Selectable item permutations</h1>
+      <h1>Selectable item permutations with disableContentStyling</h1>
       <ScreenshotArea>
-        <SpaceBetween size="xs">
-          <ul role="listbox" aria-label="list">
-            <PermutationsView permutations={permutations} render={permutation => <SelectableItem {...permutation} />} />
-          </ul>
-        </SpaceBetween>
+        <ul role="listbox" aria-label="list">
+          <PermutationsView
+            permutations={permutations}
+            render={permutation => <SelectableItem {...permutation} disableContentStyling={true} />}
+          />
+        </ul>
       </ScreenshotArea>
     </>
   );
