50509ac61d65fa0a17982816c70cafb29efabdc6
Philipp Schneider <philosr@amazon.de>
2025-12-10 18:01:03 +0100

    fix: Fix testing for tooltip

--- src/test-utils/dom/internal/tooltip.ts
+++ src/test-utils/dom/internal/tooltip.ts
@@ -1,9 +1,20 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { ElementWrapper } from '@cloudscape-design/test-utils-core/dom';
+import { ComponentWrapper, ElementWrapper } from '@cloudscape-design/test-utils-core/dom';
 
+import createWrapper from '../index.js';
+
+import popoverStyles from '../../../popover/styles.selectors.js';
 import tooltipStyles from '../../../tooltip/styles.selectors.js';
 
-export default class TooltipWrapper extends ElementWrapper {
+export default class TooltipWrapper extends ComponentWrapper {
   static rootSelector: string = tooltipStyles.root;
+
+  /**
+   * Returns the tooltip content element.
+   * Since tooltips are rendered through a Portal, this searches from document root.
+   */
+  findContent(): ElementWrapper {
+    return createWrapper().findByClassName(popoverStyles.content)!;
+  }
 }
