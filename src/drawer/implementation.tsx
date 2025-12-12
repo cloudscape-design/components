a26408b038f2c1d0c72adf13cc2becd4f366f2e2
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2025-12-12 14:47:21 +0100

    chore: Drawer sticky footer property (#4051)
    Co-authored-by: Georgii Lobko <47106899+georgylobko@users.noreply.github.com>
    Co-authored-by: Andrei Zhaleznichenka <zhalea@amazon.de>
    Co-authored-by: Joan Perals <jotresse@amazon.com>

--- src/drawer/implementation.tsx
+++ src/drawer/implementation.tsx
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import React, { RefObject } from 'react';
+import React, { RefObject, useRef } from 'react';
 import clsx from 'clsx';
 
 import { useRuntimeDrawerContext } from '../app-layout/runtime-drawer/use-runtime-drawer-context';
@@ -12,6 +12,7 @@ import { createWidgetizedComponent } from '../internal/widgets';
 import InternalLiveRegion from '../live-region/internal';
 import InternalStatusIndicator from '../status-indicator/internal';
 import { DrawerProps } from './interfaces';
+import { useStickyFooter } from './use-sticky-footer';
 
 import styles from './styles.css.js';
 
@@ -19,6 +20,7 @@ type DrawerInternalProps = DrawerProps & InternalBaseComponentProps;
 
 export function DrawerImplementation({
   header,
+  footer,
   children,
   loading,
   i18nStrings,
@@ -32,11 +34,21 @@ export function DrawerImplementation({
   const i18n = useInternalI18n('drawer');
   const containerProps = {
     ...baseProps,
-    className: clsx(baseProps.className, styles.drawer, isToolbar && styles['with-toolbar']),
+    className: clsx(
+      baseProps.className,
+      styles.drawer,
+      isToolbar && styles['with-toolbar'],
+      !!footer && styles['with-footer']
+    ),
   };
+  const footerRef = useRef<HTMLDivElement>(null);
 
   const runtimeDrawerContext = useRuntimeDrawerContext({ rootRef: __internalRootRef as RefObject<HTMLElement> });
   const hasAdditioalDrawerAction = !!runtimeDrawerContext?.isExpandable;
+  const { isSticky: isFooterSticky } = useStickyFooter({
+    drawerRef: __internalRootRef as RefObject<HTMLElement>,
+    footerRef,
+  });
 
   return loading ? (
     <div
@@ -67,11 +79,22 @@ export function DrawerImplementation({
       <div
         className={clsx(
           styles['test-utils-drawer-content'],
+          styles.content,
           !disableContentPaddings && styles['content-with-paddings']
         )}
       >
         {children}
       </div>
+      {footer && (
+        <div
+          ref={footerRef}
+          className={clsx(styles.footer, {
+            [styles['is-sticky']]: isFooterSticky,
+          })}
+        >
+          {footer}
+        </div>
+      )}
     </div>
   );
 }
