ca1423275c18eebf2e7f6edb70dcc9128f91afbf
Cansu Aksu <cansua@amazon.com>
2025-12-29 13:02:41 +0100

    remove static width

--- pages/attribute-editor/select-with-long-value.page.tsx
+++ pages/attribute-editor/select-with-long-value.page.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 import React from 'react';
 
-import { Box, Container, Input, Select, SpaceBetween } from '~components';
+import { Box, Input, Select } from '~components';
 import AttributeEditor from '~components/attribute-editor';
 
 import ScreenshotArea from '../utils/screenshot-area';
@@ -23,62 +23,51 @@ const shortOptions = [
   },
 ];
 
-function UsedAttributeEditor() {
+export default function AttributeEditorSelect() {
   const [items, setItems] = React.useState([
     {
       key: 'some-key-1',
+      value: 'some-value-1',
       longSelect: longOptions[0],
       shortSelect: shortOptions[0],
     },
     {
       key: 'some-key-2',
+      value: 'some-value-2',
       longSelect: longOptions[1],
       shortSelect: shortOptions[1],
     },
   ]);
 
   return (
-    <Container>
-      <AttributeEditor
-        items={items}
-        hideAddButton={true}
-        onRemoveButtonClick={({ detail: { itemIndex } }) => {
-          const tmpItems = [...items];
-          tmpItems.splice(itemIndex, 1);
-          setItems(tmpItems);
-        }}
-        addButtonText="Add new item"
-        removeButtonText="Remove"
-        definition={[
-          {
-            label: 'Key',
-            control: item => <Input value={item.key} placeholder="Enter key" />,
-          },
-          {
-            label: 'Long select',
-            control: item => <Select selectedOption={item.longSelect} options={longOptions} />,
-          },
-          {
-            label: 'Short select',
-            control: item => <Select selectedOption={item.shortSelect} options={shortOptions} />,
-          },
-        ]}
-      />
-    </Container>
-  );
-}
-
-export default function AttributeEditorSelect() {
-  return (
     <Box margin="xl">
       <h1>Attribute Editor - Long select</h1>
       <ScreenshotArea>
-        <SpaceBetween direction="horizontal" size="m">
-          <div style={{ width: '400px' }}>
-            <UsedAttributeEditor />
-          </div>
-          <UsedAttributeEditor />
-        </SpaceBetween>
+        <AttributeEditor
+          items={items}
+          hideAddButton={true}
+          onRemoveButtonClick={({ detail: { itemIndex } }) => {
+            const tmpItems = [...items];
+            tmpItems.splice(itemIndex, 1);
+            setItems(tmpItems);
+          }}
+          addButtonText="Add new item"
+          removeButtonText="Remove"
+          definition={[
+            {
+              label: 'Key',
+              control: item => <Input value={item.key} placeholder="Enter key" />,
+            },
+            {
+              label: 'Long select',
+              control: item => <Select selectedOption={item.longSelect} options={longOptions} />,
+            },
+            {
+              label: 'Short select',
+              control: item => <Select selectedOption={item.shortSelect} options={shortOptions} />,
+            },
+          ]}
+        />
       </ScreenshotArea>
     </Box>
   );
