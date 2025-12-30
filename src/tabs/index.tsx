8c3783437c9b4e3b82de59a2bd32a78ebb54fde7
Andrei Zhaleznichenka <zhalea@amazon.de>
2025-12-30 20:35:02 +0100

    feat: Error boundary component (#4139)

--- src/tabs/index.tsx
+++ src/tabs/index.tsx
@@ -167,6 +167,7 @@ export default function Tabs({
         {...baseProps}
         className={clsx(baseProps.className, styles.root)}
         __internalRootRef={__internalRootRef}
+        __contentKey={activeTabId}
         disableContentPaddings={true}
         variant={variant === 'stacked' ? 'stacked' : 'default'}
         fitHeight={fitHeight}
