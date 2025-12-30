cb18531cb3dd54a88b252a68e938592b60a49d6a
Amanuel Sisay <amanabiy@amazon.de>
2025-12-30 14:54:21 +0100

    chore: clean up unused files

--- pages/selectable-item/common.tsx
+++ pages/selectable-item/common.tsx
@@ -2,10 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 import React from 'react';
 
+import { ComponentPermutations } from '@cloudscape-design/build-tools/src/test-pages-util';
+
 import Option from '~components/internal/components/option';
 import { SelectableItemProps } from '~components/internal/components/selectable-item';
-
-import { ComponentPermutations } from '../utils/permutations';
 
 const simpleOption = <Option option={{ value: 'option 1', label: 'Label 1' }} />;
 const iconOption = <Option option={{ value: 'option 1', label: 'Label 1', iconName: 'lock-private' }} />;
