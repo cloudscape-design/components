6a36bda0e857b9c80de6b522535b8e804a06e51b
Philipp Schneider <philosr@amazon.de>
2026-01-12 13:22:08 +0100

    fix: Add missing import for select component

--- src/select/parts/item.tsx
+++ src/select/parts/item.tsx
@@ -14,6 +14,7 @@ import { HighlightType } from '../../internal/components/options-list/utils/use-
 import SelectableItem from '../../internal/components/selectable-item';
 import useHiddenDescription from '../../internal/hooks/use-hidden-description';
 import Tooltip from '../../tooltip/internal.js';
+import { SelectProps } from '../interfaces.js';
 
 import styles from './styles.css.js';
 
