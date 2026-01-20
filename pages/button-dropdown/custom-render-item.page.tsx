bd7b1b85af1be39a69b5467d7d14a76c497f6da0
Simon Brebeck <sbrebeck@amazon.de>
2026-01-20 10:41:56 +0100

    fix: Add dev page example for renderItem in button dropdown

--- pages/button-dropdown/custom-render-item.page.tsx
+++ pages/button-dropdown/custom-render-item.page.tsx
@@ -2,56 +2,54 @@
 // SPDX-License-Identifier: Apache-2.0
 import * as React from 'react';
 
-import { Icon } from '~components';
 import ButtonDropdown, { ButtonDropdownProps } from '~components/button-dropdown';
 
 import { SimplePage } from '../app/templates';
 
-const withExpandedGroups: ButtonDropdownProps['items'] = [
+const itemsWithGroups: ButtonDropdownProps['items'] = [
   {
-    id: 'connect',
-    text: 'Connect',
-    secondaryText: 'This is the Connect option',
-    labelTag: 'Ctrl + C',
+    id: 'item',
+    text: 'Default Item',
+    secondaryText: 'Secondary Text',
+    labelTag: 'Label Tag',
   },
   {
-    id: 'password',
-    text: 'Get password',
-    secondaryText: 'This is the Get password option',
+    id: 'checked-checkbox',
+    text: 'Checked Checkbox',
+    secondaryText: 'Secondary Text',
+    labelTag: 'Label Tag',
+    itemType: 'checkbox',
+    checked: true,
   },
   {
-    id: 'states',
-    text: 'Instance State',
+    id: 'unchecked-checkbox',
+    text: 'Unchecked Checkbox',
+    secondaryText: 'Secondary Text',
+    labelTag: 'Label Tag',
+    itemType: 'checkbox',
+    checked: false,
+  },
+  {
+    id: 'group',
+    text: 'Simple Group',
     items: [
       {
-        id: 'start',
-        text: 'Start',
+        id: 'option',
+        text: 'Option',
       },
       {
-        id: 'stop',
-        text: 'Stop',
+        id: 'disabled-option',
+        text: 'Disabled Option',
         disabled: true,
       },
       {
-        id: 'hibernate',
-        text: 'Hibernate',
-        disabled: true,
+        id: 'checked-option',
+        text: 'Option Checkbox',
+        checked: true,
       },
       {
-        id: 'reboot',
-        text: 'Reboot',
-        secondaryText: 'This is the Reboot Option',
-        labelTag: 'Ctrl + B',
-        disabled: true,
-      },
-      {
-        id: 'terminate',
-        text: 'Terminate',
-        secondaryText: 'This is the Terminate Option',
-      },
-      {
-        id: 'external',
-        text: 'Root Page',
+        id: 'external-option',
+        text: 'External Option',
         secondaryText: '',
         labelTag: 'Ctrl + P',
         external: true,
@@ -64,84 +62,30 @@ const withExpandedGroups: ButtonDropdownProps['items'] = [
 export default function ButtonDropdownPage() {
   const renderItem: ButtonDropdownProps.ItemRenderer = ({ item }) => {
     if (item.type === 'group') {
-      return (
-        <div
-          style={{
-            padding: '4px 8px',
-            fontWeight: 'bold',
-            display: 'flex',
-            justifyContent: 'space-between',
-            width: '100%',
-          }}
-        >
-          <span>
-            {item.index}. {item.option.text}
-          </span>
-          <div
-            style={{
-              transform: item.expanded ? 'rotate(180deg)' : 'rotate(0deg)',
-              transition: 'transform 80ms linear',
-            }}
-          >
-            {item.expandDirection === 'vertical' ? (
-              <Icon name="caret-down-filled" />
-            ) : (
-              <Icon name="caret-right-filled" />
-            )}
-          </div>
-        </div>
-      );
+      return <div>Group: {item.option.text}</div>;
     } else if (item.type === 'checkbox') {
-      return (
-        <div style={{ padding: '4px 8px' }}>
-          {item.index}. {item.option.text}
-        </div>
-      );
+      return <div>Checkbox: {item.option.text}</div>;
     } else {
-      return (
-        <div
-          style={{
-            padding: '4px 4px 4px 8px',
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            gap: '8px',
-          }}
-        >
-          <span
-            style={{
-              color: item.disabled ? '#999' : 'inherit',
-            }}
-          >
-            {item.parent
-              ? item.parent.index + '.' + item.index + '. ' + item.option.text
-              : item.index + '. ' + item.option.text}
-          </span>
-          {item.option.id === 'connect' || item.option.id === 'hibernate' ? (
-            <div
-              style={{
-                background: '#ff9900',
-                borderRadius: '4px',
-                color: '#252f3e',
-                fontStyle: 'italic',
-                fontSize: '10px',
-                fontWeight: 'bold',
-                paddingInline: '3px',
-              }}
-            >
-              NEW
-            </div>
-          ) : null}
-        </div>
-      );
+      return <div>Item: {item.option.text}</div>;
     }
   };
   return (
-    <SimplePage title="Button Dropdown with custom render option" screenshotArea={{}}>
-      <ButtonDropdown id="ButtonDropdown3" items={withExpandedGroups} expandableGroups={true} renderItem={renderItem}>
-        Button Action
-      </ButtonDropdown>
+    <SimplePage
+      title="Button Dropdown with custom item renderer"
+      screenshotArea={{
+        style: {
+          padding: 10,
+        },
+      }}
+    >
+      <div style={{ maxInlineSize: '400px', blockSize: '650px' }}>
+        <ButtonDropdown items={itemsWithGroups} renderItem={renderItem}>
+          Default Groups
+        </ButtonDropdown>
+        <ButtonDropdown items={itemsWithGroups} expandableGroups={true} renderItem={renderItem}>
+          Expandable Groups
+        </ButtonDropdown>
+      </div>
     </SimplePage>
   );
 }
