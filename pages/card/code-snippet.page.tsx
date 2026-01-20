5754d1451be3fbf8ba4b1f9ce87122f6c0189bc7
Joan Perals Tresserra <jotresse@amazon.com>
2026-01-20 10:05:43 +0100

    Add code snippet page

---
+++ pages/card/code-snippet.page.tsx
@@ -0,0 +1,44 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React from 'react';
+
+import Box from '~components/box';
+import CopyToClipboard from '~components/copy-to-clipboard';
+import Card from '~components/internal/components/card';
+
+import ScreenshotArea from '../utils/screenshot-area';
+
+export default function ButtonsScenario() {
+  return (
+    <article>
+      <h1>Code snippet</h1>
+      <ScreenshotArea>
+        <Card
+          header="Python"
+          actions={
+            <CopyToClipboard
+              variant="icon"
+              textToCopy="def lambda_handler(event, context):
+bucket = event['Records'][0]['s3']['bucket']['name']
+key = event['Records'][0]['s3']['object']['key']
+print(f'New file uploaded: {key} in bucket {bucket}')
+return {'statusCode': 200}"
+              copySuccessText="Code copied"
+              copyErrorText="Error while copying text"
+            />
+          }
+        >
+          <Box variant="code">
+            <pre style={{ backgroundColor: '#f8f8f8', borderRadius: 8, margin: 0, padding: 8 }}>
+              {`def lambda_handler(event, context):
+  bucket = event['Records'][0]['s3']['bucket']['name']
+  key = event['Records'][0]['s3']['object']['key']
+  print(f'New file uploaded: {key} in bucket {bucket}')
+  return {'statusCode': 200}`}
+            </pre>
+          </Box>
+        </Card>
+      </ScreenshotArea>
+    </article>
+  );
+}
