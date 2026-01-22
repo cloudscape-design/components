98b1fd68f8a5c27b329fb41cccb47ee86bf940ad
Philipp Schneider <philosr@amazon.de>
2026-01-22 12:58:43 +0100

    fix: Restore old docs for internal tooltip

--- src/test-utils/dom/internal/tooltip.ts
+++ src/test-utils/dom/internal/tooltip.ts
@@ -1,20 +1,9 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { ComponentWrapper, ElementWrapper } from '@cloudscape-design/test-utils-core/dom';
+import { ElementWrapper } from '@cloudscape-design/test-utils-core/dom';
 
-import createWrapper from '../index.js';
+import tooltipStyles from '../../../internal/components/tooltip/styles.selectors.js';
 
-import popoverStyles from '../../../popover/styles.selectors.js';
-import tooltipStyles from '../../../tooltip/test-classes/styles.selectors.js';
-
-export default class TooltipWrapper extends ComponentWrapper {
+export default class TooltipWrapper extends ElementWrapper {
   static rootSelector: string = tooltipStyles.root;
-
-  /**
-   * Returns the tooltip content element.
-   * Since tooltips are rendered through a Portal, this searches from document root.
-   */
-  findContent(): ElementWrapper {
-    return createWrapper().findByClassName(popoverStyles.content)!;
-  }
 }
