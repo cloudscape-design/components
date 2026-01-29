eeb31edfca7b622a6e909e4a5db61981ff124045
Joan Perals Tresserra <jotresse@amazon.com>
2026-01-29 18:49:40 +0100

    Fix card width

--- src/cards/index.tsx
+++ src/cards/index.tsx
@@ -375,7 +375,14 @@ const CardsList = <T,>({
               }
             >
               {visibleSectionsDefinition.map(({ width = 100, header, content, id }, index) => (
-                <div key={id || index} className={styles.section} style={{ width: `${width}%` }}>
+                <div
+                  key={id || index}
+                  className={clsx(
+                    styles.section,
+                    index === 0 && !!cardDefinition.header && styles['section-after-header']
+                  )}
+                  style={{ width: `${width}%` }}
+                >
                   {header ? <div className={styles['section-header']}>{header}</div> : ''}
                   {content ? <div className={styles['section-content']}>{content(item)}</div> : ''}
                 </div>
