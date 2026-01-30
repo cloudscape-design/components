dc880c197defc309837eb0bd6d6000d5cf32c310
Amanuel Abiy <amanabiy@amazon.de>
2026-01-30 16:20:10 +0100

    refactor: use visual context for flash variant styling instead of design tokens (#4214)

--- style-dictionary/classic/colors.ts
+++ style-dictionary/classic/colors.ts
@@ -42,7 +42,6 @@ const tokens: StyleDictionary.ColorsDictionary = {
   colorBackgroundNotificationStackBar: '{colorNeutral750}',
   colorBackgroundNotificationStackBarActive: '{colorNeutral750}',
   colorBackgroundNotificationStackBarHover: '{colorNeutral650}',
-  colorBackgroundProgressBarValueInFlash: { light: '{colorGreyOpaque70}', dark: '{colorNeutral100}' },
   colorBackgroundSegmentHover: '{colorBackgroundButtonNormalHover}',
   colorBackgroundTilesDisabled: { dark: '{colorNeutral750}' },
   colorBackgroundToggleCheckedDisabled: { dark: '{colorPrimary700}' },
