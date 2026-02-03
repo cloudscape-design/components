88f4aa6608c7ff9208fd11501c682ddf4d77310d
Georgii Lobko <globko@amazon.de>
2026-02-03 20:00:47 +0100

    chore: Expose public plugins

---
+++ src/plugins/index.ts
@@ -0,0 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+'use client';
+export * from '../internal/plugins/widget/interfaces';
+export { registerFeatureNotifications, showFeaturePromptIfPossible } from '../internal/plugins/widget/index';
