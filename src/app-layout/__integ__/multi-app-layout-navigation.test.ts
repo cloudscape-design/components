8c1cbe64fde5f0707e300ab0da673ac9875e4bd1
Andrei Zhaleznichenka <zhalea@amazon.de>
2026-02-16 11:38:23 +0100

    2nd attempt

--- src/app-layout/__integ__/multi-app-layout-navigation.test.ts
+++ src/app-layout/__integ__/multi-app-layout-navigation.test.ts
@@ -18,43 +18,43 @@ class PageObject extends BasePageObject {
   }
 }
 
-describe('Multi app layout navigation', () => {
-  describe.each([[true], [false]])('iframe=%s', iframe => {
-    const mainLayout = createWrapper().find('[data-testid="main-layout"]').findAppLayout();
-    const secondaryLayout = createWrapper().find('[data-testid="secondary-layout"]').findAppLayout();
-    const setupTest = (testFn: (page: PageObject) => Promise<void>) =>
-      useBrowser(async browser => {
-        const page = new PageObject(browser);
-        await browser.url(
-          `#/light/app-layout/multi-layout-with-hidden-instances${iframe ? '-iframe' : ''}?${getUrlParams(theme)}`
-        );
-        await testFn(page);
-      });
-
-    test(
-      'should clean up and restore previous route state',
-      setupTest(async page => {
-        await page.click(mainLayout.findToolsToggle().toSelector());
-        await expect(page.isToolsToggleActive()).resolves.toBe(true);
-        await page.runInsideIframe('#page1', !!iframe, async () => {
-          await expect(page.isClickable(secondaryLayout.findTools().toSelector())).resolves.toEqual(true);
+for (let i = 0; i < 100; i++) {
+  describe('Multi app layout navigation', () => {
+    describe.each([[true], [false]])('iframe=%s', iframe => {
+      const mainLayout = createWrapper().find('[data-testid="main-layout"]').findAppLayout();
+      const secondaryLayout = createWrapper().find('[data-testid="secondary-layout"]').findAppLayout();
+      const setupTest = (testFn: (page: PageObject) => Promise<void>) =>
+        useBrowser(async browser => {
+          const page = new PageObject(browser);
+          await browser.url(
+            `#/light/app-layout/multi-layout-with-hidden-instances${iframe ? '-iframe' : ''}?${getUrlParams(theme)}`
+          );
+          await testFn(page);
         });
 
-        await page.clickHref('page2');
-        await expect(page.isToolsToggleActive()).resolves.toBe(false);
-        await page.runInsideIframe('#page2', !!iframe, async () => {
-          await expect(page.isClickable(secondaryLayout.findTools().toSelector())).resolves.toEqual(false);
-        });
+      test(
+        'should clean up and restore previous route state',
+        setupTest(async page => {
+          await page.click(mainLayout.findToolsToggle().toSelector());
+          await expect(page.isToolsToggleActive()).resolves.toBe(true);
+          await page.runInsideIframe('#page1', !!iframe, async () => {
+            await expect(page.isClickable(secondaryLayout.findTools().toSelector())).resolves.toEqual(true);
+          });
 
-        await page.clickHref('page1');
-        await expect(page.isToolsToggleActive()).resolves.toBe(true);
-        await page.runInsideIframe('#page1', !!iframe, async () => {
-          await expect(page.isClickable(secondaryLayout.findTools().toSelector())).resolves.toEqual(true);
-        });
-      })
-    );
+          await page.clickHref('page2');
+          await expect(page.isToolsToggleActive()).resolves.toBe(false);
+          await page.runInsideIframe('#page2', !!iframe, async () => {
+            await expect(page.isClickable(secondaryLayout.findTools().toSelector())).resolves.toEqual(false);
+          });
 
-    for (let i = 0; i < 100; i++) {
+          await page.clickHref('page1');
+          await expect(page.isToolsToggleActive()).resolves.toBe(true);
+          await page.runInsideIframe('#page1', !!iframe, async () => {
+            await expect(page.isClickable(secondaryLayout.findTools().toSelector())).resolves.toEqual(true);
+          });
+        })
+      );
+
       testIf(iframe)(
         `should clean up and restore previous breadcrumb state, specific for a page, ${i}`,
         setupTest(async page => {
@@ -71,6 +71,6 @@ describe('Multi app layout navigation', () => {
           await expect(page.getText(mainLayout.findBreadcrumbs().toSelector())).resolves.toContain('page1');
         })
       );
-    }
+    });
   });
-});
+}
