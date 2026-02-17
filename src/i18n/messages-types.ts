aee203cfcce966a5387af776053b154c62e213f3
Michelle Nguyen <nguyndw@amazon.com>
2026-02-16 17:11:35 -0800

    chore: add loading text for jump to page, remove default fallbacks for i18n, track popover visibility from error state

--- src/i18n/messages-types.ts
+++ src/i18n/messages-types.ts
@@ -227,15 +227,12 @@ export interface I18nFormatArgTypes {
     "i18nStrings.loadingText": never;
   }
   "error-boundary": {
-    "i18nStrings.headerText": never;
-    "i18nStrings.descriptionText": {
-      "hasFeedback": string;
-    }
-    "i18nStrings.refreshActionText": never;
-  }
-  "features-notification-drawer": {
-    "i18nStrings.title": never;
-    "i18nStrings.viewAll": never;
+    "i18nStrings.headerText"?: never;
+    "i18nStrings.descriptionText"?: {
+      hasFeedback: boolean;
+      Feedback: (chunks: React.ReactNode[]) => React.ReactNode; 
+    };
+    "i18nStrings.refreshActionText"?: never;
   }
   "file-token-group": {
     "i18nStrings.limitShowFewer": never;
