082097710adb78c0d994fb266b538fba7e86ccdc
Avinash Dwarapu <dwaraa@amazon.com>
2026-02-26 11:21:52 +0100

    Ensure AppLayout also receives remote aria labels.

--- src/app-layout/visual-refresh-toolbar/skeleton/index.tsx
+++ src/app-layout/visual-refresh-toolbar/skeleton/index.tsx
@@ -7,7 +7,6 @@ import { getAnalyticsMetadataAttribute } from '@cloudscape-design/component-tool
 
 import { GeneratedAnalyticsMetadataAppLayoutToolbarComponent } from '../../../app-layout-toolbar/analytics-metadata/interfaces';
 import { BuiltInErrorBoundary } from '../../../error-boundary/internal';
-import RemoteI18nProvider from '../../../i18n/providers/remote-provider';
 import VisualContext from '../../../internal/components/visual-context';
 import customCssProps from '../../../internal/generated/custom-css-properties';
 import { AppLayoutInternalProps, AppLayoutPendingState } from '../interfaces';
@@ -16,7 +15,6 @@ import {
   AppLayoutBeforeMainSlot,
   AppLayoutBottomContentSlot,
   AppLayoutTopContentSlot,
-  loadFormatter,
 } from '../internal';
 import { isWidgetReady } from '../state/invariants';
 import { ToolbarProps } from '../toolbar';
@@ -66,41 +64,39 @@ export const SkeletonLayout = ({
 
   return (
     <VisualContext contextName="app-layout-toolbar">
-      <RemoteI18nProvider loadFormatter={loadFormatter}>
-        <div
-          {...getAnalyticsMetadataAttribute({ component: componentAnalyticsMetadata })}
-          ref={appLayoutState.rootRef as React.Ref<HTMLDivElement>}
-          data-awsui-app-layout-widget-loaded={isWidgetLoaded}
-          {...wrapperElAttributes}
-          className={wrapperElAttributes?.className ?? clsx(styles.root, testutilStyles.root)}
-          style={
-            wrapperElAttributes?.style ?? {
-              blockSize: `calc(100vh - ${appLayoutProps.placement.insetBlockStart + appLayoutProps.placement.insetBlockEnd}px)`,
-              [customCssProps.navigationWidth]: `${navigationWidth}px`,
-            }
+      <div
+        {...getAnalyticsMetadataAttribute({ component: componentAnalyticsMetadata })}
+        ref={appLayoutState.rootRef as React.Ref<HTMLDivElement>}
+        data-awsui-app-layout-widget-loaded={isWidgetLoaded}
+        {...wrapperElAttributes}
+        className={wrapperElAttributes?.className ?? clsx(styles.root, testutilStyles.root)}
+        style={
+          wrapperElAttributes?.style ?? {
+            blockSize: `calc(100vh - ${appLayoutProps.placement.insetBlockStart + appLayoutProps.placement.insetBlockEnd}px)`,
+            [customCssProps.navigationWidth]: `${navigationWidth}px`,
           }
-        >
-          <AppLayoutBeforeMainSlot {...mergedProps} />
-          <main {...mainElAttributes} className={mainElAttributes?.className ?? styles['main-landmark']}>
-            <AppLayoutTopContentSlot {...mergedProps} />
-            <div
-              {...contentWrapperElAttributes}
-              className={
-                contentWrapperElAttributes?.className ??
-                clsx(styles.main, { [styles['main-disable-paddings']]: appLayoutProps.disableContentPaddings })
-              }
-            >
-              {contentHeader && <div {...contentHeaderElAttributes}>{contentHeader}</div>}
-              {/* delay rendering the content until registration of this instance is complete */}
-              <div {...contentElAttributes} className={contentElAttributes?.className ?? testutilStyles.content}>
-                {registered ? <BuiltInErrorBoundary>{content}</BuiltInErrorBoundary> : null}
-              </div>
+        }
+      >
+        <AppLayoutBeforeMainSlot {...mergedProps} />
+        <main {...mainElAttributes} className={mainElAttributes?.className ?? styles['main-landmark']}>
+          <AppLayoutTopContentSlot {...mergedProps} />
+          <div
+            {...contentWrapperElAttributes}
+            className={
+              contentWrapperElAttributes?.className ??
+              clsx(styles.main, { [styles['main-disable-paddings']]: appLayoutProps.disableContentPaddings })
+            }
+          >
+            {contentHeader && <div {...contentHeaderElAttributes}>{contentHeader}</div>}
+            {/*delay rendering the content until registration of this instance is complete*/}
+            <div {...contentElAttributes} className={contentElAttributes?.className ?? testutilStyles.content}>
+              {registered ? <BuiltInErrorBoundary>{content}</BuiltInErrorBoundary> : null}
             </div>
-            <AppLayoutBottomContentSlot {...mergedProps} />
-          </main>
-          <AppLayoutAfterMainSlot {...mergedProps} />
-        </div>
-      </RemoteI18nProvider>
+          </div>
+          <AppLayoutBottomContentSlot {...mergedProps} />
+        </main>
+        <AppLayoutAfterMainSlot {...mergedProps} />
+      </div>
     </VisualContext>
   );
 };
