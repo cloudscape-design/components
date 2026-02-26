242cd1a95b46bc071964300d3ff4eeaf4ce34a44
Simon Brebeck <sbrebeck@amazon.de>
2026-02-26 13:04:10 +0100

    fix: incorrect import of component and its props

--- pages/copy-to-clipboard/permutations.page.tsx
+++ pages/copy-to-clipboard/permutations.page.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 import React from 'react';
 
-import { type CopyToClipboard, CopyToClipboardProps } from '~components';
+import { CopyToClipboard, type CopyToClipboardProps } from '~components';
 
 import createPermutations from '../utils/permutations';
 import PermutationsView from '../utils/permutations-view';
