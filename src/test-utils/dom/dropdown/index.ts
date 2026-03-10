3f2218033f82e4571b8dbe0bb49683e06057e494
Maximilian Schoell <schomax@amazon.de>
2026-03-10 23:20:41 +0100

    feat: add test utils

--- src/test-utils/dom/dropdown/index.ts
+++ src/test-utils/dom/dropdown/index.ts
@@ -1,9 +1,68 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { ComponentWrapper } from '@cloudscape-design/test-utils-core/dom';
+import { ComponentWrapper, createWrapper, ElementWrapper, usesDom } from '@cloudscape-design/test-utils-core/dom';
+import { act } from '@cloudscape-design/test-utils-core/utils-dom';
 
 import styles from '../../../dropdown/styles.selectors.js';
+import testutilStyles from '../../../dropdown/test-classes/styles.selectors.js';
 
-export default class DropdownComponentWrapper extends ComponentWrapper {
+export class DropdownContentWrapper extends ComponentWrapper {
+  findOpenDropdown(): ElementWrapper | null {
+    return this.find(`.${styles.dropdown}[data-open=true]`);
+  }
+
+  findContent(): ElementWrapper | null {
+    return this.findOpenDropdown()?.findByClassName(styles['dropdown-content']) ?? null;
+  }
+
+  findHeader(): ElementWrapper | null {
+    return this.findOpenDropdown()?.findByClassName(testutilStyles.header) ?? null;
+  }
+
+  findFooter(): ElementWrapper | null {
+    return this.findOpenDropdown()?.findByClassName(testutilStyles.footer) ?? null;
+  }
+}
+
+class PortalDropdownContentWrapper extends DropdownContentWrapper {
+  findOpenDropdown(): ElementWrapper | null {
+    return createWrapper().find(`.${styles.dropdown}[data-open=true]`);
+  }
+}
+
+export default class DropdownWrapper extends ComponentWrapper {
   static rootSelector: string = styles.root;
+
+  findTrigger(): ElementWrapper {
+    return this.findByClassName(testutilStyles.trigger)!;
+  }
+
+  /**
+   * @param options
+   * * expandToViewport (boolean) - Use this when the component under test is rendered with an `expandToViewport` flag.
+   */
+  findDropdown(options = { expandToViewport: false }): DropdownContentWrapper {
+    return options.expandToViewport
+      ? createWrapper().findComponent(`.${styles.dropdown}[data-open=true]`, PortalDropdownContentWrapper)!
+      : new DropdownContentWrapper(this.getElement());
+  }
+
+  findContent(options = { expandToViewport: false }): ElementWrapper | null {
+    return this.findDropdown(options).findContent();
+  }
+
+  findHeader(options = { expandToViewport: false }): ElementWrapper | null {
+    return this.findDropdown(options).findHeader();
+  }
+
+  findFooter(options = { expandToViewport: false }): ElementWrapper | null {
+    return this.findDropdown(options).findFooter();
+  }
+
+  @usesDom
+  openDropdown(): void {
+    act(() => {
+      this.findTrigger().fireEvent(new MouseEvent('click', { bubbles: true }));
+    });
+  }
 }
