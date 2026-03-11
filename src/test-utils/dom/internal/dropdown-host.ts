4cbb2e1e6d34d36778afa545f7d8e0e6d73a626e
Maximilian Schoell <schomax@amazon.de>
2026-03-11 23:58:22 +0100

    fix: rename internal DropdownContentWrapper to OptionsDropdownContentWrapper
    Documenter confuses internal and external DropdownContentWrapper. They need to
    be named differently.

--- src/test-utils/dom/internal/dropdown-host.ts
+++ src/test-utils/dom/internal/dropdown-host.ts
@@ -31,10 +31,10 @@ export default abstract class DropdownHostComponentWrapper extends ComponentWrap
    * @param options
    * * expandToViewport (boolean) - Use this when the component under test is rendered with an `expandToViewport` flag.
    */
-  findDropdown(options = { expandToViewport: false }): DropdownContentWrapper {
+  findDropdown(options = { expandToViewport: false }): OptionsDropdownContentWrapper {
     return options.expandToViewport
       ? createWrapper().findComponent(`.${dropdownStyles.dropdown}[data-open=true]`, PortalDropdownContentWrapper)!
-      : new DropdownContentWrapper(this.getElement());
+      : new OptionsDropdownContentWrapper(this.getElement());
   }
 
   @usesDom
@@ -149,7 +149,7 @@ export default abstract class DropdownHostComponentWrapper extends ComponentWrap
   }
 }
 
-export class DropdownContentWrapper extends ComponentWrapper {
+export class OptionsDropdownContentWrapper extends ComponentWrapper {
   findDisabledOptions(): Array<OptionWrapper> {
     return this.findAllByClassName(selectableStyles.disabled).map(
       (elementWrapper: ElementWrapper) => new OptionWrapper(elementWrapper.getElement())
@@ -253,7 +253,7 @@ export class DropdownContentWrapper extends ComponentWrapper {
   }
 }
 
-export class PortalDropdownContentWrapper extends DropdownContentWrapper {
+export class PortalDropdownContentWrapper extends OptionsDropdownContentWrapper {
   findOpenDropdown(): ElementWrapper | null {
     return createWrapper().findComponent(`.${dropdownStyles.dropdown}[data-open=true]`, DropdownWrapper);
   }
