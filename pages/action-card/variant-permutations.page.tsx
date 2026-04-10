57a2b6a1bc135d0b07c053ec4c79024aa94478ac
Simon Brebeck <sbrebeck@amazon.de>
2026-04-10 10:49:31 +0200

    feat: Improved action card dev pages

--- pages/action-card/variant-permutations.page.tsx
+++ pages/action-card/variant-permutations.page.tsx
@@ -4,9 +4,9 @@ import React from 'react';
 
 import ActionCard, { ActionCardProps } from '~components/action-card';
 
+import { PermutationsPage } from '../app/templates';
 import createPermutations from '../utils/permutations';
 import PermutationsView from '../utils/permutations-view';
-import ScreenshotArea from '../utils/screenshot-area';
 import { icon, longContent, longDescription, onClick, shortContent, shortDescription, shortHeader } from './common';
 
 /* Visual variant permutations: default vs embedded */
@@ -63,18 +63,15 @@ const permutations = createPermutations<ActionCardProps>([
 
 export default function ActionCardVariantPermutations() {
   return (
-    <>
-      <h1>Action card variant permutations</h1>
-      <ScreenshotArea disableAnimations={true}>
-        <PermutationsView
-          permutations={permutations}
-          render={permutation => (
-            <div style={{ maxInlineSize: '400px' }}>
-              <ActionCard {...permutation} />
-            </div>
-          )}
-        />
-      </ScreenshotArea>
-    </>
+    <PermutationsPage title="Action card variant permutations" i18n={{}}>
+      <PermutationsView
+        permutations={permutations}
+        render={permutation => (
+          <div style={{ maxInlineSize: '400px' }}>
+            <ActionCard {...permutation} />
+          </div>
+        )}
+      />
+    </PermutationsPage>
   );
 }
