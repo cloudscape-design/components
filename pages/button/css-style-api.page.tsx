879e785f0b51224b5a56aa728a131e2d46d6c1a8
Maximilian Schoell <schomax@amazon.de>
2026-04-16 15:45:23 +0200

    feat: add demo for focus ring styling

--- pages/button/css-style-api.page.tsx
+++ pages/button/css-style-api.page.tsx
@@ -1,20 +1,53 @@
 import React from 'react';
-import { Button } from "~components";
+import { Button, SpaceBetween } from "~components";
 import { SimplePage } from "../app/templates";
 import './css-style-api.css';
 
 export default function Page() {
     return(
         <SimplePage title="CSS Style API">
-            <h2>Buttons without icons</h2>
-            <Button className="my-normal">Normal</Button>
-            <Button className="my-primary" variant="primary">Primary</Button>
-
-            <h2>Buttons with icons</h2>
-            <Button className="my-normal" iconName="add-plus">Normal with icon</Button>
-            <Button className="my-primary" variant="primary" iconName="settings">Primary with icon</Button>
-            <Button className="my-link" variant="link" iconName="external" href="#">Link with icon</Button>
-            <Button className="my-icon" variant="icon" iconName="close" ariaLabel="Close" />
+            <SpaceBetween size="l">
+                <div>
+                    <h2>Buttons without icons</h2>
+                    <SpaceBetween direction="horizontal" size="s">
+                        <Button className="my-normal">Normal</Button>
+                        <Button className="my-primary" variant="primary">Primary</Button>
+                    </SpaceBetween>
+                </div>
+                <div>
+                    <h2>Buttons with icons</h2>
+                    <SpaceBetween direction="horizontal" size="s">
+                        <Button className="my-normal" iconName="add-plus">Normal with icon</Button>
+                        <Button className="my-primary" variant="primary" iconName="settings">Primary with icon</Button>
+                        <Button className="my-link" variant="link" iconName="external" href="#">Link with icon</Button>
+                        <Button className="my-icon" variant="icon" iconName="close" ariaLabel="Close" />
+                    </SpaceBetween>
+                </div>
+                <div>
+                    <h2>Hover states (hover over buttons)</h2>
+                    <SpaceBetween direction="horizontal" size="s">
+                        <Button className="my-normal" iconName="add-plus">Normal hover</Button>
+                        <Button className="my-primary" variant="primary" iconName="settings">Primary hover</Button>
+                        <Button className="my-link" variant="link" iconName="external" href="#">Link hover</Button>
+                        <Button className="my-icon" variant="icon" iconName="close" ariaLabel="Close" />
+                    </SpaceBetween>
+                </div>
+                <div>
+                    <h2>Disabled states (hover to see bg change)</h2>
+                    <SpaceBetween direction="horizontal" size="s">
+                        <Button className="my-normal" disabled={true} iconName="add-plus">Disabled normal</Button>
+                        <Button className="my-primary" variant="primary" disabled={true} iconName="settings">Disabled primary</Button>
+                        <Button className="my-icon" variant="icon" disabled={true} iconName="close" ariaLabel="Close" />
+                    </SpaceBetween>
+                </div>
+                <div>
+                    <h2>Focus ring (tab to see)</h2>
+                    <SpaceBetween direction="horizontal" size="s">
+                        <Button className="my-normal">Default focus</Button>
+                        <Button className="my-link" variant="link" href="#">Custom focus ring</Button>
+                    </SpaceBetween>
+                </div>
+            </SpaceBetween>
         </SimplePage>
     );
 }
