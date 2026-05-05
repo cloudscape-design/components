a38ad10f382363461ed5b92c9c369039e5453ba0
at-susie <ienakaai@amazon.de>
2026-05-05 11:09:05 +0200

    chore: Fix a11y issue in the demo page

--- pages/theming/icon-scale-provider.page.tsx
+++ pages/theming/icon-scale-provider.page.tsx
@@ -504,9 +504,9 @@ function NavigationComponents() {
         activeTabId={activeTabId}
         onChange={({ detail }) => setActiveTabId(detail.activeTabId)}
         tabs={[
-          { id: 'tab1', label: 'Tab 1', content: 'Tab 1 content', dismissible: true },
-          { id: 'tab2', label: 'Tab 2', content: 'Tab 2 content', dismissible: true },
-          { id: 'tab3', label: 'Tab 3', content: 'Tab 3 content', dismissible: true },
+          { id: 'tab1', label: 'Tab 1', content: 'Tab 1 content', dismissible: true, dismissLabel: 'Dismiss Tab 1' },
+          { id: 'tab2', label: 'Tab 2', content: 'Tab 2 content', dismissible: true, dismissLabel: 'Dismiss Tab 2' },
+          { id: 'tab3', label: 'Tab 3', content: 'Tab 3 content', dismissible: true, dismissLabel: 'Dismiss Tab 3' },
         ]}
       />
       <ColumnLayout columns={4}>
