fb6984ce03a44562860432d426e037e7fd320116
at-susie <ienakaai@amazon.de>
2026-05-07 23:14:04 +0200

    chore: Add SideNavigation with sections to icon-scale-provider page and fix icon sizing
    - Add expanded SideNavigation example with sections to the theming test page
    - Set min-inline-size on icon containers in expandable-section to prevent shrinking
    - Fix icon mapped-height calculation to use supportedLineHeight instead of size

--- pages/theming/icon-scale-provider.page.tsx
+++ pages/theming/icon-scale-provider.page.tsx
@@ -533,6 +533,62 @@ function NavigationComponents() {
             { type: 'link', text: 'Page 2', href: '#/page2' },
           ]}
         />
+        <SideNavigation
+          activeHref={activeHref}
+          header={{ href: '#/', text: 'Service name' }}
+          onFollow={event => {
+            if (!event.detail.external) {
+              event.preventDefault();
+              setActiveHref(event.detail.href);
+            }
+          }}
+          items={[
+            { type: 'link', text: 'Page 1', href: '#/page1' },
+            { type: 'link', text: 'Page 2', href: '#/page2' },
+            {
+              type: 'section',
+              text: 'Section 1',
+              items: [
+                {
+                  type: 'link',
+                  text: 'Page 4',
+                  href: '#/page4',
+                },
+                {
+                  type: 'link',
+                  text: 'Page 5',
+                  href: '#/page5',
+                },
+                {
+                  type: 'link',
+                  text: 'Page 6',
+                  href: '#/page6',
+                },
+              ],
+            },
+            {
+              type: 'section',
+              text: 'Section 2',
+              items: [
+                {
+                  type: 'link',
+                  text: 'Page 7',
+                  href: '#/page7',
+                },
+                {
+                  type: 'link',
+                  text: 'Page 8',
+                  href: '#/page8',
+                },
+                {
+                  type: 'link',
+                  text: 'Page 9',
+                  href: '#/page9',
+                },
+              ],
+            },
+          ]}
+        />
         <SpaceBetween size="s">
           <BreadcrumbGroup
             items={[
