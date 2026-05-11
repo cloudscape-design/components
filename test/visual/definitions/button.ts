20bd2e9f4e1ba4a0e50fb86c7109f4e76e45bcc8
Joan Perals Tresserra <jotresse@amazon.com>
2026-05-11 19:31:39 +0200

    chore: Add visual regression testing

---
+++ test/visual/definitions/button.ts
@@ -0,0 +1,15 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { TestSuite } from '../types';
+
+const suite: TestSuite = {
+  description: 'button',
+  tests: [
+    {
+      description: 'permutations',
+      path: 'button/permutations',
+    },
+  ],
+};
+
+export default suite;
