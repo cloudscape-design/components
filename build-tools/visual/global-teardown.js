670ef1a09abca17471653db25e193bb31676e71c
Joan Perals Tresserra <jotresse@amazon.com>
2026-05-18 19:10:56 +0200

    Run tests on Safari

--- build-tools/visual/global-teardown.js
+++ build-tools/visual/global-teardown.js
@@ -1,4 +1,12 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-const { shutdownWebdriver } = require('@cloudscape-design/browser-test-tools/chrome-launcher');
-module.exports = () => shutdownWebdriver();
+module.exports = () => {
+  if (process.env.BROWSER === 'safari') {
+    if (global.__DRIVER_PROCESS__) {
+      global.__DRIVER_PROCESS__.kill();
+    }
+  } else {
+    const { shutdownWebdriver } = require('@cloudscape-design/browser-test-tools/chrome-launcher');
+    shutdownWebdriver();
+  }
+};
