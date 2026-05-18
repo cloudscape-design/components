d5b8d6cec8eb010da58988f6c832091d0182f664
Joan Perals Tresserra <jotresse@amazon.com>
2026-05-18 19:10:56 +0200

    Run tests on Safari

--- build-tools/visual/global-setup.js
+++ build-tools/visual/global-setup.js
@@ -1,4 +1,20 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-const { startWebdriver } = require('@cloudscape-design/browser-test-tools/chrome-launcher');
-module.exports = () => startWebdriver();
+const { spawn } = require('child_process');
+const waitOn = require('wait-on');
+
+let driverProcess;
+
+module.exports = async () => {
+  if (process.env.BROWSER === 'safari') {
+    driverProcess = spawn('safaridriver', ['--port', '4444']);
+    driverProcess.on('error', err => {
+      throw err;
+    });
+    await waitOn({ resources: ['http-get://localhost:4444/status'], timeout: 10000 });
+  } else {
+    const { startWebdriver } = require('@cloudscape-design/browser-test-tools/chrome-launcher');
+    await startWebdriver();
+  }
+  global.__DRIVER_PROCESS__ = driverProcess;
+};
