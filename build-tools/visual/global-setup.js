2ee27a9aa6a78463f27379816471a9d984727f37
Joan Perals Tresserra <jotresse@amazon.com>
2026-05-19 14:00:13 +0200

    Release Safari session between tests

--- build-tools/visual/global-setup.js
+++ build-tools/visual/global-setup.js
@@ -3,18 +3,16 @@
 const { spawn } = require('child_process');
 const waitOn = require('wait-on');
 
-let driverProcess;
-
 module.exports = async () => {
   if (process.env.BROWSER === 'safari') {
-    driverProcess = spawn('safaridriver', ['--port', '4444']);
+    const driverProcess = spawn('safaridriver', ['--port', '4444']);
     driverProcess.on('error', err => {
       throw err;
     });
     await waitOn({ resources: ['http-get://localhost:4444/status'], timeout: 10000 });
+    global.__DRIVER_PROCESS__ = driverProcess;
   } else {
     const { startWebdriver } = require('@cloudscape-design/browser-test-tools/chrome-launcher');
     await startWebdriver();
   }
-  global.__DRIVER_PROCESS__ = driverProcess;
 };
