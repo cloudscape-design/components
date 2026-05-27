6efec5b9ae2c905d90e859de3af85005be246605
Joan Perals Tresserra <jotresse@amazon.com>
2026-05-27 17:05:43 +0200

    Move Safaridriver initialization back to local test setup

--- build-tools/visual/global-setup.js
+++ build-tools/visual/global-setup.js
@@ -1,11 +1,16 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+const { spawn } = require('child_process');
 const waitOn = require('wait-on');
 
 module.exports = async () => {
   if (process.env.BROWSER === 'safari') {
-    // safaridriver is started by the CI workflow on port 4444.
+    const driverProcess = spawn('safaridriver', ['--port', '4444']);
+    driverProcess.on('error', err => {
+      throw err;
+    });
     await waitOn({ resources: ['http-get://localhost:4444/status'], timeout: 10000 });
+    global.__DRIVER_PROCESS__ = driverProcess;
   } else {
     const { startWebdriver } = require('@cloudscape-design/browser-test-tools/chrome-launcher');
     await startWebdriver();
--- build-tools/visual/global-teardown.js
+++ build-tools/visual/global-teardown.js
@@ -1,9 +1,12 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 module.exports = () => {
-  if (process.env.BROWSER !== 'safari') {
+  if (process.env.BROWSER === 'safari') {
+    if (global.__DRIVER_PROCESS__) {
+      global.__DRIVER_PROCESS__.kill();
+    }
+  } else {
     const { shutdownWebdriver } = require('@cloudscape-design/browser-test-tools/chrome-launcher');
     shutdownWebdriver();
   }
-  // Safari: safaridriver is managed by the CI workflow, nothing to tear down.
 };
