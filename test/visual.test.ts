7ac84c647989df4bf6741b339bd79003716d6aad
Joan Perals Tresserra <jotresse@amazon.com>
2026-05-27 10:34:20 +0200

    Refactor

--- test/visual.test.ts
+++ test/visual.test.ts
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import { allSuites } from './definitions';
-import { runTestSuites } from './definitions/visual/compare-screenshots';
+import { runTestSuites } from './definitions/utils';
 
 runTestSuites(allSuites);
