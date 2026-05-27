a40d76822370e229f6393c9217f41aa798d98e40
Joan Perals Tresserra <jotresse@amazon.com>
2026-05-27 15:52:13 +0200

    Start safaridriver from workflow

--- build-tools/visual/global-teardown.js
+++ build-tools/visual/global-teardown.js
@@ -1,12 +1,9 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 module.exports = () => {
-  if (process.env.BROWSER === 'safari') {
-    if (global.__DRIVER_PROCESS__) {
-      global.__DRIVER_PROCESS__.kill();
-    }
-  } else {
+  if (process.env.BROWSER !== 'safari') {
     const { shutdownWebdriver } = require('@cloudscape-design/browser-test-tools/chrome-launcher');
     shutdownWebdriver();
   }
+  // Safari: safaridriver is managed by the CI workflow, nothing to tear down.
 };
