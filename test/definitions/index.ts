5c18355d56f1f37a37cc944326a4b4404a130d7a
Joan Perals Tresserra <jotresse@amazon.com>
2026-05-30 01:07:41 +0200

    Add more components tests

--- test/definitions/index.ts
+++ test/definitions/index.ts
@@ -7,5 +7,9 @@ import { TestSuite } from './types';
 import actionCard from './visual/action-card';
 import alert from './visual/alert';
 import appLayout from './visual/app-layout';
+import areaChart from './visual/area-chart';
+import attributeEditor from './visual/attribute-editor';
+import autosuggest from './visual/autosuggest';
+import badge from './visual/badge';
 
-export const allSuites: TestSuite[] = [actionCard, alert, appLayout];
+export const allSuites: TestSuite[] = [actionCard, alert, appLayout, areaChart, attributeEditor, autosuggest, badge];
---
+++ test/visual/attribute-editor.test.ts
@@ -0,0 +1,5 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { runTestSuites } from '../definitions/utils';
+import attributeEditor from '../definitions/visual/attribute-editor';
+runTestSuites([attributeEditor]);
