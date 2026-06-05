94c3f2a1a0407a08b5c802abea1a6c6920c8c315
Joan Perals Tresserra <jotresse@amazon.com>
2026-06-05 06:59:26 +0200

    Autogenerate test files

--- test/visual/action-card.test.ts
+++
@@ -1,6 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-import { runTestSuites } from '../definitions/utils';
-import actionCard from '../definitions/visual/action-card';
-
-runTestSuites([actionCard]);
