d39fd69770547c683434d3fd16bb875ffd3b8f32
Joan Perals Tresserra <jotresse@amazon.com>
2026-06-09 00:07:55 +0200

    Remove new test definitions

--- test/definitions/visual/app-layout-responsive-2540.ts
+++
@@ -1,7 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-import { responsiveTests } from './app-layout-responsive-tests';
-
-const suite = responsiveTests(2540);
-export default suite;
