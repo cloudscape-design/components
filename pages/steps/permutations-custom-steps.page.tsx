383ffd70ea9c978af3e931ae760ac0ff2cf7a6aa
Andrei Zhaleznichenka <zhalea@amazon.de>
2026-06-12 12:37:05 +0200

    refactor: Refactors steps permutations

---
+++ pages/steps/permutations-custom-steps.page.tsx
@@ -0,0 +1,41 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import React from 'react';
+
+import { Icon } from '~components';
+import Steps, { StepsProps } from '~components/steps';
+
+import { SimplePage } from '../app/templates';
+import createPermutations from '../utils/permutations';
+import PermutationsView from '../utils/permutations-view';
+import * as variants from './permutations-utils';
+
+const stepsPermutations = createPermutations<StepsProps>([
+  {
+    steps: [variants.allStatusesSteps, variants.successfulSteps],
+    ariaLabel: ['test label'],
+    orientation: ['vertical', 'horizontal'],
+    renderStep: [
+      step => ({
+        header: <b>This step header ({step.header}) is wrapped in a custom HTML tag and has very long content</b>,
+        details: step.details && <i>Custom details for {step.details}</i>,
+      }),
+      step => ({
+        header: step.header,
+        details: step.details && <i>Custom details for {step.details}</i>,
+        icon: <Icon ariaLabel="success" name="status-positive" variant="success" />,
+      }),
+    ],
+  },
+]);
+
+export default function StepsPermutations() {
+  return (
+    <SimplePage screenshotArea={{ disableAnimations: true }} title="Steps permutations: custom steps">
+      <PermutationsView
+        permutations={stepsPermutations}
+        render={permutation => <div>{<Steps {...permutation} />}</div>}
+      />
+    </SimplePage>
+  );
+}
