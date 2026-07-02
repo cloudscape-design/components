9a108af04b8b8787826f0ab0c296a851541d48be
Joan Perals Tresserra <jotresse@amazon.com>
2026-07-02 10:55:48 +0200

    Restore comment

--- test/definitions/index.ts
+++ test/definitions/index.ts
@@ -1,6 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+// Each component has its own test definition file.
+// Import them here manually to form the full test suite.
 import { TestSuite } from './types';
 export { TestSuite, TestDefinition, ScreenshotType, ScreenshotTestConfiguration } from './types';
 import actionCard from './visual/action-card';
