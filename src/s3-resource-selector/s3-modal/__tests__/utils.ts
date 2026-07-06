9eeb092bac7dc1b42d47dca70b91bb8f4afa1dd0
Gethin Webster <gethinw@amazon.com>
2026-07-06 15:42:27 +0000

    Fix CI: widen s3 getElementsText helper and update documenter snapshot

--- src/s3-resource-selector/s3-modal/__tests__/utils.ts
+++ src/s3-resource-selector/s3-modal/__tests__/utils.ts
@@ -1,5 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+import { ComponentWrapper } from '@cloudscape-design/test-utils-core/dom';
+
 import { ElementWrapper, TableWrapper } from '../../../../lib/components/test-utils/dom';
 import { buckets, i18nStrings, objects, versions, waitForFetch } from '../../__tests__/fixtures';
 
@@ -39,7 +41,7 @@ export async function navigateToTableItem(wrapper: ElementWrapper<Element>, rowI
   await waitForFetch();
 }
 
-export function getElementsText(wrappers: ReadonlyArray<ElementWrapper>) {
+export function getElementsText(wrappers: ReadonlyArray<ElementWrapper | ComponentWrapper>) {
   return wrappers.map(wrapper => wrapper.getElement().textContent!.trim());
 }
 
