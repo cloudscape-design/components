1654fea5b9d242bf6040150802cae87b3d0ae1ce
Joan Perals Tresserra <jotresse@amazon.com>
2026-07-10 14:00:27 +0200

    Refactor exports

--- test/definitions/index.ts
+++ test/definitions/index.ts
@@ -5,26 +5,10 @@
 // Import them here manually to form the full test suite.
 import { TestSuite } from './types';
 export { TestSuite, TestDefinition, ScreenshotType, ScreenshotTestConfiguration } from './types';
-export { default as actionCard } from './visual/action-card';
-export { default as alert } from './visual/alert';
-export { default as appLayout } from './visual/app-layout';
-export { default as appLayoutContentPaddings } from './visual/app-layout-content-paddings';
-export { default as appLayoutDrawers } from './visual/app-layout-drawers';
-export { default as appLayoutFlashbar } from './visual/app-layout-flashbar';
-export { default as appLayoutHeader } from './visual/app-layout-header';
-export { default as appLayoutMulti } from './visual/app-layout-multi';
-export { default as appLayoutResponsive600 } from './visual/app-layout-responsive-600';
-export { default as appLayoutResponsive1280 } from './visual/app-layout-responsive-1280';
-export { default as appLayoutResponsive1400 } from './visual/app-layout-responsive-1400';
-export { default as appLayoutResponsive1920 } from './visual/app-layout-responsive-1920';
-export { default as appLayoutResponsive2540 } from './visual/app-layout-responsive-2540';
-export { default as appLayoutStickyTableHeaderSplitPanel } from './visual/app-layout-sticky-table-header-split-panel';
-export { default as appLayoutToolbar } from './visual/app-layout-toolbar';
-export { default as appLayoutZIndex } from './visual/app-layout-z-index';
 
-import actionCard from './visual/action-card';
-import alert from './visual/alert';
-import appLayout from './visual/app-layout';
+import actionCardSuite from './visual/action-card';
+import alertSuite from './visual/alert';
+import appLayoutGeneral from './visual/app-layout';
 import appLayoutContentPaddings from './visual/app-layout-content-paddings';
 import appLayoutDrawers from './visual/app-layout-drawers';
 import appLayoutFlashbar from './visual/app-layout-flashbar';
@@ -39,10 +23,11 @@ import appLayoutStickyTableHeaderSplitPanel from './visual/app-layout-sticky-tab
 import appLayoutToolbar from './visual/app-layout-toolbar';
 import appLayoutZIndex from './visual/app-layout-z-index';
 
-export const allSuites: TestSuite[] = [
-  actionCard,
-  alert,
-  appLayout,
+// Per-component exports (grouped by component)
+export const actionCard: TestSuite[] = [actionCardSuite];
+export const alert: TestSuite[] = [alertSuite];
+export const appLayout: TestSuite[] = [
+  appLayoutGeneral,
   appLayoutContentPaddings,
   appLayoutDrawers,
   appLayoutFlashbar,
@@ -57,3 +42,5 @@ export const allSuites: TestSuite[] = [
   appLayoutToolbar,
   appLayoutZIndex,
 ];
+
+export const allSuites: TestSuite[] = [...actionCard, ...alert, ...appLayout];
