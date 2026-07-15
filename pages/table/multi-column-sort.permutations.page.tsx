edd6ed6e966f50406702a3639192591ee07246cd
Gethin Webster <gethinw@amazon.com>
2026-07-15 09:01:15 +0000

    fix: Update multi-column sort permutations i18n

--- pages/table/multi-column-sort.permutations.page.tsx
+++ pages/table/multi-column-sort.permutations.page.tsx
@@ -28,20 +28,25 @@ const columnDefinitions: TableProps.ColumnDefinition<Item>[] = [
 const noop = () => {};
 
 const i18nStrings: TableProps.I18nStrings = {
-  sortDropdown: {
-    sortAscending: 'Sort ascending',
-    sortDescending: 'Sort descending',
-    multiColumnSortGroup: 'Multi-column sort (Shift + Click)',
-    addToSortAscending: 'Add to sort (ascending)',
-    addToSortDescending: 'Add to sort (descending)',
-    removeFromSort: 'Remove from sort',
-  },
+  sortDropdownSortAscending: 'Sort ascending',
+  sortDropdownSortDescending: 'Sort descending',
+  sortDropdownMultiColumnSortGroup: 'Multi-column sort (Shift + Click)',
+  sortDropdownAddToSortAscending: 'Add to sort (ascending)',
+  sortDropdownAddToSortDescending: 'Add to sort (descending)',
+  sortDropdownRemoveFromSort: 'Remove from sort',
+  sortDropdownAddToSortDisabledReason: 'This column is already sorted',
+  sortDropdownRemoveFromSortDisabledReason: 'This column is not sorted',
   clearSort: 'Clear sort',
 };
 
 const ariaLabels: TableProps.AriaLabels<Item> = {
   tableLabel: 'Multi-column sort permutations',
   sortMenuTriggerLabel: 'Sort options',
+  sortAscending: 'ascending',
+  sortDescending: 'descending',
+  liveAnnouncementSortOrder: ({ columns }) => `Table sorted by ${columns}`,
+  liveAnnouncementSortCleared: 'Sorting cleared',
+  sortPriority: ({ priority }) => `sort priority ${priority}`,
   selectionGroupLabel: 'Item selection',
   allItemsSelectionLabel: () => 'Select all',
   itemSelectionLabel: (_data, item) => `Select ${item.name}`,
