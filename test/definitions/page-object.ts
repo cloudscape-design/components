858e56a0a6b4e9b0a10330bf81552579c9487c13
Joan Perals Tresserra <jotresse@amazon.com>
2026-07-20 15:12:07 +0200

    Refine focusInputs

--- test/definitions/page-object.ts
+++ test/definitions/page-object.ts
@@ -2,25 +2,24 @@
 // SPDX-License-Identifier: Apache-2.0
 import { RawScreenshotPageObject } from '@cloudscape-design/browser-test-tools/page-objects';
 
-import createWrapper from '../../lib/components/test-utils/selectors';
-
-const nativeInputSelector = createWrapper().findAutosuggest().findNativeInput().toSelector();
-
 /**
  * Extended page object for visual regression tests.
+ * Adds helper methods that were available in the IntegrationTests page object.
  */
 export default class VisualTestPageObject extends RawScreenshotPageObject {
   /**
-   * Dispatches a focusin event on all autosuggest native inputs to open their dropdowns.
+   * Dispatches a focusin event on all input elements to open their dropdowns.
    * Uses focusin (which bubbles) because React's event delegation listens for it.
+   * Targets native `input` elements to avoid dependency on test-utils selectors
+   * which may differ between the PR and baseline builds.
    */
   async focusInputs(): Promise<void> {
-    await this.waitForVisible(nativeInputSelector);
-    await this.browser.execute(function (selector: string) {
-      document.querySelectorAll(selector).forEach(function (input) {
+    await this.waitForVisible('input');
+    await this.browser.execute(function () {
+      document.querySelectorAll('input').forEach(function (input) {
         input.dispatchEvent(new FocusEvent('focusin', { bubbles: true, relatedTarget: null }));
       });
-    }, nativeInputSelector);
+    });
     await this.waitForJsTimers(500);
   }
 }
