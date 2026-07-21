1834b88ba2f931cd464f73584e916938740e2b12
Joan Perals <jotresse@amazon.com>
2026-07-21 11:17:02 +0200

    test: Add visual regression tests for app layout (#4714)

--- test/definitions/index.ts
+++ test/definitions/index.ts
@@ -5,7 +5,34 @@
 // Import them here manually to form the full test suite.
 import { TestSuite } from './types';
 export { TestSuite, TestDefinition, ScreenshotType, ScreenshotTestConfiguration } from './types';
-import actionCard from './visual/action-card';
-import alert from './visual/alert';
 
-export const allSuites: TestSuite[] = [actionCard, alert];
+import actionCardSuite from './visual/action-card';
+import alertSuite from './visual/alert';
+import appLayoutGeneral from './visual/app-layout';
+import appLayoutContentPaddings from './visual/app-layout-content-paddings';
+import appLayoutDrawers from './visual/app-layout-drawers';
+import appLayoutFlashbar from './visual/app-layout-flashbar';
+import appLayoutHeader from './visual/app-layout-header';
+import appLayoutMulti from './visual/app-layout-multi';
+import appLayoutResponsive from './visual/app-layout-responsive';
+import appLayoutStickyTableHeaderSplitPanel from './visual/app-layout-sticky-table-header-split-panel';
+import appLayoutToolbar from './visual/app-layout-toolbar';
+import appLayoutZIndex from './visual/app-layout-z-index';
+
+// Per-component exports (grouped by component)
+export const actionCard: TestSuite[] = [actionCardSuite];
+export const alert: TestSuite[] = [alertSuite];
+export const appLayout: TestSuite[] = [
+  appLayoutGeneral,
+  appLayoutContentPaddings,
+  appLayoutDrawers,
+  appLayoutFlashbar,
+  appLayoutHeader,
+  appLayoutMulti,
+  appLayoutResponsive,
+  appLayoutStickyTableHeaderSplitPanel,
+  appLayoutToolbar,
+  appLayoutZIndex,
+];
+
+export const allSuites: TestSuite[] = [...actionCard, ...alert, ...appLayout];
