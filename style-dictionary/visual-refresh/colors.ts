8d345a1295d207a3299f3a02e367f5802e3f9f96
Joan Perals <jotresse@amazon.com>
2026-07-27 13:26:26 +0200

    chore: Break circular dependency between themes' color palettes (#4837)

--- style-dictionary/visual-refresh/colors.ts
+++ style-dictionary/visual-refresh/colors.ts
@@ -1,6 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-import { paletteTokens } from '../core/color-palette.js';
+import { paletteTokens } from '../core/palette-values.js';
 import { expandColorDictionary } from '../utils/index.js';
 import { StyleDictionary } from '../utils/interfaces.js';
 
