7a5e27757254e17c75d3d69d96aa0e87df7dd5e2
Avinash Dwarapu <dwaraa@amazon.com>
2026-07-29 14:57:07 +0200

    Fix tests.

--- build-tools/utils/__tests__/token-versions.test.js
+++ build-tools/utils/__tests__/token-versions.test.js
@@ -7,27 +7,37 @@ const { getTokenVersions } = require('../token-versions');
 const variablesMap = {
   borderRadiusButton: 'border-radius-button',
   borderWidthField: 'border-width-field',
+  colorChartsPurple300: 'color-charts-purple-300',
   colorBorderButtonNormalDefault: 'color-border-button-normal-default',
+  colorBackgroundPopover: 'color-background-popover',
+  colorTextControlDisabled: 'color-text-control-disabled',
   colorTextAccent: 'color-text-accent',
   spaceScaledM: 'space-scaled-m',
+  sizeIconMedium: 'size-icon-m',
   fontFamilyBase: 'font-family-base',
+  shadowModal: 'shadow-modal',
 };
 
 test('versions border tokens matched by the default groups and leaves others (incl. border colors) version-less', () => {
   expect(getTokenVersions(variablesMap)).toEqual({
     borderRadiusButton: 'v3-1',
     borderWidthField: 'v3-1',
+    fontFamilyBase: 'v3-1',
   });
 });
 
 test('assigns the version of the first matching group', () => {
   const groups = [
     { pattern: /^color-border-/, version: 'v3-2' },
+    { pattern: /^color-text-/, version: 'v3-2' },
     { pattern: /^color-/, version: 'v3-1' },
   ];
   expect(getTokenVersions(variablesMap, groups)).toEqual({
+    colorBackgroundPopover: 'v3-1',
     colorBorderButtonNormalDefault: 'v3-2',
-    colorTextAccent: 'v3-1',
+    colorChartsPurple300: 'v3-1',
+    colorTextAccent: 'v3-2',
+    colorTextControlDisabled: 'v3-2',
   });
 });
 
@@ -37,6 +47,6 @@ test('a catch-all group versions every token', () => {
 });
 
 test('returns an empty map when no group matches', () => {
-  const groups = [{ pattern: /^color-background-/, version: 'v3-1' }];
+  const groups = [{ pattern: /^motion-duration-/, version: 'v3-1' }];
   expect(getTokenVersions(variablesMap, groups)).toEqual({});
 });
