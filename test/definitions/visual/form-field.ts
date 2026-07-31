488592cf9e6492c2b6e1255fab2a435b0a39eabe
Joan Perals Tresserra <jotresse@amazon.com>
2026-07-31 16:48:03 +0200

    chore: Add visual regression tests for more components (D-R)

---
+++ test/definitions/visual/form-field.ts
@@ -0,0 +1,27 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { TestSuite } from '../types';
+
+const suite: TestSuite = {
+  description: 'FormField',
+  componentName: 'form-field',
+  tests: [576, 768, 992, 1200].map<TestSuite>(width => ({
+    description: `Layout at ${width}px`,
+    tests: [
+      {
+        description: 'Permutations',
+        path: 'form-field/permutations',
+        screenshotType: 'permutations',
+        configuration: { width },
+      },
+      {
+        description: '2.1 Scenarios',
+        path: 'form-field/form-field-columns',
+        screenshotType: 'screenshotArea',
+        configuration: { width },
+      },
+    ],
+  })),
+};
+
+export default suite;
---
+++ test/definitions/visual/help-panel.ts
@@ -0,0 +1,36 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+import { TestSuite } from '../types';
+
+const suite: TestSuite = {
+  description: 'HelpPanel',
+  componentName: 'help-panel',
+  tests: [
+    {
+      description: 'permutations',
+      path: 'help-panel/permutations',
+      screenshotType: 'screenshotArea',
+      setup: async ({ page }) => {
+        await page.focusInputs();
+      },
+    },
+    {
+      description: 'with AppLayout',
+      path: 'help-panel/with-app-layout',
+      screenshotType: 'screenshotArea',
+      setup: async ({ page }) => {
+        await page.focusInputs();
+      },
+    },
+    {
+      description: 'loading state - with AppLayout',
+      path: 'help-panel/loading-with-app-layout',
+      screenshotType: 'screenshotArea',
+      setup: async ({ page }) => {
+        await page.focusInputs();
+      },
+    },
+  ],
+};
+
+export default suite;
