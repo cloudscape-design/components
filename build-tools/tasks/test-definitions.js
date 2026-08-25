823fc0071518fad14212c5da4e743e9a57ed7d29
Joan Perals Tresserra <jotresse@amazon.com>
2026-08-25 14:03:55 +0200

    Remove leftover file

--- build-tools/tasks/test-definitions.js
+++ build-tools/tasks/test-definitions.js
@@ -2,7 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 const execa = require('execa');
 const { task } = require('../utils/gulp-utils');
+const generateVisualTests = require('./generate-visual-tests');
 
-module.exports = task('test-definitions', () =>
-  execa('tsc', ['-p', 'tsconfig.test-definitions.json'], { stdio: 'inherit' })
-);
+module.exports = task('test-definitions', async () => {
+  // Generate the test/visual runner files from the definitions before type-checking,
+  // so both the generated runners and the definitions are validated together.
+  await generateVisualTests();
+  await execa('tsc', ['-p', 'tsconfig.test-definitions.json'], { stdio: 'inherit' });
+});
