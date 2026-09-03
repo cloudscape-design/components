a0f007df61ccbb84298ff4fc4979e08313e7126a
Harsh <hanandji@amazon.com>
2026-09-03 16:37:05 +0200

    Remove unused test import

--- src-themeable/__tests__/theming.test.ts
+++ src-themeable/__tests__/theming.test.ts
@@ -1,7 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import mapValues from 'lodash/mapValues.js';
-import { join } from 'path';
 
 const preset = {
   propertiesMap: {
